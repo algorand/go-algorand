ee6d1ce9793f1b10e82384c6b3960c493c12ad6d
David Shoots <david@algorand.com>
2019-06-20 01:43:35 -0400

    Clean up code
    Remove extra fields from Relay, dead code, and add copyright headers.

--- cmd/algorelay/eb/eb.go
+++ cmd/algorelay/eb/eb.go
@@ -1,34 +1,25 @@
+// Copyright (C) 2019 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
 package eb
-
-import "time"
-
-const NodeRunnerKind = "NodeRunner"
-
-// NodeRunner entities are synchronized directly from db1 using `afdb1 syncebnoderunners`
-// They should not be modified manually
-type NodeRunner struct {
-	CompanyName      string
-	InvestorID       string
-	NbRequiredRelays int    // number of relays required relays the investor needs to run in the node agreement
-	NodeRunnerToken  string // token used by eb.algorand.foundation
-}
-
-const RelayKind = "Relay"
 
 type Relay struct {
 	ID                    int64  // db key injected when loaded
-	InvestorID            string
-	ContactEmail          string // comma separated list of emails
-	NodeProvider          string
 	IPOrDNSName           string
-	Specs                 string
-	Notes                 string
-	SubmissionTime        time.Time
-	SRVRecordCreationTime time.Time
-	Telemetry             string // GUID[:name]
 	MetricsEnabled        bool
-	CheckTime             time.Time // time the check was done
 	CheckSuccess          bool // true if check was successful
-	CheckError            string // non-empty if check error, contains the error
 	DNSAlias              string // DNS Alias name used
 }
