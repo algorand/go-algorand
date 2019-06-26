69198f143721f56bb93aa2a4699812fb371111c8
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-06-25 21:22:28 -0400

    enforce cobra.NoArgs for algokey commands

--- cmd/algokey/main.go
+++ cmd/algokey/main.go
@@ -26,6 +26,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "algokey",
 	Short: "CLI for managing Algorand keys",
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		// If no arguments passed, we should fallback to help
 		cmd.HelpFunc()(cmd, args)
