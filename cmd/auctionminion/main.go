8471695bd0f26b907de98f2edc374e5ab744e266
Zachary Estep <zach.estep.nh@gmail.com>
2019-07-01 20:23:17 -0400

    further updates

--- cmd/auctionminion/main.go
+++ cmd/auctionminion/main.go
@@ -61,7 +61,7 @@ var stateFile = flag.String("statefile", "auctionminion.state", "State/config fi
 var initFlag = flag.Bool("init", false, "Initialize state file")
 
 type minionConfig struct {
-	AuctionKey DigestChecksummed
+	AuctionKey crypto.Digest
 	AuctionID  uint64
 	StartRound uint64
 	AlgodURL   string
