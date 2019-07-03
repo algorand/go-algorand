ba2c1f76764d0a78357f1d812045aa1d5bfea0b1
Evan Richard <EvanJRichard@users.noreply.github.com>
2019-07-03 10:52:40 -0400

    [GOAL2-441] Implement default timeouts for REST Server  (#107)
    * scrap previous implementation - http.Server provides for timeouts
    
    * rename "handler" to "apiHandler" so we know what requests it's handling
    
    * run make sanity
    
    * Greatly increase WriteTimeout; at least one REST response is allowed to take up to a minute to build
    
    * Change rest readtimeout/writetimeout to config values
    
    * Change expected unit of config value to be seconds
    
    * Fix broken config migrate
    
    * Repair more config tests. Update config example. Add v5 config json to testdata.
    
    * Revert "Repair more config tests. Update config example. Add v5 config json to testdata."
    
    This reverts commit 18f297fcc35de8dd861fbbf8e6107a3e8e51bb0f.
    
    * Revert "Fix broken config migrate"
    
    This reverts commit f867e870bad810c2c386b346e91af48aab01db6f.
    
    * More reversion.
    
    * Remove unneeded block in migrate

--- daemon/algod/server.go
+++ daemon/algod/server.go
@@ -28,6 +28,7 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/algorand/go-deadlock"
 
@@ -170,7 +171,7 @@ func (s *Server) Start() {
 
 	// use the data dir as the static file dir (for our API server), there's
 	// no need to separate the two yet. This lets us serve the swagger.json file.
-	handler := apiServer.NewRouter(s.log, s.node, apiToken, s.RootPath)
+	apiHandler := apiServer.NewRouter(s.log, s.node, apiToken, s.RootPath)
 
 	addr := cfg.EndpointAddress
 	if addr == "" {
@@ -185,7 +186,12 @@ func (s *Server) Start() {
 	}
 
 	addr = listener.Addr().String()
-	server = http.Server{Addr: addr, Handler: handler}
+	server = http.Server{
+		Addr:         addr,
+		Handler:      apiHandler,
+		ReadTimeout:  time.Duration(cfg.RestReadTimeoutSeconds) * time.Second,
+		WriteTimeout: time.Duration(cfg.RestWriteTimeoutSeconds) * time.Second,
+	}
 
 	defer s.Stop()
 
