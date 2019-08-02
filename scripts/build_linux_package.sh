d88db0df2178f6bf9ee9b3ed2ae589a3adc89612
Karmastic <david@algrand.com>
2019-08-02 12:28:12 -0400

    Fixed deploy_private_version

--- scripts/build_linux_package.sh
+++ scripts/build_linux_package.sh
@@ -9,9 +9,9 @@ cd ${REPO_ROOT}
 
 # Delete tmp folder so we don't include that in our context
 rm -rf ./tmp
-docker build -f /go-algorand/docker/build/Dockerfile -t algorand-build .
+docker build -f ./go-algorand/algorand/build/Dockerfile -t algorand-build .
 docker rm buildpkg || true
 docker run --name buildpkg algorand-build
 mkdir -p ./tmp/dev_linux_pkg
-docker cp buildpkg:/go/src/github.com/docker/go-algorand/tmp/dev_pkg ./tmp/dev_linux_pkg
+docker cp buildpkg:/go/src/github.com/algorand/go-algorand/tmp/dev_pkg ./tmp/dev_linux_pkg
 docker stop buildpkg
