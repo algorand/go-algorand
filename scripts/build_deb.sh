995f967109797ce58d238bab009d0f8d02061f5a
David Shoots <david@algorand.com>
2019-08-14 12:53:08 -0400

    Created algocfg to provide cli access to config.json (#237)
    * Created algocfg to provide cli access to config.json
    
    Adds `algocfg get|set|reset` commands to manipulate config.json
    in a consistent and typesafe way.
    Modified config.json is persisted minimally - only non-default
    values are written.  Versioning is taken into account and migration
    is performed as required.
    
    This also includes a fix to SaveNonDefaultValuesToFile() which was
    writing config.json files with a trailing empty line for each default
    value that was skipped.
    
    * Noticed typo in old error message
    
    * Support missing config.json; fixed rpm build

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -47,7 +47,7 @@ mkdir -p ${PKG_ROOT}/usr/bin
 
 if [ "${VARIATION}" = "" ]; then
     # NOTE: keep in sync with installer/rpm/algorand.spec
-    bin_files=("algod" "algoh" "algokey" "carpenter" "catchupsrv" "diagcfg" "goal" "kmd" "msgpacktool" "node_exporter")
+    bin_files=("algocfg" "algod" "algoh" "algokey" "carpenter" "catchupsrv" "diagcfg" "goal" "kmd" "msgpacktool" "node_exporter")
 fi
 
 for bin in "${bin_files[@]}"; do
