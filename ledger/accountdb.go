654c4e084f076823a280a71a50cc0f6340140d08
pzbitskiy <pavel.zbitskiy@gmail.com>
2019-09-10 12:08:26 -0400

    Block/Tracker DB reset on switching to archival mode
    * Add blockCount to ledger/blockdb
    
    * Reset local blocks DB if the node switches from non-archival to archival
    
    * Archival flag is now part of LoadLedger/OpenLedger signature.
    SetArchival method removed. The reason blocks DB clean up and its initialization
    needs to be done simultaneously, so that archival to non-archival detection on later
    stage actually requires to re-init blocks DB and blocks queue
    * Introduced ledger.InitState struct to reduce amount of parameters in OpenLedger
    * Most changes in tests except archical_test are mechanical
    * Fixed blockQ closing on exit
    
    * CR fixes: Log warning messages on DB reset
    
    * CR fixes: Switch from COUNT to MIN
    
    * CR fixes: accounts DB reset
    
    * CR Fixes: Do not repeat 'init' in InitState
    
    * CR fixes: Self-review

--- ledger/accountdb.go
+++ ledger/accountdb.go
@@ -52,6 +52,12 @@ var accountsSchema = []string{
 		data blob)`,
 }
 
+var accountsResetExprs = []string{
+	`DROP TABLE IF EXISTS acctrounds`,
+	`DROP TABLE IF EXISTS accounttotals`,
+	`DROP TABLE IF EXISTS accountbase`,
+}
+
 type accountDelta struct {
 	old basics.AccountData
 	new basics.AccountData
@@ -102,6 +108,16 @@ func accountsInit(tx *sql.Tx, initAccounts map[basics.Address]basics.AccountData
 		}
 	}
 
+	return nil
+}
+
+func accountsReset(tx *sql.Tx) error {
+	for _, stmt := range accountsResetExprs {
+		_, err := tx.Exec(stmt)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
