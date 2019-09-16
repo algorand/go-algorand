b98fc20c99939df7c503dabb29109663bf1d85fc
Ubuntu <ubuntu@ip-172-31-25-92.us-west-2.compute.internal>
2019-09-16 21:09:05 +0000

    Few Changes wq

--- scripts/buildhost/configure.sh
+++ scripts/buildhost/configure.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-sudo apt-get install awscli
-sudo apt-get install pq
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+sudo apt-get update -y
+sudo apt-get install awscli -y
+sudo apt-get install pq -y
+
+
+cp env.sh service_env.sh
+sudo cp ./buildhost.service /etc/systemd/system/
+WD=$(pwd)
+sudo echo "WorkingDirectory=${WD}" >> /etc/systemd/system/buildhost.service
+sudo echo "ExecStart=/bin/bash ./run.sh" >> /etc/systemd/system/buildhost.service
+sudo echo "EnvironmentFile=${WD}/service_env.sh" >> /etc/systemd/system/buildhost.service
+
+systemctl enable buildhost
+systemctl start buildhost
 
 
 
