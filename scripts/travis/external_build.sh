237333e23f95da0df7ef9cf3f1a9a87472d65bb7
Tsachi Herman <tsachi@algorand.com>
2019-09-17 16:21:51 -0400

    update

--- scripts/travis/external_build.sh
+++ scripts/travis/external_build.sh
@@ -103,7 +103,7 @@ while [ $SECONDS -lt $end ]; do
         break
     fi
     
-    aws s3 ls ${BUILD_LOG_PATH}-${LOG_SEQ} . ${NO_SIGN_REQUEST} 2> /dev/null
+    aws s3 ls ${BUILD_LOG_PATH}-${LOG_SEQ} ${NO_SIGN_REQUEST} 2> /dev/null
     if [ "$?" = "0" ]; then
         aws s3 cp ${BUILD_LOG_PATH}-${LOG_SEQ} - ${NO_SIGN_REQUEST} | cat
         if [ "$?" = "0" ]; then
