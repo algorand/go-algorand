5454a6c5466c3d9f7154e42cc65b0fdea9006beb
Tsachi Herman <tsachi@algorand.com>
2019-09-17 21:22:28 -0400

    better buffer

--- scripts/buildhost/s3streamup.sh
+++ scripts/buildhost/s3streamup.sh
@@ -9,19 +9,21 @@ BUFFER=
 while read line; do
         BUFFER=${BUFFER}${line}$'\n'
         if [ ${#BUFFER} -gt 10240 ]; then
-                echo "${BUFFER}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
+                echo "${BUFFER::-1}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
                 ((SEQ++))
                 SEND_NEXT=$((SECONDS+10))
                 BUFFER=
         fi
         if [ $SECONDS -gt $SEND_NEXT ]; then
-                echo "${BUFFER}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
-                ((SEQ++))
-                SEND_NEXT=$((SECONDS+10))
-                BUFFER=
+                if [ ${#BUFFER} -gt 0 ]; then
+                        echo "${BUFFER::-1}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
+                        ((SEQ++))
+                        SEND_NEXT=$((SECONDS+10))
+                        BUFFER=
+                fi
         fi
 done < /dev/stdin
 
-if [ ${#BUFFER} -gt 10240 ]; then
-        echo "${BUFFER}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
+if [ ${#BUFFER} -gt 0 ]; then
+        echo "${BUFFER::-1}" | aws s3 cp - ${DEST}-${SEQ} ${NO_SIGN}
 fi
