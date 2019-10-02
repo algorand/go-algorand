59966777b6f97809b5c7a2890ac7bb7a562d9b39
Tsachi Herman <tsachi@algorand.com>
2019-10-02 01:32:03 -0400

    fix few more go list

--- scripts/travis/before_build.sh
+++ scripts/travis/before_build.sh
@@ -28,7 +28,7 @@ function runGoFmt() {
 }
 
 function runGoLint() {
-    warningCount=$($GOPATH/bin/golint `go list ./... | grep -v /vendor/ | grep -v /test/e2e-go/` | wc -l)
+    warningCount=$($GOPATH/bin/golint `GO111MODULE=off go list ./... | grep -v /vendor/ | grep -v /test/e2e-go/` | wc -l)
     if [ $warningCount -eq 0 ]; then
         return 0
     fi
@@ -46,7 +46,7 @@ echo "Building libsodium-fork..."
 make crypto/lib/libsodium.a
 
 echo "Running go vet..."
-go vet `go list ./... | grep -v /test/e2e-go/`
+go vet `GO111MODULE=off go list ./... | grep -v /test/e2e-go/`
 
 echo "Running gofmt..."
 runGoFmt
