dac055561f92095ae2190979b4d061e8ab53e0a0
Derek Leung <derek@algorand.com>
2019-10-17 21:26:36 -0400

    Implement domain separation for LogicSig ed25519verify.
    Arguments passed to ed25519verify are now domain-separated with the
    constant string "ProgData" and the hash of the running program.

--- data/transactions/logic/eval_test.go
+++ data/transactions/logic/eval_test.go
@@ -2134,7 +2134,10 @@ arg 1
 addr %s
 ed25519verify`, pkStr))
 	require.NoError(t, err)
-	sig := c.SignBytes(data[:])
+	sig := c.Sign(LogicMsg{
+		ProgramHash: crypto.HashObj(Program(program)),
+		Data:        data[:],
+	})
 	var txn transactions.SignedTxn
 	txn.Lsig.Logic = program
 	txn.Lsig.Args = [][]byte{data[:], sig[:]}
