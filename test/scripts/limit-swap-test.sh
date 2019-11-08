de56055edc3f9c49072303c0060c966eae0f76cc
Brian Olson <brian.olson@algorand.com>
2019-11-08 12:39:37 -0500

    require that asset setup is before timeout update test

--- test/scripts/limit-swap-test.sh
+++ test/scripts/limit-swap-test.sh
@@ -32,12 +32,11 @@ ASSET_ID=$(goal asset info --creator $ACCOUNT --asset bogo|grep 'Asset ID'|awk '
 
 # Asset ID:         5
 
+echo "closeout part a, Algo trader"
 # quick expiration, test closeout
 
 ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
 TIMEOUT_ROUND=$((${ROUND} + 2))
-
-# closeout part a, Algo trader
 
 sed s/TMPL_ASSET/${ASSET_ID}/g < ${GOPATH}/src/github.com/algorand/go-algorand/tools/teal/templates/limit-order-a.teal.tmpl | sed s/TMPL_SWAPN/31337/g | sed s/TMPL_SWAPD/137/g | sed s/TMPL_TIMEOUT/${TIMEOUT_ROUND}/g | sed s/TMPL_OWN/${ACCOUNT}/g | sed s/TMPL_FEE/100000/g | sed s/TMPL_MINTRD/10000/g > ${TEMPDIR}/limit-order-a.teal
 
@@ -51,7 +50,12 @@ goal node wait
 goal clerk send -a 0 -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -c ${ACCOUNT} --from-program ${TEMPDIR}/limit-order-a.teal
 
 
-# closeout part b, asset trader
+echo "closeout part b, asset trader"
+# quick expiration, test closeout
+
+ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
+TIMEOUT_ROUND=$((${ROUND} + 6))
+SETUP_ROUND=$((${ROUND} + 5))
 
 sed s/TMPL_ASSET/${ASSET_ID}/g < ${GOPATH}/src/github.com/algorand/go-algorand/tools/teal/templates/limit-order-b.teal.tmpl | sed s/TMPL_SWAPN/137/g | sed s/TMPL_SWAPD/31337/g | sed s/TMPL_TIMEOUT/${TIMEOUT_ROUND}/g | sed s/TMPL_OWN/${ACCOUNT}/g | sed s/TMPL_FEE/100000/g | sed s/TMPL_MINTRD/10000/g > ${TEMPDIR}/limit-order-b.teal
 
@@ -59,29 +63,36 @@ sed s/TMPL_ASSET/${ASSET_ID}/g < ${GOPATH}/src/github.com/algorand/go-algorand/t
 
 ACCOUNT_ASSET_TRADER=$(goal clerk compile ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/limit-order-b.tealc|awk '{ print $2 }')
 
-# setup trader with Algos
-goal clerk send --amount 1000000 --from ${ACCOUNT} --to ${ACCOUNT_ASSET_TRADER}
+echo "setup trader with Algos"
+goal clerk send --amount 1000000 --from ${ACCOUNT} --to ${ACCOUNT_ASSET_TRADER} --lastvalid ${SETUP_ROUND}
 
 # goal account balance -a $ACCOUNT_ASSET_TRADER
 
-# make asset trader able to accept asset
-goal asset send -o ${TEMPDIR}/b-asset-init.tx -a 0 --assetid ${ASSET_ID} -t $ACCOUNT_ASSET_TRADER -f $ACCOUNT_ASSET_TRADER
+echo "make asset trader able to accept asset"
+ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
+goal asset send -o ${TEMPDIR}/b-asset-init.tx -a 0 --assetid ${ASSET_ID} -t $ACCOUNT_ASSET_TRADER -f $ACCOUNT_ASSET_TRADER --validrounds $((${SETUP_ROUND} - ${ROUND} - 1))
 
 goal clerk sign -i ${TEMPDIR}/b-asset-init.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/b-asset-init.stx
 
 goal clerk rawsend -f ${TEMPDIR}/b-asset-init.stx
 
-# fund account twith asset
+echo "fund account twith asset"
 goal asset send --assetid ${ASSET_ID} -f ${ACCOUNT} -t ${ACCOUNT_ASSET_TRADER} -a 1000000
 
-# recover asset
+ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
+while [ $ROUND -lt $TIMEOUT_ROUND ]; do
+    goal node wait
+    ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
+done
+
+echo "recover asset"
 goal asset send --assetid ${ASSET_ID} -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bclose.tx
 
 goal clerk sign -i ${TEMPDIR}/bclose.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/bclose.stx
 
 goal clerk rawsend -f ${TEMPDIR}/bclose.stx
 
-# recover algos
+echo "recover algos"
 goal clerk send -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bcloseA.tx
 
 goal clerk sign -i ${TEMPDIR}/bcloseA.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/bcloseA.stx
@@ -89,9 +100,11 @@ goal clerk sign -i ${TEMPDIR}/bcloseA.tx -p ${TEMPDIR}/limit-order-b.teal -o ${T
 goal clerk rawsend -f ${TEMPDIR}/bcloseA.stx
 
 # long expiration, no closeout, actual trade
+echo "test actual swap"
 
 ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
 TIMEOUT_ROUND=$((${ROUND} + 200))
+SETUP_ROUND=$((${ROUND} + 199))
 
 sed s/TMPL_ASSET/${ASSET_ID}/g < ${GOPATH}/src/github.com/algorand/go-algorand/tools/teal/templates/limit-order-b.teal.tmpl | sed s/TMPL_SWAPN/137/g | sed s/TMPL_SWAPD/31337/g | sed s/TMPL_TIMEOUT/${TIMEOUT_ROUND}/g | sed s/TMPL_OWN/${ACCOUNT}/g | sed s/TMPL_FEE/100000/g | sed s/TMPL_MINTRD/10000/g > ${TEMPDIR}/limit-order-b.teal
 
@@ -101,7 +114,8 @@ ACCOUNT_ASSET_TRADER=$(goal clerk compile ${TEMPDIR}/limit-order-b.teal -o ${TEM
 goal clerk send --amount 1000000 --from ${ACCOUNT} --to ${ACCOUNT_ASSET_TRADER}
 
 # make asset trader able to accept asset
-goal asset send -o ${TEMPDIR}/b-asset-init.tx -a 0 --assetid ${ASSET_ID} -t $ACCOUNT_ASSET_TRADER -f $ACCOUNT_ASSET_TRADER
+ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
+goal asset send -o ${TEMPDIR}/b-asset-init.tx -a 0 --assetid ${ASSET_ID} -t $ACCOUNT_ASSET_TRADER -f $ACCOUNT_ASSET_TRADER --validrounds $((${SETUP_ROUND} - ${ROUND} - 1))
 
 goal clerk sign -i ${TEMPDIR}/b-asset-init.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/b-asset-init.stx
 
