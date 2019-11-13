5d20605c0e8297b5c6e0e727b1f9df3fd8ea57d4
Brian Olson <brian.olson@algorand.com>
2019-11-13 08:50:10 -0500

    review feedback tweaks

--- test/scripts/limit-swap-test.sh
+++ test/scripts/limit-swap-test.sh
@@ -24,7 +24,7 @@ trap "goal network stop -r ${NETDIR}; rm -rf ${TEMPDIR}" 0
 export ALGORAND_DATA=${NETDIR}/Node
 
 ACCOUNT=$(goal account list|awk '{ print $3 }')
-
+ZERO_ADDRESS=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ
 
 goal asset create --creator ${ACCOUNT} --name bogocoin --unitname bogo --total 1000000000000
 
@@ -47,7 +47,7 @@ goal clerk send --amount 100000000 --from ${ACCOUNT} --to ${ACCOUNT_ALGO_TRADER}
 
 goal node wait
 
-goal clerk send -a 0 -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -c ${ACCOUNT} --from-program ${TEMPDIR}/limit-order-a.teal
+goal clerk send -a 0 -t ${ZERO_ADDRESS} -c ${ACCOUNT} --from-program ${TEMPDIR}/limit-order-a.teal
 
 
 echo "closeout part b, asset trader"
@@ -59,7 +59,6 @@ SETUP_ROUND=$((${ROUND} + 5))
 
 sed s/TMPL_ASSET/${ASSET_ID}/g < ${GOPATH}/src/github.com/algorand/go-algorand/tools/teal/templates/limit-order-b.teal.tmpl | sed s/TMPL_SWAPN/137/g | sed s/TMPL_SWAPD/31337/g | sed s/TMPL_TIMEOUT/${TIMEOUT_ROUND}/g | sed s/TMPL_OWN/${ACCOUNT}/g | sed s/TMPL_FEE/100000/g | sed s/TMPL_MINTRD/10000/g > ${TEMPDIR}/limit-order-b.teal
 
-#goal clerk compile -n ${TEMPDIR}/limit-order-b.teal
 
 ACCOUNT_ASSET_TRADER=$(goal clerk compile ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/limit-order-b.tealc|awk '{ print $2 }')
 
@@ -76,7 +75,7 @@ goal clerk sign -i ${TEMPDIR}/b-asset-init.tx -p ${TEMPDIR}/limit-order-b.teal -
 
 goal clerk rawsend -f ${TEMPDIR}/b-asset-init.stx
 
-echo "fund account twith asset"
+echo "fund account with asset"
 goal asset send --assetid ${ASSET_ID} -f ${ACCOUNT} -t ${ACCOUNT_ASSET_TRADER} -a 1000000
 
 ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
@@ -86,14 +85,14 @@ while [ $ROUND -lt $TIMEOUT_ROUND ]; do
 done
 
 echo "recover asset"
-goal asset send --assetid ${ASSET_ID} -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bclose.tx
+goal asset send --assetid ${ASSET_ID} -t ${ZERO_ADDRESS} -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bclose.tx
 
 goal clerk sign -i ${TEMPDIR}/bclose.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/bclose.stx
 
 goal clerk rawsend -f ${TEMPDIR}/bclose.stx
 
 echo "recover algos"
-goal clerk send -t AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bcloseA.tx
+goal clerk send -t ${ZERO_ADDRESS} -a 0 -c ${ACCOUNT} -f ${ACCOUNT_ASSET_TRADER} -o ${TEMPDIR}/bcloseA.tx
 
 goal clerk sign -i ${TEMPDIR}/bcloseA.tx -p ${TEMPDIR}/limit-order-b.teal -o ${TEMPDIR}/bcloseA.stx
 
@@ -121,7 +120,7 @@ goal clerk sign -i ${TEMPDIR}/b-asset-init.tx -p ${TEMPDIR}/limit-order-b.teal -
 
 goal clerk rawsend -f ${TEMPDIR}/b-asset-init.stx
 
-# fund account twith asset
+# fund account with asset
 goal asset send --assetid ${ASSET_ID} -f ${ACCOUNT} -t ${ACCOUNT_ASSET_TRADER} -a 100000000
 
 
@@ -150,7 +149,7 @@ cat ${TEMPDIR}/gx-0.stx ${TEMPDIR}/gx-1.stx > ${TEMPDIR}/group.stx
 
 goal account balance -a $ACCOUNT; goal account balance -a $ACCOUNT_ALGO_TRADER; goal account balance -a $ACCOUNT_ASSET_TRADER
 
-# goal clerk dryrun -t ${TEMPDIR}/group.stx
+goal clerk dryrun -t ${TEMPDIR}/group.stx
 goal clerk rawsend -f ${TEMPDIR}/group.stx
 
 date '+limit-swap-test OK %Y%m%d_%H%M%S'
