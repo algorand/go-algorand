25da819b5682a8ad19e8915cfc5ff532cfabfe5b
Tsachi Herman <tsachi@algorand.com>
2019-12-02 14:43:35 -0500

    Add V20.

--- protocol/consensus.go
+++ protocol/consensus.go
@@ -108,6 +108,11 @@ const ConsensusV19 = ConsensusVersion(
 	"https://github.com/algorandfoundation/specs/tree/0e196e82bfd6e327994bec373c4cc81bc878ef5c",
 )
 
+// ConsensusV20 points to adding the precision to the assets.
+const ConsensusV20 = ConsensusVersion(
+	"https://github.com/algorandfoundation/specs/tree/add_precision_to_assets",
+)
+
 // ConsensusFuture is a protocol that should not appear in any production
 // network, but is used to test features before they are released.
 const ConsensusFuture = ConsensusVersion(
@@ -120,7 +125,7 @@ const ConsensusFuture = ConsensusVersion(
 
 // ConsensusCurrentVersion is the latest version and should be used
 // when a specific version is not provided.
-const ConsensusCurrentVersion = ConsensusV19
+const ConsensusCurrentVersion = ConsensusV20
 
 // ConsensusTest0 is a version of ConsensusV0 used for testing
 // (it has different approved upgrade paths).
