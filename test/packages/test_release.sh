d51a038259e5e0a270b4c4c5218262d69bed3479
Benjamin Toll <benjam72@yahoo.com>
2019-12-09 16:58:35 -0500

    Run `update.sh` at RUN time
    This is another intermediate step.  The installer is now being run at
    runtime, but it's not allowing for testing any binaries, such as
    `algod`.
    
    At this point, there are a couple different options to proceed, and I
    think it's best if Will, Tsachi and I talk more about the options.

--- test/packages/test_release.sh
+++ test/packages/test_release.sh
@@ -4,12 +4,12 @@ BLUE_FG=$(tput setaf 4)
 GREEN_FG=$(tput setaf 2)
 RED_FG=$(tput setaf 1)
 TEAL_FG=$(tput setaf 6)
-END_COLOR=$(tput sgr0)
+END_FG_COLOR=$(tput sgr0)
 
 if [[ ! "$AWS_ACCESS_KEY_ID" || ! "$AWS_SECRET_ACCESS_KEY" ]]
 then
-    echo -e "$RED_FG[$0]$END_COLOR Missing AWS credentials." \
-        "\nExport $GREEN_FG\$AWS_ACCESS_KEY_ID$END_COLOR and $GREEN_FG\$AWS_SECRET_ACCESS_KEY$END_COLOR before running this script." \
+    echo -e "$RED_FG[$0]$END_FG_COLOR Missing AWS credentials." \
+        "\nExport $GREEN_FG\$AWS_ACCESS_KEY_ID$END_FG_COLOR and $GREEN_FG\$AWS_SECRET_ACCESS_KEY$END_FG_COLOR before running this script." \
         "\nSee https://aws.amazon.com/blogs/security/wheres-my-secret-access-key/ to obtain creds."
     exit 1
 fi
@@ -51,19 +51,17 @@ build_images () {
     # https://serverfault.com/a/72511
     IFS='' read -r -d '' TOKENIZED <<EOF
     FROM {{OS}}
-    WORKDIR /root/install
+
+    ENV AWS_ACCESS_KEY_ID=""
+    ENV AWS_SECRET_ACCESS_KEY=""
+
     {{PACMAN}}
 
-    ENV AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-    ENV AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+    WORKDIR ~
 
-    RUN curl --silent -L https://github.com/algorand/go-algorand-doc/blob/master/downloads/installers/linux_amd64/install_master_linux-amd64.tar.gz?raw=true | tar xzf - && \
-        ./update.sh -b $BUCKET -c $CHANNEL -n -p ~/node -d ~/node/data -i && \
-        cd .. && \
-        rm -rf install /var/lib/apt/lists/*
+    RUN curl --silent -L https://github.com/algorand/go-algorand-doc/blob/master/downloads/installers/linux_amd64/install_master_linux-amd64.tar.gz?raw=true | tar xzf -
 
-    WORKDIR /root/node
-    CMD ["/bin/bash"]
+    ENTRYPOINT ["./update.sh", "-n", "-p", "~/node", "-d", "~/node/data", "-i"]
 EOF
 
     for item in ${OS_LIST[*]}
@@ -73,14 +71,13 @@ EOF
         # ${parameter/pattern/substitution}
         if [[ $item =~ ubuntu ]]
         then
-            WITH_PACMAN=$(echo -e "${TOKENIZED//\{\{PACMAN\}\}/RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y curl ca-certificates --no-install-recommends}")
+            WITH_PACMAN=$(echo -e "${TOKENIZED//\{\{PACMAN\}\}/RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y curl}")
         else
-            # CentOS/Fedora must have the updated root certs already installed.
             WITH_PACMAN=$(echo -e "${TOKENIZED//\{\{PACMAN\}\}/RUN yum install -y curl}")
         fi
 
         # Finally, designate the OS and send the fully-formed Dockerfile to Docker.
-        echo -e "$BLUE_FG[$0]$END_COLOR Testing $item..."
+        echo -e "$BLUE_FG[$0]$END_FG_COLOR Testing $item..."
         if ! echo -e "${WITH_PACMAN/\{\{OS\}\}/$item}" | docker build -t "${item}-test" -
         then
             FAILED+=("$item")
@@ -91,8 +88,8 @@ EOF
 run_images () {
     for item in ${OS_LIST[*]}
     do
-        echo "$TEAL_FG[$0]$END_COLOR Running ${item}-test..."
-        if ! docker run -t "${item}-test" ./algod -v
+        echo "$TEAL_FG[$0]$END_FG_COLOR Running ${item}-test..."
+        if ! docker run --rm --name algorand -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" -t "${item}-test" -b "$BUCKET" -c "$CHANNEL"
         then
             FAILED+=("$item")
         fi
@@ -102,7 +99,7 @@ run_images () {
 check_failures() {
     if [ "${#FAILED[@]}" -gt 0 ]
     then
-        echo -e "\n$RED_FG[$0]$END_COLOR The following images could not be $1:"
+        echo -e "\n$RED_FG[$0]$END_FG_COLOR The following images could not be $1:"
 
         for failed in ${FAILED[*]}
         do
@@ -116,10 +113,11 @@ check_failures() {
 
 build_images
 check_failures built
+echo "$GREEN_FG[$0]$END_FG_COLOR Builds completed with no failures."
 
 run_images
 check_failures run
+echo "$GREEN_FG[$0]$END_FG_COLOR Runs completed with no failures."
 
-echo "$GREEN_FG[$0]$END_COLOR Tests completed with no failures."
 exit 0
 
