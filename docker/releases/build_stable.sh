01269ad4124dfccfa51786e537d33f31147720fe
Benjamin Toll <benjam72@yahoo.com>
2019-12-11 13:32:39 -0500

    Simplified the release scripts that build images to push to docker hub
    In pushing the updated images to docker hub, I noticed that the
    Dockerfiles and the shell scripts were only differentiated by the
    network name (stable|testnet).
    
    The only file in the dir is now `build_stable.sh`.  It accepts a sole
    argument, `-n` or `--name`.  It will default to "stable", so the for
    that image it's only necessary to run `./build_stable.sh` with no args.
    
    For "testnet", simply call the script like this: `build_stable.sh -n
    testnet`.
    
    The Dockerfile will be automatically created and passed to the `docker
    build` command via `stdin`.

--- docker/releases/build_stable.sh
+++ docker/releases/build_stable.sh
@@ -1,13 +1,60 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
 
 # Need to log in to Docker desktop before docker push will succeed.
 # e.g. `docker login`
-# Log in account should be 'algorand'.
+# Login name is "algorand".
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd ${SCRIPTPATH}
+GREEN_FG=$(tput setaf 2)
+RED_FG=$(tput setaf 1)
+END_FG_COLOR=$(tput sgr0)
 
-docker build -f Dockerfile-stable . -t algorand/stable:latest
-docker push algorand/stable:latest
+NAME=stable
+NETWORK=""
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        -n|--name)
+            shift
+            NAME="$1"
+            NETWORK="-g $1"
+            ;;
+        *)
+            echo "$RED_FG[$0]$END_FG_COLOR Unknown option $1."
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+build_image () {
+    IFS='' read -r -d '' DOCKERFILE <<EOF
+    FROM ubuntu
+
+    WORKDIR /root/install
+    RUN apt-get update && apt-get install -y ca-certificates curl --no-install-recommends && \
+        curl --silent -L https://github.com/algorand/go-algorand-doc/blob/master/downloads/installers/linux_amd64/install_master_linux-amd64.tar.gz?raw=true | tar xzf - && \
+        ./update.sh -c stable -n -p ~/node -d ~/node/data -i $NETWORK && \
+        cd .. && \
+        rm -rf install /var/lib/apt/lists/*
+    WORKDIR /root/node
+
+    ENTRYPOINT ["/bin/bash"]
+EOF
+
+    if ! echo "$DOCKERFILE" | docker build -t algorand/"$NAME":latest -
+    then
+        echo -e "\n$RED_FG[$0]$END_FG_COLOR The algorand/$NAME:latest image could not be built."
+        exit 1
+    fi
+}
+
+build_image
+
+if ! docker push algorand/"$NAME":latest
+then
+    echo -e "\n$RED_FG[$0]$END_FG_COLOR \`docker push\` failed."
+    exit 1
+fi
+
+echo "$GREEN_FG[$0]$END_FG_COLOR Build completed with no failures."
+
