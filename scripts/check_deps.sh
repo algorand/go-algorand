e440575f3bc87ff15627d2a3fa245de7cf6d1dad
Benjamin Toll <benjam72@yahoo.com>
2019-12-12 00:40:29 -0500

    Bring `shellcheck` into the build process
    Let's use bitwise operations to determine package presence

--- scripts/check_deps.sh
+++ scripts/check_deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# checkout_deps.sh - Quickly(!) checks the enlistment for any missing dependencies and attempts to resolve them.
+# check_deps.sh - Quickly(!) checks the enlistment for any missing dependencies and attempts to resolve them.
 #           Reports if any dependencies are still missing after attempts to resolve.
 #
-# Syntax:   checkout_deps.sh
+# Syntax:   check_deps.sh
 #
 # Outputs:  status messages
 #
@@ -11,77 +11,117 @@
 #
 # Usage:    Use before building to ensure dependencies are present (should be nop except after dependencies change)
 #
-# Examples: scripts/checkout_deps.sh
+# Examples: scripts/check_deps.sh
 
-export GOPATH=$(go env GOPATH)
-GOPATH1=$(echo $GOPATH | cut -d: -f1)
+GREEN_FG=$(tput setaf 2)
+RED_FG=$(tput setaf 1)
+TEAL_FG=$(tput setaf 6)
+YELLOW_FG=$(tput setaf 3)
+END_FG_COLOR=$(tput sgr0)
 
-ANY_MISSING=0
-# golint doesn't work with 'dep ensure' so we manually install it
-GOLINT_MISSING=0
-STRINGER_MISSING=0
-SWAGGER_MISSING=0
+GOPATH=$(go env GOPATH)
+export GOPATH
+GOPATH1=$(echo "$GOPATH" | cut -d: -f1)
+DEPS=0
 
-function check_deps() {
-    ANY_MISSING=0
-    GOLINT_MISSING=0
+installing_dep() {
+    echo "$TEAL_FG[INSTALLING]$END_FG_COLOR $1..."
+}
 
-    if [ ! -f "${GOPATH1}/bin/golint" ]; then
-        GOLINT_MISSING=1
-        ANY_MISSING=1
-        echo "... golint missing"
+check_deps() {
+    if [ ! -f "${GOPATH1}/bin/golint" ]
+    then
+        DEPS=$(( "$DEPS" | 1 ))
     fi
 
-    if [ ! -f "${GOPATH1}/bin/stringer" ]; then
-        STRINGER_MISSING=1
-        ANY_MISSING=1
-        echo "... stringer missing"
+    if [ ! -f "${GOPATH1}/bin/stringer" ]
+    then
+        DEPS=$(( "$DEPS" | 2 ))
     fi
 
-    if [ ! -f "${GOPATH1}/bin/swagger" ]; then
-        SWAGGER_MISSING=1
-        ANY_MISSING=1
-        echo "... swagger missing"
+    if [ ! -f "${GOPATH1}/bin/swagger" ]
+    then
+        DEPS=$(( "$DEPS" | 4 ))
     fi
 
-    return ${ANY_MISSING}
+    # Don't print `shellcheck`s location.
+    if ! which shellcheck > /dev/null
+    then
+        DEPS=$(( "$DEPS" | 8 ))
+    fi
 }
 
 check_deps
-if [ $? -eq 0 ]; then
-    echo Required dependencies already installed.
+
+if [ $DEPS -eq 0 ]
+then
+    echo "$GREEN_FG[$0]$END_FG_COLOR Required dependencies already installed."
     exit 0
 fi
 
-if [ ${GOLINT_MISSING} -ne 0 ]; then
-    read -p "Install golint (using go get) (y/N): " OK
-    if [ "$OK" = "y" ]; then
-        echo "Installing golint..."
+if [ $(( DEPS & 1 )) -ne 0 ]
+then
+    read -rp "${YELLOW_FG}MISSING DEPENDENCY$END_FG_COLOR \`golint\`. Install? (using go get) (Y/n): " OK
+    if [[ "$OK" =~ ^""$|Y|y ]]
+    then
+        installing_dep golint
         GO111MODULE=off go get -u golang.org/x/lint/golint
+        DEPS=$(( "$DEPS" & ~1 ))
     fi
 fi
 
-if [ ${STRINGER_MISSING} -ne 0 ]; then
-    read -p "Install stringer (using go get) (y/N): " OK
-    if [ "$OK" = "y" ]; then
-        echo "Installing stringer..."
+if [ $(( DEPS & 2 )) -ne 0 ]
+then
+    read -rp "${YELLOW_FG}MISSING DEPENDENCY$END_FG_COLOR \`stringer\`. Install? (using go get) (Y/n): " OK
+    if [[ "$OK" =~ ^""$|Y|y ]]
+    then
+        installing_dep stringer
         GO111MODULE=off go get -u golang.org/x/tools/cmd/stringer
+        DEPS=$(( "$DEPS" & ~2 ))
     fi
 fi
 
-if [ ${SWAGGER_MISSING} -ne 0 ]; then
-    read -p "Install swagger (using go get) (y/N): " OK
-    if [ "$OK" = "y" ]; then
-        echo "Installing swagger..."
+if [ $(( DEPS & 4 )) -ne 0 ]
+then
+    read -rp "${YELLOW_FG}MISSING DEPENDENCY$END_FG_COLOR \`swagger\`. Install? (using go get) (Y/n): " OK
+    if [[ "$OK" =~ ^""$|Y|y ]]
+    then
+        installing_dep swagger
         GO111MODULE=off go get -u github.com/go-swagger/go-swagger/cmd/swagger
+        DEPS=$(( "$DEPS" & ~4 ))
     fi
 fi
 
-check_deps
-if [ $? -eq 0 ]; then
-    echo Required dependencies have been installed
-    exit 0
-else
-    echo Required dependencies still missing. Build will probably fail.
-    exit 0
+if [ $(( DEPS & 8 )) -ne 0 ]
+then
+
+    read -rp "${YELLOW_FG}MISSING DEPENDENCY$END_FG_COLOR \`shellcheck\`. Install? (using go get) (Y/n): " OK
+    if [[ "$OK" =~ ^""$|Y|y ]]
+    then
+        OS=$(uname)
+
+        if [ "$OS" == "Linux" ]
+        then
+            if ! which sudo > /dev/null
+            then
+                apt-get install sudo -y
+            fi
+
+            sudo apt-get install shellcheck -y
+        elif [ "$OS" == "Darwin" ]
+        then
+            brew install shellcheck
+        fi
+
+        DEPS=$(( "$DEPS" & ~8 ))
+    fi
 fi
+
+if [ $DEPS -eq 0 ]
+then
+    echo -e "$GREEN_FG[$0]$END_FG_COLOR All required dependencies have been installed."
+else
+    echo -e "$RED_FG[$0]$END_FG_COLOR Required dependencies still missing. Build will probably fail."
+    exit 1
+fi
+
--- scripts/configure_dev-deps.sh
+++ scripts/configure_dev-deps.sh
@@ -4,7 +4,7 @@ set -ex
 
 function install_go_module {
     local OUTPUT
-    OUTPUT=$(GO111MODULE=off go get -u $1 2>&1)
+    OUTPUT=$(GO111MODULE=off go get -u "$1" 2>&1)
     if [ "${OUTPUT}" != "" ]; then
         echo "error: executing \"go get -u $1\" failed : ${OUTPUT}"
         exit 1
@@ -14,3 +14,23 @@ function install_go_module {
 install_go_module golang.org/x/lint/golint
 install_go_module golang.org/x/tools/cmd/stringer
 install_go_module github.com/go-swagger/go-swagger/cmd/swagger
+
+OS=$(uname)
+
+if [ "$OS" == "Linux" ]
+then
+    # This script is called from multiple locations, so to be safe we
+    # will do this check and not assume it's already been done.
+    if ! which sudo > /dev/null
+    then
+        apt-get update
+        apt-get install sudo -y
+    fi
+
+    sudo apt-get update
+    sudo apt-get install shellcheck -y
+elif [ "$OS" == "Darwin" ]
+then
+    brew install shellcheck
+fi
+
