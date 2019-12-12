9a106e348cbdd3f7380f4326b2dfe68eeaeae7ae
Benjamin Toll <benjam72@yahoo.com>
2019-12-12 17:00:17 -0500

    Move install of shellcheck into `scripts/configure_dev.sh`
    Also, add shellcheck dependency to other dockerfiles.

--- scripts/configure_dev-deps.sh
+++ scripts/configure_dev-deps.sh
@@ -15,22 +15,3 @@ install_go_module golang.org/x/lint/golint
 install_go_module golang.org/x/tools/cmd/stringer
 install_go_module github.com/go-swagger/go-swagger/cmd/swagger
 
-OS=$(uname)
-
-if [ "$OS" == "Linux" ]
-then
-    # This script is called from multiple locations, so to be safe we
-    # will do this check and not assume it's already been done.
-    if ! which sudo > /dev/null
-    then
-        apt-get update
-        apt-get install sudo -y
-    fi
-
-    sudo apt-get update
-    sudo apt-get install shellcheck -y
-elif [ "$OS" == "Darwin" ]
-then
-    brew install shellcheck
-fi
-
