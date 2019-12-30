15d2280eef661f4978b84e3160a4f0be71dff2b4
Tsachi Herman <tsachi@algorand.com>
2019-12-30 11:31:08 -0500

    Test PATH printout.

--- test/scripts/e2e.sh
+++ test/scripts/e2e.sh
@@ -67,6 +67,7 @@ cd "${SCRIPT_PATH}"
 
 ./timeout 200 ./e2e_basic_start_stop.sh
 
+echo "PATH is ${PATH}"
 python3 -m venv ${TEMPDIR}/ve
 . ${TEMPDIR}/ve/bin/activate
 ${TEMPDIR}/ve/bin/pip3 install --upgrade pip
