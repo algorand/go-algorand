1de7ce50ae2bb62419abe1ca1e11f57e4d019c06
Tsachi Herman <tsachi.herman@algorand.com>
2020-01-02 12:59:54 -0500

    Prepare beta release build 9 (#673)
    * add lease to asset cmds (#575)
    
    * fix Disassemble when multiple bnz have the same target label (#612)
    
    add test
    
    * Add PeerConnections to network telemetry (#607)
    
    * Add PeerConnections to network telemetry.
    
    * omit Endpoint for incoming connections.
    
    * fix asset unit name display in goal account list (#633)
    
    * Add --no-sig flag to goal clerk multisig sign (#647)
    
    * add --no-sig flag to goal clerk multisig sign
    
    * update err message
    
    * change preimage -> template
    
    * change template -> information
    
    * Scan for ledger wallets more often (#638)
    
    * add more robust ledger scanning, fix infinite recursion bug
    
    * fix comment
    
    * undo scan change
    
    * still delete wallets we fail to close
    
    * Improve missing msig preimage error message (#648)
    
    * improve missing msig preimage error message
    
    * improve err msg
    
    * Add support for https for telemetry servers (#649)
    
    * Add support for https for telemetry servers.
    
    * typo : udo -> udp
    
    * Fixed few typos.
    
    * goal listpartkeys display error (#641)
    
    * Add logging for the telemetry server connections (#661)
    
    * Add logging for the telemetry server connections.
    
    * Revert unintended change.
    
    * Improve error message.
    
    * Avoid upgrading boost on travis Mac builds (#669)
    
    * specify a boost version for the mac build.
    
    * try to prevent boost update on travis mac builds.
    
    * Build 9
    
    * Abort algod startup if logging.config file has bad permissions (#662)
    
    * This should prevent telemetry event loses on systems with
      invalid permissions on ~/.algorand/logging.config file
    * Another possible workaround is to relax default config path mask in
      **cmd/goal/commands.go:ensureCacheDir** from 700 to 744.
      This is not implemented because of possible security risk.
    
    Co-authored-by: Max Justicz <maxj@mit.edu>
    Co-authored-by: algobolson <45948765+algobolson@users.noreply.github.com>
    Co-authored-by: Evan Richard <EvanJRichard@users.noreply.github.com>
    Co-authored-by: pzbitskiy <pavel.zbitskiy@gmail.com>

--- tools/network/bootstrap.go
+++ tools/network/bootstrap.go
@@ -9,14 +9,18 @@ import (
 )
 
 // ReadFromSRV is a helper to collect SRV addresses for a given name.
-func ReadFromSRV(service string, name string, fallbackDNSResolverAddress string) (addrs []string, err error) {
+func ReadFromSRV(service string, protocol string, name string, fallbackDNSResolverAddress string) (addrs []string, err error) {
 	log := logging.Base()
 	if name == "" {
 		log.Debug("no dns lookup due to empty name")
 		return
 	}
+	if protocol != "tcp" && protocol != "udp" && protocol != "tls" {
+		err = fmt.Errorf("unsupported protocol '%s' specified", protocol)
+		return
+	}
 
-	_, records, sysLookupErr := net.LookupSRV(service, "tcp", name)
+	_, records, sysLookupErr := net.LookupSRV(service, protocol, name)
 	if sysLookupErr != nil {
 		var resolver Resolver
 		// try to resolve the address. If it's an dotted-numbers format, it would return that right away.
@@ -28,7 +32,7 @@ func ReadFromSRV(service string, name string, fallbackDNSResolverAddress string)
 			log.Infof("ReadFromBootstrap: Failed to resolve fallback DNS resolver address '%s': %v; falling back to default fallback resolver address", fallbackDNSResolverAddress, err2)
 		}
 
-		_, records, err = resolver.LookupSRV(context.Background(), service, "tcp", name)
+		_, records, err = resolver.LookupSRV(context.Background(), service, protocol, name)
 		if err != nil {
 			err = fmt.Errorf("ReadFromBootstrap: DNS LookupSRV failed when using system resolver(%v) as well as via %s due to %v", sysLookupErr, resolver.EffectiveResolverDNS(), err)
 			return
