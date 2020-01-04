b06c6a3a2f35a13dd4887512e94db8effe666fbf
Benjamin Toll <benjam72@yahoo.com>
2020-01-04 16:07:23 -0500

    Add tag stage to pipeline

---
+++ scripts/release/tag.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+TAG=foo-bar
+SIGNING_KEY_ADDR=dev@algorand.com
+
+#  # Set the TIMESTAMP to use for the genesis.json file - set here so all packages use the same number
+#  TIMESTAMP=
+#  if [ -e ./genesistimestamp.dat ]; then
+#      TIMESTAMP=$(cat ./genesistimestamp.dat)
+#  else
+#      TIMESTAMP=$(date +%s)
+#  fi
+#  export TIMESTAMP=${TIMESTAMP}
+
+cd "$REPO_ROOT" || exit
+git tag -d "${TAG}"
+git checkout HEAD
+git tag -s -u "${SIGNING_KEY_ADDR}" "${TAG}" -m "Genesis Timestamp: $(cat ./genesistimestamp.dat)"
+git tag --verify "${TAG}"
+git push -n --tags
+git push --force --tags
+
