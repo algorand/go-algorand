cfce52c45d1817528a9b40b3f20e96030fde317b
Benjamin Toll <benjam72@yahoo.com>
2020-01-08 02:06:03 -0500

    Move build_env build.sh -> setup.sh
    Moved socket.sh -> controller/socket.sh

--- scripts/release/controller/build.sh
+++ scripts/release/controller/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+. "${HOME}/build_env"
+
 date "+build_release start %Y%m%d_%H%M%S"
 
 set -ex
@@ -8,63 +10,57 @@ set -ex
 REPO_ROOT=/home/ubuntu/go/src/github.com/algorand/go-algorand/
 
 cd ${REPO_ROOT}
-export RELEASE_GENESIS_PROCESS=true
-PLATFORM=$(./scripts/osarchtype.sh)
-PLATFORM_SPLIT=(${PLATFORM//\// })
-OS=${PLATFORM_SPLIT[0]}
-ARCH=${PLATFORM_SPLIT[1]}
-# TODO - branch needs to come from user input
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export BRANCH
-CHANNEL=$(./scripts/compute_branch_channel.sh "${BRANCH}")
-export CHANNEL
-DEFAULTNETWORK=$(./scripts/compute_branch_network.sh)
-export DEFAULTNETWORK
-export PKG_ROOT=${HOME}/node_pkg
-export VARIATIONS="base"
+#export RELEASE_GENESIS_PROCESS=true
+#PLATFORM=$(./scripts/osarchtype.sh)
+#PLATFORM_SPLIT=(${PLATFORM//\// })
+#OS=${PLATFORM_SPLIT[0]}
+#ARCH=${PLATFORM_SPLIT[1]}
+#DEFAULTNETWORK=$(./scripts/compute_branch_network.sh)
+#export DEFAULTNETWORK
+#export PKG_ROOT=${HOME}/node_pkg
+#export VARIATIONS="base"
 # tell underlying 'build' scripts we already built
 
 # What's my default IP address?
 # get the datacenter IP address for this EC2 host.
 # this might equivalently be gotten from `netstat -rn` and `ifconfig -a`
-DC_IP=$(curl --silent http://169.254.169.254/latest/meta-data/local-ipv4)
-if [ -z "${DC_IP}" ]; then
-    echo "ERROR: need DC_IP to be set to your local (but not localhost) IP"
-    exit 1
-fi
+#DC_IP=$(curl --silent http://169.254.169.254/latest/meta-data/local-ipv4)
+#if [ -z "${DC_IP}" ]; then
+#    echo "ERROR: need DC_IP to be set to your local (but not localhost) IP"
+#    exit 1
+#fi
 
 # Update version file for this build
-if [ ! -z "${BUILD_NUMBER}" ]; then
-    echo "using externally set BUILD_NUMBER=${BUILD_NUMBER} without incrementing"
-else
-    if [ -e buildnumber.dat ]; then
-	BUILD_NUMBER=$(cat ./buildnumber.dat)
-	BUILD_NUMBER=$(( BUILD_NUMBER + 1 ))
-    else
-	BUILD_NUMBER=0
-    fi
-    echo ${BUILD_NUMBER} > ./buildnumber.dat
-    git add -A
-    git commit -m "Build ${BUILD_NUMBER}"
-fi
-FULLVERSION=$(./scripts/compute_build_number.sh -f)
-export FULLVERSION
+#if [ ! -z "${BUILD_NUMBER}" ]; then
+#    echo "using externally set BUILD_NUMBER=${BUILD_NUMBER} without incrementing"
+#else
+#    if [ -e buildnumber.dat ]; then
+#	BUILD_NUMBER=$(cat ./buildnumber.dat)
+#	BUILD_NUMBER=$(( BUILD_NUMBER + 1 ))
+#    else
+#	BUILD_NUMBER=0
+#    fi
+#    echo ${BUILD_NUMBER} > ./buildnumber.dat
+#    git add -A
+#    git commit -m "Build ${BUILD_NUMBER}"
+#fi
+#FULLVERSION=$(./scripts/compute_build_number.sh -f)
+#export FULLVERSION
 
 # a bash user might `source build_env` to manually continue a broken build
-cat <<EOF>"${HOME}"/build_env
-export RELEASE_GENESIS_PROCESS=${RELEASE_GENESIS_PROCESS}
-PLATFORM=${PLATFORM}
-OS=${OS}
-ARCH=${ARCH}
-export BRANCH=${BRANCH}
-export CHANNEL=${CHANNEL}
-export DEFAULTNETWORK=${DEFAULTNETWORK}
-export PKG_ROOT=${PKG_ROOT}
-export VARIATIONS=${VARIATIONS}
-BUILD_NUMBER=${BUILD_NUMBER}
-export FULLVERSION=${FULLVERSION}
-DC_IP=${DC_IP}
-EOF
+#cat <<EOF>"${HOME}"/build_env
+#export RELEASE_GENESIS_PROCESS=${RELEASE_GENESIS_PROCESS}
+#PLATFORM=${PLATFORM}
+#OS=${OS}
+#ARCH=${ARCH}
+#export CHANNEL=${CHANNEL}
+#export DEFAULTNETWORK=${DEFAULTNETWORK}
+#export PKG_ROOT=${PKG_ROOT}
+#export VARIATIONS=${VARIATIONS}
+#BUILD_NUMBER=${BUILD_NUMBER}
+#export FULLVERSION=${FULLVERSION}
+#DC_IP=${DC_IP}
+#EOF
 
 # strip leading 'export ' for docker --env-file
 sed 's/^export //g' < "${HOME}"/build_env > "${HOME}"/build_env_docker
--- scripts/release/controller/setup.sh
+++ scripts/release/controller/setup.sh
@@ -109,5 +109,57 @@ make install
 
 gpgconf --launch gpg-agent
 
+REPO_PATH="${HOME}/go/src/github.com/algorand/go-algorand"
+export RELEASE_GENESIS_PROCESS=true
+PLATFORM=$("${REPO_PATH}"/scripts/osarchtype.sh)
+PLATFORM_SPLIT=(${PLATFORM//\// })
+OS=${PLATFORM_SPLIT[0]}
+ARCH=${PLATFORM_SPLIT[1]}
+DEFAULTNETWORK=$("${REPO_PATH}"/scripts/compute_branch_network.sh)
+export DEFAULTNETWORK
+export PKG_ROOT=${HOME}/node_pkg
+export VARIATIONS="base"
+FULLVERSION=$("${REPO_PATH}"/scripts/compute_build_number.sh -f)
+export FULLVERSION
+
+# Update version file for this build
+if [ ! -z "${BUILD_NUMBER}" ]; then
+    echo "using externally set BUILD_NUMBER=${BUILD_NUMBER} without incrementing"
+else
+    if [ -e buildnumber.dat ]; then
+	BUILD_NUMBER=$(cat ./buildnumber.dat)
+	BUILD_NUMBER=$(( BUILD_NUMBER + 1 ))
+    else
+	BUILD_NUMBER=0
+    fi
+    echo ${BUILD_NUMBER} > ./buildnumber.dat
+    git add -A
+    git commit -m "Build ${BUILD_NUMBER}"
+fi
+
+# What's my default IP address?
+# get the datacenter IP address for this EC2 host.
+# this might equivalently be gotten from `netstat -rn` and `ifconfig -a`
+DC_IP=$(curl --silent http://169.254.169.254/latest/meta-data/local-ipv4)
+if [ -z "${DC_IP}" ]; then
+    echo "ERROR: need DC_IP to be set to your local (but not localhost) IP"
+    exit 1
+fi
+
+# a bash user might `source build_env` to manually continue a broken build
+cat <<EOF>"${HOME}"/build_env
+export RELEASE_GENESIS_PROCESS=${RELEASE_GENESIS_PROCESS}
+PLATFORM=${PLATFORM}
+OS=${OS}
+ARCH=${ARCH}
+export CHANNEL=${CHANNEL}
+export DEFAULTNETWORK=${DEFAULTNETWORK}
+export PKG_ROOT=${PKG_ROOT}
+export VARIATIONS=${VARIATIONS}
+BUILD_NUMBER=${BUILD_NUMBER}
+export FULLVERSION=${FULLVERSION}
+DC_IP=${DC_IP}
+EOF
+
 date "+setup finish %Y%m%d_%H%M%S"
 
