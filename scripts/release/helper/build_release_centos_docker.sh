2b7ccdb81ebfe056dac798f72646eae048ec5aa7
Benjamin Toll <benjam72@yahoo.com>
2020-01-14 14:12:40 -0500

    fix build

--- scripts/release/helper/build_release_centos_docker.sh
+++ scripts/release/helper/build_release_centos_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=2012
+# shellcheck disable=1090,2012
 #
 # build centos rpm from inside docker
 #
@@ -15,8 +15,25 @@
 set -ex
 
 export HOME=/root
-export GOPATH=${HOME}/go
-export PATH=${GOPATH}/bin:/usr/local/go/bin:${PATH}
+
+. "${HOME}"/subhome/build_env
+
+GIT_REPO_PATH=https://github.com/btoll/go-algorand
+mkdir -p "${HOME}/go/src/github.com/algorand"
+cd "${HOME}/go/src/github.com/algorand" && git clone --single-branch --branch "${HASH}" "${GIT_REPO_PATH}" go-algorand
+
+# Get golang 1.12 and build its own copy of go-algorand.
+python3 "${HOME}/go/src/github.com/algorand/go-algorand/scripts/get_latest_go.py" --version-prefix=1.12
+sudo bash -c "cd /usr/local && tar zxf ${HOME}/go*.tar.gz"
+
+GOPATH=$(/usr/local/go/bin/go env GOPATH)
+export PATH=${HOME}/gpgbin:${GOPATH}/bin:/usr/local/go/bin:${PATH}
+export GOPATH
+
+# Build!
+scripts/configure_dev.sh
+make crypto/lib/libsodium.a
+make build
 
 # Anchor our repo root reference location
 REPO_DIR=/root/go/src/github.com/algorand/go-algorand
