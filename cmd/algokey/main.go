f2729f787f8a246864fe268c0c2c3c9259c1bab6
Will Winder <wwinder.unh@gmail.com>
2020-01-21 11:59:16 -0500

    Generate docs for algokey.

--- cmd/algokey/main.go
+++ cmd/algokey/main.go
@@ -21,6 +21,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
 var rootCmd = &cobra.Command{
@@ -43,6 +44,17 @@ func init() {
 }
 
 func main() {
+	// Hidden command to generate docs in a given directory
+	// goal generate-docs [path]
+	if len(os.Args) == 3 && os.Args[1] == "generate-docs" {
+		err := doc.GenMarkdownTree(rootCmd, os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
