c2eaf15b129a3419a054d876abe1236f1dcec5e8
Evan Richard <evan.richard@algorand.com>
2020-01-21 13:27:23 -0500

    wip checkin

--- cmd/goal/asset.go
+++ cmd/goal/asset.go
@@ -211,7 +211,7 @@ func lookupAssetID(cmd *cobra.Command, creator string, client libgoal.Client) {
 var createAssetCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create an asset",
-	Long: "Post a transaction declaring and issuing a new layer-one asset on the network.",
+	Long:  "Post a transaction declaring and issuing a new layer-one asset on the network.",
 	Args:  validateNoPosArgsFn,
 	Run: func(cmd *cobra.Command, _ []string) {
 		checkTxValidityPeriodCmdFlags(cmd)
@@ -288,7 +288,7 @@ var createAssetCmd = &cobra.Command{
 var destroyAssetCmd = &cobra.Command{
 	Use:   "destroy",
 	Short: "Destroy an asset",
-	Long: `Issue a transaction deleting an asset from the network. This transaction must be issued by the asset owner, who must hold all outstanding asset tokens.`
+	Long:  `Issue a transaction deleting an asset from the network. This transaction must be issued by the asset owner, who must hold all outstanding asset tokens.`,
 	Args:  validateNoPosArgsFn,
 	Run: func(cmd *cobra.Command, _ []string) {
 		checkTxValidityPeriodCmdFlags(cmd)
@@ -356,7 +356,7 @@ var destroyAssetCmd = &cobra.Command{
 var configAssetCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Configure an asset",
-	Long: `Change an asset configuration. This transaction must be issued by the asset manager. This allows any management address to be changed: manager, freezer, reserve, or clawback.`,
+	Long:  `Change an asset configuration. This transaction must be issued by the asset manager. This allows any management address to be changed: manager, freezer, reserve, or clawback.`,
 	Args:  validateNoPosArgsFn,
 	Run: func(cmd *cobra.Command, _ []string) {
 		checkTxValidityPeriodCmdFlags(cmd)
@@ -526,7 +526,7 @@ var sendAssetCmd = &cobra.Command{
 var freezeAssetCmd = &cobra.Command{
 	Use:   "freeze",
 	Short: "Freeze assets",
-	Long: `Freeze or unfreeze assets for a target account. The transaction must be issued by the freeze address for the asset in question.`,
+	Long:  `Freeze or unfreeze assets for a target account. The transaction must be issued by the freeze address for the asset in question.`,
 	Args:  validateNoPosArgsFn,
 	Run: func(cmd *cobra.Command, _ []string) {
 		checkTxValidityPeriodCmdFlags(cmd)
@@ -605,7 +605,7 @@ func assetDecimalsFmt(amount uint64, decimals uint32) string {
 var infoAssetCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Look up current parameters for an asset",
-	Long: `Look up asset information stored on the network, such as asset creator, management addresses, or asset name.`,
+	Long:  `Look up asset information stored on the network, such as asset creator, management addresses, or asset name.`,
 	Args:  validateNoPosArgsFn,
 	Run: func(cmd *cobra.Command, _ []string) {
 		dataDir := ensureSingleDataDir()
