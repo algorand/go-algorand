bb40775b68a4ba0012c565518ad5554dd7cf139b
algobolson <45948765+algobolson@users.noreply.github.com>
2020-01-22 14:37:31 -0500

    ledger/eval refactor (#700)
    refactor ledger/eval block validation
    don't do crypto+lsig validation in eval
    fix sync in backlog executer queue
    clean up lots of logging to make tests quieter

--- data/pools/transactionPool_test.go
+++ data/pools/transactionPool_test.go
@@ -102,7 +102,7 @@ func newBlockEvaluator(t TestingT, l *ledger.Ledger) *ledger.BlockEvaluator {
 	require.NoError(t, err)
 
 	next := bookkeeping.MakeBlock(prev)
-	eval, err := l.StartEvaluator(next.BlockHeader, &alwaysVerifiedPool{}, nil)
+	eval, err := l.StartEvaluator(next.BlockHeader)
 	require.NoError(t, err)
 
 	return eval
