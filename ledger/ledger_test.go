c78ada09f230a3c66cd934860700f93ff31a93eb
algobolson <45948765+algobolson@users.noreply.github.com>
2020-01-22 14:37:31 -0500

    ledger/eval refactor (#700)
    refactor ledger/eval block validation
    don't do crypto+lsig validation in eval
    fix sync in backlog executer queue
    clean up lots of logging to make tests quieter

--- ledger/ledger_test.go
+++ ledger/ledger_test.go
@@ -202,7 +202,8 @@ func TestLedgerBasic(t *testing.T) {
 	genesisInitState, _ := testGenerateInitState(t, protocol.ConsensusCurrentVersion)
 	const inMem = true
 	const archival = true
-	l, err := OpenLedger(logging.Base(), t.Name(), inMem, genesisInitState, archival)
+	log := logging.TestingLog(t)
+	l, err := OpenLedger(log, t.Name(), inMem, genesisInitState, archival)
 	require.NoError(t, err, "could not open ledger")
 	defer l.Close()
 }
@@ -353,7 +354,8 @@ func TestLedgerSingleTx(t *testing.T) {
 	genesisInitState, initSecrets := testGenerateInitState(t, protocol.ConsensusV7)
 	const inMem = true
 	const archival = true
-	l, err := OpenLedger(logging.Base(), t.Name(), inMem, genesisInitState, archival)
+	log := logging.TestingLog(t)
+	l, err := OpenLedger(log, t.Name(), inMem, genesisInitState, archival)
 	a.NoError(err, "could not open ledger")
 	defer l.Close()
 
@@ -488,6 +490,11 @@ func TestLedgerSingleTx(t *testing.T) {
 	sbadTx.Sig = crypto.Signature{}
 	a.Error(l.appendUnvalidatedSignedTx(t, initAccounts, sbadTx, ad), "added tx with no signature")
 
+	badTx = correctPay
+	sbadTx = sign(initSecrets, badTx)
+	sbadTx.Sig[5]++
+	a.Error(l.appendUnvalidatedSignedTx(t, initAccounts, sbadTx, ad), "added tx with corrupt signature")
+
 	// TODO set multisig and test
 
 	badTx = correctPay
@@ -538,7 +545,8 @@ func testLedgerSingleTxApplyData(t *testing.T, version protocol.ConsensusVersion
 	genesisInitState, initSecrets := testGenerateInitState(t, version)
 	const inMem = true
 	const archival = true
-	l, err := OpenLedger(logging.Base(), t.Name(), inMem, genesisInitState, archival)
+	log := logging.TestingLog(t)
+	l, err := OpenLedger(log, t.Name(), inMem, genesisInitState, archival)
 	a.NoError(err, "could not open ledger")
 	defer l.Close()
 
