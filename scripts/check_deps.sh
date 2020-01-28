01ca66867a9ea954caf95470612da262fbb3f480
Tsachi Herman <tsachi.herman@algorand.com>
2020-01-27 21:56:49 -0500

    Fix checkdep message. (#745)

--- scripts/check_deps.sh
+++ scripts/check_deps.sh
@@ -60,7 +60,7 @@ if [ $MISSING -eq 0 ]
 then
     echo "$GREEN_FG[$0]$END_FG_COLOR Required dependencies installed."
 else
-    echo -e "$RED_FG[$0]$END_FG_COLOR Required dependencies missing. Run \`${TEAL_FG}./scripts/configure-dev.sh$END_FG_COLOR\` to install."
+    echo -e "$RED_FG[$0]$END_FG_COLOR Required dependencies missing. Run \`${TEAL_FG}./scripts/configure_dev.sh$END_FG_COLOR\` to install."
     exit 1
 fi
 
