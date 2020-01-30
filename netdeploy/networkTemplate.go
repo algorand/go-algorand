c5c37ebd9f80ec63c4f5f7f03df4d5e4d27931a4
pzbitskiy <pavel@algorand.com>
2020-01-30 12:42:56 -0500

    Implement local net template generation with netgoal (#762)
    * Usage:
      netgoal generate -n 1 -R 1 -w 100 -o mynettemplate.json -r . -t goalnet
      goal network create -t mynettemplate.json -r mynet -n mynet
    
    * Remove duplicate definitions from netdeploy/networkTemplate

--- netdeploy/networkTemplate.go
+++ netdeploy/networkTemplate.go
@@ -30,25 +30,14 @@ import (
 	"github.com/algorand/go-algorand/crypto"
 	"github.com/algorand/go-algorand/gen"
 	"github.com/algorand/go-algorand/libgoal"
+	"github.com/algorand/go-algorand/netdeploy/remote"
 	"github.com/algorand/go-algorand/util"
 )
-
-type walletTemplateData struct {
-	Name              string
-	ParticipationOnly bool
-}
-
-type nodeConfig struct {
-	Name              string
-	IsRelay           bool
-	Wallets           []walletTemplateData
-	DeadlockDetection int
-}
 
 // NetworkTemplate represents the template used for creating private named networks
 type NetworkTemplate struct {
 	Genesis gen.GenesisData
-	Nodes   []nodeConfig
+	Nodes   []remote.NodeConfigGoal
 }
 
 var defaultNetworkTemplate = NetworkTemplate{
@@ -146,7 +135,7 @@ func (t NetworkTemplate) createNodeDirectories(targetFolder string, binDir strin
 
 		// Create any necessary config.json file for this node
 		nodeCfg := filepath.Join(nodeDir, config.ConfigFilename)
-		err = cfg.createConfigFile(nodeCfg, len(t.Nodes)-1) // minus 1 to avoid counting self
+		err = createConfigFile(cfg, nodeCfg, len(t.Nodes)-1) // minus 1 to avoid counting self
 		if err != nil {
 			return
 		}
@@ -218,7 +207,7 @@ func (t NetworkTemplate) Validate() error {
 }
 
 // TODO: Build the JSON object using a real encoder
-func (node nodeConfig) createConfigFile(configFile string, numNodes int) error {
+func createConfigFile(node remote.NodeConfigGoal, configFile string, numNodes int) error {
 	// Override default :8080 REST endpoint, and disable SRV lookup
 	configString := `{ "GossipFanout": ` + fmt.Sprintf("%d", numNodes) +
 		`, "EndpointAddress": "127.0.0.1:0", "DNSBootstrapID": "", "EnableProfiler": true`
