5c3c8fb4db63a2eeb5b57553359c35941e28e376
btoll <benjam72@yahoo.com>
2020-02-01 20:03:49 -0500

    Release build pipeline step 2: Test (#773)
    * Reorganize
    
    * more restructuring
    
    * begin test stuff
    
    * restructure
    
    * fix deb test
    
    * fix rpm test
    
    * fix build
    
    * restructure
    
    * fix bug
    
    * remove temporary feature branch
    
    * added new gpg.sh
    
    * removed buildnumber.dat

---
+++ scripts/release/ci/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Create and export the fake keys used by the local test repos.
+
+set -ex
+
+echo
+date "+build_release begin TEST stage %Y%m%d_%H%M%S"
+echo
+
+echo Setup GPG
+"${HOME}"/ben-branch/scripts/release/ci/gpg.sh
+
+"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/deb/run_ubuntu.sh
+date "+build_release done testing ubuntu %Y%m%d_%H%M%S"
+
+"${HOME}"/go/src/github.com/algorand/go-algorand/scripts/release/test/rpm/run_centos.sh
+date "+build_release done testing centos %Y%m%d_%H%M%S"
+
+echo
+date "+build_release end TEST stage %Y%m%d_%H%M%S"
+echo
+
---
+++ scripts/release/test/rpm/run_centos.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# shellcheck disable=2012
+
+set -ex
+
+. "${HOME}"/build_env
+
+echo
+date "+build_release begin TEST stage %Y%m%d_%H%M%S"
+echo
+
+# Run RPM build in Centos7 Docker container
+sg docker "docker build -t algocentosbuild - < ${HOME}/go/src/github.com/algorand/go-algorand/scripts/release/rpm/centos-build.Dockerfile"
+
+cat <<EOF>"${HOME}"/dummyrepo/algodummy.repo
+[algodummy]
+name=Algorand
+baseurl=http://${DC_IP}:8111/
+enabled=1
+gpgcheck=1
+gpgkey=https://releases.algorand.com/rpm/rpm_algorand.pub
+EOF
+
+cd "${HOME}"/dummyrepo && python3 "${HOME}"/go/src/github.com/algorand/go-algorand/scripts/httpd.py --pid "${HOME}"/phttpd.pid &
+trap "${HOME}"/go/src/github.com/algorand/go-algorand/scripts/kill_httpd.sh 0
+
+sg docker "docker run --rm --env-file ${HOME}/build_env --mount type=bind,src=/run/user/1000/gnupg/S.gpg-agent,dst=/root/S.gpg-agent --mount type=bind,src=${HOME}/dummyrepo,dst=/root/dummyrepo --mount type=bind,src=${HOME}/keys,dst=/root/keys --mount type=bind,src=${HOME},dst=/root/subhome algocentosbuild /root/subhome/go/src/github.com/algorand/go-algorand/scripts/release/test/rpm/test_algorand.sh"
+
+echo
+date "+build_release end TEST stage %Y%m%d_%H%M%S"
+echo
+
