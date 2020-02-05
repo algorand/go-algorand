39961b46e764bfea8780a2b683fb19e958ca67e0
btoll <benjam72@yahoo.com>
2020-02-04 22:34:53 -0500

    Restructure release/ dir (#782)
    * Restructure release/ dir for each build release pipeline stage
    
    First step is the `build` pipeline.
    
    * More restructuring
    
    Removed `release/ci/`.  Every dir under `release/` will now be a
    pipeline.
    
    * Added "test" pipeline
    
    * update readme
    
    * Remove temp location and remove code cruft
    
    * removed outdated readme
    
    * more cleanup
    
    * implement reviewer changes

---
+++ scripts/release/test/stage/setup/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=2029
+
+# Path(s) are relative to the root of the Jenkins workspace.
+INSTANCE=$(cat scripts/release/tmp/instance)
+BUCKET="$1"
+CHANNEL="$2"
+RELEASE="$3"
+
+rm -rf pkg/* && mkdir -p pkg/"$FULLVERSION"
+aws s3 sync s3://"$BUCKET"/"$CHANNEL"/"$RELEASE" pkg/ --exclude "*" --include "*.deb" --include "*.rpm"
+ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" mkdir node_pkg
+# Upload the packages and their signatures.
+scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r pkg/* ubuntu@"$INSTANCE":~/node_pkg/
+
+aws s3 cp s3://algorand-devops-misc/tools/gnupg2.2.9_centos7_amd64.tar.bz2 .
+scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r gnupg2.2.9_centos7_amd64.tar.bz2 ubuntu@"$INSTANCE":
+
+scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r scripts/release/test/stage/setup/task.sh ubuntu@"$INSTANCE":
+ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" bash task.sh "$CHANNEL" "$RELEASE"
+
--- scripts/release/test/stage/test/run.sh
+++ scripts/release/test/stage/test/run.sh
@@ -9,5 +9,5 @@ python3 scripts/get_current_installers.py "$1/$2"
 
 # Copy previous installers into ~.
 scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no ./*.deb ubuntu@"$INSTANCE":
-ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" bash go/src/github.com/algorand/go-algorand/scripts/release/ci/test.sh
+ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" bash go/src/github.com/algorand/go-algorand/scripts/release/test/stage/test/task.sh
 
