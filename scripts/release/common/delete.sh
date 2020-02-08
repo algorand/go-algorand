e0d5dfa7b0dfb9f602b5105e343eec4e2273a927
btoll <benjam72@yahoo.com>
2020-02-08 18:37:47 -0500

    Some release abstraction (#796)
    * Added "prod" pipeline to `release/`
    
    * Added snapshot.sh to be invoked manually
    
    * Restructuring
    
    * test stage
    
    * add common build params
    
    * Remove temp github location

--- scripts/release/common/delete.sh
+++ scripts/release/common/delete.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Path(s) are relative to the root of the Jenkins workspace.
-scripts/release/start_ec2_instance.sh "$1" "$2" "$3"
+scripts/release/common/ec2/shutdown.sh "$1"
 
