bedbdafb4f3193f5151c0d59ceccf1627a51db19
algonautshant <shant@algorand.com>
2020-02-12 17:29:06 -0500

    Adding comments and error handling.

--- network/topics.go
+++ network/topics.go
@@ -17,6 +17,7 @@
 package network
 
 import (
+	 
 	"bytes"
 	"encoding/binary"
 	"fmt"
@@ -25,46 +26,59 @@ import (
 	//	"github.com/algorand/go-algorand/crypto"
 )
 
-
+// Topic is a key-value pair
 type Topic struct {
 	key string
 	data []byte
 }
 
+// Topics is an array of type Topic
 type Topics []Topic
 
+// MarshalTopics serializes the topics into a byte array
 func (ta Topics) MarshalTopics()(b []byte, e error) {
-	bufferSize := binary.MaxVarintLen32 // 1 for the size of topic array
-	
+
+	// Calculate the total buffer size required to store the topics
+	bufferSize := binary.MaxVarintLen32 // store topic array size
+
 	for _, val := range ta {
-		bufferSize += 2*binary.MaxVarintLen32 // 1 for key size, one for data size
+		bufferSize += 2*binary.MaxVarintLen32 // store key size and the data size
 		bufferSize += len(val.key)
 		bufferSize += len(val.data)
 	}
 
+	
 	buffer := make([]byte, bufferSize)
 	bidx := binary.PutUvarint(buffer, uint64(len(ta)))	
 	for _, val := range ta {
-		// copy the key
+		// write the key size
 		n := binary.PutUvarint(buffer[bidx:], uint64(len(val.key)))
-		bidx +=n 
+		bidx +=n
+		// write the key
 		n = copy(buffer[bidx:], []byte(val.key))
 		bidx += n
 
-		// copy the data
+		// write the data size
 		n = binary.PutUvarint(buffer[bidx:], uint64(len(val.data)))
 		bidx += n
+		// write the data
 		n = copy(buffer[bidx:], val.data)
 		bidx += n
 	}
-	if bidx > bufferSize {
-		e = fmt.Errorf("Topic Marshal is broken!")
+
+	// the size could be smaller than estimated because used
+	// MaxVarintLen32 instead of the real size during estimation
+	if bidx > bufferSize { 
+		e = fmt.Errorf("Unexpected error during Marshalling.!")
 	}
 	return buffer, e
 }
 
+// Unmarshall the topics from the byte array
 func UnmarshalTopics(buffer []byte) (ts Topics, err error) {
 	reader := bytes.NewReader(buffer)
+
+	// Get the number of topics
 	numTopics, e := binary.ReadUvarint(reader)
 	if e != nil {
 		return nil, e
@@ -72,16 +86,30 @@ func UnmarshalTopics(buffer []byte) (ts Topics, err error) {
 	topics := make([]Topic, numTopics)
 	
 	for x:=0; x < int(numTopics); x++ {
+		// read the key length
 		len, e := binary.ReadUvarint(reader)
 		if e != nil {
 			return nil, e
 		}
+		// read the key
 		tmpBuffer := make([]byte, len)
-		io.ReadFull(io.LimitReader(reader, int64(len)), tmpBuffer)
+		_, e = io.ReadAtLeast(reader, tmpBuffer, int(len))
+		if e != nil {
+			return nil, e
+		}
 		topics[x].key = string(tmpBuffer)
+
+		// read the data length
 		len, e = binary.ReadUvarint(reader)
+		if e != nil {
+			return nil, e
+		}
+		// read the data
 		topics[x].data = make([]byte, len)
-		io.ReadFull(io.LimitReader(reader, int64(len)), topics[x].data)
+		_, e = io.ReadAtLeast(reader, topics[x].data, int(len))
+		if e != nil {
+			return nil, e
+		}
 	}
 	return topics, e
 }
