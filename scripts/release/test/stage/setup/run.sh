230c5f9983cdc73f63ff0dd8346f8eb286af2e60
Benjamin Toll <benjam72@yahoo.com>
2020-02-14 16:27:57 -0500

    updating test phase

--- scripts/release/test/stage/setup/run.sh
+++ scripts/release/test/stage/setup/run.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 # shellcheck disable=2029
 
+# This is temporary (taken from `compute_branch_channel.sh`).
+if [ "$1" = "master" ]; then
+    CHANNEL=master
+elif [ "$1" = "rel/nightly" ]; then
+    CHANNEL=nightly
+elif [ "$1" = "rel/stable" ]; then
+    CHANNEL=stable
+elif [ "$1" = "rel/beta" ]; then
+    CHANNEL=beta
+else
+    CHANNEL=dev
+fi
+
 # Path(s) are relative to the root of the Jenkins workspace.
 INSTANCE=$(cat scripts/release/common/ec2/tmp/instance)
-BUCKET="$1"
-BRANCH="$2"
-CHANNEL="$3"
+#BUCKET="$1"
+BRANCH="$1"
 RELEASE="$4"
 
 rm -rf pkg/* && mkdir -p pkg/"$FULLVERSION"
-aws s3 sync s3://"$BUCKET"/"$CHANNEL"/"$RELEASE" pkg/ --exclude "*" --include "*.deb" --include "*.rpm"
+#aws s3 sync s3://"$BUCKET"/"$CHANNEL"/"$RELEASE" pkg/ --exclude "*" --include "*.deb" --include "*.rpm"
+aws s3 sync s3://ben-test-2.0.3/"$CHANNEL"/"$RELEASE" pkg/ --exclude "*" --include "*.deb" --include "*.rpm"
 ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" mkdir node_pkg
 # Upload the packages and their signatures.
 scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r pkg/* ubuntu@"$INSTANCE":~/node_pkg/
--- scripts/release/test/stage/test/run.sh
+++ scripts/release/test/stage/test/run.sh
@@ -2,10 +2,24 @@
 
 # Path(s) are relative to the root of the Jenkins workspace.
 
+# This is temporary (taken from `compute_branch_channel.sh`).
+if [ "$1" = "master" ]; then
+    CHANNEL=master
+elif [ "$1" = "rel/nightly" ]; then
+    CHANNEL=nightly
+elif [ "$1" = "rel/stable" ]; then
+    CHANNEL=stable
+elif [ "$1" = "rel/beta" ]; then
+    CHANNEL=beta
+else
+    CHANNEL=dev
+fi
+
 INSTANCE=$(cat scripts/release/common/ec2/tmp/instance)
 
 rm -rf ./*.deb ./*.rpm
-python3 scripts/get_current_installers.py "$1/$2"
+#python3 scripts/get_current_installers.py "$1/$CHANNEL"
+python3 scripts/get_current_installers.py "algorand-builds/$CHANNEL"
 
 # Copy previous installers into ~.
 scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no ./*.deb ubuntu@"$INSTANCE":
