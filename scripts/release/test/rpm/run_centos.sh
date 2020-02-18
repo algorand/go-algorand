ef78ffa1ea296fa5010ea228924d15023de3e582
btoll <benjam72@yahoo.com>
2020-02-18 07:11:45 -0500

    More release build fixes (#836)
    * Added "prod" pipeline to `release/`
    
    * Added snapshot.sh to be invoked manually
    
    * Restructuring
    
    * test stage
    
    * add common build params
    
    * fix build
    
    * test
    
    * Remove build parameters
    
    * wip
    
    * remove test dir
    
    * still trying to fix random build errors
    
    * updating test phase
    
    * extract build_env values
    
    * add trigger for test phase
    
    * derp
    
    * remove test location

--- scripts/release/test/rpm/run_centos.sh
+++ scripts/release/test/rpm/run_centos.sh
@@ -23,7 +23,7 @@ EOF
 cd "${HOME}"/dummyrepo && python3 "${HOME}"/go/src/github.com/algorand/go-algorand/scripts/httpd.py --pid "${HOME}"/phttpd.pid &
 trap "${HOME}"/go/src/github.com/algorand/go-algorand/scripts/kill_httpd.sh 0
 
-sg docker "docker run --rm --env-file ${HOME}/build_env --mount type=bind,src=/run/user/1000/gnupg/S.gpg-agent,dst=/root/S.gpg-agent --mount type=bind,src=${HOME}/dummyrepo,dst=/root/dummyrepo --mount type=bind,src=${HOME}/keys,dst=/root/keys --mount type=bind,src=${HOME},dst=/root/subhome algocentosbuild /root/subhome/go/src/github.com/algorand/go-algorand/scripts/release/test/rpm/test_algorand.sh"
+sg docker "docker run --rm --env-file ${HOME}/build_env_docker --mount type=bind,src=/run/user/1000/gnupg/S.gpg-agent,dst=/root/S.gpg-agent --mount type=bind,src=${HOME}/dummyrepo,dst=/root/dummyrepo --mount type=bind,src=${HOME}/keys,dst=/root/keys --mount type=bind,src=${HOME},dst=/root/subhome algocentosbuild /root/subhome/go/src/github.com/algorand/go-algorand/scripts/release/test/rpm/test_algorand.sh"
 
 echo
 date "+build_release end TEST stage %Y%m%d_%H%M%S"
