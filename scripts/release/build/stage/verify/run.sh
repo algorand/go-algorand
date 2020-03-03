654269233f1686a1f5f0685fba95a5d8e3fb9277
btoll <benjam72@yahoo.com>
2020-03-03 09:51:46 -0500

    Adding a verifying signatures step to the build release pipeline (#878)

---
+++ scripts/release/build/stage/verify/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+trap 'bash ./scripts/release/common/ec2/shutdown.sh' ERR
+
+# Path(s) are relative to the root of the Jenkins workspace.
+ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$(cat scripts/release/common/ec2/tmp/instance)" bash go/src/github.com/algorand/go-algorand/scripts/release/build/stage/verify/task.sh
+
