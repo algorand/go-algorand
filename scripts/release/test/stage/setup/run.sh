3e0123f581197e07aaa6c2f2d57478e8149eea50
btoll <benjam72@yahoo.com>
2020-03-06 09:14:55 -0500

    Strip remote repo name from branch variable name in build release pipeline (#897)

--- scripts/release/test/stage/setup/run.sh
+++ scripts/release/test/stage/setup/run.sh
@@ -6,8 +6,8 @@ set -ex
 trap 'bash ./scripts/release/common/ec2/shutdown.sh' ERR
 
 # Path(s) are relative to the root of the Jenkins workspace.
+BRANCH=$(./scripts/release/util/check_remote.sh "$1")
 INSTANCE=$(cat scripts/release/common/ec2/tmp/instance)
-BRANCH="$1"
 
 aws s3 cp s3://algorand-devops-misc/tools/gnupg2.2.9_centos7_amd64.tar.bz2 .
 scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r gnupg2.2.9_centos7_amd64.tar.bz2 ubuntu@"$INSTANCE":
