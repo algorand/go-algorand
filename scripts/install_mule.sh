609653917da29abe14f8da0c9982d0921d2b516d
Brice Rising <brice@algorand.com>
2020-03-09 12:10:08 -0400

    Add ability to install from https or ssh git url
    I also added getopts options for this scenario

--- scripts/install_mule.sh
+++ scripts/install_mule.sh
@@ -2,6 +2,28 @@
 
 set -e
 
+MULE_VERSION='0.0.0'
+
+while getopts ":v:u" opt; do
+  case ${opt} in
+    v ) MULE_VERSION=$OPTARG
+      ;;
+    u ) USER_AUTH="True"
+      ;;
+    \? ) echo """
+Usage: cmd [-v] [-u]
+
+Options:
+    -v        Version of mule cli
+    -u        Authenticate to github (algorand/go-algorand-ci repository) with user credentials (Default is over ssh)
+"""
+        exit 1
+      ;;
+  esac
+done
+
+
+
 if mule -v &> /dev/null; then
     echo "Mule version $(mule -v) is already installed"
     exit 0
@@ -24,6 +46,10 @@ fi
 TIME=$(date +%s)
 INSTALL_DIR="/tmp/mule-install-${TIME}"
 mkdir -p ${INSTALL_DIR}
-git clone git@github.com:algorand/go-algorand-ci.git ${INSTALL_DIR}/go-algorand-ci
-pip3 install ${INSTALL_DIR}/go-algorand-ci/cli/dist/mule-0.0.0-py3-none-any.whl
+if [[ ${USER_AUTH} == "True" ]]; then
+    git clone https://github.com/algorand/go-algorand-ci.git ${INSTALL_DIR}/go-algorand-ci
+else
+    git clone git@github.com:algorand/go-algorand-ci.git ${INSTALL_DIR}/go-algorand-ci
+fi
+pip3 install ${INSTALL_DIR}/go-algorand-ci/cli/dist/mule-${MULE_VERSION}-py3-none-any.whl --upgrade
 rm -rf ${INSTALL_DIR}
