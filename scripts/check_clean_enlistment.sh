e3f56aa33f97cbcc785753c40eeb1849635ebcbd
egieseke <eric_gieseke@yahoo.com>
2020-04-02 15:57:54 -0400

    Updated check_clean_enlistment.sh to use parens for integer comparison.

--- scripts/check_clean_enlistment.sh
+++ scripts/check_clean_enlistment.sh
@@ -13,7 +13,7 @@
 # Examples: scripts/check_clean_enlistment.sh
 
 # Check for clean enlistment ignoring the update to buildnumber.dat which is expected during a release build
-if [ $(git status --porcelain | grep -v "buildnumber.dat" | wc -l) == 0 ]; then
+if (( $(git status --porcelain | grep -v "buildnumber.dat" | wc -l) == 0 )); then
    exit 0
 else
    exit 1
