293b0c014ac17d3e49058d3ad7dd5e1d513e9ee6
Max Justicz <max@justi.cz>
2020-04-02 12:16:32 -0400

    add more automated app benchmarks

--- ledger/ledger_perf_test.go
+++ ledger/ledger_perf_test.go
@@ -39,12 +39,18 @@ import (
 	"github.com/algorand/go-algorand/protocol"
 )
 
-const heavyWriteCount = 768
+type testParams struct {
+	txType     string
+	name       string
+	program    string
+	schemaSize uint64
+}
 
-var testprogheavy string
-var testproglight string
+var testCases map[string]testParams
 
-func makeUnsignedApplicationCallTxPerf(appIdx uint64, load string, onCompletion transactions.OnCompletion, round int) (tx transactions.Transaction, err error) {
+func makeUnsignedApplicationCallTxPerf(appIdx uint64, params testParams, onCompletion transactions.OnCompletion, round int) transactions.Transaction {
+	var tx transactions.Transaction
+
 	tx.Type = protocol.ApplicationCallTx
 	tx.ApplicationID = basics.AppIndex(appIdx)
 	tx.OnCompletion = onCompletion
@@ -54,32 +60,20 @@ func makeUnsignedApplicationCallTxPerf(appIdx uint64, load string, onCompletion 
 
 	// If creating, set programs
 	if appIdx == 0 {
-		testprog := testproglight
-		var schemaamt uint64
-		if load == "appheavy" {
-			testprog = testprogheavy
-			schemaamt = heavyWriteCount
-		}
-		tx.ApprovalProgram = testprog
-		tx.ClearStateProgram = testprog
+		tx.ApprovalProgram = params.program
+		tx.ClearStateProgram = params.program
 		tx.GlobalStateSchema = basics.StateSchema{
-			NumByteSlice: schemaamt,
+			NumByteSlice: params.schemaSize,
 		}
 		tx.LocalStateSchema = basics.StateSchema{
-			NumByteSlice: schemaamt,
+			NumByteSlice: params.schemaSize,
 		}
 	}
 
-	return tx, nil
+	return tx
 }
 
-type alwaysVerifiedCache struct{}
-
-func (vc *alwaysVerifiedCache) Verified(txn transactions.SignedTxn, params verify.Params) bool {
-	return true
-}
-
-func makeUnsignedPayment(sender basics.Address, round int) transactions.Transaction {
+func makeUnsignedPaymentTx(sender basics.Address, round int) transactions.Transaction {
 	return transactions.Transaction{
 		Type: protocol.PaymentTx,
 		Header: transactions.Header{
@@ -94,7 +88,13 @@ func makeUnsignedPayment(sender basics.Address, round int) transactions.Transact
 	}
 }
 
-func benchmarkFullAppBlocks(txtype string, b *testing.B) {
+type alwaysVerifiedCache struct{}
+
+func (vc *alwaysVerifiedCache) Verified(txn transactions.SignedTxn, params verify.Params) bool {
+	return true
+}
+
+func benchmarkFullBlocks(params testParams, b *testing.B) {
 	dbTempDir, err := ioutil.TempDir("", "testdir"+b.Name())
 	require.NoError(b, err)
 	dbName := fmt.Sprintf("%s.%d", b.Name(), crypto.RandUint64())
@@ -132,6 +132,7 @@ func benchmarkFullAppBlocks(txtype string, b *testing.B) {
 	cert := agreement.Certificate{}
 	var blocks []bookkeeping.Block
 	var createdAppIdx uint64
+	var txPerBlock int
 	onCompletion := transactions.OptInOC
 	for i := 0; i < numBlocks+2; i++ {
 		blk.BlockHeader.Round++
@@ -155,13 +156,20 @@ func benchmarkFullAppBlocks(txtype string, b *testing.B) {
 		require.NoError(b, err)
 
 		// build a payset
-		var j uint64
+		var j int
 		for {
 			j++
-			// make a transaction that will create an application or call it
+			// make a transaction of the appropriate type
 			var tx transactions.Transaction
-			tx, err = makeUnsignedApplicationCallTxPerf(createdAppIdx, txtype, onCompletion, i)
-			require.NoError(b, err)
+			switch params.txType {
+			case "pay":
+				tx = makeUnsignedPaymentTx(creator, i)
+			case "app":
+				tx = makeUnsignedApplicationCallTxPerf(createdAppIdx, params, onCompletion, i)
+			default:
+				panic("unknown tx type")
+			}
+
 			tx.Sender = creator
 			tx.Note = []byte(fmt.Sprintf("%d,%d", i, j))
 			tx.GenesisHash = crypto.Digest{1}
@@ -173,13 +181,13 @@ func benchmarkFullAppBlocks(txtype string, b *testing.B) {
 
 			// check if block is full
 			if err == ErrNoSpace {
-				b.Logf("made full block with %d txns", j)
+				txPerBlock = len(eval.block.Payset)
 				break
 			} else {
 				require.NoError(b, err)
 			}
 
-			// First block just creates app, don't fill it up
+			// First block just creates app
 			if i == 1 {
 				onCompletion = transactions.NoOpOC
 				createdAppIdx = eval.state.txnCounter()
@@ -199,15 +207,17 @@ func benchmarkFullAppBlocks(txtype string, b *testing.B) {
 			continue
 		}
 
+		// For all other blocks, add just to the first ledger, and stash
+		// away to be replayed in the second ledger while running timer
 		err = l0.AddBlock(lvb.blk, cert)
 		require.NoError(b, err)
 
 		blocks = append(blocks, lvb.blk)
 	}
 
-	b.Logf("built %d blocks", numBlocks)
+	b.Logf("built %d blocks, each with %d txns", numBlocks, txPerBlock)
 
-	// eval + add all the (valid) blocks to the second ledger, timing it
+	// eval + add all the (valid) blocks to the second ledger, measuring it this time
 	vc := alwaysVerifiedCache{}
 	b.ResetTimer()
 	for _, blk := range blocks {
@@ -218,240 +228,256 @@ func benchmarkFullAppBlocks(txtype string, b *testing.B) {
 	}
 }
 
-func benchmarkBlockEvalPerf(txtype string, txPerBlock int, b *testing.B) {
-	dbTempDir, err := ioutil.TempDir("", "testdir"+b.Name())
-	require.NoError(b, err)
-	dbName := fmt.Sprintf("%s.%d", b.Name(), crypto.RandUint64())
-	dbPrefix := filepath.Join(dbTempDir, dbName)
-	defer os.RemoveAll(dbTempDir)
-
-	genesisInitState := getInitState()
-
-	// Use future protocol
-	genesisInitState.Block.BlockHeader.GenesisHash = crypto.Digest{}
-	genesisInitState.Block.CurrentProtocol = protocol.ConsensusFuture
-	genesisInitState.GenesisHash = crypto.Digest{1}
-	genesisInitState.Block.BlockHeader.GenesisHash = crypto.Digest{1}
-
-	// We will delete apps, generating 2x as many transactions
-	if txtype == "appheavy" || txtype == "applight" {
-		txPerBlock = txPerBlock / 2
-	}
-
-	// give creators money for min balance
-	var creators []basics.Address
-	numCreators := txPerBlock
-	for i := 0; i < numCreators; i++ {
-		creator := basics.Address{}
-		_, err = rand.Read(creator[:])
-		require.NoError(b, err)
-		creators = append(creators, creator)
-		genesisInitState.Accounts[creator] = basics.MakeAccountData(basics.Offline, basics.MicroAlgos{Raw: 1234567890})
-	}
-
-	// open first ledger
-	const inMem = false // use persistent storage
-	const archival = true
-	l0, err := OpenLedger(logging.Base(), dbPrefix, inMem, genesisInitState, archival)
-	require.NoError(b, err)
-
-	// open second ledger
-	dbName = fmt.Sprintf("%s.%d.2", b.Name(), crypto.RandUint64())
-	dbPrefix = filepath.Join(dbTempDir, dbName)
-	l1, err := OpenLedger(logging.Base(), dbPrefix, inMem, genesisInitState, archival)
-	require.NoError(b, err)
-
-	blk := genesisInitState.Block
-
-	// init both ledgers, and build all the blocks in the first ledger
-	numBlocks := b.N
-	cert := agreement.Certificate{}
-	var blocks []bookkeeping.Block
-	for i := 0; i < numBlocks; i++ {
-		blk.BlockHeader.Round++
-		blk.BlockHeader.TimeStamp += int64(crypto.RandUint64() % 100 * 1000)
-		blk.BlockHeader.GenesisID = "x"
-
-		if i == 0 {
-			err = l0.AddBlock(blk, cert)
-			require.NoError(b, err)
-
-			err = l1.AddBlock(blk, cert)
-			require.NoError(b, err)
-			continue
-		}
-
-		prev, err := l0.BlockHdr(basics.Round(i))
-		require.NoError(b, err)
-
-		newBlk := bookkeeping.MakeBlock(prev)
-		eval, err := l0.StartEvaluator(newBlk.BlockHeader)
-		require.NoError(b, err)
-
-		// build a payset
-		for j := 0; j < txPerBlock; j++ {
-			// make a transaction that will create an asset or application
-			var tx transactions.Transaction
-
-			if txtype == "appheavy" || txtype == "applight" {
-				tx, err = makeUnsignedApplicationCallTxPerf(0, txtype, transactions.OptInOC, i)
-			} else if txtype == "asset" {
-				creatorEncoded := creators[j].String()
-				tx, err = makeUnsignedAssetCreateTx(basics.Round(i), basics.Round(i)+1000, 100, false, creatorEncoded, creatorEncoded, creatorEncoded, creatorEncoded, "m", "m", "", nil)
-			} else if txtype == "pay" {
-				tx = makeUnsignedPayment(creators[j], i)
-			} else {
-				b.Error("unknown tx type")
-			}
-			require.NoError(b, err)
-			tx.Sender = creators[j]
-			tx.Note = []byte(fmt.Sprintf("%d,%d", i, j))
-			tx.GenesisHash = crypto.Digest{1}
-
-			var stxn transactions.SignedTxn
-			stxn.Txn = tx
-
-			err = eval.Transaction(stxn, transactions.ApplyData{})
-			require.NoError(b, err)
-
-			// Delete the newly created app
-			if txtype == "appheavy" || txtype == "applight" {
-				createdAppID := eval.state.txnCounter()
-				tx, err = makeUnsignedApplicationCallTxPerf(createdAppID, txtype, transactions.DeleteApplicationOC, i)
-				require.NoError(b, err)
-				tx.Sender = creators[j]
-				tx.Note = []byte(fmt.Sprintf("%d,%d", i, j))
-				tx.GenesisHash = crypto.Digest{1}
-
-				var stxn transactions.SignedTxn
-				stxn.Txn = tx
-
-				err = eval.Transaction(stxn, transactions.ApplyData{})
-				require.NoError(b, err)
-			}
-		}
-
-		lvb, err := eval.GenerateBlock()
-		require.NoError(b, err)
-
-		err = l0.AddBlock(lvb.blk, cert)
-		require.NoError(b, err)
-
-		blocks = append(blocks, lvb.blk)
-	}
-
-	b.Logf("built %d blocks, %d transactions", numBlocks, txPerBlock)
-
-	// eval + add all the (valid) blocks to the second ledger, timing it
-	vc := alwaysVerifiedCache{}
-	b.ResetTimer()
-	for _, blk := range blocks {
-		_, err = l1.eval(context.Background(), blk, true, &vc, nil)
-		require.NoError(b, err)
-		err = l1.AddBlock(blk, cert)
-		require.NoError(b, err)
-	}
+func BenchmarkAppLocal1NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-1-no-diffs"], b)
+}
+func BenchmarkAppLocal250NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-250-no-diffs"], b)
+}
+func BenchmarkAppLocal750NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-750-no-diffs"], b)
 }
 
-func BenchmarkPaymentEvalPerf100(b *testing.B)  { benchmarkBlockEvalPerf("pay", 100, b) }
-func BenchmarkPaymentEvalPerf500(b *testing.B)  { benchmarkBlockEvalPerf("pay", 500, b) }
-func BenchmarkPaymentEvalPerf1000(b *testing.B) { benchmarkBlockEvalPerf("pay", 1000, b) }
-func BenchmarkPaymentEvalPerf1500(b *testing.B) { benchmarkBlockEvalPerf("pay", 1500, b) }
-func BenchmarkPaymentEvalPerf5000(b *testing.B) { benchmarkBlockEvalPerf("pay", 5000, b) }
+func BenchmarkAppGlobal1NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-1-no-diffs"], b)
+}
+func BenchmarkAppGlobal250NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-250-no-diffs"], b)
+}
+func BenchmarkAppGlobal750NoDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-750-no-diffs"], b)
+}
 
-func BenchmarkAssetEvalPerf100(b *testing.B)  { benchmarkBlockEvalPerf("asset", 100, b) }
-func BenchmarkAssetEvalPerf500(b *testing.B)  { benchmarkBlockEvalPerf("asset", 500, b) }
-func BenchmarkAssetEvalPerf1000(b *testing.B) { benchmarkBlockEvalPerf("asset", 1000, b) }
-func BenchmarkAssetEvalPerf1500(b *testing.B) { benchmarkBlockEvalPerf("asset", 1500, b) }
-func BenchmarkAssetEvalPerf2000(b *testing.B) { benchmarkBlockEvalPerf("asset", 2000, b) }
+func BenchmarkAppLocal1BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-1-big-diffs"], b)
+}
+func BenchmarkAppLocal250BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-250-big-diffs"], b)
+}
+func BenchmarkAppLocal750BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-local-750-big-diffs"], b)
+}
 
-func BenchmarkAppLightEvalPerf100(b *testing.B)  { benchmarkBlockEvalPerf("applight", 100, b) }
-func BenchmarkAppLightEvalPerf500(b *testing.B)  { benchmarkBlockEvalPerf("applight", 500, b) }
-func BenchmarkAppLightEvalPerf1000(b *testing.B) { benchmarkBlockEvalPerf("applight", 1000, b) }
-func BenchmarkAppLightEvalPerf1500(b *testing.B) { benchmarkBlockEvalPerf("applight", 1500, b) }
-func BenchmarkAppLightEvalPerf2000(b *testing.B) { benchmarkBlockEvalPerf("applight", 2000, b) }
+func BenchmarkAppGlobal1BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-1-big-diffs"], b)
+}
+func BenchmarkAppGlobal250BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-250-big-diffs"], b)
+}
+func BenchmarkAppGlobal750BigDiffs(b *testing.B) {
+	benchmarkFullBlocks(testCases["bench-global-750-big-diffs"], b)
+}
 
-func BenchmarkAppHeavyEvalPerf100(b *testing.B)  { benchmarkBlockEvalPerf("appheavy", 100, b) }
-func BenchmarkAppHeavyEvalPerf500(b *testing.B)  { benchmarkBlockEvalPerf("appheavy", 500, b) }
-func BenchmarkAppHeavyEvalPerf1000(b *testing.B) { benchmarkBlockEvalPerf("appheavy", 1000, b) }
-func BenchmarkAppHeavyEvalPerf1500(b *testing.B) { benchmarkBlockEvalPerf("appheavy", 1500, b) }
-func BenchmarkAppHeavyEvalPerf2000(b *testing.B) { benchmarkBlockEvalPerf("appheavy", 2000, b) }
+func BenchmarkAppInt1(b *testing.B) { benchmarkFullBlocks(testCases["int-1"], b) }
 
-func BenchmarkAppFullHeavy(b *testing.B) { benchmarkFullAppBlocks("appheavy", b) }
-func BenchmarkAppFullLight(b *testing.B) { benchmarkFullAppBlocks("applight", b) }
+func BenchmarkPay(b *testing.B) { benchmarkFullBlocks(testCases["pay"], b) }
 
 func init() {
-	heavyPrefix := `
-		int 1
-		itob
-		app_global_get
-		bnz delete
-	`
+	testCases = make(map[string]testParams)
 
-	heavyWritePrefix := `
-		write:
-		int 0
-	`
+	// No diffs
+	params := genAppTestParams(1, false, "local")
+	testCases[params.name] = params
 
-	heavyBlockWrite := `
-		int 1
-		+
-		dup
-		itob
-		dup
-		app_global_put
-	`
+	params = genAppTestParams(250, false, "local")
+	testCases[params.name] = params
 
-	heavyWriteSuffix := `
-		pop
-		int 1
-		return
-	`
+	params = genAppTestParams(750, false, "local")
+	testCases[params.name] = params
 
-	heavyDeletePrefix := `
-		delete:
-		int 0
-	`
+	params = genAppTestParams(1, false, "global")
+	testCases[params.name] = params
 
-	heavyBlockDelete := `
-		int 1
-		+
-		dup
-		itob
-		app_global_del
-	`
+	params = genAppTestParams(250, false, "global")
+	testCases[params.name] = params
 
-	heavyDeleteSuffix := `
-		pop
-		int 1
-		return
-	`
+	params = genAppTestParams(750, false, "global")
+	testCases[params.name] = params
 
-	var heavyProgParts []string
-	heavyProgParts = append(heavyProgParts, heavyPrefix)
-	heavyProgParts = append(heavyProgParts, heavyWritePrefix)
-	for i := 0; i < heavyWriteCount; i++ {
-		heavyProgParts = append(heavyProgParts, heavyBlockWrite)
-	}
-	heavyProgParts = append(heavyProgParts, heavyWriteSuffix)
-	heavyProgParts = append(heavyProgParts, heavyDeletePrefix)
-	for i := 0; i < heavyWriteCount; i++ {
-		heavyProgParts = append(heavyProgParts, heavyBlockDelete)
-	}
-	heavyProgParts = append(heavyProgParts, heavyDeleteSuffix)
+	// Big diffs
+	params = genAppTestParams(1, true, "local")
+	testCases[params.name] = params
 
-	testasm := strings.Join(heavyProgParts, "\n")
+	params = genAppTestParams(250, true, "local")
+	testCases[params.name] = params
 
-	heavyBytes, err := logic.AssembleString(testasm)
+	params = genAppTestParams(750, true, "local")
+	testCases[params.name] = params
+
+	params = genAppTestParams(1, true, "global")
+	testCases[params.name] = params
+
+	params = genAppTestParams(250, true, "global")
+	testCases[params.name] = params
+
+	params = genAppTestParams(750, true, "global")
+	testCases[params.name] = params
+
+	// Int 1
+	progBytes, err := logic.AssembleString(`int 1`)
 	if err != nil {
 		panic(err)
 	}
-	testprogheavy = string(heavyBytes)
 
-	testasm = `int 1`
-	lightBytes, err := logic.AssembleString(testasm)
+	params = testParams{
+		txType:  "app",
+		name:    fmt.Sprintf("int-1"),
+		program: string(progBytes),
+	}
+	testCases[params.name] = params
+
+	// Payments
+	params = testParams{
+		txType: "pay",
+		name:   "pay",
+	}
+	testCases[params.name] = params
+}
+
+func genAppTestParams(numKeys int, bigDiffs bool, stateType string) testParams {
+	var deleteBranch string
+	var writePrefix, writeBlock, writeSuffix string
+	var deletePrefix, deleteBlock, deleteSuffix string
+
+	switch stateType {
+	case "local":
+		// goto delete if first key exists
+		deleteBranch = `
+			int 0
+			int 0
+			int 1
+			itob
+			app_local_get
+			bnz delete
+		`
+
+		writePrefix = `
+			write:
+			int 0
+			store 0
+		`
+
+		writeBlock = `
+			int 0
+			load 0
+			int 1
+			+
+			dup
+			store 0
+			itob
+			dup
+			app_local_put
+		`
+
+		writeSuffix = `
+			int 1
+			return
+		`
+
+		deletePrefix = `
+			delete:
+			int 0
+			store 0
+		`
+
+		deleteBlock = `
+			int 0
+			load 0
+			int 1
+			+
+			dup
+			store 0
+			itob
+			app_local_del
+		`
+
+		deleteSuffix = `
+			int 1
+			return
+		`
+	case "global":
+		// goto delete if first key exists
+		deleteBranch = `
+			int 1
+			itob
+			app_global_get
+			bnz delete
+		`
+
+		writePrefix = `
+			write:
+			int 0
+		`
+
+		writeBlock = `
+			int 1
+			+
+			dup
+			itob
+			dup
+			app_global_put
+		`
+
+		writeSuffix = `
+			int 1
+			return
+		`
+
+		deletePrefix = `
+			delete:
+			int 0
+		`
+
+		deleteBlock = `
+			int 1
+			+
+			dup
+			itob
+			app_global_del
+		`
+
+		deleteSuffix = `
+			int 1
+			return
+		`
+	default:
+		panic("unknown state type")
+	}
+
+	testDiffName := "big-diffs"
+	if !bigDiffs {
+		deleteBranch = ``
+		deletePrefix = ``
+		deleteBlock = ``
+		deleteSuffix = ``
+		testDiffName = "no-diffs"
+	}
+
+	// generate assembly
+	var progParts []string
+	progParts = append(progParts, deleteBranch)
+	progParts = append(progParts, writePrefix)
+	for i := 0; i < numKeys; i++ {
+		progParts = append(progParts, writeBlock)
+	}
+	progParts = append(progParts, writeSuffix)
+	progParts = append(progParts, deletePrefix)
+	for i := 0; i < numKeys; i++ {
+		progParts = append(progParts, deleteBlock)
+	}
+	progParts = append(progParts, deleteSuffix)
+	progAsm := strings.Join(progParts, "\n")
+
+	// assemble
+	progBytes, err := logic.AssembleString(progAsm)
 	if err != nil {
 		panic(err)
 	}
-	testproglight = string(lightBytes)
+
+	return testParams{
+		txType:     "app",
+		name:       fmt.Sprintf("bench-%s-%d-%s", stateType, numKeys, testDiffName),
+		schemaSize: uint64(numKeys),
+		program:    string(progBytes),
+	}
 }
