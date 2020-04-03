cee8a930745cb2ac15affb03e8209fb1aeaba9c5
egieseke <eric_gieseke@yahoo.com>
2020-04-03 10:03:04 -0400

    Eric/fix beta release network (#941)
    * Default to network betanet when installing beta channel.
    
    * Revert changes to update.sh.
    
    * Updated enlistment to consider builds run on Jenkin's as clean so that
    the correct default network is configured upstream in
    compute_branch_network.sh.
    
    * Removed enlistment check from compute_branch_channel.sh and
    compute_branch_network.sh so that the channel and network are computed
    correctly in all cases.
    
    * Updated enlistment check to ignore modifications to buildnumber.dat.
    Removed conditional based on TRAVIS or JENKINS build.
    
    * Removed enlistment check form compute_branch_deadlock_default.sh.
    
    * Updated check_clean_enlistment.sh to use parens for integer comparison.
    
    * Replaced word count with grep -c option.

--- scripts/compute_branch_deadlock_default.sh
+++ scripts/compute_branch_deadlock_default.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# If enlistment isn't clean, it's 'dev'
-./scripts/check_clean_enlistment.sh
-if [[ $? -ne 0 ]]; then
-    # dev / modified enlistment -- not production.  Enable deadlock detection
-    echo "enable"
-    exit 0
-fi
-
 if [[ "$1" =~ ^rel/ ]]; then
     echo "disable"
 else
