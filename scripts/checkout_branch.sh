f9312860141fdc47a06a1bf04e819ffa41289681
Benjamin Toll <benjam72@yahoo.com>
2020-04-08 12:42:53 -0400

    Remove `check_clean_enlistment.sh` script
    This file was only being referenced from one file, so I removed the
    conditional that was in it to where it was being referenced.
    
    - Fixed bug where files like `buildnumber.datdat` are not flagged.
    - Fixed warnings discovered by shellcheck.
    - Improved warning message when the workspace is dirty.

--- scripts/checkout_branch.sh
+++ scripts/checkout_branch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # checkout_branch.sh - Safely switches enlistment to the specified remote branch, checked out locally
-#           Verifies the enlistment is completely clean to ensure nothing will be clobbered or lost
+#           Verifies the workspace is completely clean to ensure nothing will be clobbered or lost
 #
 # Syntax:   checkout_branch.sh <branch>
 #               -f     Compute full version string (e.g. 1.0.1234)
@@ -20,26 +20,28 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+BRANCH="$1"
+
 # Anchor our repo root reference location
-REPO_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"/..
+REPO_ROOT="$( cd "$(dirname "$0")" || exit ; pwd -P )"/..
 
-cd ${REPO_ROOT}
+cd "${REPO_ROOT}" || exit
 
-scripts/check_clean_enlistment.sh
-if [ $? -ne 0 ]; then
-    echo Enlistment has changes. Please clean your enlistment before trying again.
+# Check for clean workspace ignoring the update to buildnumber.dat which is expected during a release build.
+# Use before a forced checkout to ensure we don't clobber or lose any changes.
+if (( $(git status --porcelain | grep -cve "buildnumber.dat$" ) != 0 )); then
+    echo "[WARNING] Your workspace has changes, and this operation will destroy them! Please clean your workspace before trying again."
     exit 1
 fi
 
-git fetch
-if [ $? -ne 0 ]; then
+if ! git fetch; then
     echo Error fetching repo
     exit 1
 fi
 
-git checkout $1
-git reset --hard origin/$1
-if [ $? -ne 0 ]; then
-    echo Error checking out branch "$1"
+git checkout "$BRANCH"
+if ! git reset --hard "origin/$BRANCH"; then
+    echo Error checking out branch "$BRANCH"
     exit 1
 fi
+
