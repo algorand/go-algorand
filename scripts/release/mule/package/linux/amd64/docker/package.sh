8d4c8cf6b7fb6bab7c5fe93c23c1397a9fb3cf40
Benjamin Toll <benjam72@yahoo.com>
2020-04-21 22:55:35 -0400

    Create `mule` task for creating the docker image tarball

--- scripts/release/mule/package/linux/amd64/docker/package.sh
+++ scripts/release/mule/package/linux/amd64/docker/package.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck disable=2064
 
 set -ex
 
@@ -16,26 +15,14 @@ if [ -z "$OS_TYPE" ] || [ -z "$ARCH" ] || [ -z "$WORKDIR" ]; then
     exit 1
 fi
 
-export REPO_DIR="$WORKDIR"
-BRANCH=$("$REPO_DIR/scripts/compute_branch.sh")
+BRANCH=$(./scripts/compute_branch.sh)
 export BRANCH
-CHANNEL=$("$REPO_DIR/scripts/compute_branch_channel.sh" "$BRANCH")
+CHANNEL=$(./scripts/compute_branch_channel.sh "$BRANCH")
 export CHANNEL
 
-mkdir -p "$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH/pkg"
-TARBALLS="$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH"
-export OUTDIR="$TARBALLS/pkg"
-BRANCH=$("$REPO_DIR/scripts/compute_branch.sh")
-export BRANCH
-CHANNEL=$("$REPO_DIR/scripts/compute_branch_channel.sh" "$BRANCH")
-export CHANNEL
-FULLVERSION=$("$REPO_DIR/scripts/compute_build_number.sh" -f)
+TARBALLS="./tmp/node_pkgs/$OS_TYPE/$ARCH"
+FULLVERSION=$(./scripts/compute_build_number.sh -f)
 export FULLVERSION
-
-PKG_ROOT=$(mktemp -d)
-trap "rm -rf $PKG_ROOT" 0
-
-mkdir -p "$PKG_ROOT/usr/bin"
 
 ALGOD_INSTALL_TAR_FILE="$TARBALLS/node_${CHANNEL}_${OS_TYPE}-${ARCH}_${FULLVERSION}.tar.gz"
 
@@ -52,42 +39,35 @@ PKG_DIR="algod_pkg_$CHANNEL_VERSION"
 DOCKER_EXPORT_FILE="algod_docker_export_$CHANNEL_VERSION.tar.gz"
 DOCKER_PKG_FILE="algod_docker_package_$CHANNEL_VERSION.tar.gz"
 DOCKER_IMAGE="algorand/algod_$CHANNEL_VERSION:latest"
-DOCKERFILE="$REPO_DIR/docker/build/algod.Dockerfile"
-START_ALGOD_FILE="$REPO_DIR/docker/release/start_algod_docker.sh"
+DOCKERFILE=./docker/build/algod.Dockerfile
+START_ALGOD_FILE=./docker/release/start_algod_docker.sh
+ALGOD_DOCKER_INIT=./docker/release/algod_docker_init.sh
 
 echo "building '$DOCKERFILE' with install file $ALGOD_INSTALL_TAR_FILE"
-cp "$ALGOD_INSTALL_TAR_FILE" "./$INPUT_ALGOD_TAR_FILE"
-docker build --build-arg ALGOD_INSTALL_TAR_FILE="$INPUT_ALGOD_TAR_FILE" . -t "$DOCKER_IMAGE" -f "$DOCKERFILE"
+cp "$ALGOD_INSTALL_TAR_FILE" "/tmp/$INPUT_ALGOD_TAR_FILE"
+cp "$ALGOD_DOCKER_INIT" /tmp
+docker build --build-arg ALGOD_INSTALL_TAR_FILE="$INPUT_ALGOD_TAR_FILE" /tmp -t "$DOCKER_IMAGE" -f "$DOCKERFILE"
 
-#echo "pushing '${DOCKER_IMAGE}'"
-#docker push ${DOCKER_IMAGE}
-
-mkdir -p "$PKG_DIR"
+mkdir -p "/tmp/$PKG_DIR"
 
 echo "exporting image '$DOCKER_IMAGE' to file '$DOCKER_EXPORT_FILE'"
-docker save --output "$PKG_DIR/$DOCKER_EXPORT_FILE" "$DOCKER_IMAGE"
+docker save --output "/tmp/$PKG_DIR/$DOCKER_EXPORT_FILE" "$DOCKER_IMAGE"
 
-dockerExportStatus=$?
-if [ "$dockerExportStatus" -ne 0 ]; then
-    echo "Error exporting docker image: $dockerExportStatus"
-    exit "$dockerExportStatus"
+DOCKER_EXPORT_STATUS=$?
+if [ "$DOCKER_EXPORT_STATUS" -ne 0 ]; then
+    echo "Error exporting docker image: $DOCKER_EXPORT_STATUS"
+    exit "$DOCKER_EXPORT_STATUS"
 fi
 
 echo "creating docker package tar file $DOCKER_PKG_FILE"
-cp "$START_ALGOD_FILE" "$PKG_DIR/"
-cp "$REPO_DIR/docker/release/deploy_README.md" "$PKG_DIR/README.md"
-sed -i.bak "s/%CHANNEL_VERSION%/$CHANNEL_VERSION/g" "$PKG_DIR/$START_ALGOD_FILE" && rm "$PKG_DIR/$START_ALGOD_FILE.bak"
+cp "$START_ALGOD_FILE" "/tmp/$PKG_DIR/"
+cp ./docker/release/deploy_README.md "/tmp/$PKG_DIR/README.md"
+sed -i "s/%CHANNEL_VERSION%/$CHANNEL_VERSION/g" "/tmp/$PKG_DIR/start_algod_docker.sh"
 
-tar cvf "$DOCKER_PKG_FILE" "$PKG_DIR"
+tar cvf "/tmp/$DOCKER_PKG_FILE" "/tmp/$PKG_DIR"
 
-echo "moving resulting docker package to ${OUTDIR}${DOCKER_PKG_FILE}"
-mkdir -p "$OUTDIR"
-cp "$DOCKER_PKG_FILE" "$OUTDIR"
-
-echo "cleaning up temporary files"
-rm ./"$INPUT_ALGOD_TAR_FILE"
-rm ./"$DOCKER_PKG_FILE"
-rm -rf "$PKG_DIR"
+echo "moving resulting docker package to ${TARBALLS}${DOCKER_PKG_FILE}"
+cp "/tmp/$DOCKER_PKG_FILE" "$TARBALLS"
 
 echo
 date "+build_release end PACKAGE DOCKER stage %Y%m%d_%H%M%S"
