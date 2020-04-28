5d09daf9174916873f367074938ad793e4c9c095
Benjamin Toll <benjam72@yahoo.com>
2020-04-28 19:58:24 -0400

    Change staging bucket to `algorand-staging`

--- scripts/release/build/stage/upload/run.sh
+++ scripts/release/build/stage/upload/run.sh
@@ -17,6 +17,5 @@ rm -rf pkg && mkdir -p pkg/"$FULLVERSION"
 ssh -i ReleaseBuildInstanceKey.pem -A ubuntu@"$INSTANCE" bash go/src/github.com/algorand/go-algorand/scripts/release/build/stage/upload/task.sh
 scp -i ReleaseBuildInstanceKey.pem -o StrictHostKeyChecking=no -r ubuntu@"$INSTANCE":~/node_pkg/* pkg/"$FULLVERSION"/
 
-#aws s3 sync --exclude dev* --exclude master* --exclude nightly* --exclude stable* --acl public-read pkg/"$FULLVERSION" s3://"$BUCKET_LOCATION"/"$CHANNEL"/"$FULLVERSION"/
-aws s3 sync --exclude dev* --exclude master* --exclude nightly* --exclude stable* --acl public-read pkg/"$FULLVERSION" s3://algorand-builds/channel/"$CHANNEL"/"$FULLVERSION"/
+aws s3 sync --exclude dev* --exclude master* --exclude nightly* --exclude stable* --acl public-read pkg/"$FULLVERSION" "s3://algorand-staging/releases/$CHANNEL/$FULLVERSION/"
 
