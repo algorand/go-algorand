cc1bfc98d0f8d7ba37dba966ede04239038a47c1
Will Winder <wwinder.unh@gmail.com>
2020-05-04 10:28:35 -0400

    Update OpenAPI v2 Spec
    Add root endpoints for documentation
    Exclude root endpoints from code generation
    Sort keys during v2 to v3 conversion for a more sensible diff

--- daemon/algod/api/generate.sh
+++ daemon/algod/api/generate.sh
@@ -11,13 +11,12 @@ popd
 
 # Convert v2 to v3
 curl -s -X POST "https://converter.swagger.io/api/convert" -H "accept: application/json" -H "Content-Type: application/json" -d @./algod.oas2.json  -o 3.json
-cat 3.json | json_pp > algod.oas3.yml
-rm 3.json
-# The line below was removed, as it doesn't seem to be required anymore, and currently giving us portability issues.
-#sed -i '' 's/\*.\*/application\/json/g' algod.oas3.yml
+
+# Sort keys, format json and rename 3.json -> algod.oas3.yml
+python3 -c "import json; import sys; json.dump(json.load(sys.stdin), sys.stdout, indent=2, sort_keys=True)" < 3.json > algod.oas3.yml
 
 echo "generating code."
-oapi-codegen -package generated -type-mappings integer=uint64 -generate types -exclude-tags=private -o ./server/v2/generated/types.go algod.oas3.yml
-oapi-codegen -package generated -type-mappings integer=uint64 -generate server,spec -exclude-tags=private -o ./server/v2/generated/routes.go algod.oas3.yml
+oapi-codegen -package generated -type-mappings integer=uint64 -generate types -exclude-tags=private,common -o ./server/v2/generated/types.go algod.oas3.yml
+oapi-codegen -package generated -type-mappings integer=uint64 -generate server,spec -exclude-tags=private,common -o ./server/v2/generated/routes.go algod.oas3.yml
 oapi-codegen -package private -type-mappings integer=uint64 -generate types -include-tags=private -o ./server/v2/generated/private/types.go algod.oas3.yml
 oapi-codegen -package private -type-mappings integer=uint64 -generate server,spec -include-tags=private -o ./server/v2/generated/private/routes.go algod.oas3.yml
