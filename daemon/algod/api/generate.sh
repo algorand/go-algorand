105d041b646222a4d667f6d195f08942f277f752
Will Winder <wwinder.unh@gmail.com>
2020-05-04 14:22:05 -0400

    Cleanup generate.sh

--- daemon/algod/api/generate.sh
+++ daemon/algod/api/generate.sh
@@ -4,9 +4,8 @@ set -e
 rootdir=`dirname $0`
 pushd ${rootdir}
 
-cd ${rootdir}/../../..
-$(GO111MODULE=on go get -u "github.com/algorand/oapi-codegen/...@v1.3.5-algorand4")
-
+pushd ${rootdir}/../../..
+  $(GO111MODULE=on go get -u "github.com/algorand/oapi-codegen/...@v1.3.5-algorand4")
 popd
 
 # Convert v2 to v3
@@ -14,6 +13,7 @@ curl -s -X POST "https://converter.swagger.io/api/convert" -H "accept: applicati
 
 # Sort keys, format json and rename 3.json -> algod.oas3.yml
 python3 -c "import json; import sys; json.dump(json.load(sys.stdin), sys.stdout, indent=2, sort_keys=True)" < 3.json > algod.oas3.yml
+rm 3.json
 
 echo "generating code."
 oapi-codegen -package generated -type-mappings integer=uint64 -generate types -exclude-tags=private,common -o ./server/v2/generated/types.go algod.oas3.yml
