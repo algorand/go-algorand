856c1bc7647008eb5cde491b7b488b1659a1b4af
Benjamin Toll <benjam72@yahoo.com>
2020-05-04 21:38:45 -0400

    More simplification

--- scripts/release/mule/test/util/test_package.sh
+++ scripts/release/mule/test/util/test_package.sh
@@ -56,7 +56,7 @@ run_images () {
     do
         echo "[$0] Running ${item}-test..."
 
-        if ! docker run --rm --name algorand -t "${item}-run-tests" bash ./scripts/release/mule/test/util/run_tests.sh -b "$BRANCH" -c "$CHANNEL" -h "$SHA" -p "$PKG_TYPE" -r "$VERSION"
+        if ! docker run --rm --name algorand -t "${item}-run-tests" bash ./scripts/release/mule/test/tests/run_tests -b "$BRANCH" -c "$CHANNEL" -h "$SHA" -p "$PKG_TYPE" -r "$VERSION"
         then
             FAILED+=("$item")
         fi
