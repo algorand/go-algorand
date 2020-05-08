8a517bea811b401eb05ee372edf008ccfc5ec241
Benjamin Toll <benjam72@yahoo.com>
2020-05-08 17:06:00 -0400

    Move statements into bash functions

--- scripts/release/mule/sign/linux/amd64/sign.sh
+++ scripts/release/mule/sign/linux/amd64/sign.sh
@@ -41,40 +41,58 @@ then
     fi
 fi
 
+make_hashes () {
+    # We need to futz a bit with "source" to make the hashes correct.
+    local HASH_TYPE=${1:-$PKG_TYPE}
+    local PACKAGE_TYPE=${2:-$PKG_TYPE}
+
+    HASHFILE="hashes_${CHANNEL}_${OS_TYPE}_${ARCH_TYPE}_${VERSION}_${HASH_TYPE}"
+    # Remove any previously-generated hashes.
+    rm -f "$HASHFILE"*
+
+    {
+        md5sum ./*"$VERSION"*."$PACKAGE_TYPE" ;
+        shasum -a 256 ./*"$VERSION"*."$PACKAGE_TYPE" ;
+        shasum -a 512 ./*"$VERSION"*."$PACKAGE_TYPE" ;
+    } >> "$HASHFILE"
+
+    gpg -u "$SIGNING_KEY_ADDR" --detach-sign "$HASHFILE"
+    gpg -u "$SIGNING_KEY_ADDR" --clearsign "$HASHFILE"
+}
+
+make_sigs () {
+    local PACKAGE_TYPE=${1:-$PKG_TYPE}
+
+    # Remove any previously-generated signatures.
+    rm -f ./*"$VERSION"*."$PACKAGE_TYPE".sig
+
+    for item in *"$VERSION"*."$1"
+    do
+        gpg -u "$SIGNING_KEY_ADDR" --detach-sign "$item"
+    done
+}
+
+pushd "$PKG_DIR"
+
 GPG_HOME_DIR=$(gpgconf --list-dirs | grep homedir | awk -F: '{ print $2 }')
 chmod 400 "$GPG_HOME_DIR"
 
 if [ "$PKG_TYPE" == "source" ]
 then
     git archive --prefix="algorand-$FULLVERSION/" "$BRANCH" | gzip >| "$PKG_DIR/algorand_${CHANNEL}_source_${VERSION}.tar.gz"
-    PKG_TYPE="tar.gz"
-elif [ "$PKG_TYPE" == "rpm" ]
-then
-    SIGNING_KEY_ADDR=rpm@algorand.com
+    make_sigs tar.gz
+    make_hashes source tar.gz
+else
+    if [ "$PKG_TYPE" == "rpm" ]
+    then
+        SIGNING_KEY_ADDR=rpm@algorand.com
+    fi
+
+    make_sigs "$PKG_TYPE"
+    make_hashes
 fi
 
-cd "$PKG_DIR"
-
-# Remove any previously-generated signatures.
-rm -f ./*"$VERSION"*."$PKG_TYPE".sig
-
-for item in *"$VERSION"*."$PKG_TYPE"
-do
-    gpg -u "$SIGNING_KEY_ADDR" --detach-sign "$item"
-done
-
-HASHFILE="hashes_${CHANNEL}_${OS_TYPE}_${ARCH_TYPE}_${VERSION}_${PKG_TYPE}"
-# Remove any previously-generated hashes.
-rm -f "$HASHFILE"*
-
-{
-    md5sum ./*"$VERSION"*."$PKG_TYPE" ;
-    shasum -a 256 ./*"$VERSION"*."$PKG_TYPE" ;
-    shasum -a 512 ./*"$VERSION"*."$PKG_TYPE" ;
-} >> "$HASHFILE"
-
-gpg -u "$SIGNING_KEY_ADDR" --detach-sign "$HASHFILE"
-gpg -u "$SIGNING_KEY_ADDR" --clearsign "$HASHFILE"
+popd
 
 echo
 date "+build_release end SIGN stage %Y%m%d_%H%M%S"
