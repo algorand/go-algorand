af59c104fed5ac3df0c9e74dacdbfe222eb0aeff
Benjamin Toll <benjam72@yahoo.com>
2020-05-08 14:29:34 -0400

    Added support for USE_CACHE option

--- scripts/release/mule/sign/linux/amd64/sign.sh
+++ scripts/release/mule/sign/linux/amd64/sign.sh
@@ -15,27 +15,38 @@ then
 fi
 
 OS_TYPE="$2"
-ARCH="$3"
-VERSION=${VERSION:-$4}
-PKG_TYPE="$5"
+ARCH_TYPE="$3"
+ARCH_BIT="$4"
+VERSION=${VERSION:-$5}
+PKG_TYPE="$6"
 
 BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 CHANNEL=${CHANNEL:-$("$WORKDIR/scripts/compute_branch_channel.sh" "$BRANCH")}
-PKG_DIR="$WORKDIR/tmp/node_pkgs/$OS_TYPE/$ARCH"
+PKG_DIR="$WORKDIR/tmp/node_pkgs/$OS_TYPE/$ARCH_TYPE"
 SIGNING_KEY_ADDR=dev@algorand.com
 
-# TODO
-#if ! $USE_CACHE
-#then
-#    mule -f package-test.yaml "package-test-setup-$PKG_TYPE"
-#fi
+if ! $USE_CACHE
+then
+    export ARCH_BIT
+    export ARCH_TYPE
+    export CHANNEL
+    export OS_TYPE
+    export VERSION
+
+    if [ "$PKG_TYPE" == "tar.gz" ]
+    then
+        mule -f package-sign.yaml package-sign-setup-tarball
+    else
+        mule -f package-sign.yaml "package-sign-setup-$PKG_TYPE"
+    fi
+fi
 
 GPG_HOME_DIR=$(gpgconf --list-dirs | grep homedir | awk -F: '{ print $2 }')
 chmod 400 "$GPG_HOME_DIR"
 
 if [ "$PKG_TYPE" == "source" ]
 then
-    git archive --prefix="algorand-$FULLVERSION/" "$BRANCH" | gzip > "$PKG_DIR/algorand_${CHANNEL}_source_${VERSION}.tar.gz"
+    git archive --prefix="algorand-$FULLVERSION/" "$BRANCH" | gzip >| "$PKG_DIR/algorand_${CHANNEL}_source_${VERSION}.tar.gz"
     PKG_TYPE="tar.gz"
 elif [ "$PKG_TYPE" == "rpm" ]
 then
@@ -52,7 +63,7 @@ do
     gpg -u "$SIGNING_KEY_ADDR" --detach-sign "$item"
 done
 
-HASHFILE="hashes_${CHANNEL}_${OS_TYPE}_${ARCH}_${VERSION}_${PKG_TYPE}"
+HASHFILE="hashes_${CHANNEL}_${OS_TYPE}_${ARCH_TYPE}_${VERSION}_${PKG_TYPE}"
 rm -f "$HASHFILE"
 touch "$HASHFILE"
 
