e27965ff03e4de71dbfa91e03fe388bf43bafef3
Russell Fustino <russ@algorand.com>
2020-05-11 15:30:13 -0400

    Update server.go

--- cmd/dispenser/server.go
+++ cmd/dispenser/server.go
@@ -75,14 +75,9 @@ const topPageTemplate = `
 	  
 	 <script>
 	  function loadparm(){
-			const queryString = window.location.search;
-			const urlParams = new URLSearchParams(queryString);
-			const parmTarget = urlParams.get('account');
-			var target1 = document.getElementById('target');
-			target1.value = "";
-			target1.value = parmTarget;
-			var $target = $('target');
-			$target.html(target1.value);
+				const queryString = window.location.search;
+				const urlParams = new URLSearchParams(queryString);
+				$('#target').html(urlParams.get('account'));
 	  }  
       function onload() {
             $('#dispense').click(function (e) {
