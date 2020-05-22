52b41438ef61c2c1f5fb70156b0c9006eb91339f
btoll <benjam72@yahoo.com>
2020-05-22 16:40:55 -0400

    Allow env vars to be set for mule package task (#1075)

--- scripts/release/mule/package/linux/amd64/rpm/package.sh
+++ scripts/release/mule/package/linux/amd64/rpm/package.sh
@@ -13,19 +13,19 @@ if [ -z "$OS_TYPE" ] || [ -z "$ARCH" ] || [ -z "$WORKDIR" ]; then
     exit 1
 fi
 
-REPO_DIR="$WORKDIR"
-FULLVERSION=$("$REPO_DIR/scripts/compute_build_number.sh" -f)
-BRANCH=$("$REPO_DIR/scripts/compute_branch.sh")
-CHANNEL=$("$REPO_DIR/scripts/compute_branch_channel.sh" "$BRANCH")
-ALGO_BIN="$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH/$CHANNEL/$OS_TYPE-$ARCH/bin"
+FULLVERSION=${VERSION:-$("$WORKDIR/scripts/compute_build_number.sh" -f)}
+BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+CHANNEL=${CHANNEL:-$("$WORKDIR/scripts/compute_branch_channel.sh" "$BRANCH")}
+ALGO_BIN="$WORKDIR/tmp/node_pkgs/$OS_TYPE/$ARCH/$CHANNEL/$OS_TYPE-$ARCH/bin"
 # TODO: Should there be a default network?
 DEFAULTNETWORK=devnet
-DEFAULT_RELEASE_NETWORK=$("$REPO_DIR/scripts/compute_branch_release_network.sh" "${DEFAULTNETWORK}")
-PKG_NAME=$("$REPO_DIR/scripts/compute_package_name.sh" "${CHANNEL:-stable}")
+DEFAULT_RELEASE_NETWORK=$("$WORKDIR/scripts/compute_branch_release_network.sh" "${DEFAULTNETWORK}")
+PKG_NAME=$("$WORKDIR/scripts/compute_package_name.sh" "${CHANNEL:-stable}")
 
 # The following need to be exported for use in ./go-algorand/installer/rpm/algorand.spec.
 export DEFAULT_NETWORK
 export DEFAULT_RELEASE_NETWORK
+REPO_DIR="$WORKDIR"
 export REPO_DIR
 export ALGO_BIN
 
@@ -34,13 +34,12 @@ trap 'rm -rf $RPMTMP' 0
 
 TEMPDIR=$(mktemp -d)
 trap 'rm -rf $TEMPDIR' 0
-< "$REPO_DIR/installer/rpm/algorand.spec" \
+< "$WORKDIR/installer/rpm/algorand.spec" \
     sed -e "s,@PKG_NAME@,${PKG_NAME}," \
         -e "s,@VER@,$FULLVERSION," \
     > "$TEMPDIR/algorand.spec"
 
 rpmbuild --buildroot "$HOME/foo" --define "_rpmdir $RPMTMP" --define "RELEASE_GENESIS_PROCESS x$RELEASE_GENESIS_PROCESS" --define "LICENSE_FILE ./COPYING" -bb "$TEMPDIR/algorand.spec"
 
-mkdir -p "$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH/pkg"
-cp -p "$RPMTMP"/*/*.rpm "$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH/pkg"
+cp -p "$RPMTMP"/*/*.rpm "$WORKDIR/tmp/node_pkgs/$OS_TYPE/$ARCH"
 
