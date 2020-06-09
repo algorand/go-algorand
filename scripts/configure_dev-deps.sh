d374ae09189247b71b8faceb317a01040722dbb7
John Lee <john.lee@algorand.com>
2020-06-09 14:44:51 -0400

    Remove -u option when specifying a version to go get

--- scripts/configure_dev-deps.sh
+++ scripts/configure_dev-deps.sh
@@ -7,7 +7,7 @@ function install_go_module {
     # Match msgp version to go.mod version
     if [ "$1" = "github.com/algorand/msgp" ]; then
 	VERSION=$(cd $(dirname "$0") && cat ../go.mod | grep msgp | awk -F " " '{print $NF}')
-	OUTPUT=$(GO111MODULE=on go get -u "$1@${VERSION}" 2>&1)
+	OUTPUT=$(GO111MODULE=on go get "$1@${VERSION}" 2>&1)
     else
 	OUTPUT=$(GO111MODULE=off go get -u "$1" 2>&1)
     fi
