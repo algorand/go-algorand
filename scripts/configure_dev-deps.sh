014486252c54b3f1100aa458b7a466b10a8abdaa
John Lee <john.lee@algorand.com>
2020-06-09 14:30:58 -0400

    If this is msgp, grep the version from go.mod

--- scripts/configure_dev-deps.sh
+++ scripts/configure_dev-deps.sh
@@ -4,7 +4,13 @@ set -ex
 
 function install_go_module {
     local OUTPUT
-    OUTPUT=$(GO111MODULE=off go get -u "$1" 2>&1)
+    # Match msgp version to go.mod version
+    if [ "$1" = "github.com/algorand/msgp" ]; then
+	VERSION=$(cd $(dirname "$0") && cat ../go.mod | grep msgp | awk -F " " '{print $NF}')
+	OUTPUT=$(GO111MODULE=on go get -u "$1@${VERSION}" 2>&1)
+    else
+	OUTPUT=$(GO111MODULE=off go get -u "$1" 2>&1)
+    fi
     if [ "${OUTPUT}" != "" ]; then
         echo "error: executing \"go get -u $1\" failed : ${OUTPUT}"
         exit 1
