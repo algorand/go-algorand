b2cac0abe150cf623794a49861bfe6aa53113ea7
Max Justicz <max@justi.cz>
2020-06-23 13:34:56 -0400

    fix function signature parameter name

--- data/transactions/transaction.go
+++ data/transactions/transaction.go
@@ -83,7 +83,7 @@ type Balances interface {
 type StateEvaluator interface {
 	Eval(program []byte) (pass bool, stateDelta basics.EvalDelta, err error)
 	Check(program []byte) (cost int, err error)
-	InitLedger(balances Balances, acctWhitelist []basics.Address, appGlobalWhitelist []basics.AppIndex, appIdx basics.AppIndex, params basics.StateSchemas) error
+	InitLedger(balances Balances, acctWhitelist []basics.Address, appGlobalWhitelist []basics.AppIndex, appIdx basics.AppIndex, schemas basics.StateSchemas) error
 }
 
 // Header captures the fields common to every transaction type.
