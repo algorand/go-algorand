9bb25152c60fc24582f408b57ff02fa9e2c18445
egieseke <eric_gieseke@yahoo.com>
2020-07-02 11:53:14 -0400

    revert unintended formatting changes

--- config/consensus.go
+++ config/consensus.go
@@ -496,7 +496,7 @@ func initConsensusProtocols() {
 
 		MaxBalLookback: 320,
 
-		MaxTxGroupSize: 1,
+		MaxTxGroupSize:               1,
 	}
 
 	v7.ApprovedUpgrades = map[protocol.ConsensusVersion]uint64{}
