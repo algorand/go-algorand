3f3d450fcebf2a3768b692483ed0ac3af8fe8771
egieseke <eric_gieseke@yahoo.com>
2020-07-13 13:21:23 -0400

    Fixed typo.

--- scripts/get_golang_version.sh
+++ scripts/get_golang_version.sh
@@ -5,9 +5,9 @@ GOLANG_VERSION="1.12.17"
 # Check to make sure that it matches what is specified in go.mod
 
 GOMOD_VERSION=$(awk '/^go[ \t]+[0-9]+\.[0-9]+(\.[0-9]+)?[ \t]*$/{print $2}' go.mod)
-TRIMED_GOLANG_VERSION=$(echo ${GOLANG_VERSION} |awk -F. '{ print $1 "." $2 }')
+TRIMMED_GOLANG_VERSION=$(echo ${GOLANG_VERSION} |awk -F. '{ print $1 "." $2 }')
 
-if ! [[ ${GOLANG_VERSION} == "${GOMOD_VERSION}" || ${TRIMED_GOLANG_VERSION} == "${GOMOD_VERSION}" ]]
+if ! [[ ${GOLANG_VERSION} == "${GOMOD_VERSION}" || ${TRIMMED_GOLANG_VERSION} == "${GOMOD_VERSION}" ]]
 then
     echo "go version mismatch, go mod version ${GOMOD_VERSION} does not match required version ${GOLANG_VERSION}"
     exit 1;
