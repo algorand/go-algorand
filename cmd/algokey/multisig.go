9770534c1814346e6cad0c13788dbaf5ccc8c4e5
Rotem Hemo <rotem@algorand.com>
2020-07-22 16:40:12 -0400

    -- Add the ability to convert a regular transaction to a multisig with signer field for rekeying.

--- cmd/algokey/multisig.go
+++ cmd/algokey/multisig.go
@@ -18,9 +18,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/algorand/go-algorand/data/basics"
 	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -34,13 +37,22 @@ var multisigTxfile string
 var multisigOutfile string
 var multisigMnemonic string
 
+var msigParams string
+
 func init() {
+
+	multisigCmd.AddCommand(convertCmd)
+
 	multisigCmd.Flags().StringVarP(&multisigKeyfile, "keyfile", "k", "", "Private key filename")
 	multisigCmd.Flags().StringVarP(&multisigMnemonic, "mnemonic", "m", "", "Private key mnemonic")
 	multisigCmd.Flags().StringVarP(&multisigTxfile, "txfile", "t", "", "Transaction input filename")
 	multisigCmd.MarkFlagRequired("txfile")
 	multisigCmd.Flags().StringVarP(&multisigOutfile, "outfile", "o", "", "Transaction output filename")
 	multisigCmd.MarkFlagRequired("outfile")
+
+	convertCmd.Flags().StringVarP(&msigParams, "params", "p", "", "Multisig params -  Threshold PK1 PK2 ...")
+	convertCmd.Flags().StringVarP(&multisigTxfile, "txfile", "t", "", "Transaction input filename")
+
 }
 
 var multisigCmd = &cobra.Command{
@@ -89,6 +101,81 @@ var multisigCmd = &cobra.Command{
 		err = ioutil.WriteFile(multisigOutfile, outBytes, 0600)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot write signed transactions to %s: %v\n", multisigOutfile, err)
+			os.Exit(1)
+		}
+	},
+}
+
+var convertCmd = &cobra.Command{
+	Use:   "convert -t [transaction file] -p \"[threshold] [PK1] [PK2] ...\"",
+	Short: "Adds the necessary fields to a transaction that is sent from an account to was rekeied to a multisig account.",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, _ []string) {
+
+		// Read Transaction
+		txdata, err := ioutil.ReadFile(multisigTxfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot read transactions from %s: %v\n", multisigTxfile, err)
+			os.Exit(1)
+		}
+
+		var outBytes []byte
+		dec := protocol.NewDecoderBytes(txdata)
+
+		var stxn transactions.SignedTxn
+		err = dec.Decode(&stxn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot decode transaction: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Decode params
+		params := strings.Split(msigParams, " ")
+		if len(params) < 2 {
+			_, _ = fmt.Fprint(os.Stderr, "Not enough arguments to create the multisig address.\nPlease make sure to specify the threshold and addresses")
+			os.Exit(1)
+		}
+
+		threshold, err := strconv.ParseUint(params[0], 10, 8)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse the threshold. Make sure it's a number between 1 and 255: %v\n", err)
+			os.Exit(1)
+		}
+
+		// convert addresses to pks
+		// convert the addresses into public keys
+		pks := make([]crypto.PublicKey, len(params[1:]))
+		for i, addrStr := range params[1:] {
+			addr, err := basics.UnmarshalChecksumAddress(addrStr)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Cannot decode address: %v\n", err)
+				os.Exit(1)
+			}
+			pks[i] = crypto.PublicKey(addr)
+		}
+
+		addr, err := crypto.MultisigAddrGen(1, uint8(threshold), pks)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot generate multisig addr: %v\n", err)
+			os.Exit(1)
+		}
+
+		//gen the multisig and assign to the txn
+		stxn.Msig = crypto.MultisigPreimageFromPKs(1, uint8(threshold), pks)
+
+		//append the signer since it's a rekey txn
+		if basics.Address(addr) == stxn.Txn.Sender {
+			fmt.Fprintf(os.Stderr, "The sender at the msig address should not be the same: %v\n", err)
+			os.Exit(1)
+		}
+		stxn.AuthAddr = basics.Address(addr)
+
+		// Write the txn
+		outBytes = append(outBytes, protocol.Encode(&stxn)...)
+
+		err = ioutil.WriteFile(multisigTxfile, outBytes, 0600)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot write transactions to %s: %v\n", multisigOutfile, err)
 			os.Exit(1)
 		}
 	},
