d9d626c01605a428c6660183499beb529f037546
Tsachi Herman <tsachi.herman@algorand.com>
2020-07-27 14:11:16 -0400

    copy after mount

--- scripts/travis/configure_dev.sh
+++ scripts/travis/configure_dev.sh
@@ -21,8 +21,8 @@ if [[ "${OS}" == "linux" ]]; then
         sudo mv /tmp /old_tmp
         sudo mkdir -p /tmp
         sudo chmod 777 /tmp
-        cp -r /old_tmp/ /tmp
         sudo mount -t tmpfs -o rw,size=512M tmpfs /tmp
+        cp -r /old_tmp/ /tmp
     fi
 elif [[ "${OS}" == "darwin" ]]; then
     # we don't want to upgrade boost if we already have it, as it will try to update
