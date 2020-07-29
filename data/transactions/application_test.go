628f1e9aaf46420d865f41764604693e944aad52
Max Justicz <max@justi.cz>
2020-07-29 13:16:48 -0400

    add WellFormed and Empty checks for ForeignAssets

--- data/transactions/application_test.go
+++ data/transactions/application_test.go
@@ -34,7 +34,7 @@ func TestApplicationCallFieldsNotChanged(t *testing.T) {
 	af := ApplicationCallTxnFields{}
 	s := reflect.ValueOf(&af).Elem()
 
-	if s.NumField() != 10 {
+	if s.NumField() != 11 {
 		t.Errorf("You added or removed a field from ApplicationCallTxnFields. " +
 			"Please ensure you have updated the Empty() method and then " +
 			"fix this test")
