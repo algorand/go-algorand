41b881589bd248962cc56cd4e1a0ef247456be2a
Max Justicz <max@justi.cz>
2020-07-31 15:59:41 -0400

    add apps to goal account list

--- cmd/goal/accountsList.go
+++ cmd/goal/accountsList.go
@@ -231,6 +231,21 @@ func (accountList *AccountsList) outputAccount(addr string, acctInfo v1.Account,
 		}
 		fmt.Printf("]")
 	}
+	if len(acctInfo.AppParams) > 0 {
+		fmt.Printf("\t[created apps:")
+		for aid := range acctInfo.AppParams {
+			fmt.Printf(" %d", aid)
+		}
+		fmt.Printf("]")
+	}
+	if len(acctInfo.AppLocalStates) > 0 {
+		fmt.Printf("\t[opted in apps:")
+		for aid := range acctInfo.AppLocalStates {
+			fmt.Printf(" %d", aid)
+		}
+		fmt.Printf("]")
+	}
+
 	if accountList.isDefault(addr) {
 		fmt.Printf("\t*Default")
 	}
