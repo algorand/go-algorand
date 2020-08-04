98b0c579f0e88bf4fb7ff46c9c8a6d1e19e00203
Benjamin Toll <benjam72@yahoo.com>
2020-08-04 12:49:54 -0400

    Remove expect test in favor of expanding existing rekey.sh tests

--- test/scripts/e2e_subs/rekey.sh
+++ test/scripts/e2e_subs/rekey.sh
@@ -29,5 +29,42 @@ if [ "$BALANCEB" -ne 23000000 ]; then
     false
 fi
 
+# Rekey from A to C back to A [A -> C -> A].
+${gcmd} clerk send -a 10000 -f "${ACCOUNT}" -t "${ACCOUNTB}" --rekey-to "${ACCOUNT}" -s -o "${TEMPDIR}/ntxn2"
+${gcmd} clerk sign -S "${ACCOUNTC}" -i "${TEMPDIR}/ntxn2" -o "${TEMPDIR}/nstxn2"
+${gcmd} clerk rawsend -f "${TEMPDIR}/nstxn2"
+
+BALANCEB=$(${gcmd} account balance -a "${ACCOUNTB}" | awk '{ print $1 }')
+if [ "$BALANCEB" -ne 33000000 ]; then
+    date "+e2e_subs/rekey.sh FAIL wanted balance=23000000 but got ${BALANCEB} %Y%m%d_%H%M%S"
+    false
+fi
+
+# Rekey from A to B to D [A -> B -> D].
+ACCOUNTD=$(${gcmd} account new|awk '{ print $6 }')
+ACCOUNTE=$(${gcmd} account new|awk '{ print $6 }')
+
+${gcmd} clerk send -a 10000000 -f "${ACCOUNT}" -t "${ACCOUNTE}" --rekey-to "${ACCOUNTB}"
+${gcmd} clerk send -a 10000000 -f "${ACCOUNT}" -t "${ACCOUNTE}" -o "${TEMPDIR}/ntxn3"
+${gcmd} clerk sign -S "${ACCOUNTB}" -i "${TEMPDIR}/ntxn3" -o "${TEMPDIR}/nstxn3"
+${gcmd} clerk rawsend -f "${TEMPDIR}/nstxn3"
+
+BALANCEE=$(${gcmd} account balance -a "${ACCOUNTE}" | awk '{ print $1 }')
+if [ "$BALANCEE" -ne 20000000 ]; then
+    date "+e2e_subs/rekey.sh FAIL wanted balance=23000000 but got ${BALANCEE} %Y%m%d_%H%M%S"
+    false
+fi
+echo "$BALANCEE"
+
+${gcmd} clerk send -a 10000000 -f "${ACCOUNT}" -t "${ACCOUNTE}" --rekey-to "${ACCOUNTD}" -s -o "${TEMPDIR}/ntxn4"
+${gcmd} clerk sign -S "${ACCOUNTB}" -i "${TEMPDIR}/ntxn4" -o "${TEMPDIR}/nstxn4"
+${gcmd} clerk rawsend -f "${TEMPDIR}/nstxn4"
+
+BALANCEE=$(${gcmd} account balance -a "${ACCOUNTE}" | awk '{ print $1 }')
+if [ "$BALANCEE" -ne 30000000 ]; then
+    date "+e2e_subs/rekey.sh FAIL wanted balance=30000000 but got ${BALANCEE} %Y%m%d_%H%M%S"
+    false
+fi
+
 date '+e2e_subs/rekey.sh OK %Y%m%d_%H%M%S'
 
