858b97d6c3379907e2edd990aeaf6c680730a1a5
algonautshant <shant@algorand.com>
2020-08-07 15:11:57 -0400

    e2e test: create+delete create+clear in 1 tx
    create + delete in one tx should pass
    create + clear in one tx should fail

---
+++ test/scripts/e2e_subs/e2e-app-other.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+date '+keyreg-teal-test start %Y%m%d_%H%M%S'
+
+set -e
+set -x
+set -o pipefail
+export SHELLOPTS
+
+WALLET=$1
+
+gcmd="goal -w ${WALLET}"
+
+ACCOUNT=$(${gcmd} account list|awk '{ print $3 }')
+
+# Version 2 approval program
+printf '#pragma version 2\nint 1' > "${TEMPDIR}/simple.teal"
+PROGRAM=($(${gcmd} clerk compile "${TEMPDIR}/simple.teal"))
+
+GLOBAL_INTS=2
+
+# Succeed in creating app with on-completion delete
+APPID=$(${gcmd} app create --creator ${ACCOUNT}  --on-completion "DeleteApplication" --approval-prog "${TEMPDIR}/simple.teal" --clear-prog "${TEMPDIR}/simple.teal" --global-byteslices 0 --global-ints ${GLOBAL_INTS} --local-byteslices 0 --local-ints 0 | grep Created | awk '{ print $6 }')
+# Check that parameters were set correctly
+APPID_CHECK=$(${gcmd} app info --app-id $APPID 2>&1 || true)
+EXPERROR="application does not exist"
+echo "((((((" $APPID_CHECK "))))))))"
+if [[ $APPID_CHECK != *"${EXPERROR}"* ]]; then
+    date '+app-create-delete-test FAIL the deleted application should not exist %Y%m%d_%H%M%S'
+    false
+fi
+
+# Fail if creating app with on-completion clear
+RES=$(${gcmd} app create --creator ${ACCOUNT}  --on-completion "ClearState" --approval-prog "${TEMPDIR}/simple.teal" --clear-prog "${TEMPDIR}/simple.teal" --global-byteslices 0 --global-ints ${GLOBAL_INTS} --local-byteslices 0 --local-ints 0 2>&1 || true  ) 
+EXPERROR1='cannot clear state for app'
+EXPERROR2='is not currently opted in'
+if [[ $RES != *"${EXPERROR1}"*"${EXPERROR2}"* ]]; then
+    date '+app-create-clear FAIL should fail to create app with on-completion ClearState %Y%m%d_%H%M%S'
+    false
+fi
+
