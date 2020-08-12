cfc5a2eabc4bffd98de8d56b1a98e3fac9b4f8af
Will Winder <wwinder.unh@gmail.com>
2020-08-12 15:06:05 -0400

    Add tests for /v2/assets endpoint

--- test/scripts/e2e_subs/rest.sh
+++ test/scripts/e2e_subs/rest.sh
@@ -16,12 +16,16 @@ PUB_TOKEN=$(cat "$ALGORAND_DATA"/algod.token)
 ADMIN_TOKEN=$(cat "$ALGORAND_DATA"/algod.admin.token)
 NET=$(cat "$ALGORAND_DATA"/algod.net)
 
+function base_call {
+  curl -q -s -H "Authorization: Bearer $1" "$NET$2"
+}
+
 function call_admin {
-  curl -q -s -H "Authorization: Bearer ${ADMIN_TOKEN}" "$NET$1"
+  base_call "$ADMIN_TOKEN" "$1"
 }
 
 function call {
-  curl -q -s -H "Authorization: Bearer ${PUB_TOKEN}" "$NET$1"
+  base_call "$PUB_TOKEN" "$1"
 }
 
 # $1 - test description.
@@ -58,11 +62,52 @@ function test_applications_endpoint {
     }
 '
   # Some invalid path parameters
-  call_and_verify "Parameter parsing error." /v2/applications/-2 "Invalid format for parameter application-id"
-  call_and_verify "Parameter parsing error." /v2/applications/not-a-number "Invalid format for parameter application-id"
+  call_and_verify "App parameter parsing error 1." "/v2/applications/-2" "Invalid format for parameter application-id"
+  call_and_verify "App parameter parsing error 2." "/v2/applications/not-a-number" "Invalid format for parameter application-id"
 
   # Good request, but invalid query parameters
-  call_and_verify "Invalid parameter" "/v2/applications/$APPID?this-should-fail=200" 'Unknown parameter detected: this-should-fail'
+  call_and_verify "App invalid parameter" "/v2/applications/$APPID?this-should-fail=200" 'Unknown parameter detected: this-should-fail'
 }
 
-test_applications_endpoint
+function test_assets_endpoint {
+  local DIR
+  local ASSET_ID
+
+  # Directory of helper TEAL programs
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/tealprogs"
+  RES=$(${gcmd} asset create --creator "${ACCOUNT}" --total 10000 --decimals 19 --name "spanish coin" --unitname "doubloon")
+  echo $RES
+  ASSET_ID=$(echo $RES | grep "Created asset with asset index" | rev | cut -d ' ' -f 1 | rev)
+  echo "ASSET_ID: $ASSET_ID"
+
+  # Good request, non-existant asset id
+  call_and_verify "Should not find asset." "/v2/assets/987654321" 'asset does not exist'
+  # Good request
+  call_and_verify "Should contain asset data." "/v2/assets/$ASSET_ID" '","decimals":19,"default-frozen":false,"freeze":"'
+  # Good request, pretty response
+  call_and_verify "Should contain asset data." "/v2/assets/$ASSET_ID?pretty" '
+    "decimals": 19,
+    "default-frozen": false,
+    "freeze": "'
+  # Some invalid path parameters
+  call_and_verify "Asset parameter parsing error 1." "/v2/assets/-2" "Invalid format for parameter asset-id"
+  call_and_verify "Asset parameter parsing error 2." "/v2/assets/not-a-number" "Invalid format for parameter asset-id"
+
+  # Good request, but invalid query parameters
+  call_and_verify "Asset invalid parameter" "/v2/assets/$ASSET_ID?this-should-fail=200" 'parameter detected: this-should-fail'
+}
+
+pids=()
+# Run all the tests in parallel
+test_applications_endpoint & pids+=($!)
+test_assets_endpoint & pids+=($!)
+
+# Wait for them to complete and propogate the error code
+EXIT=0
+for pid in ${pids[*]}; do
+  wait $pid
+  if [ $? -ne 0 ]; then
+    EXIT=1
+  fi
+done
+exit $EXIT
