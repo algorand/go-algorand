d247532ba6a55ff06d095884109343c5b84aa2fc
Will Winder <wwinder.unh@gmail.com>
2020-08-17 22:21:12 -0400

    Finish tests, cannot run them in parallel while curl reuses the same temporary file.

--- test/scripts/e2e_subs/rest.sh
+++ test/scripts/e2e_subs/rest.sh
@@ -37,11 +37,11 @@ function call_and_verify {
   CODE=$(call "$2" out.txt)
   RES=$(cat out.txt)
   if [[ "$CODE" != "$3" ]]; then
-    echo "Failed test - $2: unexpected status code $CODE != $3"
+    printf "\n\nFailed test - $2 ($1): unexpected status code $CODE != $3\n\n"
     exit 1
   fi
   if [[ "$RES" != *"$4"* ]]; then
-    echo "Failed test - $2: $1"
+    printf "\n\nFailed test - $2 ($1): unexpected response '$RES' != *'$4'*\n\n"
     exit 1
   fi
 }
@@ -80,7 +80,7 @@ function test_assets_endpoint {
   ASSET_ID=$(${gcmd} asset create --creator "${ACCOUNT}" --total 10000 --decimals 19 --name "spanish coin" --unitname "doubloon" | grep "Created asset with asset index" | rev | cut -d ' ' -f 1 | rev)
 
   # Good request, non-existant asset id
-  call_and_verify "Should not find asset." "/v2/assets/987654321" 405 'asset does not exist'
+  call_and_verify "Should not find asset." "/v2/assets/987654321" 404 'asset does not exist'
   # Good request
   call_and_verify "Should contain asset data." "/v2/assets/$ASSET_ID" 200 '","decimals":19,"default-frozen":false,"freeze":"'
   # Good request, pretty response
@@ -89,25 +89,15 @@ function test_assets_endpoint {
     "default-frozen": false,
     "freeze": "'
   # Some invalid path parameters
-  call_and_verify "Asset parameter parsing error 1." "/v2/assets/-2" 200 "Invalid format for parameter asset-id"
-  call_and_verify "Asset parameter parsing error 2." "/v2/assets/not-a-number" 200 "Invalid format for parameter asset-id"
+  call_and_verify "Asset parameter parsing error 1." "/v2/assets/-2" 400 "Invalid format for parameter asset-id"
+  call_and_verify "Asset parameter parsing error 2." "/v2/assets/not-a-number" 400 "Invalid format for parameter asset-id"
 
   # Good request, but invalid query parameters
-  call_and_verify "Asset invalid parameter" "/v2/assets/$ASSET_ID?this-should-fail=200" 200 'parameter detected: this-should-fail'
+  call_and_verify "Asset invalid parameter" "/v2/assets/$ASSET_ID?this-should-fail=200" 400 'parameter detected: this-should-fail'
 }
 
-pids=()
-# Run all the tests in parallel
-test_applications_endpoint & pids+=($!)
-#test_assets_endpoint & pids+=($!)
+# Run the tests.
+test_applications_endpoint
+test_assets_endpoint
 
-# Wait for them to complete and propogate the error code
-EXIT=0
-for pid in ${pids[*]}; do
-  #wait "$pid"
-  if wait "$pid" -ne 0 ; then
-    EXIT=1
-  fi
-done
-#exit $EXIT
-exit 1
+exit $EXIT
