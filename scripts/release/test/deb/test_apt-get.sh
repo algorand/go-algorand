5fe918844d79c517801a0e678b75810b41bc6d9d
btoll <benjam72@yahoo.com>
2020-08-19 17:37:30 -0400

    Dynamically install deb package in legacy pipeline (#1433)
    The legacy pipeline isn't distinguishing between stable and beta release
    packages. It needs to test algorand for the former and algorand-beta for
    the latter.

--- scripts/release/test/deb/test_apt-get.sh
+++ scripts/release/test/deb/test_apt-get.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-#set -xv
+set -ex
 
 echo "test_apt-get starting within docker container"
 
 export GOPATH=${HOME}/go
 export PATH=${GOPATH}/bin:/usr/local/go/bin:${PATH}
 
+PKG_NAME=algorand
+if [ "${CHANNEL}" = beta ]; then
+    PKG_NAME=algorand-beta
+fi
+
 apt-get update
 apt-get install -y gnupg2 curl software-properties-common python3
 apt-key add /root/keys/dev.pub
 add-apt-repository -y "deb [trusted=yes] http://${DC_IP}:8111/ stable main"
 apt-get update
-apt-get install -y algorand
+apt-get install -y "${PKG_NAME}"
 apt-get install -y expect
 
 algod -v
