7e1a5ccf09ebb821abbc2d2fa2ec4182b0dd9c0e
Nicholas Guo <nicholas.guo@algorand.com>
2020-08-19 11:45:31 -0700

    increased round for periodic-teal-test

--- test/scripts/e2e_subs/periodic-teal-test.sh
+++ test/scripts/e2e_subs/periodic-teal-test.sh
@@ -21,7 +21,7 @@ sed s/TMPL_RCV/${ACCOUNTB}/g < ${GOPATH}/src/github.com/algorand/go-algorand/too
 ACCOUNT_PERIODIC=$(${gcmd} clerk compile ${TEMPDIR}/periodic.teal -o ${TEMPDIR}/periodic.tealc|awk '{ print $2 }')
 
 ROUND=5
-DUR_ROUND=$((${ROUND} + 2))
+DUR_ROUND=$((${ROUND} + 4))
 ${gcmd} clerk send -a 1000000 -t ${ACCOUNTB} --from-program ${TEMPDIR}/periodic.teal --firstvalid ${ROUND} --lastvalid ${DUR_ROUND} -x ${LEASE} -o ${TEMPDIR}/a.tx
 ${gcmd} clerk dryrun -t ${TEMPDIR}/a.tx
 
@@ -37,7 +37,7 @@ while [ $sendcount -lt 3 ]; do
 	false
     fi
     ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
-    DUR_ROUND=$((${ROUND} + 2))
+    DUR_ROUND=$((${ROUND} + 4))
     if ${gcmd} clerk send -a 1000000 -t ${ACCOUNTB} --from-program ${TEMPDIR}/periodic.teal --firstvalid ${ROUND} --lastvalid ${DUR_ROUND} -x ${LEASE}; then
 	sendcount=$(($sendcount + 1))
 	date '+periodic-teal-test sent one at ${ROUND} %Y%m%d_%H%M%S'
@@ -53,7 +53,7 @@ if [ $BALANCEB -ne 3000000 ]; then
 fi
 
 ROUND=25
-DUR_ROUND=$((${ROUND} + 2))
+DUR_ROUND=$((${ROUND} + 4))
 ${gcmd} clerk send -a 0 -t ${ZERO_ADDRESS} -c ${ACCOUNTB} --from-program ${TEMPDIR}/periodic.teal --firstvalid ${ROUND} --lastvalid ${DUR_ROUND} -x ${LEASE} -o ${TEMPDIR}/a.tx
 ${gcmd} clerk dryrun -t ${TEMPDIR}/a.tx
 
@@ -66,7 +66,7 @@ while [ $sendcount -lt 1 ]; do
 	false
     fi
     ROUND=$(goal node status | grep 'Last committed block:'|awk '{ print $4 }')
-    DUR_ROUND=$((${ROUND} + 2))
+    DUR_ROUND=$((${ROUND} + 4))
     if ${gcmd} clerk send -a 0 -t ${ZERO_ADDRESS} -c ${ACCOUNTB} --from-program ${TEMPDIR}/periodic.teal --firstvalid ${ROUND} --lastvalid ${DUR_ROUND} -x ${LEASE}; then
 	sendcount=$(($sendcount + 1))
 	date '+periodic-teal-test sent one at ${ROUND} %Y%m%d_%H%M%S'
