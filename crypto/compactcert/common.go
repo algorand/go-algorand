ef9f42290189e703d064639dd153d4765eeafdb2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-08-28 16:49:54 -0400

    refer to more careful analysis in the writeup

--- crypto/compactcert/common.go
+++ crypto/compactcert/common.go
@@ -56,15 +56,7 @@ func hashCoin(j uint64, sigcom crypto.Digest, signedWeight uint64) uint64 {
 //
 // signedWeight ^ numReveals >= 2^(k+q) * provenWeight ^ numReveals
 //
-// We use bigFloat for this computation, which is deterministic but adds
-// an error factor (rounding-down) of at most 1-2^-31 at each multiplication step.
-// The second-order (multiplied) error factors are negligible, but the additive
-// error factors add up to at most a multiplicative factor of 1-numReveals*2^-31.
-//
-// This is negligible in terms of the effect on the security parameter (k+q).
-// In order to reduce the security parameter by just one bit, it would need to
-// be the case that numReveals*2^-31=1/2, which would mean the loop ran for
-// numReveals=2^30 iterations.
+// See section 8.1 for the analysis of the rounding errors.
 func numReveals(signedWeight uint64, provenWeight uint64, secKQ uint64, bound uint64) (uint64, error) {
 	n := uint64(0)
 
