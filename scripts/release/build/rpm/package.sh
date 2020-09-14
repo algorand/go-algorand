223cd9c886deec84c4e733b44c9477ffca9cc29f
Benjamin Toll <benjam72@yahoo.com>
2020-09-14 12:11:54 -0400

    Build rpm packages for legacy pipeline

--- scripts/release/build/rpm/package.sh
+++ scripts/release/build/rpm/package.sh
@@ -6,31 +6,37 @@ echo "Building RPM package"
 
 cd "$(dirname "$0")"/..
 
-REPO_DIR=${HOME}/subhome/go/src/github.com/algorand/go-algorand
+REPO_DIR="$HOME/subhome/go/src/github.com/algorand/go-algorand"
 export REPO_DIR
-DEFAULT_RELEASE_NETWORK=$("$REPO_DIR/scripts/compute_branch_release_network.sh" "${DEFAULTNETWORK}")
+DEFAULT_RELEASE_NETWORK=$("$REPO_DIR/scripts/compute_branch_release_network.sh" "$DEFAULTNETWORK")
 export DEFAULT_RELEASE_NETWORK
 DEFAULTNETWORK=devnet
 export DEFAULT_NETWORK
 ALGO_BIN="$HOME/subhome/go/bin"
 export ALGO_BIN
 
-RPMTMP=$(mktemp -d 2>/dev/null || mktemp -d -t "rpmtmp")
-trap 'rm -rf ${RPMTMP}' 0
-
 BRANCH=$("$REPO_DIR/scripts/compute_branch.sh")
 CHANNEL=$("$REPO_DIR/scripts/compute_branch_channel.sh" "$BRANCH")
-PKG_NAME=$("$REPO_DIR/scripts/compute_package_name.sh" "${CHANNEL:-stable}")
+NAME=$("$REPO_DIR/scripts/compute_package_name.sh" "${CHANNEL:-stable}")
 
-TEMPDIR=$(mktemp -d)
-trap 'rm -rf $TEMPDIR' 0
-< "$REPO_DIR/installer/rpm/algorand.spec" \
-    sed -e "s,@PKG_NAME@,${PKG_NAME:-algorand}," \
-        -e "s,@VER@,$FULLVERSION," \
-    > "$TEMPDIR/algorand.spec"
+PKG_NAMES=("$NAME" "algorand-devtools")
+for pkg_name in "${PKG_NAMES[@]}"; do
+    RPMTMP=$(mktemp -d 2>/dev/null || mktemp -d -t "rpmtmp")
+    trap 'rm -rf $RPMTMP' 0
 
-rpmbuild --define "_rpmdir ${RPMTMP}" --define "RELEASE_GENESIS_PROCESS x${RELEASE_GENESIS_PROCESS}" --define "LICENSE_FILE $REPO_DIR/COPYING" -bb "${TEMPDIR}/algorand.spec"
+    TEMPDIR=$(mktemp -d)
+    trap 'rm -rf $TEMPDIR' 0
 
-mkdir -p /root/subhome/node_pkg
-cp -p "${RPMTMP}"/*/*.rpm /root/subhome/node_pkg
+    mkdir "$TEMPDIR/$pkg_name"
+
+    < "$REPO_DIR/installer/rpm/$pkg_name/$pkg_name.spec" \
+        sed -e "s,@PKG_NAME@,$pkg_name," \
+            -e "s,@VER@,$FULLVERSION," \
+        > "$TEMPDIR/$pkg_name/$pkg_name.spec"
+
+    rpmbuild --define "_rpmdir $RPMTMP" --define "RELEASE_GENESIS_PROCESS x${RELEASE_GENESIS_PROCESS}" --define "LICENSE_FILE $REPO_DIR/COPYING" -bb "$TEMPDIR/$pkg_name/$pkg_name.spec"
+
+    mkdir -p /root/subhome/node_pkg
+    cp -p "$RPMTMP"/*/*.rpm /root/subhome/node_pkg
+done
 
