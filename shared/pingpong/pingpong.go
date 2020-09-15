cba707a95230538c0bc6afc0414b092fd0de2893
egieseke <eric_gieseke@yahoo.com>
2020-09-15 10:58:42 -0400

    Fixed license header.

--- shared/pingpong/pingpong.go
+++ shared/pingpong/pingpong.go
@@ -7,7 +7,7 @@
 // License, or (at your option) any later version.
 //
 // go-algorand is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty ofprepareApps
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
 //
