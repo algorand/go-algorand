e16b5ab06691893ca89f59ecefd27a80f21af6c3
btoll <benjam72@yahoo.com>
2020-09-16 17:05:41 -0400

    Create new `algorand-devtools` package (#1492)

--- scripts/release/mule/package/rpm/package.sh
+++ scripts/release/mule/package/rpm/package.sh
@@ -14,9 +14,10 @@ ALGO_BIN="$REPO_DIR/tmp/node_pkgs/$OS_TYPE/$ARCH/$CHANNEL/$OS_TYPE-$ARCH/bin"
 # TODO: Should there be a default network?
 DEFAULTNETWORK=devnet
 DEFAULT_RELEASE_NETWORK=$(./scripts/compute_branch_release_network.sh "$DEFAULTNETWORK")
-PKG_NAME=$(./scripts/compute_package_name.sh "${CHANNEL:-stable}")
+# A make target in Makefile.mule may pass the name as an argument.
+PKG_NAME=${1:-$(./scripts/compute_package_name.sh "${CHANNEL:-stable}")}
 
-# The following need to be exported for use in ./go-algorand/installer/rpm/algorand.spec.
+# The following need to be exported for use in ./go-algorand/installer/rpm/$PKG_NAME/$PKG_NAME.spec.
 export DEFAULT_NETWORK
 export DEFAULT_RELEASE_NETWORK
 export REPO_DIR
@@ -27,12 +28,13 @@ trap 'rm -rf $RPMTMP' 0
 
 TEMPDIR=$(mktemp -d)
 trap 'rm -rf $TEMPDIR' 0
-< "./installer/rpm/algorand.spec" \
-    sed -e "s,@PKG_NAME@,$PKG_NAME," \
+< "./installer/rpm/$PKG_NAME/$PKG_NAME.spec" \
+    sed -e "s,@PKG_NAME@,${PKG_NAME}," \
         -e "s,@VER@,$FULLVERSION," \
-    > "$TEMPDIR/algorand.spec"
+        -e "s,@ARCH@,$ARCH," \
+    > "$TEMPDIR/$PKG_NAME.spec"
 
-rpmbuild --buildroot "$HOME/foo" --define "_rpmdir $RPMTMP" --define "RELEASE_GENESIS_PROCESS x$RELEASE_GENESIS_PROCESS" --define "LICENSE_FILE ./COPYING" -bb "$TEMPDIR/algorand.spec"
+rpmbuild --buildroot "$HOME/foo" --define "_rpmdir $RPMTMP" --define "RELEASE_GENESIS_PROCESS x$RELEASE_GENESIS_PROCESS" --define "LICENSE_FILE ./COPYING" -bb "$TEMPDIR/$PKG_NAME.spec"
 
 cp -p "$RPMTMP"/*/*.rpm "./tmp/node_pkgs/$OS_TYPE/$ARCH"
 
