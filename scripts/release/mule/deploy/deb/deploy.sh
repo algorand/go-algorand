b748d54e6f9df02b3a76cc49f76e853c6cd483e6
Benjamin Toll <benjam72@yahoo.com>
2020-09-18 19:04:58 -0400

    Update deploy script to handle all existing projects

--- scripts/release/mule/deploy/deb/deploy.sh
+++ scripts/release/mule/deploy/deb/deploy.sh
@@ -2,39 +2,52 @@
 
 set -ex
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
-    echo "Usage: $0 CHANNEL VERSION"
+    echo "Usage: $0 PROJECT CHANNEL VERSION"
     exit 1
 fi
 
-#ARCH_BIT=$(uname -m)
-#ARCH_TYPE=$(./scripts/archtype.sh)
-#OS_TYPE=$(./scripts/ostype.sh)
-#VERSION=${VERSION:-$(./scripts/compute_build_number.sh -f)}
-#BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
-#CHANNEL=${CHANNEL:-$(./scripts/compute_branch_channel.sh "$BRANCH")}
-#PKG_DIR="./tmp/node_pkgs/$OS_TYPE/$ARCH_TYPE"
-#SIGNING_KEY_ADDR=dev@algorand.com
-
-CHANNEL="$1"
-VERSION="$2"
+PROJECT="$1"
+CHANNEL="$2"
+VERSION="$3"
+SNAPSHOT="$4"
 REPO=algorand
-SNAPSHOT=${3:-"$CHANNEL-$VERSION"}
-PKG_DIR="$HOME/packages/deb/$CHANNEL"
+FILENAME_PREFIX="$REPO"
+
+if [ "$PROJECT" = indexer ]
+then
+    echo abort
+    exit 1
+fi
+
+PKG_DIR="$HOME/packages-test/deb/$PROJECT/$CHANNEL"
+
+if [ "$PROJECT" != algorand ]
+then
+    REPO="$REPO-$PROJECT"
+    FILENAME_PREFIX="$REPO"
+fi
 
 if [ "$CHANNEL" = beta ]
 then
-    REPO=algorand-beta
-elif [ "$CHANNEL" = indexer ]
-then
-    REPO=algorand-indexer
+    REPO="$REPO-beta"
 fi
 
-aws s3 cp "s3://algorand-staging/releases/$CHANNEL/$VERSION/algorand_${CHANNEL}_linux-amd64_${VERSION}.deb" "$PKG_DIR"
+if [ -z "$SNAPSHOT" ]
+then
+    SNAPSHOT="$REPO-$VERSION"
+fi
+
+echo -e "PROJECT $PROJECT\nREPO $REPO\nSNAPSHOT $SNAPSHOT\nFILENAME_PREFIX $FILENAME_PREFIX"
+
+aws s3 cp "s3://algorand-staging/releases/$CHANNEL/$VERSION/${FILENAME_PREFIX}_${CHANNEL}_linux-amd64_${VERSION}.deb" "$PKG_DIR"
+
+#aptly repo create -distribution="devtools" -architectures="amd64" -component="main" -comment="devtools-stable" algorand-devtools
+#aptly repo create -distribution="devtools-beta" -architectures="amd64" -component="main" -comment="devtools-beta" algorand-devtools-beta
 
 aptly repo add "$REPO" "$PKG_DIR/"*.deb
 aptly snapshot create "$SNAPSHOT" from repo "$REPO"
-aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
 #aptly publish snapshot -gpg-key=dev@algorand.com -origin=Algorand -label=Algorand "$SNAPSHOT" s3:algorand-releases:
+aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
 
