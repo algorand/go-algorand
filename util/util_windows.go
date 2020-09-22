8235d7c1cb42e3bdd4d561da1031e038afb75073
Mauro Leggieri <mxmauro@mauroleggieri.com>
2020-09-22 19:30:31 -0300

    Fixed build exception on intended target OS

--- util/util_windows.go
+++ util/util_windows.go
@@ -16,8 +16,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
-// +build !windows
-
 package util
 
 /* misc */
