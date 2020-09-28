eb48485e9776d9317212b22ab38b7386efda676f
Mauro Leggieri <mxmauro@mauroleggieri.com>
2020-09-28 10:08:11 -0300

    Merged Windows dependencies installer script with configure_dev.sh

--- scripts/configure_dev.sh
+++ scripts/configure_dev.sh
@@ -42,40 +42,29 @@ function install_or_upgrade {
 }
 
 function install_windoows_shellcheck() {
-    filename="shellcheck-v0.7.0.zip"
-    filechecksum="10ee2474845eeb76d8a13992457472b723edf470c7cf182a20b32ecee4ad009ec6b2ca542db8f66127cf19e24baf3a06838a0d101494a5a6c11b3b568f9f5a99"
-    wget https://shellcheck.storage.googleapis.com/$filename -O /tmp/$filename
-    if [ $? -ne 0 ]
-    then
-        rm /tmp/$filename &> /dev/null
-        echo "Error downloading $filename"
+    version="v0.7.1"
+    wget https://github.com/koalaman/shellcheck/releases/download/$version/shellcheck-$version.zip -O /tmp/shellcheck-$version.zip
+    if [ $? -ne 0 ]; then
+        rm /tmp/shellcheck-$version.zip &> /dev/null
+        echo "Error downloading shellcheck $version"
         return 1
     fi
 
-    if [ "$(cat /tmp/$filename | sha512sum | head -c 128)" != "$filechecksum" ]
-    then
-        rm /tmp/$filename &> /dev/null
-        echo "$filename checksum mismatch"
+    unzip -o /tmp/shellcheck-$version.zip shellcheck-$version.exe -d /tmp
+    if [ $? -ne 0 ]; then
+        rm /tmp/shellcheck-$version.zip &> /dev/null
+        echo "Unable to decompress shellcheck $version"
         return 1
     fi
 
-    unzip -o /tmp/shellcheck-v0.7.0.zip shellcheck-v0.7.0.exe -d /tmp
-    if [ $? -ne 0 ]
-    then
-        rm /tmp/$filename &> /dev/null
-        echo "Unable to decompress shellcheck file"
-        return 1
-    fi
-
-    mv -f /tmp/shellcheck-v0.7.0.exe /usr/bin/shellcheck.exe
-    if [ $? -ne 0 ]
-    then
-        rm /tmp/$filename &> /dev/null
+    mv -f /tmp/shellcheck-$version.exe /usr/bin/shellcheck.exe
+    if [ $? -ne 0 ]; then
+        rm /tmp/shellcheck-$version.zip &> /dev/null
         echo "Unable to move shellcheck to /usr/bin"
-        exit 1
+        return 1
     fi
 
-    rm /tmp/$filename &> /dev/null
+    rm /tmp/shellcheck-$version.zip &> /dev/null
 
     return 0
 }
@@ -101,13 +90,14 @@ elif [ "${OS}" = "darwin" ]; then
     install_or_upgrade shellcheck
     install_or_upgrade python3
 elif [ "${OS}" = "windows" ]; then
-    pacman -S --disable-download-timeout --noconfirm git automake autoconf m4 libtool mingw-w64-x86_64-python3 make mingw-w64-x86_64-gcc mingw-w64-x86_64-go mingw-w64-x86_64-boost mingw-w64-x86_64-python unzip procps
+    pacman -S --disable-download-timeout --noconfirm git automake autoconf m4 libtool make mingw-w64-x86_64-gcc mingw-w64-x86_64-go mingw-w64-x86_64-boost mingw-w64-x86_64-python mingw-w64-x86_64-jq unzip procps
     if [ $? -ne 0 ]
     then
         echo "Error installing pacman dependencies"
         exit 1
     fi
 
+    # Golang probably is not installed under MSYS2 so add the environment variable temporarily
     export GOPATH=$HOME/go
 
     install_windoows_shellcheck
@@ -115,6 +105,9 @@ elif [ "${OS}" = "windows" ]; then
     then
         exit 1
     fi
+
+    # This is required because http://github.com/karalabe/hid library compiles with non-static libraries
+    cp /mingw64/bin/libwinpthread-1.dll $GOPATH/bin/ 
 fi
 
 if ${SKIP_GO_DEPS} ; then
@@ -122,4 +115,3 @@ if ${SKIP_GO_DEPS} ; then
 fi
 
 "$SCRIPTPATH"/configure_dev-deps.sh
-
