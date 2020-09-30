5da80d75e87543396b8207551736a8cda554ccbd
Benjamin Toll <benjam72@yahoo.com>
2020-09-30 12:52:43 -0400

    Less redirection

--- scripts/release/mule/test/test.sh
+++ scripts/release/mule/test/test.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
+# shellcheck disable=2045
 
 set -ex
 
 export PKG_TYPE="$1"
 ARCH_BIT=$(uname -m)
-export ARCH_BIT
 ARCH_TYPE=$(./scripts/archtype.sh)
-export ARCH_TYPE
 OS_TYPE=$(./scripts/ostype.sh)
-export OS_TYPE
 
 export BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 export CHANNEL=${CHANNEL:-$(./scripts/compute_branch_channel.sh "$BRANCH")}
@@ -16,25 +14,37 @@ export NETWORK=${NETWORK:-$(./scripts/compute_branch_network.sh "$BRANCH")}
 export SHA=${SHA:-$(git rev-parse HEAD)}
 export VERSION=${VERSION:-$(./scripts/compute_build_number.sh -f)}
 
-#if ! $USE_CACHE
-#then
-#    mule -f package-test.yaml "package-test-setup-$PKG_TYPE"
-#    agent: deb
-#    bucketName: algorand-staging
-#    objectName: releases/$CHANNEL/$VERSION/algorand_${CHANNEL}_${OS_TYPE}-${ARCH_TYPE}_${VERSION}.deb
-#    outputDir: /projects/go-algorand/tmp/node_pkgs/${OS_TYPE}/${ARCH_TYPE}
-#
-#    name: rpm
-#    bucketName: algorand-staging
-#    objectName: releases/$CHANNEL/$VERSION/algorand-${VERSION}-1.${ARCH_BIT}.rpm
-#    outputDir: /projects/go-algorand/tmp/node_pkgs/${OS_TYPE}/${ARCH_TYPE}
-#fi
+if ! $USE_CACHE
+then
+    SRC_DIR="s3://algorand-staging/releases/$CHANNEL/$VERSION"
+    DEST_DIR="/projects/go-algorand/tmp/node_pkgs/$OS_TYPE/$ARCH_TYPE"
 
-./scripts/release/mule/test/tests/run_tests
-#if [[ "$ARCH_TYPE" =~ "arm" ]]
-#then
-#    ./scripts/release/mule/test/tests/run_tests -b "$BRANCH" -c "$CHANNEL" -h "$SHA" -n "$NETWORK" -r "$VERSION"
+    # deb
+    aws s3 cp "$SRC_DIR/algorand_${CHANNEL}_${OS_TYPE}-${ARCH_TYPE}_${VERSION}.deb" "$DEST_DIR"
+    aws s3 cp "$SRC_DIR/algorand-devtools_${CHANNEL}_${OS_TYPE}-${ARCH_TYPE}_${VERSION}.deb" "$DEST_DIR"
+
+    # rpm
+    aws s3 cp "$SRC_DIR/algorand-devtools-$VERSION-1.$ARCH_BIT.rpm" "$DEST_DIR"
+    aws s3 cp "$SRC_DIR/algorand-$VERSION-1.$ARCH_BIT.rpm" "$DEST_DIR"
+fi
+
+if [ "$PKG_TYPE" == "deb" ]
+then
+    for deb in $(ls "./tmp/node_pkgs/$OS_TYPE/$ARCH_TYPE"/*"$VERSION"*.deb); do
+        if [[ ! "$deb" =~ devtools ]]; then
+            dpkg -i "$deb"
+        fi
+    done
 #else
-#    ./scripts/release/mule/test/util/test_package.sh
-#fi
+#    for rpm in $(ls "./tmp/node_pkgs/$OS_TYPE/$ARCH_TYPE"/*"$VERSION"*.rpm); do
+#        if [[ ! "$rpm" =~ devtools ]]; then
+#            yum install "$rpm" -y
+#        fi
+#    done
+fi
+
+for test in $(ls ./scripts/release/mule/test/tests/*.sh)
+do
+    bash "$test"
+done
 
