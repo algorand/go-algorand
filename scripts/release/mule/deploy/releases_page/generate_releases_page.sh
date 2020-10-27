1d95bba0b9ae749abffb826c21e10578e99f601c
Benjamin Toll <benjam72@yahoo.com>
2020-10-27 15:39:43 -0400

    Improved generate releases page script

--- scripts/release/mule/deploy/releases_page/generate_releases_page.sh
+++ scripts/release/mule/deploy/releases_page/generate_releases_page.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# Note: For this script to correctly pick up the new release, the new repo containing the new
+#       packages MUST have been already pushed to S3! (See the deb deployment mule step.)
+#
+# 1. Generate the new releases page from the contents of `algorand-dev-deb-repo/releases/CHANNEL`.
+# 2. Backup up the current releases page (index.html).
+# 3. Copy the new index.html to staging.
+# 4. Copy the new index.html to `algorand-releases`.
+
 set -ex
 
 ./generate_releases_page.py > index.html
+aws s3 cp s3://algorand-releases/index.html s3://algorand-staging/releases-page/index.html-previous
 aws s3 cp index.html s3://algorand-staging/releases-page/
-aws s3 cp s3://algorand-staging/releases-page/index.html s3://algorand-releases/
+aws s3 cp index.html s3://algorand-releases/
 
