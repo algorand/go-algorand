a0dc9af0a98cfab81bf8bc0316393b75cb176732
Benjamin Toll <benjam72@yahoo.com>
2020-10-30 00:28:58 -0400

    Added OS_TYPE

--- scripts/release/mule/package/deb/package.sh
+++ scripts/release/mule/package/deb/package.sh
@@ -10,6 +10,7 @@ echo
 ARCH_TYPE=$(./scripts/archtype.sh)
 BRANCH=${BRANCH:-$(./scripts/compute_branch.sh)}
 CHANNEL=${CHANNEL:-$(./scripts/compute_branch_channel.sh "$BRANCH")}
+OS_TYPE=$(./scripts/ostype.sh)
 VERSION=${VERSION:-$(./scripts/compute_build_number.sh -f)}
 # A make target in Makefile.mule may pass the name as an argument.
 ALGORAND_PACKAGE_NAME=${1:-$(./scripts/compute_package_name.sh "$CHANNEL")}
@@ -135,7 +136,7 @@ EOF
 
     pushd "$PKG_DIR"
 
-    STATUSFILE=build_status_${CHANNEL}-${ARCH_TYPE}_${VERSION}
+    STATUSFILE=build_status_${CHANNEL}_${OS_TYPE}-${ARCH_TYPE}_${VERSION}
 
     cat >> "$STATUSFILE" << EOF
 go version:
