be1e6c2bc7c247660eedbccf5e3b3c400c8596e4
Pavel Zbitskiy <pavel@algorand.com>
2020-11-03 15:06:26 -0500

    Restore logic legder tests

--- ledger/applications_test.go
+++ ledger/applications_test.go
@@ -16,87 +16,19 @@
 
 package ledger
 
-/*
 import (
 	"crypto/rand"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/crypto"
 	"github.com/algorand/go-algorand/data/basics"
+	"github.com/algorand/go-algorand/data/bookkeeping"
+	"github.com/algorand/go-algorand/protocol"
 	"github.com/stretchr/testify/require"
 )
-type testBalances struct {
-	appCreators   map[basics.AppIndex]basics.Address
-	assetCreators map[basics.AssetIndex]basics.Address
-	balances      map[basics.Address]basics.AccountData
-}
-
-type testBalancesPass struct {
-	testBalances
-}
-
-const appIdxError basics.AppIndex = 0x11223344
-const appIdxOk basics.AppIndex = 1
-
-func (b *testBalances) Get(addr basics.Address, withPendingRewards bool) (basics.BalanceRecord, error) {
-	ad, ok := b.balances[addr]
-	if !ok {
-		return basics.BalanceRecord{}, fmt.Errorf("mock balance not found")
-	}
-	return basics.BalanceRecord{Addr: addr, AccountData: ad}, nil
-}
-
-func (b *testBalances) Put(record basics.BalanceRecord) error {
-	return nil
-}
-
-func (b *testBalances) PutWithCreatable(record basics.BalanceRecord, newCreatable *basics.CreatableLocator, deletedCreatable *basics.CreatableLocator) error {
-	return nil
-}
-
-func (b *testBalances) GetCreator(cidx basics.CreatableIndex, ctype basics.CreatableType) (basics.Address, bool, error) {
-	if ctype == basics.AppCreatable {
-		aidx := basics.AppIndex(cidx)
-		if aidx == appIdxError { // magic for test
-			return basics.Address{}, false, fmt.Errorf("mock synthetic error")
-		}
-
-		creator, ok := b.appCreators[aidx]
-		return creator, ok, nil
-	}
-	if ctype == basics.AssetCreatable {
-		aidx := basics.AssetIndex(cidx)
-		creator, ok := b.assetCreators[aidx]
-		return creator, ok, nil
-	}
-	return basics.Address{}, false, nil
-}
-
-func (b *testBalances) Move(src, dst basics.Address, amount basics.MicroAlgos, srcRewards *basics.MicroAlgos, dstRewards *basics.MicroAlgos) error {
-	return nil
-}
-
-func (b *testBalancesPass) Get(addr basics.Address, withPendingRewards bool) (basics.BalanceRecord, error) {
-	ad, ok := b.balances[addr]
-	if !ok {
-		return basics.BalanceRecord{}, fmt.Errorf("mock balance not found")
-	}
-	return basics.BalanceRecord{Addr: addr, AccountData: ad}, nil
-}
-
-func (b *testBalancesPass) Put(record basics.BalanceRecord) error {
-	return nil
-}
-
-func (b *testBalancesPass) PutWithCreatable(record basics.BalanceRecord, newCreatable *basics.CreatableLocator, deletedCreatable *basics.CreatableLocator) error {
-	return nil
-}
-
-func (b *testBalances) ConsensusParams() config.ConsensusParams {
-	return config.ConsensusParams{}
-}
 
 func getRandomAddress(a *require.Assertions) basics.Address {
 	const rl = 16
@@ -109,171 +41,375 @@ func getRandomAddress(a *require.Assertions) basics.Address {
 	return basics.Address(address)
 }
 
-func TestNewAppLedger(t *testing.T) {
-	a := require.New(t)
-
-	_, err := newAppLedger(nil, 0, basics.StateSchemas{}, AppTealGlobals{})
-	a.Error(err)
-	a.Contains(err.Error(), "nil balances")
-
-	b := testBalances{}
-
-	_, err = newAppLedger(&b, 0, basics.StateSchemas{}, AppTealGlobals{})
-	a.Error(err)
-	a.Contains(err.Error(), "cannot create appLedger for appIdx 0")
-
-	appIdx := basics.AppIndex(1)
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
-	a.NoError(err)
-	a.NotNil(l)
-	a.Equal(appIdx, l.appIdx)
-	a.NotNil(l.balances)
-
-	dl, err := MakeDebugAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
-	a.NoError(err)
-	a.NotNil(dl)
+type modsData struct {
+	addr  basics.Address
+	cidx  basics.CreatableIndex
+	ctype basics.CreatableType
 }
 
-func TestAppLedgerBalances(t *testing.T) {
+func getCow(creatables []modsData) *roundCowState {
+	cs := &roundCowState{
+		mods: StateDelta{
+			creatables: make(map[basics.CreatableIndex]modifiedCreatable),
+			hdr:        &bookkeeping.BlockHeader{},
+		},
+		proto: config.Consensus[protocol.ConsensusCurrentVersion],
+	}
+	for _, e := range creatables {
+		cs.mods.creatables[e.cidx] = modifiedCreatable{ctype: e.ctype, creator: e.addr, created: true}
+	}
+	return cs
+}
+
+func TestLogicLedgerMake(t *testing.T) {
 	a := require.New(t)
 
-	b := testBalances{}
-	addr1 := getRandomAddress(a)
-	appIdx := appIdxOk
+	_, err := makeLogicLedger(nil, 0)
+	a.Error(err)
+	a.Contains(err.Error(), "cannot make logic ledger for app index 0")
 
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
+	addr := getRandomAddress(a)
+	aidx := basics.AppIndex(1)
+	c := getCow([]modsData{{addr, basics.CreatableIndex(aidx), basics.AppCreatable}})
+
+	_, err = makeLogicLedger(c, 0)
+	a.Error(err)
+	a.Contains(err.Error(), "cannot make logic ledger for app index 0")
+
+	l, err := makeLogicLedger(c, aidx)
+	a.NoError(err)
+	a.NotNil(l)
+	a.Equal(aidx, l.aidx)
+	a.Equal(c, l.cow)
+}
+
+func TestLogicLedgerBalances(t *testing.T) {
+	a := require.New(t)
+
+	addr := getRandomAddress(a)
+	aidx := basics.AppIndex(1)
+	c := getCow([]modsData{{addr, basics.CreatableIndex(aidx), basics.AppCreatable}})
+	l, err := makeLogicLedger(c, aidx)
 	a.NoError(err)
 	a.NotNil(l)
 
+	addr1 := getRandomAddress(a)
 	ble := basics.MicroAlgos{Raw: 100}
-	b.balances = map[basics.Address]basics.AccountData{addr1: {MicroAlgos: ble}}
+	c.mods.accts = map[basics.Address]miniAccountDelta{addr1: {new: basics.AccountData{MicroAlgos: ble}}}
 	bla, err := l.Balance(addr1)
 	a.NoError(err)
 	a.Equal(ble, bla)
+
+	// ensure other requests go down to roundCowParent
+	a.Panics(func() { l.Balance(getRandomAddress(a)) })
 }
 
-func TestAppLedgerGetters(t *testing.T) {
+func TestLogicLedgerGetters(t *testing.T) {
 	a := require.New(t)
 
-	b := testBalances{}
-	appIdx := appIdxOk
+	addr := getRandomAddress(a)
+	aidx := basics.AppIndex(1)
+	c := getCow([]modsData{{addr, basics.CreatableIndex(aidx), basics.AppCreatable}})
+	l, err := makeLogicLedger(c, aidx)
+	a.NoError(err)
+	a.NotNil(l)
+
 	round := basics.Round(1234)
+	c.mods.hdr.Round = round
 	ts := int64(11223344)
-	globals := AppTealGlobals{round, ts}
+	c.mods.prevTimestamp = ts
 
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, globals)
-	a.NoError(err)
-	a.NotNil(l)
+	addr1 := getRandomAddress(a)
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr1: {storagePtr{aidx, false}: &storageDelta{action: allocAction}},
+	}
 
-	a.Equal(appIdx, l.ApplicationID())
+	a.Equal(aidx, l.ApplicationID())
 	a.Equal(round, l.Round())
 	a.Equal(ts, l.LatestTimestamp())
+	a.True(l.OptedIn(addr1, aidx))
 
-	// check there no references/pointers
-	globals.CurrentRound = 101
-	globals.LatestTimestamp = 102
-	a.Equal(round, l.Round())
-	a.Equal(ts, l.LatestTimestamp())
+	// ensure other requests go down to roundCowParent
+	a.Panics(func() { l.OptedIn(getRandomAddress(a), aidx) })
 }
 
-func TestAppLedgerAsset(t *testing.T) {
+func TestLogicLedgerAsset(t *testing.T) {
 	a := require.New(t)
 
-	b := testBalances{}
+	addr := getRandomAddress(a)
 	addr1 := getRandomAddress(a)
-	appIdx := appIdxOk
-
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
+	aidx := basics.AppIndex(1)
+	assetIdx := basics.AssetIndex(2)
+	c := getCow([]modsData{
+		{addr, basics.CreatableIndex(aidx), basics.AppCreatable},
+		{addr1, basics.CreatableIndex(assetIdx), basics.AssetCreatable},
+	})
+	l, err := makeLogicLedger(c, aidx)
 	a.NoError(err)
 	a.NotNil(l)
 
-	assetIdx := basics.AssetIndex(2)
-	b.balances = map[basics.Address]basics.AccountData{addr1: {}}
-	b.assetCreators = map[basics.AssetIndex]basics.Address{assetIdx: addr1}
-	_, err = l.AssetParams(assetIdx)
+	_, err = l.AssetParams(basics.AssetIndex(aidx))
 	a.Error(err)
-	a.Contains(err.Error(), "has not created asset")
+	a.Contains(err.Error(), fmt.Sprintf("asset %d does not exist", aidx))
 
-	b.balances[addr1] = basics.AccountData{
-		AssetParams: map[basics.AssetIndex]basics.AssetParams{assetIdx: {Total: 1000}},
+	c.mods.accts = map[basics.Address]miniAccountDelta{
+		addr1: {
+			new: basics.AccountData{
+				AssetParams: map[basics.AssetIndex]basics.AssetParams{assetIdx: {Total: 1000}},
+			},
+		},
 	}
 	ap, err := l.AssetParams(assetIdx)
 	a.NoError(err)
 	a.Equal(uint64(1000), ap.Total)
-	delete(b.balances, addr1)
 
-	b.balances = map[basics.Address]basics.AccountData{addr1: {}}
 	_, err = l.AssetHolding(addr1, assetIdx)
 	a.Error(err)
 	a.Contains(err.Error(), "has not opted in to asset")
 
-	b.balances[addr1] = basics.AccountData{
-		Assets: map[basics.AssetIndex]basics.AssetHolding{assetIdx: {Amount: 99}},
+	c.mods.accts = map[basics.Address]miniAccountDelta{
+		addr1: {
+			new: basics.AccountData{
+				AssetParams: map[basics.AssetIndex]basics.AssetParams{assetIdx: {Total: 1000}},
+				Assets:      map[basics.AssetIndex]basics.AssetHolding{assetIdx: {Amount: 99}},
+			},
+		},
 	}
+
 	ah, err := l.AssetHolding(addr1, assetIdx)
 	a.NoError(err)
 	a.Equal(uint64(99), ah.Amount)
+
+	// ensure other requests go down to roundCowParent
+	a.Panics(func() { l.AssetHolding(getRandomAddress(a), assetIdx) })
+	a.Panics(func() { l.AssetParams(assetIdx + 1) })
 }
 
-func TestAppLedgerAppGlobalState(t *testing.T) {
+func TestLogicLedgerGetKey(t *testing.T) {
 	a := require.New(t)
 
-	b := testBalances{}
+	addr := getRandomAddress(a)
 	addr1 := getRandomAddress(a)
-	appIdx := appIdxOk
-
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
+	aidx := basics.AppIndex(1)
+	assetIdx := basics.AssetIndex(2)
+	c := getCow([]modsData{
+		{addr, basics.CreatableIndex(aidx), basics.AppCreatable},
+		{addr1, basics.CreatableIndex(assetIdx), basics.AssetCreatable},
+	})
+	l, err := makeLogicLedger(c, aidx)
 	a.NoError(err)
 	a.NotNil(l)
 
-	_, err = l.AppGlobalState(appIdx)
+	_, ok, err := l.GetGlobal(basics.AppIndex(assetIdx), "gkey")
 	a.Error(err)
-	a.Contains(err.Error(), fmt.Sprintf("app %d does not exist", appIdx))
+	a.False(ok)
+	a.Contains(err.Error(), fmt.Sprintf("app %d does not exist", assetIdx))
 
-	_, err = l.AppGlobalState(0)
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {storagePtr{aidx, true}: &storageDelta{action: deallocAction}},
+	}
+	_, ok, err = l.GetGlobal(aidx, "gkey")
 	a.Error(err)
-	a.Contains(err.Error(), fmt.Sprintf("app %d does not exist", appIdx))
+	a.False(ok)
+	a.Contains(err.Error(), "cannot fetch key")
 
-	_, err = l.AppGlobalState(appIdxError)
-	a.Error(err)
-	a.NotContains(err.Error(), "cannot access global state")
-	a.NotContains(err.Error(), fmt.Sprintf("app %d does not exist", appIdxError))
-
-	b.appCreators = map[basics.AppIndex]basics.Address{appIdx: addr1}
-	_, err = l.AppGlobalState(appIdx)
-	a.Error(err)
-	a.NotContains(err.Error(), fmt.Sprintf("app %d does not exist", appIdx))
-
-	b.balances = map[basics.Address]basics.AccountData{addr1: {}}
-	_, err = l.AppGlobalState(appIdx)
-	a.Error(err)
-	a.Contains(err.Error(), "not found in account")
-
-	b.balances[addr1] = basics.AccountData{AppParams: map[basics.AppIndex]basics.AppParams{appIdx: {}}}
-	kv, err := l.AppGlobalState(appIdx)
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {storagePtr{aidx, true}: &storageDelta{action: allocAction}},
+	}
+	_, ok, err = l.GetGlobal(aidx, "gkey")
 	a.NoError(err)
-	a.NotNil(kv)
+	a.False(ok)
+	_, ok, err = l.GetGlobal(0, "gkey")
+	a.NoError(err)
+	a.False(ok)
+
+	tv := basics.TealValue{Type: basics.TealUintType, Uint: 1}
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action: allocAction,
+				kvCow:  stateDelta{"gkey": valueDelta{new: tv, newExists: false}},
+			},
+		},
+	}
+	val, ok, err := l.GetGlobal(aidx, "gkey")
+	a.NoError(err)
+	a.False(ok)
+
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action: allocAction,
+				kvCow:  stateDelta{"gkey": valueDelta{new: tv, newExists: true}},
+			},
+		},
+	}
+	val, ok, err = l.GetGlobal(aidx, "gkey")
+	a.NoError(err)
+	a.True(ok)
+	a.Equal(tv, val)
+
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action: remainAllocAction,
+				kvCow:  stateDelta{"gkey": valueDelta{new: tv, newExists: true}},
+			},
+		},
+	}
+
+	// check local
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, false}: &storageDelta{
+				action: allocAction,
+				kvCow:  stateDelta{"lkey": valueDelta{new: tv, newExists: true}},
+			},
+		},
+	}
+
+	val, ok, err = l.GetLocal(addr, aidx, "lkey")
+	a.NoError(err)
+	a.True(ok)
+	a.Equal(tv, val)
 }
 
-func TestAppLedgerAppLocalState(t *testing.T) {
+func TestLogicLedgerSetKey(t *testing.T) {
 	a := require.New(t)
 
-	b := testBalances{}
-	addr1 := getRandomAddress(a)
-	appIdx := basics.AppIndex(100) // not in app
-
-	l, err := newAppLedger(&b, appIdx, basics.StateSchemas{}, AppTealGlobals{})
+	addr := getRandomAddress(a)
+	aidx := basics.AppIndex(1)
+	c := getCow([]modsData{
+		{addr, basics.CreatableIndex(aidx), basics.AppCreatable},
+	})
+	l, err := makeLogicLedger(c, aidx)
 	a.NoError(err)
 	a.NotNil(l)
 
-	b.balances = map[basics.Address]basics.AccountData{addr1: {}}
-	_, err = l.AppLocalState(addr1, appIdx)
+	key := strings.Repeat("key", 100)
+	val := "val"
+	tv := basics.TealValue{Type: basics.TealBytesType, Bytes: val}
+	err = l.SetGlobal(key, tv)
 	a.Error(err)
-	a.Contains(err.Error(), "not opted in to app")
+	a.Contains(err.Error(), "key too long")
 
-	b.balances[addr1] = basics.AccountData{AppLocalStates: map[basics.AppIndex]basics.AppLocalState{appIdx: {}}}
-	kv, err := l.AppLocalState(addr1, appIdx)
+	key = "key"
+	val = strings.Repeat("val", 100)
+	tv = basics.TealValue{Type: basics.TealBytesType, Bytes: val}
+	err = l.SetGlobal(key, tv)
+	a.Error(err)
+	a.Contains(err.Error(), "value too long")
+
+	val = "val"
+	tv = basics.TealValue{Type: basics.TealBytesType, Bytes: val}
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {storagePtr{aidx, true}: &storageDelta{action: deallocAction}},
+	}
+	err = l.SetGlobal(key, tv)
+	a.Error(err)
+	a.Contains(err.Error(), "cannot set key")
+
+	counts := basics.StateSchema{}
+	maxCounts := basics.StateSchema{}
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action:    allocAction,
+				kvCow:     make(stateDelta),
+				counts:    &counts,
+				maxCounts: &maxCounts,
+			},
+		},
+	}
+	err = l.SetGlobal(key, tv)
+	a.Error(err)
+	a.Contains(err.Error(), "exceeds schema bytes")
+
+	counts = basics.StateSchema{NumUint: 1}
+	maxCounts = basics.StateSchema{NumByteSlice: 1}
+	err = l.SetGlobal(key, tv)
+	a.Error(err)
+	a.Contains(err.Error(), "exceeds schema integer")
+
+	tv2 := basics.TealValue{Type: basics.TealUintType, Uint: 1}
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action:    allocAction,
+				kvCow:     stateDelta{key: valueDelta{new: tv2, newExists: true}},
+				counts:    &counts,
+				maxCounts: &maxCounts,
+			},
+		},
+	}
+	err = l.SetGlobal(key, tv)
 	a.NoError(err)
-	a.NotNil(kv)
+
+	counts = basics.StateSchema{NumUint: 1}
+	maxCounts = basics.StateSchema{NumByteSlice: 1, NumUint: 1}
+	err = l.SetGlobal(key, tv)
+	a.NoError(err)
+
+	// check local
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, false}: &storageDelta{
+				action:    allocAction,
+				kvCow:     stateDelta{key: valueDelta{new: tv2, newExists: true}},
+				counts:    &counts,
+				maxCounts: &maxCounts,
+			},
+		},
+	}
+	err = l.SetLocal(addr, key, tv)
+	a.NoError(err)
 }
-*/
+
+func TestLogicLedgerDelKey(t *testing.T) {
+	a := require.New(t)
+
+	addr := getRandomAddress(a)
+	aidx := basics.AppIndex(1)
+	c := getCow([]modsData{
+		{addr, basics.CreatableIndex(aidx), basics.AppCreatable},
+	})
+	l, err := makeLogicLedger(c, aidx)
+	a.NoError(err)
+	a.NotNil(l)
+
+	key := "key"
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {storagePtr{aidx, true}: &storageDelta{action: deallocAction}},
+	}
+	err = l.DelGlobal(key)
+	a.Error(err)
+	a.Contains(err.Error(), "cannot del key")
+
+	counts := basics.StateSchema{}
+	maxCounts := basics.StateSchema{}
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, true}: &storageDelta{
+				action:    allocAction,
+				kvCow:     make(stateDelta),
+				counts:    &counts,
+				maxCounts: &maxCounts,
+			},
+		},
+	}
+	err = l.DelGlobal(key)
+	a.NoError(err)
+
+	c.mods.sdeltas = map[basics.Address]map[storagePtr]*storageDelta{
+		addr: {
+			storagePtr{aidx, false}: &storageDelta{
+				action:    allocAction,
+				kvCow:     make(stateDelta),
+				counts:    &counts,
+				maxCounts: &maxCounts,
+			},
+		},
+	}
+	err = l.DelLocal(addr, key)
+	a.NoError(err)
+}
