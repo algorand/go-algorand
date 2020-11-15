eeafd4b63695b4d4a22839ae43f424c675b7ded1
Benjamin Toll <benjam72@yahoo.com>
2020-11-15 12:11:25 -0500

    Use go tooling to get go version string in go.mod

--- scripts/get_golang_version.sh
+++ scripts/get_golang_version.sh
@@ -4,13 +4,13 @@ GOLANG_VERSION="1.14.7"
 
 # Check to make sure that it matches what is specified in go.mod
 
-GOMOD_VERSION=$(awk '/^go[ \t]+[0-9]+\.[0-9]+(\.[0-9]+)?[ \t]*$/{print $2}' go.mod)
+GOMOD_VERSION=$(go mod edit -json | jq -r ".Go")
 TRIMMED_GOLANG_VERSION=$(echo ${GOLANG_VERSION} |awk -F. '{ print $1 "." $2 }')
 
 if ! [[ ${GOLANG_VERSION} == "${GOMOD_VERSION}" || ${TRIMMED_GOLANG_VERSION} == "${GOMOD_VERSION}" ]]
 then
     echo "go version mismatch, go mod version ${GOMOD_VERSION} does not match required version ${GOLANG_VERSION}"
-    exit 1;
+    exit 1
 else
     echo ${GOLANG_VERSION}
 fi
