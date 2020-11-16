78bc2ef7127e7ba124281130f2562f5cd01c8328
Benjamin Toll <benjam72@yahoo.com>
2020-11-16 12:27:02 -0500

    Add additional err message if system go version is too old

--- scripts/check_golang_version.sh
+++ scripts/check_golang_version.sh
@@ -4,8 +4,14 @@ set -eo pipefail
 
 GOLANG_VERSION=$(./scripts/get_golang_version.sh)
 
-# Check to make sure that it matches what is specified in go.mod
+# https://golang.org/doc/go1.11#modules
+if [[ "${GOLANG_VERSION}" < 1.1.1 ]]
+then
+    echo "The version of go on the system is too old and does not support go modules, please update to ${GOLANG_VERSION}"
+    exit 1
+fi
 
+# Check to make sure that it matches what is specified in go.mod
 GOMOD_VERSION=$(go mod edit -json | jq -r ".Go")
 TRIMMED_GOLANG_VERSION=$(awk -F. '{ print $1 "." $2 }' <<< "${GOLANG_VERSION}")
 
