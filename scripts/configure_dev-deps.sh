88a7b4ae984499be92b44f72a750e70b51115d5c
Tsachi Herman <tsachi.herman@algorand.com>
2020-11-17 08:51:19 -0500

    Derive the swagger version from the go.mod file.

--- scripts/configure_dev-deps.sh
+++ scripts/configure_dev-deps.sh
@@ -3,35 +3,32 @@
 set -ex
 
 function get_go_version {
-    if [[ -z "$2" ]]
-    then
-        (
-        cd "$(dirname "$0")"
-        VERSION=$( grep "$1" 2>/dev/null < ../go.mod | awk -F " " '{print $2}')
-        echo "$VERSION"
-        )
-    else
-        (echo "$2")
-    fi
+    cd "$(dirname "$0")"
+    VERSION=$( grep "$1" 2>/dev/null < ../go.mod | awk -F " " '{print $2}')
+    echo "$VERSION"
     return
 }
 
 function install_go_module {
     local OUTPUT
+    local MODULE = $1
+    if [[ "$2" != "" ]]; then
+        MODULE = "$2"
+    fi
     # Check for version to go.mod version
-    VERSION=$(get_go_version "$1" "$2")
+    VERSION=$(get_go_version "$1")
     if [ -z "$VERSION" ]; then
-     	OUTPUT=$(GO111MODULE=off go get -u "$1" 2>&1)
+        OUTPUT=$(GO111MODULE=off go get -u "${MODULE}" 2>&1)
     else
-     	OUTPUT=$(cd && GO111MODULE=on go get "$1@${VERSION}" 2>&1)
+        OUTPUT=$(cd && GO111MODULE=on go get "${MODULE}@${VERSION}" 2>&1)
     fi
     if [ $? != 0 ]; then
-        echo "error: executing \"go get $1\" failed : ${OUTPUT}"
+        echo "error: executing \"go get ${MODULE}\" failed : ${OUTPUT}"
         exit 1
     fi
 }
 
 install_go_module golang.org/x/lint/golint
 install_go_module golang.org/x/tools/cmd/stringer
-install_go_module github.com/go-swagger/go-swagger/cmd/swagger v0.25.0
+install_go_module github.com/go-swagger/go-swagger github.com/go-swagger/go-swagger/cmd/swagger
 install_go_module github.com/algorand/msgp
