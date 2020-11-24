8a31d881cbd1b63ee8a714e54b5b85dc07895c2a
Benjamin Toll <benjam72@yahoo.com>
2020-11-24 17:45:32 -0500

    Import public keys for sig test

--- scripts/release/mule/test/tests/pre/verify_signature.sh
+++ scripts/release/mule/test/tests/pre/verify_signature.sh
@@ -4,5 +4,8 @@ set -ex
 
 echo "[$0] Verifying gpg signatures"
 
+wget -O - https://releases.algorand.com/key.pub | gpg --import
+wget -O - https://releases.algorand.com/rpm/rpm_algorand.pub | gpg --import
+
 find tmp/node_pkgs -type f -name "*.sig" -exec gpg --verify {} \;
 
