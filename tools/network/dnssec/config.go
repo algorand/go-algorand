1b3284db987e7535c7a296a31ed2ffc690528952
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2020-12-04 11:08:10 -0500

    Get system DNS on Windows (#1747)
    Use GetNetworkParams to get list system DNS servers on Windows.

--- tools/network/dnssec/config.go
+++ tools/network/dnssec/config.go
@@ -17,12 +17,7 @@
 package dnssec
 
 import (
-	"fmt"
-	"io"
-	"os"
 	"time"
-
-	"github.com/miekg/dns"
 )
 
 // DefaultMaxHops sets max hops for DNS request
@@ -43,51 +38,15 @@ const maxTimeout = 5 * time.Second
 // Other - no DNSSEC - last check 2020-12-01
 // Alibaba 223.6.6.6:53
 
-// ResolverAddress is ip addr + port as string
-type ResolverAddress string
-
 // DefaultDnssecAwareNSServers is a list of known public DNSSEC-aware servers
 var DefaultDnssecAwareNSServers = []ResolverAddress{"1.1.1.1:53", "208.67.222.222:53", "8.8.8.8:53", "77.88.8.8:53", "8.26.56.26:53", "180.76.76.76:53"}
 
 const defaultConfigFile = "/etc/resolv.conf"
 
+// ResolverAddress is ip addr + port as string
+type ResolverAddress string
+
 // MakeResolverAddress creates a new ResolverAddress instance from address and port
 func MakeResolverAddress(addr, port string) ResolverAddress {
 	return ResolverAddress(addr + ":" + port)
-}
-
-// SystemConfig return list of servers and timeout from
-// This is Linux only.
-//
-// For Windows need to implement DNS servers retrieval from GetNetworkParams
-//  see https://docs.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getnetworkparams
-func SystemConfig() (servers []ResolverAddress, timeout time.Duration, err error) {
-	f, err := os.Open(defaultConfigFile)
-	defer f.Close()
-	if err != nil {
-		return
-	}
-	return systemConfig(f)
-}
-
-func systemConfig(configFile io.Reader) (servers []ResolverAddress, timeout time.Duration, err error) {
-	if configFile == nil {
-		err = fmt.Errorf("empty config reader")
-		return
-	}
-	cc, err := dns.ClientConfigFromReader(configFile)
-	if err != nil {
-		return
-	}
-	for _, addr := range cc.Servers {
-		servers = append(servers, MakeResolverAddress(addr, cc.Port))
-	}
-	timeout = DefaultTimeout
-	if cc.Timeout != 0 && len(servers) > 0 {
-		timeout = time.Duration(cc.Timeout) * time.Second
-	}
-	if timeout > maxTimeout {
-		timeout = maxTimeout
-	}
-	return
 }
