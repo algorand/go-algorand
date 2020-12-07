0b19b99519eed6db92b326a6859d06ea46ad353d
Tsachi Herman <tsachi.herman@algorand.com>
2020-12-07 16:30:51 -0500

    Add unit test

--- data/transactions/verify/txn_test.go
+++ data/transactions/verify/txn_test.go
@@ -17,6 +17,7 @@
 package verify
 
 import (
+	"context"
 	"math/rand"
 	"testing"
 
@@ -25,8 +26,10 @@ import (
 	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/crypto"
 	"github.com/algorand/go-algorand/data/basics"
+	"github.com/algorand/go-algorand/data/bookkeeping"
 	"github.com/algorand/go-algorand/data/transactions"
 	"github.com/algorand/go-algorand/protocol"
+	"github.com/algorand/go-algorand/util/execpool"
 )
 
 var feeSink = basics.Address{0x7, 0xda, 0xcb, 0x4b, 0x6d, 0x9e, 0xd1, 0x41, 0xb1, 0x75, 0x76, 0xbd, 0x45, 0x9a, 0xe6, 0x42, 0x1d, 0x48, 0x6d, 0xa3, 0xd4, 0xef, 0x22, 0x47, 0xc4, 0x9, 0xa3, 0x96, 0xb8, 0x2e, 0xa2, 0x21}
@@ -44,7 +47,7 @@ func keypair() *crypto.SignatureSecrets {
 	return s
 }
 
-func generateTestObjects(numTxs, numAccs int) ([]transactions.Transaction, []transactions.SignedTxn, []*crypto.SignatureSecrets, []basics.Address) {
+func generateTestObjects(numTxs, numAccs int, blockRound basics.Round) ([]transactions.Transaction, []transactions.SignedTxn, []*crypto.SignatureSecrets, []basics.Address) {
 	txs := make([]transactions.Transaction, numTxs)
 	signed := make([]transactions.SignedTxn, numTxs)
 	secrets := make([]*crypto.SignatureSecrets, numAccs)
@@ -56,22 +59,28 @@ func generateTestObjects(numTxs, numAccs int) ([]transactions.Transaction, []tra
 		secrets[i] = secret
 		addresses[i] = addr
 	}
-
+	var iss, exp int
 	for i := 0; i < numTxs; i++ {
 		s := rand.Intn(numAccs)
 		r := rand.Intn(numAccs)
 		a := rand.Intn(1000)
 		f := config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee + uint64(rand.Intn(10))
-		iss := 50 + rand.Intn(30)
-		exp := iss + 10
+		if blockRound == 0 {
+			iss = 50 + rand.Intn(30)
+			exp = iss + 10
+		} else {
+			iss = int(blockRound) / 2
+			exp = int(blockRound) + rand.Intn(30)
+		}
 
 		txs[i] = transactions.Transaction{
 			Type: protocol.PaymentTx,
 			Header: transactions.Header{
-				Sender:     addresses[s],
-				Fee:        basics.MicroAlgos{Raw: f},
-				FirstValid: basics.Round(iss),
-				LastValid:  basics.Round(exp),
+				Sender:      addresses[s],
+				Fee:         basics.MicroAlgos{Raw: f},
+				FirstValid:  basics.Round(iss),
+				LastValid:   basics.Round(exp),
+				GenesisHash: crypto.Hash([]byte{1, 2, 3, 4, 5}),
 			},
 			PaymentTxnFields: transactions.PaymentTxnFields{
 				Receiver: addresses[r],
@@ -87,7 +96,7 @@ func generateTestObjects(numTxs, numAccs int) ([]transactions.Transaction, []tra
 func TestSignedPayment(t *testing.T) {
 	proto := config.Consensus[protocol.ConsensusCurrentVersion]
 
-	payments, stxns, secrets, addrs := generateTestObjects(1, 1)
+	payments, stxns, secrets, addrs := generateTestObjects(1, 1, 0)
 	payment, stxn, secret, addr := payments[0], stxns[0], secrets[0], addrs[0]
 
 	require.NoError(t, payment.WellFormed(spec, proto), "generateTestObjects generated an invalid payment")
@@ -104,7 +113,7 @@ func TestSignedPayment(t *testing.T) {
 }
 
 func TestTxnValidationEncodeDecode(t *testing.T) {
-	_, signed, _, _ := generateTestObjects(100, 50)
+	_, signed, _, _ := generateTestObjects(100, 50, 0)
 
 	for _, txn := range signed {
 		if Txn(&txn, Context{Params: Params{CurrSpecAddrs: spec, CurrProto: protocol.ConsensusCurrentVersion}}) != nil {
@@ -122,7 +131,7 @@ func TestTxnValidationEncodeDecode(t *testing.T) {
 }
 
 func TestTxnValidationEmptySig(t *testing.T) {
-	_, signed, _, _ := generateTestObjects(100, 50)
+	_, signed, _, _ := generateTestObjects(100, 50, 0)
 
 	for _, txn := range signed {
 		if Txn(&txn, Context{Params: Params{CurrSpecAddrs: spec, CurrProto: protocol.ConsensusCurrentVersion}}) != nil {
@@ -207,4 +216,80 @@ func TestDecodeNil(t *testing.T) {
 		// This used to panic when run on a zero value of SignedTxn.
 		Txn(&st, Context{Params: Params{CurrSpecAddrs: spec, CurrProto: protocol.ConsensusCurrentVersion}})
 	}
+}
+
+func TestPaysetGroups(t *testing.T) {
+	_, signedTxn, _, _ := generateTestObjects(10000, 20, 50)
+	blk := bookkeeping.Block{
+		BlockHeader: bookkeeping.BlockHeader{
+			Round:       50,
+			GenesisHash: crypto.Hash([]byte{1, 2, 3, 4, 5}),
+			UpgradeState: bookkeeping.UpgradeState{
+				CurrentProtocol: protocol.ConsensusCurrentVersion,
+			},
+			RewardsState: bookkeeping.RewardsState{
+				FeeSink:     feeSink,
+				RewardsPool: poolAddr,
+			},
+		},
+	}
+	execPool := execpool.MakePool(t)
+	verificationPool := execpool.MakeBacklog(execPool, 64, execpool.LowPriority, t)
+	defer verificationPool.Shutdown()
+
+	// divide the transactions into transaction groups.
+	txnGroups := make([][]transactions.SignedTxn, 0, len(signedTxn))
+	for i := 0; i < len(signedTxn)-16; i++ {
+		txnPerGroup := rand.Intn(16)
+		txnGroups = append(txnGroups, signedTxn[i:i+txnPerGroup+1])
+		i += txnPerGroup
+	}
+
+	err := PaysetGroups(context.Background(), txnGroups, blk, verificationPool)
+	require.NoError(t, err)
+
+	// break the signature and see if it fails.
+	txnGroups[0][0].Sig[0] = txnGroups[0][0].Sig[0] + 1
+	err = PaysetGroups(context.Background(), txnGroups, blk, verificationPool)
+	require.Error(t, err)
+
+	// ensure the rest are fine
+	err = PaysetGroups(context.Background(), txnGroups[1:], blk, verificationPool)
+	require.NoError(t, err)
+}
+
+func BenchmarkPaysetGroups(b *testing.B) {
+	if b.N < 2000 {
+		b.N = 2000
+	}
+	_, signedTxn, _, _ := generateTestObjects(b.N, 20, 50)
+	blk := bookkeeping.Block{
+		BlockHeader: bookkeeping.BlockHeader{
+			Round:       50,
+			GenesisHash: crypto.Hash([]byte{1, 2, 3, 4, 5}),
+			UpgradeState: bookkeeping.UpgradeState{
+				CurrentProtocol: protocol.ConsensusCurrentVersion,
+			},
+			RewardsState: bookkeeping.RewardsState{
+				FeeSink:     feeSink,
+				RewardsPool: poolAddr,
+			},
+		},
+	}
+	execPool := execpool.MakePool(b)
+	verificationPool := execpool.MakeBacklog(execPool, 64, execpool.LowPriority, b)
+	defer verificationPool.Shutdown()
+
+	// divide the transactions into transaction groups.
+	txnGroups := make([][]transactions.SignedTxn, 0, len(signedTxn))
+	for i := 0; i < len(signedTxn)-16; i++ {
+		txnPerGroup := rand.Intn(16)
+		txnGroups = append(txnGroups, signedTxn[i:i+txnPerGroup+1])
+		i += txnPerGroup
+	}
+
+	b.ResetTimer()
+	err := PaysetGroups(context.Background(), txnGroups, blk, verificationPool)
+	require.NoError(b, err)
+	b.StopTimer()
 }
