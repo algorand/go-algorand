fc9b1d572f5117a9cfb50cd17698d6046c209470
aybehrouz <behrouz_ayati@yahoo.com>
2021-01-01 16:59:09 +0330

    Update instructions doc

--- tools/teal/converter/doc.go
+++ tools/teal/converter/doc.go
@@ -15,84 +15,138 @@
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
 /*
-   app_local_get
-       Opcode: 0x62 {uint8[0..0xFE] i: position in local memory segment}
-       Pops: ... stack, {uint64 A}
-       Pushes: any
-       read from account specified by Txn.Accounts[A] from local memory segment of the current application at position 'i' => value
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: account index. Return: value. The value is zero if the key or index does not exist.
+app_local_get
+	Opcode: 0x62 {uint8[0..0xFE] i: position in local memory segment}
+	Pops: ... stack, {uint64 A}
+	Pushes: any
+	read from account specified by Txn.Accounts[A] from local memory segment of the current application at position 'i' => value
+	LogicSigVersion >= 3
+	Mode: Application
+	params: account index. Return: value. The value is zero if the key or index does not exist.
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears the bytes constants block
 
-   app_local_get_ex
-       Opcode: 0x63 {uint8[0..0xFE] i: position in local memory segment}
-       Pops: ... stack, {uint64 A}, {uint64 B}
-       Pushes: uint64, any
-       read from account specified by Txn.Accounts[A] from local memory segment of the application B at position 'i' => {0 or 1 (top), value}
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: account index, application id. Return: did_exist flag (top of the stack, 1 if exist and 0 otherwise), value.
+Converts to:
+	byte []byte{i}
+	app_local_get
+------------------------------
 
-   app_global_get
-       Opcode: 0x64 {uint8[0..0xFE] i: position in global memory segment}
-       Pops: None
-       Pushes: any
-       read from global memory segment of a current application at position 'i' => value
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: none. Return: value. The value is zero if the index does not exist.
+app_local_get_ex
+	Opcode: 0x63 {uint8[0..0xFE] i: position in local memory segment}
+	Pops: ... stack, {uint64 A}, {uint64 B}
+	Pushes: uint64, any
+	read from account specified by Txn.Accounts[A] from local memory segment of the application B at position 'i' => {0 or 1 (top), value}
+	LogicSigVersion >= 3
+	Mode: Application
+	params: account index, application id. Return: did_exist flag (top of the stack, 1 if exist and 0 otherwise), value.
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears the bytes constants block
 
-   app_global_get_ex
-       Opcode: 0x65 {uint8[0..0xFE] i: position in global memory segment}
-       Pops: ... stack, {uint64 A}
-       Pushes: uint64, any
-       read from application Txn.ForeignApps[A] global memory segment at position 'i' => {0 or 1 (top), value}. A is specified as an account index in the ForeignApps field of the ApplicationCall transaction, zero index means this app
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: application index, state key. Return: did_exist flag (top of the stack, 1 if exist and 0 otherwise), value.
+Converts to:
+	byte []byte{i}
+	app_local_get_ex
+------------------------------
 
-   app_local_put
-       Opcode: 0x66 {uint8[0..0xFE] i: position in local memory segment}
-       Pops: ... stack, {uint64 A}, {[]byte B}, {any C}
-       Pushes: None
-       write to account specified by Txn.Accounts[A] to local memory segment of a current application key B at position 'i' with value C
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: account index, value.
+app_global_get
+	Opcode: 0x64 {uint8[0..0xFE] i: position in global memory segment}
+	Pops: None
+	Pushes: any
+	read from global memory segment of a current application at position 'i' => value
+	LogicSigVersion >= 3
+	Mode: Application
+	params: none. Return: value. The value is zero if the index does not exist.
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears the bytes constants block
 
-   app_global_put
-       Opcode: 0x67 {uint8[0..0xFE] i: position in global memory segment}
-       Pops: ... stack, {any B}
-       Pushes: None
-       write value B to global memory segment of the current application at position 'i'
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
+Converts to:
+	byte []byte{i}
+	app_global_get
+------------------------------
 
-   app_local_del
-       Opcode: 0x68 {uint8[0..0xFE] i: position in local memory segment}
-       Pops: ... stack, {uint64 A}
-       Pushes: None
-       delete from account specified by Txn.Accounts[A] local memory segment of the current application at position 'i'
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   params: account index
-   Deleting an index which is already absent has no effect on the application local state. (In particular, it does not cause the program to fail.)
+app_global_get_ex
+	Opcode: 0x65 {uint8[0..0xFE] i: position in global memory segment}
+	Pops: ... stack, {uint64 A}
+	Pushes: uint64, any
+	read from application Txn.ForeignApps[A] global memory segment at position 'i' => {0 or 1 (top), value}. A is specified as an account index in the ForeignApps field of the ApplicationCall transaction, zero index means this app
+	LogicSigVersion >= 3
+	Mode: Application
+	params: application index, state key. Return: did_exist flag (top of the stack, 1 if exist and 0 otherwise), value.
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears the bytes constants block
 
-   app_global_del {uint8[0..0xFE] i: position in global memory segment}
-       Opcode: 0x69
-       Pops: None
-       Pushes: None
-       delete position 'i' from a global state of the current application
-       LogicSigVersion >= 3
-       Mode: Application
-   'i' must be an index between [0, 0xFE] (inclusive)
-   Deleting an index which is already absent has no effect on the application global state. (In particular, it does not cause the program to fail.)
+Converts to:
+	byte []byte{i}
+	app_global_get_ex
+------------------------------
+
+app_local_put
+	Opcode: 0x66 {uint8[0..0xFE] i: position in local memory segment}
+	Pops: ... stack, {uint64 A}, {any C}
+	Pushes: None
+	write to account specified by Txn.Accounts[A] to local memory segment of a current application at position 'i' with value C
+	LogicSigVersion >= 3
+	Mode: Application
+	params: account index, value.
+
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears position 0xFF of scratch space and the bytes constants block
+
+Converts To:
+	store 255
+	byte []byte{i}
+	load 255
+	app_local_put
+------------------------------
+
+app_global_put
+	Opcode: 0x67 {uint8[0..0xFE] i: position in global memory segment}
+	Pops: ... stack, {any B}
+	Pushes: None
+	write value B to global memory segment of the current application at position 'i'
+	LogicSigVersion >= 3
+	Mode: Application
+'i' must be an index between [0, 0xFE] (inclusive)
+Note for version 3: this opcode clears position 0xFF of scratch space and the bytes constants block
+
+Converts to:
+	store 255
+	byte []byte{i}
+	load 255
+	app_global_put
+------------------------------
+
+app_local_del
+	Opcode: 0x68 {uint8[0..0xFE] i: position in local memory segment}
+	Pops: ... stack, {uint64 A}
+	Pushes: None
+	delete from account specified by Txn.Accounts[A] local memory segment of the current application at position 'i'
+	LogicSigVersion >= 3
+	Mode: Application
+	params: account index
+
+'i' must be an index between [0, 0xFE] (inclusive)
+Deleting an index which is already absent has no effect on the application local state. (In particular, it does not cause the program to fail.)
+Note for version 3: this opcode clears position 0xFF of scratch space and the bytes constants block
+
+Converts to:
+	byte []byte{i}
+	app_local_del
+------------------------------
+
+app_global_del
+	Opcode: 0x69 {uint8[0..0xFE] i: position in global memory segment}
+	Pops: None
+	Pushes: None
+	delete position 'i' from a global state of the current application
+	LogicSigVersion >= 3
+	Mode: Application
+
+'i' must be an index between [0, 0xFE] (inclusive)
+Deleting an index which is already absent has no effect on the application global state. (In particular, it does not cause the program to fail.)
+Note for version 3: this opcode clears the bytes constants block
+
+Converts to:
+	byte []byte{i}
+	app_global_del
 */
 package converter
