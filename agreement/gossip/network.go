b8b1360dd5805ed8b889656aaa1f79bb844e3919
Nicholas Guo <nicholas.guo@algorand.com>
2021-03-04 16:52:36 -0800

    fix test

--- agreement/gossip/network.go
+++ agreement/gossip/network.go
@@ -211,7 +211,6 @@ func (i *networkImpl) Disconnect(h agreement.MessageHandle) {
 // In test code we want to queue up a bunch of outbound packets and then see that they got through, so we need to wait at least a little bit for them to all go out.
 // Normal agreement state machine code uses GossipNode.Broadcast non-blocking and may drop outbound packets.
 func (i *networkImpl) broadcastTimeout(t protocol.Tag, data []byte, timeout time.Duration) error {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
 	return i.net.Broadcast(ctx, t, data, true, nil)
 }
