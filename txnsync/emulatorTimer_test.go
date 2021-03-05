866df6ac444fdfd38b06ade0fd6f1715042d1093
Tsachi Herman <tsachi.herman@algorand.com>
2021-03-05 15:41:24 -0500

    Avoid using deadlock mutex, and use a channel locking instead.

--- txnsync/emulatorTimer_test.go
+++ txnsync/emulatorTimer_test.go
@@ -20,7 +20,7 @@ import (
 	"sort"
 	"time"
 
-	"github.com/algorand/go-deadlock"
+	//"github.com/algorand/go-deadlock"
 
 	"github.com/algorand/go-algorand/util/timers"
 )
@@ -31,22 +31,24 @@ type guidedClock struct {
 	adv      time.Duration
 	timers   map[time.Duration]chan time.Time
 	children []*guidedClock
-	mu       deadlock.Mutex
+	lockCh   chan struct{}
 }
 
 func makeGuidedClock() *guidedClock {
 	return &guidedClock{
-		zero: time.Now(),
+		zero:   time.Now(),
+		lockCh: make(chan struct{}, 1),
 	}
 }
 func (g *guidedClock) Zero() timers.Clock {
 	// the real monotonic clock doesn't return the same clock object, which is fine.. but for our testing
 	// we want to keep the same clock object so that we can tweak with it.
 	child := &guidedClock{
-		zero: g.zero.Add(g.adv),
+		zero:   g.zero.Add(g.adv),
+		lockCh: make(chan struct{}, 1),
 	}
-	g.mu.Lock()
-	defer g.mu.Unlock()
+	g.lock()
+	defer g.unlock()
 	g.children = append(g.children, child)
 	return child
 }
@@ -57,8 +59,8 @@ func (g *guidedClock) TimeoutAt(delta time.Duration) <-chan time.Time {
 		close(c)
 		return c
 	}
-	g.mu.Lock()
-	defer g.mu.Unlock()
+	g.lock()
+	defer g.unlock()
 	if g.timers == nil {
 		g.timers = make(map[time.Duration]chan time.Time)
 	}
@@ -94,7 +96,7 @@ func (g *guidedClock) Advance(adv time.Duration) {
 		ch       chan time.Time
 	}
 	expiredClocks := []entryStruct{}
-	g.mu.Lock()
+	g.lock()
 	// find all the expired clocks.
 	for delta, ch := range g.timers {
 		if delta < g.adv {
@@ -109,14 +111,22 @@ func (g *guidedClock) Advance(adv time.Duration) {
 	for _, entry := range expiredClocks {
 		delete(g.timers, entry.duration)
 	}
-	g.mu.Unlock()
+	g.unlock()
 	// fire expired clocks
 	for _, entry := range expiredClocks {
 		entry.ch <- g.zero.Add(g.adv)
 	}
-	g.mu.Lock()
-	defer g.mu.Unlock()
+	g.lock()
+	defer g.unlock()
 	for _, child := range g.children {
 		child.Advance(adv)
 	}
+}
+
+func (g *guidedClock) lock() {
+	g.lockCh <- struct{}{}
+}
+
+func (g *guidedClock) unlock() {
+	<-g.lockCh
 }
