a42275aac0156b6ddf2c4c81c31a0e8d14d92d01
Mauro Leggieri <mxmauro@mauroleggieri.com>
2021-03-10 14:32:45 -0300

    Improved process tree kill in Windows

--- util/process_windows.go
+++ util/process_windows.go
@@ -30,7 +30,15 @@ import (
 const (
 	ERROR_INVALID_PARAMETER = syscall.Errno(87)
 
+	STATUS_CANCELLED = uint32(0xC0000120)
+
 	processTerminateWaitInMs = 1000
+
+	killChildsPassCount = 4
+)
+
+var (
+	errFinishedProcess = errors.New("os: process already finished")
 )
 
 // FindProcess looks for a running process by its pid
@@ -39,15 +47,8 @@ func FindProcess(pid int) (*os.Process, error) {
 
 	process, err := os.FindProcess(pid)
 	if err != nil {
-		// NOTE: Unlike Unix, Windows tries to open the target process in order to kill it.
-		//       ERROR_INVALID_PARAMETER is returned if the process does not exists.
-		//       To mimic other OS behavior, if the process does not exist, don't return an error
-		var syscallError syscall.Errno
-
-		if errors.As(err, &syscallError) {
-			if syscallError == ERROR_INVALID_PARAMETER {
-				return nil, nil
-			}
+		if isInvalidParameterError(err) { // NOTE: See function definition for details
+			return nil, nil
 		}
 		return nil, err
 	}
@@ -63,6 +64,10 @@ func FindProcess(pid int) (*os.Process, error) {
 		if e2 == nil && ret == syscall.WAIT_OBJECT_0 {
 			return nil, nil
 		}
+	} else {
+		if isInvalidParameterError(err) { // NOTE: See function definition for details
+			return nil, nil
+		}
 	}
 
 	return process, nil
@@ -70,72 +75,105 @@ func FindProcess(pid int) (*os.Process, error) {
 
 // KillProcess kills a running OS process
 func KillProcess(pid int, signal os.Signal) error {
-	p, err := FindProcess(pid)
-	if err == nil {
-		if p != nil {
-			if signal != syscall.Signal(0) {
-				for _, v := range getChildrenProcesses(pid) {
-					err2 := v.Kill()
-					if err2 == nil {
-						waitUntilProcessEnds(v.Pid)
-					}
-				}
-
-				err = p.Kill()
-				waitUntilProcessEnds(p.Pid)
-			}
-		} else {
-			// Signal(0) only checks if we have access to kill a process and if it is really dead
-			if p != nil {
-				var h syscall.Handle
-
-				h, err = syscall.OpenProcess(syscall.SYNCHRONIZE|syscall.PROCESS_TERMINATE, false, uint32(pid))
-				if err == nil {
-					ret, e2 := syscall.WaitForSingleObject(h, 0)
-					if e2 == nil && ret == syscall.WAIT_OBJECT_0 {
-						err = errors.New("os: process already finished")
-					}
-					_ = syscall.CloseHandle(h)
-				}
-			} else {
-				err = errors.New("os: process already finished")
-			}
-		}
+	// Signal(0) only checks if we have access to kill a process and if it is really dead
+	if signal == syscall.Signal(0) {
+		return isProcessAlive(pid)
 	}
+
+	return killProcessTree(pid)
+}
+
+func isProcessAlive(pid int) error {
+	var ret uint32
+
+	h, err := syscall.OpenProcess(syscall.SYNCHRONIZE|syscall.PROCESS_TERMINATE, false, uint32(pid))
+	if err != nil {
+		if isInvalidParameterError(err) { // NOTE: See function definition for details
+			return errFinishedProcess
+		}
+		return err
+	}
+	ret, err = syscall.WaitForSingleObject(h, 0)
+	if err == nil && ret == syscall.WAIT_OBJECT_0 {
+		err = errFinishedProcess
+	}
+
+	_ = syscall.CloseHandle(h)
 	return err
 }
 
-func getChildrenProcesses(parentPid int) []*os.Process {
-	out := []*os.Process{}
-	snap, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, uint32(0))
-	if err == nil {
-		var pe32 windows.ProcessEntry32
+func killProcessTree(pid int) error {
+	err := killProcess(pid)
+	if err != nil {
+		return err
+	}
 
-		defer windows.CloseHandle(snap)
-
-		pe32.Size = uint32(unsafe.Sizeof(pe32))
-		if err := windows.Process32First(snap, &pe32); err == nil {
-			for {
-				if pe32.ParentProcessID == uint32(parentPid) {
-					p, err := FindProcess(int(pe32.ProcessID))
-					if err == nil && p != nil {
-						out = append(out, p)
-					}
-				}
-				if err = windows.Process32Next(snap, &pe32); err != nil {
-					break
-				}
-			}
+	// We do several passes just in case the process being killed spawns a new one
+	for pass := 1; pass <= killChildsPassCount; pass++ {
+		childProcessList := getChildProcesses(pid)
+		if len(childProcessList) == 0 {
+			break
+		}
+		for _, childPid := range childProcessList {
+			killProcessTree(childPid)
 		}
 	}
+
+	return nil
+}
+
+func getChildProcesses(pid int) []int {
+	var pe32 windows.ProcessEntry32
+
+	out := make([]int, 0)
+
+	snap, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, uint32(0))
+	if err != nil {
+		return out
+	}
+
+	defer func() {
+		_ = windows.CloseHandle(snap)
+	}()
+
+	pe32.Size = uint32(unsafe.Sizeof(pe32))
+	err = windows.Process32First(snap, &pe32)
+	for err != nil {
+		if pe32.ParentProcessID == uint32(pid) {
+			// Add to list
+			out = append(out, int(pe32.ProcessID))
+		}
+
+		err = windows.Process32Next(snap, &pe32)
+	}
+
 	return out
 }
 
-func waitUntilProcessEnds(pid int) {
-	h, err := syscall.OpenProcess(syscall.SYNCHRONIZE, false, uint32(pid))
+func killProcess(pid int) error {
+	h, err := syscall.OpenProcess(syscall.SYNCHRONIZE | syscall.PROCESS_TERMINATE, false, uint32(pid))
 	if err == nil {
-		_, _ = syscall.WaitForSingleObject(h, processTerminateWaitInMs)
+		err = syscall.TerminateProcess(h, STATUS_CANCELLED)
+		if err == nil {
+			_, _ = syscall.WaitForSingleObject(h, processTerminateWaitInMs)
+		}
 
 		_ = syscall.CloseHandle(h)
 	}
+
+	return err
+}
+
+// NOTE: Unlike Unix, Windows tries to open the target process in order to kill it.
+//       ERROR_INVALID_PARAMETER is returned if the process does not exists.
+//       To mimic other OS behavior, if the process does not exist, don't return an error
+func isInvalidParameterError(err error) bool {
+	var syscallError syscall.Errno
+
+	if errors.As(err, &syscallError) {
+		if syscallError == ERROR_INVALID_PARAMETER {
+			return true
+		}
+	}
+	return false
 }
