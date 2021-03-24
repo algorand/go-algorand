c7821892e113fd3a4cc82e9c145d78aad2e77471
algonautshant <shant@algorand.com>
2021-03-24 10:14:04 -0400

    Fix lint add comment to exported function

--- rpcs/blockService.go
+++ rpcs/blockService.go
@@ -397,6 +397,7 @@ func RawBlockBytes(l *data.Ledger, round basics.Round) ([]byte, error) {
 	}), nil
 }
 
+// FormatBlockQuery formats a block request query for the given network and round number
 func FormatBlockQuery(round uint64, parsedURL string, net network.GossipNode) string {
 	return net.SubstituteGenesisID(path.Join(parsedURL, "/v1/{genesisID}/block/"+strconv.FormatUint(uint64(round), 36)))
 }
