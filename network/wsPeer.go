05ff833a2653188e9c4af4c0b1ca061626e65f67
Nicholas Guo <nicholas.guo@algorand.com>
2021-03-29 11:57:16 -0700

    temp remove skip received

--- network/wsPeer.go
+++ network/wsPeer.go
@@ -604,10 +604,10 @@ func (wp *wsPeer) writeLoopSend(msgs sendMessages) disconnectReason {
 		default:
 		}
 
-		if len(msg.data) > 2 && msgToTrack(protocol.Tag(msg.data[:2])) && wp.receiveMsgTracker.exists(crypto.Hash(msg.data[2:])) {
-			numSkippedReceiver++
-			continue
-		}
+		//if len(msg.data) > 2 && msgToTrack(protocol.Tag(msg.data[:2])) && wp.receiveMsgTracker.exists(crypto.Hash(msg.data[2:])) {
+		//	numSkippedReceiver++
+		//	continue
+		//}
 
 		if len(msg.data) > 2 && wp.sendMsgTracker.exists(crypto.Hash(msg.data[2:])) {
 			numSkippedSender++
