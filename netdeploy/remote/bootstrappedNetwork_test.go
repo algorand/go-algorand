041e1f92d9c190bdc6d6c78b1dd04ef19b8ec03b
shiqizng <80276844+shiqizng@users.noreply.github.com>
2021-04-14 23:18:36 -0400

    netgoal: add pre-generated network support (#2046)
    This PR creates the database files for a given network. It meant to emulate a mature running network.

---
+++ netdeploy/remote/bootstrappedNetwork_test.go
@@ -0,0 +1,49 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package remote
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadBootstrappedData(t *testing.T) {
+	badSpecPath := filepath.Join("./../../test", "testdata/deployednettemplates/networks/bootstrapped/badSpec.json")
+	_, err := LoadBootstrappedData(badSpecPath)
+	require.NotEqual(t, nil, err)
+
+	okSpecPath := filepath.Join("./../../test", "testdata/deployednettemplates/networks/bootstrapped/okSpec.json")
+	var data BootstrappedNetwork
+	data, err = LoadBootstrappedData(okSpecPath)
+	expected := BootstrappedNetwork{
+		NumRounds:                 65000,
+		RoundTransactionsCount:    1000,
+		GeneratedAccountsCount:    7000000,
+		GeneratedAssetsCount:      200000,
+		GeneratedApplicationCount: 1000000,
+		SourceWalletName:          "wallet1",
+	}
+	require.Equal(t, nil, err)
+	require.Equal(t, data.NumRounds, expected.NumRounds)
+	require.Equal(t, data.RoundTransactionsCount, expected.RoundTransactionsCount)
+	require.Equal(t, data.GeneratedAccountsCount, expected.GeneratedAccountsCount)
+	require.Equal(t, data.GeneratedAssetsCount, expected.GeneratedAssetsCount)
+	require.Equal(t, data.GeneratedApplicationCount, expected.GeneratedApplicationCount)
+	require.Equal(t, data.SourceWalletName, expected.SourceWalletName)
+}
