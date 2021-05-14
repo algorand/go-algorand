ad2ab985fe21e798cc319d58b04baccb27eb95b7
Will Winder <wwinder.unh@gmail.com>
2021-05-14 09:32:16 -0400

    Split e2e rest tests to avoid endlessly increasing the timeout over time.

---
+++ test/scripts/e2e_subs/rest-proof-endpoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# TIMEOUT=300
+
+my_dir="$(dirname "$0")"
+#"$my_dir/rest.sh" "$@"
+source "$my_dir/rest.sh" "$@"
+
+date "+$0 start %Y%m%d_%H%M%S"
+
+NUM_TRANSACTIONS=0
+
+# Create a transaction with no siblings
+while [[ "${NUM_TRANSACTIONS}" != "1" ]]; do
+  SEND_OUTPUT=$(${gcmd} clerk send -a 0 -f "${ACCOUNT}" -t "${ACCOUNT}")
+  TXID=$(echo "$SEND_OUTPUT" | head -n 1 | sed 's/.*transaction ID: \([^.]*\).*/\1/')
+  ROUND=$(echo "$SEND_OUTPUT" | tail -n 1 | sed 's/.*committed in round \([[:digit:]]*\).*/\1/')
+
+  # check if the transaction was all alone in the round
+  call_and_verify "Checking block" "/v2/blocks/${ROUND}" 200 'txns'
+  #TODO: The check with jq can be re-enabled after fixing JSONStrictHandle.
+  #NUM_TRANSACTIONS=$(cat "${TEMPDIR}/curl_out.txt" | jq '.block.txns | length')
+  NUM_TRANSACTIONS=$(cat "${TEMPDIR}/curl_out.txt" | grep type | wc -l | tr -d ' ')
+done
+
+call_and_verify "The proof should not be null." "/v2/blocks/${ROUND}/transactions/${TXID}/proof" 200 '"proof":""'
--- test/scripts/e2e_subs/rest.sh
+++ test/scripts/e2e_subs/rest.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-# TIMEOUT=450
+# TIMEOUT=50
 
-date '+rest.sh start %Y%m%d_%H%M%S'
+# Helpers for REST API tests.
+# Use the following boilerplate code at the top of new REST tests:
+
+#    #!/usr/bin/env bash
+#    # TIMEOUT=300
+#    
+#    my_dir="$(dirname "$0")"
+#    #"$my_dir/rest.sh" "$@"
+#    source "$my_dir/rest.sh" "$@"
+#    
+#    date "+$0 start %Y%m%d_%H%M%S"
 
 set -ex
 set -o pipefail
@@ -63,115 +73,3 @@ function call_and_verify {
     fail_and_exit "$1" "$2" "unexpected response. should contain '$4', actual: $RES"
   fi
 }
-
-
-function test_applications_endpoint {
-  # Create an application
-  printf '#pragma version 2\nint 1' > "${TEMPDIR}/simple.teal"
-  APPID=$(${gcmd} app create --creator "${ACCOUNT}" --approval-prog "${TEMPDIR}/simple.teal" --clear-prog "${TEMPDIR}/simple.teal" --global-byteslices 0 --global-ints 2 --local-byteslices 0 --local-ints 0 | grep Created | awk '{ print $6 }')
-
-  # Good request, non-existant app id
-  call_and_verify "Should not find app." "/v2/applications/987654321" 404 'application does not exist'
-  # Good request
-  call_and_verify "Should contain app data." "/v2/applications/$APPID" 200 '"global-state-schema":{"num-byte-slice":0,"num-uint":2}'
-  # Good request, pretty response
-  call_and_verify "Should contain app data." "/v2/applications/$APPID?pretty" 200 '
-    "global-state-schema": {
-      "num-byte-slice": 0,
-      "num-uint": 2
-    },
-    "local-state-schema": {
-      "num-byte-slice": 0,
-      "num-uint": 0
-    }
-'
-  # Some invalid path parameters
-  call_and_verify "App parameter parsing error 1." "/v2/applications/-2" 400 "Invalid format for parameter application-id"
-  call_and_verify "App parameter parsing error 2." "/v2/applications/not-a-number" 400 "Invalid format for parameter application-id"
-
-  # Good request, but invalid query parameters
-  call_and_verify "App invalid parameter" "/v2/applications/$APPID?this-should-fail=200" 400 'Unknown parameter detected: this-should-fail'
-}
-
-
-function test_assets_endpoint {
-  local ASSET_ID
-  ASSET_ID=$(${gcmd} asset create --creator "${ACCOUNT}" --total 10000 --decimals 19 --name "spanish coin" --unitname "doubloon" | grep "Created asset with asset index" | rev | cut -d ' ' -f 1 | rev)
-
-  # Good request, non-existant asset id
-  call_and_verify "Should not find asset." "/v2/assets/987654321" 404 'asset does not exist'
-  # Good request
-  call_and_verify "Should contain asset data." "/v2/assets/$ASSET_ID" 200 '","decimals":19,"default-frozen":false,"freeze":"'
-  # Good request, pretty response
-  call_and_verify "Should contain asset data." "/v2/assets/$ASSET_ID?pretty" 200 '
-    "decimals": 19,
-    "default-frozen": false,
-    "freeze": "'
-  # Some invalid path parameters
-  call_and_verify "Asset parameter parsing error 1." "/v2/assets/-2" 400 "Invalid format for parameter asset-id"
-  call_and_verify "Asset parameter parsing error 2." "/v2/assets/not-a-number" 400 "Invalid format for parameter asset-id"
-
-  # Good request, but invalid query parameters
-  call_and_verify "Asset invalid parameter" "/v2/assets/$ASSET_ID?this-should-fail=200" 400 'parameter detected: this-should-fail'
-}
-
-function pprof_test {
-  # URL Auth - valid
-  CODE=$(curl -o "${TEMPDIR}/curl_out.txt" -w "%{http_code}" -q -s "$PRIMARY_NET/urlAuth/$PRIMARY_ADMIN_TOKEN/debug/pprof/block")
-  if [[ "$CODE" != "200" ]]; then
-    fail_and_exit "Call pprof with valid token" "/urlAuth/:token/debug/pprof" "Invalid exit code expected 200 (actual $CODE)"
-  fi
-
-  # URL Auth - invalid
-  CODE=$(curl -o "${TEMPDIR}/curl_out.txt" -w "%{http_code}" -q -s "$PRIMARY_NET/urlAuth/invalid_token/debug/pprof/block")
-  if [[ "$CODE" != "401" ]]; then
-    fail_and_exit "Call pprof with invalid token" "/urlAuth/invalid_token/debug/pprof" "Invalid exit code expected 401 (actual $CODE)"
-  fi
-
-  # Header Auth - valid
-  CODE=$(curl -o "${TEMPDIR}/curl_out.txt" -w "%{http_code}" -q -s "$PRIMARY_NET/debug/pprof/block" -H "Authorization: Bearer $PRIMARY_ADMIN_TOKEN")
-  if [[ "$CODE" != "200" ]]; then
-    fail_and_exit "Call pprof with valid token" "/debug/pprof" "Invalid exit code expected 200 (actual $CODE)"
-  fi
-
-  # Header Auth - invalid
-  CODE=$(curl -o "${TEMPDIR}/curl_out.txt" -w "%{http_code}" -q -s "$PRIMARY_NET/debug/pprof/block" -H "Authorization: Bearer invalid_token")
-  if [[ "$CODE" != "401" ]]; then
-    fail_and_exit "Call pprof with invalid token" "/debug/pprof" "Invalid exit code expected 401 (actual $CODE)"
-  fi
-}
-
-function test_genesis_endpoint {
-  call_and_verify "There should be a genesis endpoint." "/genesis" 200 '
-  "id": "v1",
-  "network": "tbd",
-  "proto": "future",
-  "rwd": "7777777777777777777777777777777777777777777777777774MSJUVU"
-}'
-}
-
-function test_proof {
-  NUM_TRANSACTIONS=0
-
-  # Create a transaction with no siblings
-  while [[ "${NUM_TRANSACTIONS}" != "1" ]]; do
-    SEND_OUTPUT=$(${gcmd} clerk send -a 0 -f "${ACCOUNT}" -t "${ACCOUNT}")
-    TXID=$(echo "$SEND_OUTPUT" | head -n 1 | sed 's/.*transaction ID: \([^.]*\).*/\1/')
-    ROUND=$(echo "$SEND_OUTPUT" | tail -n 1 | sed 's/.*committed in round \([[:digit:]]*\).*/\1/')
-
-    # check if the transaction was all alone in the round
-    call_and_verify "Checking block" "/v2/blocks/${ROUND}" 200 'txns'
-    #TODO: The check with jq can be re-enabled after fixing JSONStrictHandle.
-    #NUM_TRANSACTIONS=$(cat "${TEMPDIR}/curl_out.txt" | jq '.block.txns | length')
-    NUM_TRANSACTIONS=$(cat "${TEMPDIR}/curl_out.txt" | grep type | wc -l | tr -d ' ')
-  done
-
-  call_and_verify "The proof should not be null." "/v2/blocks/${ROUND}/transactions/${TXID}/proof" 200 '"proof":""'
-}
-
-# Run the tests.
-test_applications_endpoint
-test_assets_endpoint
-pprof_test
-test_genesis_endpoint
-test_proof
