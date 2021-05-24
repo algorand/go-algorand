83cefb975035cde4c3416e4815b37c7aa9288bc2
Will Winder <wwinder.unh@gmail.com>
2021-05-24 10:04:17 -0400

    Not sure where to put this...

--- scripts/configure_dev.sh
+++ scripts/configure_dev.sh
@@ -40,6 +40,27 @@ function install_or_upgrade {
     fi
 }
 
+function install_go_module {
+    local OUTPUT
+    local MODULE
+    if [[ "$2" != "" ]]; then
+        MODULE=$2
+    else
+        MODULE=$1
+    fi
+    # Check for version to go.mod version
+    VERSION=$(get_go_version "$1")
+    if [ -z "$VERSION" ]; then
+        OUTPUT=$(GO111MODULE=off go get -u "${MODULE}" 2>&1)
+    else
+        OUTPUT=$(cd && GO111MODULE=on go get "${MODULE}@${VERSION}" 2>&1)
+    fi
+    if [ $? != 0 ]; then
+        echo "error: executing \"go get ${MODULE}\" failed : ${OUTPUT}"
+        exit 1
+    fi
+}
+
 function install_windows_shellcheck() {
     version="v0.7.1"
     if ! wget https://github.com/koalaman/shellcheck/releases/download/$version/shellcheck-$version.zip -O /tmp/shellcheck-$version.zip; then
@@ -92,3 +113,5 @@ elif [ "${OS}" = "windows" ]; then
         exit 1
     fi
 fi
+
+install_go_module gotest.tools/gotestsum
