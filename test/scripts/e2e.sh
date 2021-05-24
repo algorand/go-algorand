1f7985e9e5ee9df154a4ca62c0a55fdde67b06c1
Will Winder <wwinder.unh@gmail.com>
2021-05-24 09:33:51 -0400

    Print intermediate e2e.sh durations.

--- test/scripts/e2e.sh
+++ test/scripts/e2e.sh
@@ -69,6 +69,13 @@ function reset_dirs() {
     mkdir -p ${DATADIR}
 }
 
+# $1 - Start time in seconds
+# $2 - Message
+function duration() {
+  ELAPSED=$((SECONDS - $1))
+  printf '%s: %02dh:%02dm:%02ds\n' "$2" $(($ELAPSED/3600)) $(($ELAPSED%3600/60)) $(($ELAPSED%60))
+}
+
 #----------------------
 # Start E2E tests by installing the current build after killing all instances
 reset_dirs
@@ -93,19 +100,31 @@ export GOPATH=$(go env GOPATH)
 # Change current directory to test/scripts so we can just use ./test.sh to exec.
 cd "${SCRIPT_PATH}"
 
+E2E_BASIC_START_STOP=$SECONDS
 ./timeout 200 ./e2e_basic_start_stop.sh
+duration "$E2E_BASIC_START_STOP" "Run duration e2e_basic_start_stop.sh"
 
 python3 -m venv "${TEMPDIR}/ve"
 . "${TEMPDIR}/ve/bin/activate"
 "${TEMPDIR}/ve/bin/pip3" install --upgrade pip
 "${TEMPDIR}/ve/bin/pip3" install --upgrade py-algorand-sdk cryptography
+
+E2E_CLIENT_RUNNER_PARALLEL=$SECONDS
 "${TEMPDIR}/ve/bin/python3" e2e_client_runner.py ${RUN_KMD_WITH_UNSAFE_SCRYPT} "$SRCROOT"/test/scripts/e2e_subs/*.sh
+duration "$E2E_CLIENT_RUNNER_PARALLEL" "Run duration parallel client runner (e2e_subs/*.sh)"
+
+E2E_CLIENT_RUNNER_VDIR=$SECONDS
 for vdir in "$SRCROOT"/test/scripts/e2e_subs/v??; do
     "${TEMPDIR}/ve/bin/python3" e2e_client_runner.py ${RUN_KMD_WITH_UNSAFE_SCRYPT} --version "$(basename "$vdir")" "$vdir"/*.sh
 done
+duration "$E2E_CLIENT_RUNNER_VDIR" "Run duration vdir client runners (e2e_subs/v??)"
+
+E2E_CLIENT_RUNNER_SERIAL=$SECONDS
 for script in "$SRCROOT"/test/scripts/e2e_subs/serial/*; do
     "${TEMPDIR}/ve/bin/python3" e2e_client_runner.py ${RUN_KMD_WITH_UNSAFE_SCRYPT} $script
 done
+duration "$E2E_CLIENT_RUNNER_SERIAL" "Run duration serial client runners (e2e_subs/serial/*)"
+
 deactivate
 
 # Export our root temp folder as 'TESTDIR' for tests to use as their root test folder
@@ -114,7 +133,9 @@ export TESTDIR=${TEMPDIR}
 export TESTDATADIR=${SRCROOT}/test/testdata
 export SRCROOT=${SRCROOT}
 
+E2E_GO_TESTS=$SECONDS
 ./e2e_go_tests.sh ${GO_TEST_ARGS}
+duration "$E2E_GO_TESTS" "Run duration e2e_go_tests.sh"
 
 rm -rf "${TEMPDIR}"
 
