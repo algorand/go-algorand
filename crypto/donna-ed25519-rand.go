0305f75f382730c2e526ff7c90346baf729b6c49
algoidan <f>
2021-05-26 15:12:11 +0300

    we embed ed25519 donna C code into a go file using cgo so there is no need to build it externally.

---
+++ crypto/donna-ed25519-rand.go
@@ -0,0 +1,31 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+package crypto
+
+// #cgo CFLAGS: -I${SRCDIR}/ed25519-donna/ -Werror -Wall -std=c99 -Wno-incompatible-pointer-types-discards-qualifiers -m64 -O3 -DED25519_REFHASH -DED25519_CUSTOMRANDOM -Wno-macro-redefined
+import "C"
+import "unsafe"
+
+//export ed25519_randombytes_unsafe
+func ed25519_randombytes_unsafe(p unsafe.Pointer, len C.size_t) {
+	randLen := int(len)
+	rand := make([]byte, randLen)
+	RandBytes(rand)
+	for i := 0; i < randLen; i++ {
+		*(*byte)(unsafe.Pointer(uintptr(p) + uintptr(i*C.sizeof_uchar))) = rand[i]
+	}
+
+}
