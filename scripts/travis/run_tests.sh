9705f017aae6da4341f587ae199d63ee1e6d9d6d
Will Winder <wwinder.unh@gmail.com>
2021-06-01 14:27:34 -0400

    Switch back to shorttest.

--- scripts/travis/run_tests.sh
+++ scripts/travis/run_tests.sh
@@ -22,6 +22,6 @@ else
     if [[ "${OS}" != "darwin" ]]; then
         # setting it to 1 disable parallel making. This is done specicifically for travis, as travis seems to
         # have memory limitations and setting this to 1 could reduce the likelihood of hitting these.
-	    make test -j1
+	    make shorttest -j1
     fi
 fi
