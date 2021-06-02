c51ee20bc750ff8ad6daa9702d83a238c57e2230
Will Winder <wwinder.unh@gmail.com>
2021-06-02 10:20:03 -0400

    Maybe this is what MacOS doesn't like...

--- scripts/travis/build_test.sh
+++ scripts/travis/build_test.sh
@@ -15,7 +15,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ "${USER}" = "travis" ]; then
     # we're running on a travis machine
-    "${SCRIPTPATH}/travis_wait.sh" 120 "${SCRIPTPATH}/build.sh" --make_debug
+    "${SCRIPTPATH}/build.sh" --make_debug
     "${SCRIPTPATH}/travis_wait.sh" 120 "${SCRIPTPATH}/test.sh"
 else
     # we're running on an ephermal build machine
