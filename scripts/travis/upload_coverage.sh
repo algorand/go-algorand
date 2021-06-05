267031f19cf58bd46ce7e255fd7dccdf6cd49c46
Will Winder <wwinder.unh@gmail.com>
2021-06-05 13:17:35 -0400

    Travis doesn't have --ignore-missing.

--- scripts/travis/upload_coverage.sh
+++ scripts/travis/upload_coverage.sh
@@ -14,8 +14,12 @@ curl -fLso codecov https://codecov.io/bash
 VERSION=$(grep -o 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2)
 for i in 1 256 512
 do
-  curl -so sum "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM"
-  shasum -a $i -c --ignore-missing sum
+  curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM" | grep codecov > sum
+  if [[ $(cat sum) != *"codecov" ]]; then
+    echo "sum not found."
+    exit 1
+  fi
+  shasum -a $i -c sum
 done
 rm sum
 
