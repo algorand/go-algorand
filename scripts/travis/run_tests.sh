53ce2959275b9495ca2b7fcfc3b60852682a4016
Will Winder <wwinder.unh@gmail.com>
2021-06-10 10:06:01 -0400

    Enable fulltest for PRs.

--- scripts/travis/run_tests.sh
+++ scripts/travis/run_tests.sh
@@ -14,14 +14,10 @@ if [ "${BUILD_TYPE}" = "integration" ]; then
     fi
     export SHORTTEST 
     make integration
-elif [ "${TRAVIS_EVENT_TYPE}" = "cron" ] || [[ "${TRAVIS_BRANCH}" =~ ^rel/ ]]; then
-    if [[ "${OS}" != "darwin" ]]; then
-	    make fulltest -j2
-    fi
 else
     if [[ "${OS}" != "darwin" ]]; then
         # setting it to 1 disable parallel making. This is done specicifically for travis, as travis seems to
         # have memory limitations and setting this to 1 could reduce the likelihood of hitting these.
-	    make shorttest -j1
+	    make fulltest -j1
     fi
 fi
