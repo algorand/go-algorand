89981d066080bda4cfd53f30c8f25ef290fb866f
algoidan <f>
2021-07-04 15:02:32 +0300

    adding the batch verification object + tests

---
+++ crypto/batchverifier.go
@@ -0,0 +1,82 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package crypto
+
+// BatchVerifier enqueues signatures to be validated in batch.
+type BatchVerifier struct {
+	messages   [][]byte            // contains a slice of messages to be hashed. Each message is varible length
+	publicKeys []SignatureVerifier // contains a slice of public keys. Each individual public key is 32 bytes.
+	signatures []Signature         // contains a slice of signatures keys. Each individual signature is 64 bytes.
+}
+
+const minBatchVerifierAlloc = 16
+
+// MakeBatchVerifier create a BatchVerifier instance, and initialize it using the provided hint.
+func MakeBatchVerifier(hint int) *BatchVerifier {
+	// preallocate enough storage for the expected usage. We will reallocate as needed.
+	if hint < minBatchVerifierAlloc {
+		hint = minBatchVerifierAlloc
+	}
+	return &BatchVerifier{
+		messages:   make([][]byte, 0, hint),
+		publicKeys: make([]SignatureVerifier, 0, hint),
+		signatures: make([]Signature, 0, hint),
+	}
+}
+
+// EnqueueSignature enqueues a signature to be enqueued
+func (b *BatchVerifier) EnqueueSignature(sigVerifier SignatureVerifier, message Hashable, sig Signature) {
+	b.enqueueRaw(sigVerifier, hashRep(message), sig)
+}
+
+func (b *BatchVerifier) enqueueRaw(sigVerifier SignatureVerifier, message []byte, sig Signature) {
+	b.messages = append(b.messages, message)
+	b.publicKeys = append(b.publicKeys, sigVerifier)
+	b.signatures = append(b.signatures, sig)
+}
+
+func (b *BatchVerifier) expand() {
+	messages := make([][]byte, len(b.messages), len(b.messages)*2)
+	publicKeys := make([]SignatureVerifier, len(b.publicKeys), len(b.publicKeys)*2)
+	signatures := make([]Signature, len(b.signatures), len(b.signatures)*2)
+	copy(messages, b.messages)
+	copy(publicKeys, b.publicKeys)
+	copy(signatures, b.signatures)
+	b.messages = messages
+	b.publicKeys = publicKeys
+	b.signatures = signatures
+}
+
+// GetNumberOfEnqueuedSignatures returns the number of signatures current enqueue onto the bacth verifier object
+func (b *BatchVerifier) GetNumberOfEnqueuedSignatures() int {
+	return len(b.messages)
+}
+
+// Verify verifies that all the signatures are valid.
+func (b *BatchVerifier) Verify() bool {
+	if b.GetNumberOfEnqueuedSignatures() == 0 {
+		return false
+	}
+
+	for i := range b.messages {
+		verifier := SignatureVerifier(b.publicKeys[i])
+		if !verifier.VerifyBytes(b.messages[i], b.signatures[i]) {
+			return false
+		}
+	}
+	return true
+}
---
+++ crypto/batchverifier_test.go
@@ -0,0 +1,113 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBatchVerifierSingle(t *testing.T) {
+	// test expected success
+	bv := MakeBatchVerifier(1)
+	msg := randString()
+	var s Seed
+	RandBytes(s[:])
+	sigSecrets := GenerateSignatureSecrets(s)
+	sig := sigSecrets.Sign(msg)
+	bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+	require.True(t, bv.Verify())
+
+	// test expected failuire
+	bv = MakeBatchVerifier(1)
+	msg = randString()
+	RandBytes(s[:])
+	sigSecrets = GenerateSignatureSecrets(s)
+	sig = sigSecrets.Sign(msg)
+	// break the signature:
+	sig[0] = sig[0] + 1
+	bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+	require.False(t, bv.Verify())
+}
+
+func TestBatchVerifierBulk(t *testing.T) {
+	for i := 1; i < 64*2+3; i++ {
+		n := i
+		bv := MakeBatchVerifier(n)
+		var s Seed
+		RandBytes(s[:])
+
+		for i := 0; i < n; i++ {
+			msg := randString()
+			sigSecrets := GenerateSignatureSecrets(s)
+			sig := sigSecrets.Sign(msg)
+			bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+		}
+		require.True(t, bv.Verify())
+	}
+
+}
+
+func TestBatchVerifierBulkWithExpand(t *testing.T) {
+	n := 64
+	bv := MakeBatchVerifier(1)
+	var s Seed
+	RandBytes(s[:])
+
+	for i := 0; i < n; i++ {
+		msg := randString()
+		sigSecrets := GenerateSignatureSecrets(s)
+		sig := sigSecrets.Sign(msg)
+		bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+	}
+	require.True(t, bv.Verify())
+}
+
+func TestBatchVerifierWithInvalidSiganture(t *testing.T) {
+	n := 64
+	bv := MakeBatchVerifier(1)
+	var s Seed
+	RandBytes(s[:])
+
+	for i := 0; i < n-1; i++ {
+		msg := randString()
+		sigSecrets := GenerateSignatureSecrets(s)
+		sig := sigSecrets.Sign(msg)
+		bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+	}
+
+	msg := randString()
+	sigSecrets := GenerateSignatureSecrets(s)
+	sig := sigSecrets.Sign(msg)
+	sig[0] = sig[0] + 1
+	bv.EnqueueSignature(sigSecrets.SignatureVerifier, msg, sig)
+
+	require.False(t, bv.Verify())
+}
+
+func BenchmarkBatchVerifier(b *testing.B) {
+	c := makeCurve25519Secret()
+	bv := MakeBatchVerifier(1)
+	for i := 0; i < b.N; i++ {
+		str := randString()
+		bv.EnqueueSignature(c.SignatureVerifier, str, c.Sign(str))
+	}
+
+	b.ResetTimer()
+	require.True(b, bv.Verify())
+}
