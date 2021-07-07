1c84ad92bbabfd88e940827005e572edc61e8612
Will Winder <wwinder.unh@gmail.com>
2021-07-07 16:08:15 -0400

    Fix 10 minute timeout in travis. (Revert parts of #2324) (#2494)
    We started seeing the 10-minute timeout error on travis after the recent changes to where travis_retry gets called.
    
    This means travis_wait is still needed. travis_wait and travis_retry don't play well together on the ephemeral build machine, so we basically need to rollback the entire change in #2324
    
    Note: while making this change I noticed that we don't use travis_retry for all of the build_test.sh / integration_test.sh entries. Not sure why but I left them as they were before #2324

--- scripts/travis/build_test.sh
+++ scripts/travis/build_test.sh
@@ -13,5 +13,14 @@ ALGORAND_DEADLOCK=enable
 export ALGORAND_DEADLOCK
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-"${SCRIPTPATH}/build.sh" --make_debug
-"${SCRIPTPATH}/test.sh"
+if [ "${USER}" = "travis" ]; then
+    # we're running on a travis machine
+    "${SCRIPTPATH}/build.sh" --make_debug
+    # Need to call travis_retry first, if travis_wait calls travis_retry
+    # it doesn't show the output.
+    "${SCRIPTPATH}/travis_retry.sh" "${SCRIPTPATH}/travis_wait.sh" 90 "${SCRIPTPATH}/test.sh"
+else
+    # we're running on an ephermal build machine
+    "${SCRIPTPATH}/build.sh" --make_debug
+    "${SCRIPTPATH}/travis_retry.sh" "${SCRIPTPATH}/test.sh"
+fi
