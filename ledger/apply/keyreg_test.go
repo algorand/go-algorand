02414eeb6da6a5ba76436d75a99b843fca07b1d7
Jonathan Weiss <jonathan.weiss@algorand.com>
2021-07-11 16:03:39 +0300

    added unit test to keyreg apply

--- ledger/apply/keyreg_test.go
+++ ledger/apply/keyreg_test.go
@@ -17,6 +17,7 @@
 package apply
 
 import (
+	"math"
 	"testing"
 
 	"github.com/algorand/go-algorand/config"
@@ -166,5 +167,31 @@ func TestKeyregApply(t *testing.T) {
 
 		err = Keyreg(tx.KeyregTxnFields, tx.Header, mockBal, transactions.SpecialAddresses{FeeSink: feeSink}, nil, basics.Round(1100))
 		require.NoError(t, err)
+
+		// test that an invalid blockProofPK wont be stored.
+		attemptToStoreBadBlockProofPK(t, tx, mockBal)
+
+		testBlockProofPKBeingStored(t, tx, mockBal)
 	}
+}
+
+func testBlockProofPKBeingStored(t *testing.T, tx transactions.Transaction, mockBal keyregTestBalances) {
+	tx.KeyregTxnFields.BlockProofPK = crypto.VerifyingKey{Type: crypto.PlaceHolderType}
+	err := Keyreg(tx.KeyregTxnFields, tx.Header, mockBal, transactions.SpecialAddresses{FeeSink: feeSink}, nil, basics.Round(1100))
+	require.NoError(t, err) // expects no error with empty keyRegistration attempt
+
+	rec, err := mockBal.Get(tx.Header.Sender, false)
+	require.NoError(t, err) // expects no error with empty keyRegistration attempt
+	require.Equal(t, tx.KeyregTxnFields.BlockProofPK, rec.BlockProofID)
+}
+
+func attemptToStoreBadBlockProofPK(t *testing.T, tx transactions.Transaction, mockBal keyregTestBalances) {
+	tx.KeyregTxnFields.BlockProofPK = crypto.VerifyingKey{Type: math.MaxUint64}
+	err := Keyreg(tx.KeyregTxnFields, tx.Header, mockBal, transactions.SpecialAddresses{FeeSink: feeSink}, nil, basics.Round(1100))
+	require.NoError(t, err) // expects no error with empty keyRegistration attempt
+
+	rec, err := mockBal.Get(tx.Header.Sender, false)
+	require.NoError(t, err) // expects no error with empty keyRegistration attempt
+	require.Equal(t, crypto.VerifyingKey{}, rec.BlockProofID)
+	return
 }
