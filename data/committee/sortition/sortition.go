049745d344079fb47a857d93608f2b5122870027
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-07-12 17:05:32 -0400

    enabling go version

--- data/committee/sortition/sortition.go
+++ data/committee/sortition/sortition.go
@@ -30,36 +30,37 @@ import (
 	"github.com/algorand/go-algorand/crypto"
 )
 
-// Select runs the sortition function and returns the number of time the key was selected
-func Select(money uint64, totalMoney uint64, expectedSize float64, vrfOutput crypto.Digest) uint64 {
-	binomialN := float64(money)
-	binomialP := expectedSize / float64(totalMoney)
-
-	t := &big.Int{}
-	t.SetBytes(vrfOutput[:])
-
-	precision := uint(8 * (len(vrfOutput) + 1))
-	max, b, err := big.ParseFloat("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 0, precision, big.ToNearestEven)
-	if b != 16 || err != nil {
-		panic("failed to parse big float constant in sortition")
-	}
-
-	h := big.Float{}
-	h.SetPrec(precision)
-	h.SetInt(t)
-
-	ratio := big.Float{}
-	cratio, _ := ratio.Quo(&h, max).Float64()
-
-	//return uint64(C.sortition_binomial_cdf_walk(C.double(binomialN), C.double(binomialP), C.double(cratio), C.uint64_t(money)))
-	return boostCdfWalk(binomialN, binomialP, cratio, money)
-}
-
+//
+//// Select runs the sortition function and returns the number of time the key was selected
+//func Select(money uint64, totalMoney uint64, expectedSize float64, vrfOutput crypto.Digest) uint64 {
+//	binomialN := float64(money)
+//	binomialP := expectedSize / float64(totalMoney)
+//
+//	t := &big.Int{}
+//	t.SetBytes(vrfOutput[:])
+//
+//	precision := uint(8 * (len(vrfOutput) + 1))
+//	max, b, err := big.ParseFloat("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 0, precision, big.ToNearestEven)
+//	if b != 16 || err != nil {
+//		panic("failed to parse big float constant in sortition")
+//	}
+//
+//	h := big.Float{}
+//	h.SetPrec(precision)
+//	h.SetInt(t)
+//
+//	ratio := big.Float{}
+//	cratio, _ := ratio.Quo(&h, max).Float64()
+//
+//	//return uint64(C.sortition_binomial_cdf_walk(C.double(binomialN), C.double(binomialP), C.double(cratio), C.uint64_t(money)))
+//	return boostCdfWalk(binomialN, binomialP, cratio, money)
+//}
+//
 func boostCdfWalk(binomialN, binomialP, cratio float64, money uint64) uint64 {
 	return uint64(C.sortition_binomial_cdf_walk(C.double(binomialN), C.double(binomialP), C.double(cratio), C.uint64_t(money)))
 }
 
-func SelectG(money uint64, totalMoney uint64, expectedSize float64, vrfOutput crypto.Digest) uint64 {
+func Select(money uint64, totalMoney uint64, expectedSize float64, vrfOutput crypto.Digest) uint64 {
 	//binomialN := float64(money)
 	binomialP := expectedSize / float64(totalMoney)
 
