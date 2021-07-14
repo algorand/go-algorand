fbf36e085126d1a837b55e13f5bb7318e188956f
Stephen Akiki <stephen.akiki@algorand.com>
2021-07-14 09:56:22 -0400

    PR and license fix

--- txnsync/service_test.go
+++ txnsync/service_test.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
 package txnsync
 
 import (
@@ -76,7 +92,7 @@ func TestStartStopTransactionSyncService(t *testing.T) {
 
 	mLogger := mockLogger{}
 	mNodeConnector := makeMockNodeConnector(&calledEventsInNodeConnector)
-	cfg := config.Local{}
+	cfg := config.GetDefaultLocal()
 	mThreadPool := mockThreadPool{}
 
 	hashDigest := crypto.Hash([]byte{0x41, 0x6b, 0x69, 0x6b, 0x69})
@@ -102,7 +118,7 @@ func TestMakeTransactionSyncService(t *testing.T) {
 
 	mLogger := mockLogger{}
 	mNodeConnector := &mockNodeConnector{}
-	cfg := config.Local{}
+	cfg := config.GetDefaultLocal()
 	mThreadPool := mockThreadPool{}
 
 	hashDigest := crypto.Hash([]byte{0x41, 0x6b, 0x69, 0x6b, 0x69})
