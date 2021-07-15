17386d3d2d45b1dc74b4a8e99f25d925df3936c5
Jonathan Weiss <jonathan.weiss@algorand.com>
2021-07-15 07:27:27 +0300

    fix: go fmt

--- config/consensus.go
+++ config/consensus.go
@@ -380,7 +380,7 @@ type ConsensusParams struct {
 	// 4. checking that in the case of going online the VoteLast is non-zero and greater then the current network round.
 	// 5. checking that in the case of going online the VoteFirst is less or equal to the LastValid+1.
 	// 6. checking that in the case of going online the VoteFirst is less or equal to the next network round.
-	EnableKeyregCoherencyCheck  bool
+	EnableKeyregCoherencyCheck bool
 
 	// EnableBlockProofKeyregCheck enables the check for blockProof key on key registration
 	EnableBlockProofKeyregCheck bool
