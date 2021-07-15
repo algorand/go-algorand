e72931d7c2271b5793448941727f1dee5118cfaf
Jack Smith <jack.smith@algorand.com>
2021-07-15 16:48:50 -0400

    Updated custom search to use regex

--- scripts/custom_search.py
+++ scripts/custom_search.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import time
+import re
 start_time = time.time()
 
 def main():
@@ -12,9 +13,11 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--path', default='../', help='Path to search for files (includes subfolders, default="../")')
     ap.add_argument('--file-suffix', default='_test.go', help='checks only files with this suffix (default="_test.go")')
-    ap.add_argument('--search-string-one', default='func Test', help='searches for line with this string first (default="func Test")')
+    ap.add_argument('--search-string-one', default=r'func\s+\w+\s*\(.*t\s+\*testing\.T.*\)\s*{\s*\n', help=r'searches for line with this regex string first (default="func\s+\w+\s*\(.*t\s+\*testing\.T.*\)\s*{\s*\n')
     ap.add_argument('--search-string-two', default='testpartitioning.PartitionTest(t)', help='searches for second line without this second string (default="testpartitioning.PartitionTest(t)")')
     args = ap.parse_args()
+
+    regex_object = re.compile(args.search_string_one, re.IGNORECASE)
 
     # Iterate through all files in a given dir and all of it's subdirs
     total_results_found = 0
@@ -22,17 +25,16 @@ def main():
         for file in files:
             if file.endswith(args.file_suffix):
                 filepath = subdir + os.sep + file
-                total_results_found += check_file_for_missing_search_string(filepath, args.search_string_one, args.search_string_two)
+                total_results_found += check_file_for_missing_search_string(filepath, regex_object, args.search_string_two)
     print("==============================")
     print("Finished running parser in {} seconds".format(round(time.time() - start_time, 2)))
     print("{} total things found".format(total_results_found))
 
-def check_file_for_missing_search_string(filepath, search_string_one, search_string_two):
+def check_file_for_missing_search_string(filepath, regex_object, search_string_two):
     results_found = 0
     with open(filepath, 'r') as openfile:
         found_first_line = False
         for index, line in enumerate(openfile):
-
             # If previous line had search_string_one
             if found_first_line:
                 if search_string_two not in line:
@@ -41,7 +43,7 @@ def check_file_for_missing_search_string(filepath, search_string_one, search_str
                 found_first_line = False
 
             # If current line has search_string_one
-            elif search_string_one in line:
+            elif regex_object.match(line):
                 found_first_line = line
     return results_found
 
