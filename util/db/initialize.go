53be1059425b87148550f8a3d8285ff232cb7db1
Will Winder <wwinder.unh@gmail.com>
2021-08-03 15:41:09 -0400

    Add db initialize helper and tests.

---
+++ util/db/initialize.go
@@ -0,0 +1,111 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+// Migration is used to upgrade a database from one version to the next.
+// The Migration slice is ordered and must contain all prior migrations
+// in order to determine which need to be called.
+type Migration func(ctx context.Context, tx *sql.Tx, newDatabase bool) error
+
+func init() {
+	UnableToReadErr = errors.New("unable to read database")
+	NoOpMigrationErr = errors.New("migration no-op")
+}
+
+// Initialize creates or upgrades a DB accessor in a new atomic context.
+// The Migration slice is ordered and must contain all prior migrations
+// in order to determine which need to be called.
+func Initialize(accessor Accessor, migrations []Migration) error {
+	return accessor.Atomic(func(ctx context.Context, tx *sql.Tx) error {
+		return InitializeWithContext(ctx, tx, migrations)
+	})
+}
+
+// InitializeWithContext creates or upgrades a DB accessor.
+func InitializeWithContext(ctx context.Context, tx *sql.Tx, migrations []Migration) error {
+	// check current database version
+	dbVersion, err := GetUserVersion(ctx, tx)
+	if err != nil {
+		return UnableToReadErr
+	}
+
+	version := int32(len(migrations))
+
+	// if database version is greater than supported by current binary, write a warning. This would keep the existing
+	// fallback behavior where we could use an older binary iff the schema happen to be backward compatible.
+	if dbVersion > version {
+		return MakeUnknownVersionErr(dbVersion, version)
+	}
+
+	// if database is not up to date run migration functions.
+	if dbVersion < version {
+		var newDatabase bool
+		for i := dbVersion; i < version; i++ {
+			err = migrations[i](ctx, tx, newDatabase)
+			if err != nil && err != NoOpMigrationErr {
+				return MakeUpgradeFailureErr(dbVersion, i)
+			}
+
+			if i == 0 && err != NoOpMigrationErr {
+				newDatabase = true
+			}
+
+			// update version
+			_, err = SetUserVersion(ctx, tx, i + 1)
+			if err != nil {
+				return MakeUpgradeFailureErr(dbVersion, i)
+			}
+		}
+	}
+
+	return nil
+}
+
+// UnableToReadErr is returned when the accessor cannot be read.
+var UnableToReadErr error
+
+// NoOpMigrationErr is returned when there was no work for the migration to perform.
+var NoOpMigrationErr error
+
+// UnknownVersionErr is returned when a migration to the current version is not available.
+type UnknownVersionErr struct {
+	CurrentVersion   int32
+	SupportedVersion int32
+}
+
+// Error implements the error interface.
+func (err *UnknownVersionErr) Error() string {
+	return fmt.Sprintf("database schema version is %d, but algod only supports up to %d", err.CurrentVersion, err.SupportedVersion)
+}
+
+// MakeUnknownVersionErr makes an UnknownVersionErr.
+func MakeUnknownVersionErr(currentVersion, supportedVersion int32) *UnknownVersionErr {
+	return &UnknownVersionErr{
+		CurrentVersion:   currentVersion,
+		SupportedVersion: supportedVersion,
+	}
+}
+
+// UpgradeFailureErr is returned when a migration returns an error.
+type UpgradeFailureErr struct {
+	SchemaVersionFrom int32
+	SchemaVersionTo   int32
+}
+
+// Error implements the error interface.
+func (err *UpgradeFailureErr) Error() string {
+	return fmt.Sprintf("failed to upgrade database from schema %d to %d", err.SchemaVersionFrom, err.SchemaVersionTo)
+}
+
+// MakeUpgradeFailureErr makes an UpgradeFailureErr.
+func MakeUpgradeFailureErr(from, to int32) *UpgradeFailureErr {
+	return &UpgradeFailureErr{
+		SchemaVersionFrom: from,
+		SchemaVersionTo:   to,
+	}
+}
---
+++ util/db/initialize_test.go
@@ -0,0 +1,219 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/algorand/go-algorand/test/partitiontest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// A few migrations functions to mix and match in tests.
+var (
+	createFoo = func(ctx context.Context, tx *sql.Tx, newDatabase bool) error {
+		_, err := tx.Exec(`CREATE TABLE foo (field INTEGER)`)
+		return err
+	}
+
+	addToFoo = func(amount int) Migration {
+		return func(ctx context.Context, tx *sql.Tx, newDatabase bool) error {
+			_, err := tx.Exec(`INSERT INTO foo (field) VALUES(?)`, amount)
+			return err
+		}
+	}
+
+	returnError = func(err error) Migration {
+		return func(ctx context.Context, tx *sql.Tx, newDatabase bool) error {
+			return err
+		}
+	}
+
+	// Check the sum of the field column.
+	verifyFoo = func(expected int) func(t *testing.T, ctx context.Context, tx *sql.Tx) {
+		return func(t *testing.T, ctx context.Context, tx *sql.Tx) {
+			var field int
+			err := tx.QueryRow(`SELECT COALESCE(SUM(field), 0) FROM foo`).Scan(&field)
+			assert.NoError(t, err)
+			assert.Equal(t, expected, field)
+		}
+	}
+)
+
+func TestInitialize(t *testing.T) {
+	partitiontest.PartitionTest(t)
+
+	testcases := []struct {
+		name string
+		migrations []Migration
+		expectedVersion int32
+		verify func(t *testing.T, ctx context.Context, tx *sql.Tx)
+		expectedError error
+	} {
+		{
+			name: "Simple",
+			migrations: []Migration{
+				createFoo,
+			},
+			expectedVersion: 1,
+			verify: verifyFoo(0),
+		},
+		{
+			name: "Multiple",
+			migrations: []Migration{
+				createFoo,
+				addToFoo(1),
+				addToFoo(10),
+				addToFoo(100),
+				addToFoo(1000),
+			},
+			expectedVersion: 5,
+			verify: verifyFoo(1111),
+		},
+		{
+			name: "Error + rollback",
+			migrations: []Migration{
+				createFoo,
+				addToFoo(1),
+				returnError(errors.New("did not finish")),
+				addToFoo(10),
+			},
+			expectedVersion: 0,
+			verify: nil,
+			expectedError: MakeUpgradeFailureErr(0, 2),
+		},
+	}
+
+	for _, testcase := range testcases {
+		testcase := testcase
+		t.Run(testcase.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Setup
+			accessor, err := MakeAccessor("test_" + testcase.name, false, true)
+			require.NoError(t, err)
+
+			err = Initialize(accessor, testcase.migrations)
+
+			// Check error.
+			if testcase.expectedError == nil {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, testcase.expectedError.Error())
+			}
+
+			// Check results.
+			accessor.Atomic(func(ctx context.Context, tx *sql.Tx) error {
+				version, err := GetUserVersion(ctx, tx)
+				assert.NoError(t, err)
+				assert.Equal(t, testcase.expectedVersion, version)
+
+				if testcase.verify != nil {
+					testcase.verify(t, ctx, tx)
+				}
+				return nil
+			})
+		})
+	}
+}
+
+func TestReadOnlyError(t *testing.T) {
+	partitiontest.PartitionTest(t)
+
+	expiredContext, expiredContextCancelFunc := context.WithCancel(context.Background())
+	expiredContextCancelFunc()
+	err := InitializeWithContext(expiredContext, nil, []Migration{createFoo})
+
+	require.EqualError(t, err, UnableToReadErr.Error())
+}
+
+func TestUnknownVersionError(t *testing.T) {
+	accessor, err := MakeAccessor("test-unknown-version", false, true)
+	require.NoError(t, err)
+
+	migrations := []Migration{
+		createFoo,
+		addToFoo(1),
+	}
+
+	// Initialize to version 2
+	err = Initialize(accessor, migrations)
+	require.NoError(t, err)
+
+	// Initialize with only version 1
+	err = Initialize(accessor, []Migration{createFoo})
+	require.EqualError(t, err, MakeUnknownVersionErr(1, 0).Error())
+}
+
+func TestNewDBFlag(t *testing.T) {
+	var captureNewDB bool
+	newDBCheck := func(ctx context.Context, tx *sql.Tx, newDatabase bool) error {
+		captureNewDB = newDatabase
+		return nil
+	}
+
+	testcases := []struct {
+		name string
+		migrations []Migration
+		expectedNewDB bool
+	} {
+		{
+			name: "no-op-migration-0",
+			migrations: []Migration{
+				returnError(NoOpMigrationErr),
+				newDBCheck,
+			},
+			expectedNewDB: false,
+		},
+		{
+			name: "regular-migration",
+			migrations: []Migration{
+				newDBCheck,
+				newDBCheck,
+			},
+			expectedNewDB: true,
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			accessor, err := MakeAccessor("test_" + testcase.name, false, true)
+			require.NoError(t, err)
+
+			err = Initialize(accessor, testcase.migrations)
+			require.NoError(t, err)
+
+			require.Equal(t, testcase.expectedNewDB, captureNewDB)
+		})
+	}
+}
+
+func TestResumeUpgrading(t *testing.T) {
+	accessor, err := MakeAccessor("test-resume", false, true)
+	require.NoError(t, err)
+
+	// Initialize to version 2
+	migrations := []Migration{
+		createFoo,
+		addToFoo(1),
+	}
+	err = Initialize(accessor, migrations)
+	require.NoError(t, err)
+
+	// Re-initialize and upgrade to version 4
+	migrations = []Migration{
+		createFoo,
+		addToFoo(1),
+		addToFoo(10),
+		addToFoo(100),
+	}
+	err = Initialize(accessor, migrations)
+	require.NoError(t, err)
+
+	accessor.Atomic(func(ctx context.Context, tx *sql.Tx) error {
+		verifyFoo(111)(t, ctx, tx)
+		return nil
+	})
+}
