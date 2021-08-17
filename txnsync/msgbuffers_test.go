5bafb90b4cf129b5e61e176fa9f0a044c5ce5dc1
Stephen Akiki <stephen.akiki@algorand.com>
2021-08-17 15:37:58 -0400

    Addressed PR comments

--- txnsync/msgbuffers_test.go
+++ txnsync/msgbuffers_test.go
@@ -17,6 +17,7 @@
 package txnsync
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -26,6 +27,18 @@ import (
 
 // TestMessageBuffersPool tests that a buffer pool can be retrieved and has proper length/capacity properties
 func TestMessageBuffersPool(t *testing.T) {
+
+	counter := 0
+
+	oldFunc := msgBuffersPool.New
+
+	msgBuffersPool = sync.Pool{
+		New: func() interface{} {
+			counter++
+			return oldFunc()
+		},
+	}
+
 	partitiontest.PartitionTest(t)
 
 	for i := 0; i < 10; i++ {
@@ -34,8 +47,34 @@ func TestMessageBuffersPool(t *testing.T) {
 		require.GreaterOrEqual(t, cap(bytes), messageBufferDefaultInitialSize)
 
 		releaseMessageBuffer(bytes)
+		require.Equal(t, 1, counter)
 	}
 
+	collector := [][]byte{}
+
+	counter = 1
+
+	for i := 0; i < 10; i++ {
+		bytes := getMessageBuffer()
+		collector = append(collector, bytes)
+		require.Equal(t, 0, len(bytes))
+		require.GreaterOrEqual(t, cap(bytes), messageBufferDefaultInitialSize)
+
+		require.Equal(t, i+1, counter)
+	}
+
+	for _, b := range collector {
+		releaseMessageBuffer(b)
+	}
+
+	for i := 0; i < 10; i++ {
+		bytes := getMessageBuffer()
+		collector = append(collector, bytes)
+		require.Equal(t, 0, len(bytes))
+		require.GreaterOrEqual(t, cap(bytes), messageBufferDefaultInitialSize)
+
+		require.Equal(t, 10, counter)
+	}
 }
 
 // TestTxIDSlicePool tests that the transaction id pool can be retrieved and has proper length/capacity properties
@@ -47,13 +86,15 @@ func TestTxIDSlicePool(t *testing.T) {
 		require.Equal(t, 0, len(txIDs))
 		require.GreaterOrEqual(t, cap(txIDs), i)
 		releaseTxIDSliceBuffer(txIDs)
-
 	}
 
 	// Test that one of the previous buffers can be reused
+	// We can assess this because all the previous buffers created
+	// had a capacity greater than 10, so if one of these buffers
+	// has a buffer size of at least 10 (when we asked for 5), we can
+	// be assured that we have reused a previous buffer
 	txIDs := getTxIDSliceBuffer(5)
 	require.Equal(t, 0, len(txIDs))
-	require.GreaterOrEqual(t, cap(txIDs), 5)
+	require.GreaterOrEqual(t, cap(txIDs), 10)
 	releaseTxIDSliceBuffer(txIDs)
-
 }
