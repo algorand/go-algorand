965c61dfb716d14d2e2d3c00c5f55b69db5788e9
Jack Smith <jack.smith@algorand.com>
2021-08-20 18:31:35 -0400

    Added color and switched to nightly

--- scripts/buildtools/check_tests.py
+++ scripts/buildtools/check_tests.py
@@ -1,17 +1,20 @@
+print("===== STARTED RUNNING check_tests.py =====")
+
 import json
 import sys
+import argparse
 
-print("===== STARTED RUNNING check_tests.py =====")
-if len(sys.argv) != 2:
-    print("Wrong number of arguments passed. Please pass one argument: json format test results file path (e.g. /tmp/results/testresults.json)")
-    sys.exit(1)
-filepath = sys.argv[1]
+# Arguments parsing / help menu
+parser = argparse.ArgumentParser(description='Verify test results for skipped tests and tests with multiple passes.')
+parser.add_argument('tests_results_filepath', metavar='RESULTS_FILE',
+    help='json format test results file path (e.g. /tmp/results/testresults.json)')
+args = parser.parse_args()
 
 # Go through the given file one json object at a time, and record into lists
 total = set()
 passedWDupes = []
 partitionSkipped = set()
-with open(filepath) as f:
+with open(args.tests_results_filepath) as f:
     for jsonObj in f:
         testDict = json.loads(jsonObj)
         if 'Test' not in testDict:
@@ -50,29 +53,18 @@ skippedNotPartition = total - passed.union(partitionSkipped)
 # Total not passed (deduped)
 notPassed = total - passed
 
-# === Summary ===
-print("==================================================")
-print("Saw {} tests total".format(len(total)))
-print("{} passed before dedup".format(len(passedWDupes)))
-print("{} passed after dedup".format(len(passed)))
-print("{} skipped total".format(len(notPassed)))
-print("{} skipped due to partition".format(len(partitionSkippedNotPass)))
-print("{} skipped for other reasons (Maybe on purpose)".format(len(skippedNotPartition)))
-print("==================================================")
+# Sort and print messages with colored prefix
+red_text_color = "\033[0;31m"
+green_text_color = "\033[0;32m"
+yellow_text_color = "\033[0;33m"
+normal_text_color = "\033[0;0m"
+def printColor(message, color=normal_text_color):
+    print("{}{}{}".format(color, message, normal_text_color))
 
+# Record error message for sys.exit(errorMessage)
 errorMessage = ''
-# Check tests not passed due to partition
-print("==================================================")
-if len(partitionSkippedNotPass):
-    print("{} tests didn't pass due to partition!!".format(len(partitionSkippedNotPass)))
-    print("Here are the ones that didn't pass due to partition: ")
-    print(*sorted(list(partitionSkippedNotPass)), sep = "\n")
-    errorMessage += "FAIL ERROR: {} tests didn't pass due to partition!! (Scroll up to see which)\n".format(len(partitionSkippedNotPass))
-else:
-    print("No tests skipped due to partition.")
-print("==================================================")
 
-# Check tests not passed for other reasons
+# Check tests not passed for misc reasons
 print("==================================================")
 if len(skippedNotPartition):
     print("{} tests didn't pass due to other reasons (Maybe on purpose)".format(len(skippedNotPartition)))
@@ -92,6 +84,27 @@ if len(passedWDupes) != len(passed):
 else:
     print("All tests that passed, passed only once ... OK")
 print("==================================================")
-print("===== FINISHED RUNNING check_tests.py =====")
 
+# Check tests not passed due to partition
+print("==================================================")
+if len(partitionSkippedNotPass):
+    print("{} tests didn't pass due to partition!!".format(len(partitionSkippedNotPass)))
+    print("Here are the ones that didn't pass due to partition: ")
+    [printColor(x, red_text_color) for x in sorted(list(partitionSkippedNotPass))]
+    errorMessage += "{}FAIL ERROR:{} {} tests didn't pass due to partition!! (Scroll up top to see which)\n".format(red_text_color, normal_text_color, len(partitionSkippedNotPass))
+else:
+    print("No tests skipped due to partition.")
+print("==================================================")
+
+# === Summary ===
+print("==================================================")
+print("Saw {} tests total".format(len(total)))
+print("{} passed before dedup".format(len(passedWDupes)))
+print("{} passed after dedup".format(len(passed)))
+print("{} skipped due to partition".format(len(partitionSkippedNotPass)))
+print("{} skipped for misc reasons (Maybe on purpose)".format(len(skippedNotPartition)))
+print("{} total skipped".format(len(notPassed)))
+print("==================================================")
+
+print("===== FINISHED RUNNING check_tests.py =====")
 sys.exit(errorMessage)
