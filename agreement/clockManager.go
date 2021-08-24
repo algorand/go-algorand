ddd9013ef010b9b650fefd6c986956bac4b4abc3
Nickolai Zeldovich <nickolai@csail.mit.edu>
2021-08-24 16:14:53 -0400

    drop clockManager.zm

--- agreement/clockManager.go
+++ agreement/clockManager.go
@@ -16,20 +16,18 @@ import (
 type clockManager struct {
 	mu deadlock.Mutex
 	m  map[round]timers.Clock
-	zm map[round]time.Time // XXX mostly just to support testing with testingClock
 
 	t0 timers.Clock
 }
 
 func makeClockManager(t0 timers.Clock) *clockManager {
-	return &clockManager{m: make(map[round]timers.Clock), zm: make(map[round]time.Time), t0: t0}
+	return &clockManager{m: make(map[round]timers.Clock), t0: t0}
 }
 
 func (cm *clockManager) setZero(r round) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 
-	cm.zm[r] = time.Now().UTC()
 	cm.m[r] = cm.t0.Zero()
 }
 
@@ -44,10 +42,8 @@ func (cm *clockManager) nextDeadlineCh(es []externalDemuxSignals) (<-chan time.T
 	defer cm.mu.Unlock()
 
 	sort.Slice(es, func(i, j int) bool {
-		ti := cm.zm[es[i].CurrentRound].Add(es[i].Deadline)
-		tj := cm.zm[es[j].CurrentRound].Add(es[j].Deadline)
-		//ti := cm.m[es[i].CurrentRound].GetTimeout(es[i].Deadline)
-		//tj := cm.m[es[j].CurrentRound].GetTimeout(es[j].Deadline)
+		ti := cm.m[es[i].CurrentRound].GetTimeout(es[i].Deadline)
+		tj := cm.m[es[j].CurrentRound].GetTimeout(es[j].Deadline)
 		return ti.Before(tj)
 	})
 	r := es[0].CurrentRound
@@ -70,17 +66,15 @@ func (cm *clockManager) nextFastDeadlineCh(es []externalDemuxSignals) (<-chan ti
 	defer cm.mu.Unlock()
 
 	sort.Slice(es, func(i, j int) bool {
-		ti := cm.zm[es[i].CurrentRound].Add(es[i].FastRecoveryDeadline)
-		tj := cm.zm[es[j].CurrentRound].Add(es[j].FastRecoveryDeadline)
-		//ti := cm.m[es[i].CurrentRound].GetTimeout(es[i].FastRecoveryDeadline)
-		//tj := cm.m[es[j].CurrentRound].GetTimeout(es[j].FastRecoveryDeadline)
+		ti := cm.m[es[i].CurrentRound].GetTimeout(es[i].FastRecoveryDeadline)
+		tj := cm.m[es[j].CurrentRound].GetTimeout(es[j].FastRecoveryDeadline)
 		return ti.Before(tj)
 	})
 	return cm.m[es[0].CurrentRound].TimeoutAt(es[0].FastRecoveryDeadline), es[0].CurrentRound
 }
 
 type clockManagerSerialized struct {
-	Clocks []struct{ R, C, Z []byte }
+	Clocks []struct{ R, C []byte }
 }
 
 func (cm *clockManager) Decode(data []byte) (*clockManager, error) {
@@ -91,7 +85,6 @@ func (cm *clockManager) Decode(data []byte) (*clockManager, error) {
 		return nil, err
 	}
 	m := make(map[round]timers.Clock)
-	zm := make(map[round]time.Time)
 	for _, rc := range s.Clocks {
 		var r round
 		err := protocol.DecodeReflect(rc.R, &r)
@@ -102,15 +95,9 @@ func (cm *clockManager) Decode(data []byte) (*clockManager, error) {
 		if err != nil {
 			return nil, err
 		}
-		var zero time.Time
-		err = protocol.DecodeReflect(rc.Z, &zero)
-		if err != nil {
-			return nil, err
-		}
 		m[r] = clk
-		zm[r] = zero
 	}
-	return &clockManager{m: m, zm: zm, t0: cm.t0}, err
+	return &clockManager{m: m, t0: cm.t0}, err
 }
 
 func (cm *clockManager) Encode() []byte {
@@ -119,10 +106,9 @@ func (cm *clockManager) Encode() []byte {
 
 	var s clockManagerSerialized
 	for r, c := range cm.m {
-		s.Clocks = append(s.Clocks, struct{ R, C, Z []byte }{
+		s.Clocks = append(s.Clocks, struct{ R, C []byte }{
 			R: protocol.EncodeReflect(r),
 			C: c.Encode(),
-			Z: protocol.EncodeReflect(cm.zm[r]),
 		})
 	}
 	return protocol.EncodeReflect(&s)
