0014533251bd6eb7375a8175b0afa34aaba969e2
chris erway <chris.erway@algorand.com>
2021-08-27 10:32:37 -0400

    Add no-op pipeline e2e-go test

---
+++ test/e2e-go/features/pipeline/pipeline_test.go
@@ -0,0 +1,33 @@
+package pipeline
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/algorand/go-algorand/config"
+	"github.com/algorand/go-algorand/protocol"
+	"github.com/algorand/go-algorand/test/framework/fixtures"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPipeline(t *testing.T) {
+	r := require.New(fixtures.SynchronizedTest(t))
+
+	configurableConsensus := make(config.ConsensusProtocols)
+	cp := config.Consensus[protocol.ConsensusCurrentVersion]
+	cp.EnablePipelining = true
+	cp.AgreementPipelineDepth = 5
+	cp.AgreementPipelineDelayHistory = 32
+	// vFuture.AgreementPipelineDelay = 30
+	cp.AgreementPipelineDelay = 0
+	configurableConsensus[protocol.ConsensusVersion("vPipeline")] = cp
+
+	var fixture fixtures.RestClientFixture
+	fixture.SetConsensus(configurableConsensus)
+	fixture.Setup(t, filepath.Join("nettemplates", "PipelineFiveNodes.json"))
+	defer fixture.Shutdown()
+
+	_, err := fixture.NC.AlgodClient()
+	r.NoError(err)
+
+}
