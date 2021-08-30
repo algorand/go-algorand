acf6669fe01a32427f43475fdd2a00685c6940e4
John Jannotti <jj@cs.brown.edu>
2021-08-30 10:54:28 -0400

    Prevent rewards from confusing integration test

--- test/scripts/e2e_subs/app-accounts.sh
+++ test/scripts/e2e_subs/app-accounts.sh
@@ -16,26 +16,32 @@ TEAL=test/scripts/e2e_subs/tealprogs
 gcmd="goal -w ${WALLET}"
 
 ACCOUNT=$(${gcmd} account list|awk '{ print $3 }')
-
-APPID=$(${gcmd} app create --creator "${ACCOUNT}" --approval-prog=${TEAL}/app-escrow.teal --global-byteslices 4 --global-ints 0 --local-byteslices 0 --local-ints 1  --clear-prog=${TEAL}/approve-all.teal | grep Created | awk '{ print $6 }')
-
-function call {
-    method=$1; shift
-    ${gcmd} app call --app-id="$APPID" --from="$ACCOUNT" --app-arg="str:$method" "$@"
-}
-
-APPACCT=$(python -c "import algosdk.encoding as e; print(e.encode_address(e.checksum(b'app'+($APPID).to_bytes(8, 'big'))))")
+# Create a smaller account so rewards won't change balances.
+SMALL=$(${gcmd} account new | awk '{ print $6 }')
+# Under one algo receives no rewards
+${gcmd} clerk send -a 1000000 -f "$ACCOUNT" -t "$SMALL"
 
 function balance {
     acct=$1; shift
     goal account balance -a "$acct" | awk '{print $1}'
 }
 
-[ "$(balance "$ACCOUNT")" = 999999999000 ]
+[ "$(balance "$ACCOUNT")" = 999998999000 ]
+[ "$(balance "$SMALL")" =        1000000 ]
+
+APPID=$(${gcmd} app create --creator "${SMALL}" --approval-prog=${TEAL}/app-escrow.teal --global-byteslices 4 --global-ints 0 --local-byteslices 0 --local-ints 1  --clear-prog=${TEAL}/approve-all.teal | grep Created | awk '{ print $6 }')
+[ "$(balance "$SMALL")" = 999000 ] # 1000 fee
+
+function call {
+    method=$1; shift
+    ${gcmd} app call --app-id="$APPID" --from="$SMALL" --app-arg="str:$method" "$@"
+}
+
+APPACCT=$(python -c "import algosdk.encoding as e; print(e.encode_address(e.checksum(b'app'+($APPID).to_bytes(8, 'big'))))")
 
 function payin {
     amount=$1; shift
-    ${gcmd} clerk send -f "$ACCOUNT" -t "$APPACCT" -a "$amount" "$@"
+    ${gcmd} clerk send -f "$SMALL" -t "$APPACCT" -a "$amount" "$@"
 }
 
 T=$TEMPDIR
@@ -44,45 +50,45 @@ function sign {
     ${gcmd} clerk sign -i "$T/$1.tx" -o "$T/$1.stx"
 }
 
-${gcmd} app optin --app-id "$APPID" --from "${ACCOUNT}"
-[ "$(balance "$ACCOUNT")" = 999999998000 ]
+${gcmd} app optin --app-id "$APPID" --from "${SMALL}"
+[ "$(balance "$SMALL")" = 998000 ] # 1000 fee
 
 call "deposit():void" -o "$T/deposit.tx"
 payin 150000 -o "$T/pay1.tx"
 cat "$T/deposit.tx" "$T/pay1.tx" | ${gcmd} clerk group -i - -o "$T/group.tx"
 sign group
 ${gcmd} clerk rawsend -f "$T/group.stx"
-[ "$(balance "$ACCOUNT")" = 999999846000 ] # 2 fees, 150,000 deposited
+[ "$(balance "$SMALL")" = 846000 ] # 2 fees, 150,000 deposited
 [ "$(balance "$APPACCT")" = 150000 ]
 
 call "withdraw(uint64):void" --app-arg="int:20000"
-[ "$(balance "$ACCOUNT")" = 999999865000 ] # 1 fee, 20,000 withdrawn
-[ "$(balance "$APPACCT")" = 129000 ]       # 20k withdraw, fee paid by app account
+[ "$(balance "$SMALL")" = 865000 ]   # 1 fee, 20,000 withdrawn
+[ "$(balance "$APPACCT")" = 129000 ] # 20k withdraw, fee paid by app account
 
 call "withdraw(uint64):void" --app-arg="int:10000" --fee 2000
-[ "$(balance "$ACCOUNT")" = 999999873000 ] # 2000 fee, 10k withdrawn
-[ "$(balance "$APPACCT")" = 119000 ]       # 10k withdraw, fee credit used
+[ "$(balance "$SMALL")" = 873000 ]   # 2000 fee, 10k withdrawn
+[ "$(balance "$APPACCT")" = 119000 ] # 10k withdraw, fee credit used
 
 # Try to get app account below zero
 # (By app logic, it's OK - 150k was deposited, but fees have cut in)
 call "withdraw(uint64):void" --app-arg="int:120000" && exit 1
-[ "$(balance "$ACCOUNT")" = 999999873000 ] # no change
-[ "$(balance "$APPACCT")" = 119000 ]       # no change
+[ "$(balance "$SMALL")" = 873000 ]   # no change
+[ "$(balance "$APPACCT")" = 119000 ] # no change
 
 # Try to get app account below min balance by withdrawing too much
 call "withdraw(uint64):void" --app-arg="int:20000" && exit 1
-[ "$(balance "$ACCOUNT")" = 999999873000 ] # no change
-[ "$(balance "$APPACCT")" = 119000 ]       # no change
+[ "$(balance "$SMALL")" = 873000 ]   # no change
+[ "$(balance "$APPACCT")" = 119000 ] # no change
 
 # Try to get app account below min balance b/c of fee
 call "withdraw(uint64):void" --app-arg="int:18001" && exit 1
-[ "$(balance "$ACCOUNT")" = 999999873000 ] # no change
-[ "$(balance "$APPACCT")" = 119000 ]       # no change
+[ "$(balance "$SMALL")" = 873000 ]   # no change
+[ "$(balance "$APPACCT")" = 119000 ] # no change
 
 # Show that it works AT exactly min balance
 call "withdraw(uint64):void" --app-arg="int:18000"
-[ "$(balance "$ACCOUNT")" = 999999890000 ] # +17k (18k - fee)
-[ "$(balance "$APPACCT")" = 100000 ]       # -19k (18k + fee)
+[ "$(balance "$SMALL")" = 890000 ]   # +17k (18k - fee)
+[ "$(balance "$APPACCT")" = 100000 ] # -19k (18k + fee)
 
 
 date "+${scriptname} OK %Y%m%d_%H%M%S"
