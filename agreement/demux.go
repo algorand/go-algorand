992b04581a4756c33015147d3d095e2d79c03705
Nickolai Zeldovich <nickolai@csail.mit.edu>
2021-09-01 18:01:57 -0400

    cryptoVerifier gets branch-free rounds when protocol does not support branches

--- agreement/demux.go
+++ agreement/demux.go
@@ -162,6 +162,10 @@ func (d *demux) tokenizeMessages(ctx context.Context, net Network, tag protocol.
 // verifyVote enqueues a vote message to be verified.
 func (d *demux) verifyVote(ctx context.Context, m message, taskIndex int, r round, p period, b bookkeeping.BlockHash) {
 	l := LedgerBranchReader{lr: d.ledger, branch: b}
+	params, err := l.ConsensusParams(ParamsRound(r.Number))
+	if err != nil && !params.AgreementMessagesContainBranch {
+		r.Branch = bookkeeping.BlockHash{}
+	}
 	d.UpdateEventsQueue(eventQueueCryptoVerifierVote, 1)
 	d.monitor.inc(cryptoVerifierCoserviceType)
 	d.crypto.VerifyVote(ctx, cryptoVoteRequest{message: m, TaskIndex: taskIndex, Round: r, Period: p, ledger: l})
@@ -170,6 +174,10 @@ func (d *demux) verifyVote(ctx context.Context, m message, taskIndex int, r roun
 // verifyPayload enqueues a proposal payload message to be verified.
 func (d *demux) verifyPayload(ctx context.Context, m message, r round, p period, pinned bool, b bookkeeping.BlockHash) {
 	l := LedgerBranchReader{lr: d.ledger, branch: b}
+	params, err := l.ConsensusParams(ParamsRound(r.Number))
+	if err != nil && !params.AgreementMessagesContainBranch {
+		r.Branch = bookkeeping.BlockHash{}
+	}
 	d.UpdateEventsQueue(eventQueueCryptoVerifierProposal, 1)
 	d.monitor.inc(cryptoVerifierCoserviceType)
 	d.crypto.VerifyProposal(ctx, cryptoProposalRequest{message: m, Round: r, Period: p, Pinned: pinned, ledger: l})
@@ -178,6 +186,10 @@ func (d *demux) verifyPayload(ctx context.Context, m message, r round, p period,
 // verifyBundle enqueues a bundle message to be verified.
 func (d *demux) verifyBundle(ctx context.Context, m message, r round, p period, s step, b bookkeeping.BlockHash) {
 	l := LedgerBranchReader{lr: d.ledger, branch: b}
+	params, err := l.ConsensusParams(ParamsRound(r.Number))
+	if err != nil && !params.AgreementMessagesContainBranch {
+		r.Branch = bookkeeping.BlockHash{}
+	}
 	d.UpdateEventsQueue(eventQueueCryptoVerifierBundle, 1)
 	d.monitor.inc(cryptoVerifierCoserviceType)
 	d.crypto.VerifyBundle(ctx, cryptoBundleRequest{message: m, Round: r, Period: p, Certify: s == cert, ledger: l})
