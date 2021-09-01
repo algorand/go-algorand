30ffc3b66b1df4d5b58f48efdd28968ecdefb3e8
John Jannotti <jj@cs.brown.edu>
2021-09-01 11:58:15 -0400

    Confirm blocks API and inner-txns is ommitted when empty

--- test/scripts/e2e_subs/app-accounts.sh
+++ test/scripts/e2e_subs/app-accounts.sh
@@ -33,9 +33,6 @@ function balance {
     goal account balance -a "$acct" | awk '{print $1}'
 }
 
-function txid {
-    grep -o -E 'txid [A-Z0-9]{52}' | cut -c 6- | head -1
-}
 [ "$(balance "$ACCOUNT")" = 999998999000 ]
 [ "$(balance "$SMALL")" =        1000000 ]
 
@@ -45,6 +42,12 @@ APPID=$(${gcmd} app create --creator "${SMALL}" --approval-prog=${TEAL}/app-escr
 function appl {
     method=$1; shift
     ${gcmd} app call --app-id="$APPID" --from="$SMALL" --app-arg="str:$method" "$@"
+}
+function app-txid {
+    # When app (call or optin) submits, this is how the txid is
+    # printed.  Not in appl() because appl is also used with -o to
+    # create tx
+    grep -o -E 'txid [A-Z0-9]{52}' | cut -c 6- | head -1
 }
 
 APPACCT=$(python -c "import algosdk.encoding as e; print(e.encode_address(e.checksum(b'app'+($APPID).to_bytes(8, 'big'))))")
@@ -60,7 +63,9 @@ function sign {
     ${gcmd} clerk sign -i "$T/$1.tx" -o "$T/$1.stx"
 }
 
-${gcmd} app optin --app-id "$APPID" --from "${SMALL}"
+TXID=$(${gcmd} app optin --app-id "$APPID" --from "${SMALL}" | app-txid)
+# Rest succeeds, no stray inner-txn array
+[ "$(rest "/v2/transactions/pending/$TXID" | jq '.["inner-txn"]')" == null ]
 [ "$(balance "$SMALL")" = 998000 ] # 1000 fee
 
 appl "deposit():void" -o "$T/deposit.tx"
@@ -68,16 +73,18 @@ payin 150000 -o "$T/pay1.tx"
 cat "$T/deposit.tx" "$T/pay1.tx" | ${gcmd} clerk group -i - -o "$T/group.tx"
 sign group
 ${gcmd} clerk rawsend -f "$T/group.stx"
-
 [ "$(balance "$SMALL")" = 846000 ] # 2 fees, 150,000 deposited
 [ "$(balance "$APPACCT")" = 150000 ]
 
-# Withdraw 20,000 in app. Confirm that inner txn is visible to API.
-TXID=$(appl "withdraw(uint64):void" --app-arg="int:20000" | txid)
-[ $(rest "/v2/transactions/pending/$TXID" \
-        | jq '.["inner-txns"][0].txn.txn.amt') = 20000 ]
-[ $(rest "/v2/transactions/pending/$TXID?format=msgpack" | msgpacktool -d \
-        | jq '.["inner-txns"][0].txn.txn.type') = '"pay"' ]
+# Withdraw 20,000 in app. Confirm that inner txn is visible to transaction API.
+TXID=$(appl "withdraw(uint64):void" --app-arg="int:20000" | app-txid)
+[ "$(rest "/v2/transactions/pending/$TXID" \
+        | jq '.["inner-txns"][0].txn.txn.amt')" = 20000 ]
+[ "$(rest "/v2/transactions/pending/$TXID?format=msgpack" | msgpacktool -d \
+        | jq '.["inner-txns"][0].txn.txn.type')" = '"pay"' ]
+# Now confirm it's in blocks API (this time in our internal form)
+ROUND=$(rest "/v2/transactions/pending/$TXID" | jq '.["confirmed-round"]')
+rest "/v2/blocks/$ROUND" | jq .block.txns[0].dt.itx
 
 [ "$(balance "$SMALL")" = 865000 ]   # 1 fee, 20,000 withdrawn
 [ "$(balance "$APPACCT")" = 129000 ] # 20k withdraw, fee paid by app account
