6d25a03ac6899fca1ef901d2aa5ee3892d628f66
Travis Cline <travis.cline@gmail.com>
2021-09-03 18:08:11 -0700

    crypto: Include license header in new files

--- crypto/compare_verify_implementations.go
+++ crypto/compare_verify_implementations.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2019-2021 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
 // +build compare_purego_implementation
 
 package crypto
