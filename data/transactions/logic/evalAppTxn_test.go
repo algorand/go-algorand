1d6c04e1d37c923322fa7e4fe5e8e56f494a1e3b
Tsachi Herman <tsachi.herman@algorand.com>
2021-09-20 17:19:27 -0400

    merge

--- data/transactions/logic/evalAppTxn_test.go
+++ data/transactions/logic/evalAppTxn_test.go
@@ -21,77 +21,87 @@ import (
 	"testing"
 
 	"github.com/algorand/go-algorand/data/basics"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestActionTypes(t *testing.T) {
 	ep, ledger := makeSampleEnv()
-	testApp(t, "tx_submit; int 1;", ep, "tx_submit without tx_begin")
-	testApp(t, "int pay; tx_field TypeEnum; tx_submit; int 1;", ep, "tx_field without tx_begin")
-	testApp(t, "tx_begin; tx_submit; int 1;", ep, "Invalid inner transaction type")
+	testApp(t, "itxn_submit; int 1;", ep, "itxn_submit without itxn_begin")
+	testApp(t, "int pay; itxn_field TypeEnum; itxn_submit; int 1;", ep, "itxn_field without itxn_begin")
+	testApp(t, "itxn_begin; itxn_submit; int 1;", ep, "Invalid inner transaction type")
 	// bad type
-	testApp(t, "tx_begin; byte \"pya\"; tx_field Type; tx_submit; int 1;", ep, "pya is not a valid Type")
+	testApp(t, "itxn_begin; byte \"pya\"; itxn_field Type; itxn_submit; int 1;", ep, "pya is not a valid Type")
 	// mixed up the int form for the byte form
-	testApp(t, obfuscate("tx_begin; int pay; tx_field Type; tx_submit; int 1;"), ep, "Type arg not a byte array")
+	testApp(t, obfuscate("itxn_begin; int pay; itxn_field Type; itxn_submit; int 1;"), ep, "Type arg not a byte array")
 	// or vice versa
-	testApp(t, obfuscate("tx_begin; byte \"pay\"; tx_field TypeEnum; tx_submit; int 1;"), ep, "not a uint64")
+	testApp(t, obfuscate("itxn_begin; byte \"pay\"; itxn_field TypeEnum; itxn_submit; int 1;"), ep, "not a uint64")
 
 	// good types, not alllowed yet
-	testApp(t, "tx_begin; byte \"keyreg\"; tx_field Type; tx_submit; int 1;", ep, "keyreg is not a valid Type for tx_field")
-	testApp(t, "tx_begin; byte \"acfg\"; tx_field Type; tx_submit; int 1;", ep, "acfg is not a valid Type for tx_field")
-	testApp(t, "tx_begin; byte \"afrz\"; tx_field Type; tx_submit; int 1;", ep, "afrz is not a valid Type for tx_field")
-	testApp(t, "tx_begin; byte \"appl\"; tx_field Type; tx_submit; int 1;", ep, "appl is not a valid Type for tx_field")
+	testApp(t, "itxn_begin; byte \"keyreg\"; itxn_field Type; itxn_submit; int 1;", ep, "keyreg is not a valid Type for itxn_field")
+	testApp(t, "itxn_begin; byte \"appl\"; itxn_field Type; itxn_submit; int 1;", ep, "appl is not a valid Type for itxn_field")
 	// same, as enums
-	testApp(t, "tx_begin; int keyreg; tx_field TypeEnum; tx_submit; int 1;", ep, "keyreg is not a valid Type for tx_field")
-	testApp(t, "tx_begin; int acfg; tx_field TypeEnum; tx_submit; int 1;", ep, "acfg is not a valid Type for tx_field")
-	testApp(t, "tx_begin; int afrz; tx_field TypeEnum; tx_submit; int 1;", ep, "afrz is not a valid Type for tx_field")
-	testApp(t, "tx_begin; int appl; tx_field TypeEnum; tx_submit; int 1;", ep, "appl is not a valid Type for tx_field")
-	testApp(t, "tx_begin; int 42; tx_field TypeEnum; tx_submit; int 1;", ep, "42 is not a valid TypeEnum")
-	testApp(t, "tx_begin; int 0; tx_field TypeEnum; tx_submit; int 1;", ep, "0 is not a valid TypeEnum")
+	testApp(t, "itxn_begin; int keyreg; itxn_field TypeEnum; itxn_submit; int 1;", ep, "keyreg is not a valid Type for itxn_field")
+	testApp(t, "itxn_begin; int appl; itxn_field TypeEnum; itxn_submit; int 1;", ep, "appl is not a valid Type for itxn_field")
+	testApp(t, "itxn_begin; int 42; itxn_field TypeEnum; itxn_submit; int 1;", ep, "42 is not a valid TypeEnum")
+	testApp(t, "itxn_begin; int 0; itxn_field TypeEnum; itxn_submit; int 1;", ep, "0 is not a valid TypeEnum")
 
 	// "insufficient balance" because app account is charged fee
 	// (defaults make these 0 pay|axfer to zero address, from app account)
-	testApp(t, "tx_begin; byte \"pay\"; tx_field Type; tx_submit; int 1;", ep, "insufficient balance")
-	testApp(t, "tx_begin; byte \"axfer\"; tx_field Type; tx_submit; int 1;", ep, "insufficient balance")
-	testApp(t, "tx_begin; int pay; tx_field TypeEnum; tx_submit; int 1;", ep, "insufficient balance")
-	testApp(t, "tx_begin; int axfer; tx_field TypeEnum; tx_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; byte \"pay\"; itxn_field Type; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; byte \"axfer\"; itxn_field Type; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; int pay; itxn_field TypeEnum; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; int axfer; itxn_field TypeEnum; itxn_submit; int 1;", ep, "insufficient balance")
+
+	testApp(t, "itxn_begin; byte \"acfg\"; itxn_field Type; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; byte \"afrz\"; itxn_field Type; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; int acfg; itxn_field TypeEnum; itxn_submit; int 1;", ep, "insufficient balance")
+	testApp(t, "itxn_begin; int afrz; itxn_field TypeEnum; itxn_submit; int 1;", ep, "insufficient balance")
 
 	// Establish 888 as the app id, and fund it.
 	ledger.NewApp(ep.Txn.Txn.Receiver, 888, basics.AppParams{})
 	ledger.NewAccount(basics.AppIndex(888).Address(), 200000)
 
-	testApp(t, "tx_begin; byte \"pay\"; tx_field Type; tx_submit; int 1;", ep)
-	testApp(t, "tx_begin; int pay; tx_field TypeEnum; tx_submit; int 1;", ep)
+	testApp(t, "itxn_begin; byte \"pay\"; itxn_field Type; itxn_submit; int 1;", ep)
+	testApp(t, "itxn_begin; int pay; itxn_field TypeEnum; itxn_submit; int 1;", ep)
+	// Can't submit because we haven't finished setup, but type passes itxn_field
+	testApp(t, "itxn_begin; byte \"axfer\"; itxn_field Type; int 1;", ep)
+	testApp(t, "itxn_begin; int axfer; itxn_field TypeEnum; int 1;", ep)
+	testApp(t, "itxn_begin; byte \"acfg\"; itxn_field Type; int 1;", ep)
+	testApp(t, "itxn_begin; int acfg; itxn_field TypeEnum; int 1;", ep)
+	testApp(t, "itxn_begin; byte \"afrz\"; itxn_field Type; int 1;", ep)
+	testApp(t, "itxn_begin; int afrz; itxn_field TypeEnum; int 1;", ep)
 }
 
 func TestFieldTypes(t *testing.T) {
 	ep, _ := makeSampleEnv()
-	testApp(t, "tx_begin; byte \"pay\"; tx_field Sender;", ep, "not an address")
-	testApp(t, obfuscate("tx_begin; int 7; tx_field Receiver;"), ep, "not an address")
-	testApp(t, "tx_begin; byte \"\"; tx_field CloseRemainderTo;", ep, "not an address")
-	testApp(t, "tx_begin; byte \"\"; tx_field AssetSender;", ep, "not an address")
+	testApp(t, "itxn_begin; byte \"pay\"; itxn_field Sender;", ep, "not an address")
+	testApp(t, obfuscate("itxn_begin; int 7; itxn_field Receiver;"), ep, "not an address")
+	testApp(t, "itxn_begin; byte \"\"; itxn_field CloseRemainderTo;", ep, "not an address")
+	testApp(t, "itxn_begin; byte \"\"; itxn_field AssetSender;", ep, "not an address")
 	// can't really tell if it's an addres, so 32 bytes gets further
-	testApp(t, "tx_begin; byte \"01234567890123456789012345678901\"; tx_field AssetReceiver;",
+	testApp(t, "itxn_begin; byte \"01234567890123456789012345678901\"; itxn_field AssetReceiver;",
 		ep, "invalid Account reference")
 	// but a b32 string rep is not an account
-	testApp(t, "tx_begin; byte \"GAYTEMZUGU3DOOBZGAYTEMZUGU3DOOBZGAYTEMZUGU3DOOBZGAYZIZD42E\"; tx_field AssetCloseTo;",
+	testApp(t, "itxn_begin; byte \"GAYTEMZUGU3DOOBZGAYTEMZUGU3DOOBZGAYTEMZUGU3DOOBZGAYZIZD42E\"; itxn_field AssetCloseTo;",
 		ep, "not an address")
 
-	testApp(t, obfuscate("tx_begin; byte \"pay\"; tx_field Fee;"), ep, "not a uint64")
-	testApp(t, obfuscate("tx_begin; byte 0x01; tx_field Amount;"), ep, "not a uint64")
-	testApp(t, obfuscate("tx_begin; byte 0x01; tx_field XferAsset;"), ep, "not a uint64")
-	testApp(t, obfuscate("tx_begin; byte 0x01; tx_field AssetAmount;"), ep, "not a uint64")
+	testApp(t, obfuscate("itxn_begin; byte \"pay\"; itxn_field Fee;"), ep, "not a uint64")
+	testApp(t, obfuscate("itxn_begin; byte 0x01; itxn_field Amount;"), ep, "not a uint64")
+	testApp(t, obfuscate("itxn_begin; byte 0x01; itxn_field XferAsset;"), ep, "not a uint64")
+	testApp(t, obfuscate("itxn_begin; byte 0x01; itxn_field AssetAmount;"), ep, "not a uint64")
 
 }
 
 func TestAppPay(t *testing.T) {
 	pay := `
-  tx_begin
-  tx_field Amount
-  tx_field Receiver
-  tx_field Sender
+  itxn_begin
+  itxn_field Amount
+  itxn_field Receiver
+  itxn_field Sender
   int pay
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
   int 1
 `
 
@@ -116,10 +126,10 @@ func TestAppPay(t *testing.T) {
 	testApp(t, "txn Receiver; balance; int 100; ==", ep)
 
 	close := `
-  tx_begin
-  int pay;      tx_field TypeEnum
-  txn Receiver; tx_field CloseRemainderTo
-  tx_submit
+  itxn_begin
+  int pay;      itxn_field TypeEnum
+  txn Receiver; itxn_field CloseRemainderTo
+  itxn_submit
   int 1
 `
 	testApp(t, close, ep)
@@ -135,24 +145,24 @@ func TestAppAssetOptIn(t *testing.T) {
 	ledger.NewAccount(basics.AppIndex(888).Address(), 200000)
 
 	axfer := `
-tx_begin
-int axfer;  tx_field TypeEnum;
-int 25;     tx_field XferAsset;
-int 2;      tx_field AssetAmount;
-txn Sender; tx_field AssetReceiver;
-tx_submit
+itxn_begin
+int axfer;  itxn_field TypeEnum;
+int 25;     itxn_field XferAsset;
+int 2;      itxn_field AssetAmount;
+txn Sender; itxn_field AssetReceiver;
+itxn_submit
 int 1
 `
 	testApp(t, axfer, ep, "invalid Asset reference")
 	ep.Txn.Txn.ForeignAssets = append(ep.Txn.Txn.ForeignAssets, 25)
 	testApp(t, axfer, ep, "not opted in") // app account not opted in
 	optin := `
-tx_begin
-int axfer; tx_field TypeEnum;
-int 25;    tx_field XferAsset;
-int 0;     tx_field AssetAmount;
-global CurrentApplicationAddress; tx_field AssetReceiver;
-tx_submit
+itxn_begin
+int axfer; itxn_field TypeEnum;
+int 25;    itxn_field XferAsset;
+int 0;     itxn_field AssetAmount;
+global CurrentApplicationAddress; itxn_field AssetReceiver;
+itxn_submit
 int 1
 `
 	testApp(t, optin, ep, "does not exist")
@@ -174,13 +184,13 @@ int 1
 	testApp(t, "global CurrentApplicationAddress; int 25; asset_holding_get AssetBalance; assert; int 1; ==", ep)
 
 	close := `
-tx_begin
-int axfer;  tx_field TypeEnum;
-int 25;     tx_field XferAsset;
-int 0;      tx_field AssetAmount;
-txn Sender; tx_field AssetReceiver;
-txn Sender; tx_field AssetCloseTo;
-tx_submit
+itxn_begin
+int axfer;  itxn_field TypeEnum;
+int 25;     itxn_field XferAsset;
+int 0;      itxn_field AssetAmount;
+txn Sender; itxn_field AssetReceiver;
+txn Sender; itxn_field AssetCloseTo;
+itxn_submit
 int 1
 `
 	testApp(t, close, ep)
@@ -189,13 +199,13 @@ int 1
 
 func TestRekeyPay(t *testing.T) {
 	pay := `
-  tx_begin
-  tx_field Amount
-  tx_field Receiver
-  tx_field Sender
+  itxn_begin
+  itxn_field Amount
+  itxn_field Receiver
+  itxn_field Sender
   int pay
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
@@ -212,12 +222,12 @@ func TestRekeyPay(t *testing.T) {
 
 func TestDefaultSender(t *testing.T) {
 	pay := `
-  tx_begin
-  tx_field Amount
-  tx_field Receiver
+  itxn_begin
+  itxn_field Amount
+  itxn_field Receiver
   int pay
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
@@ -231,15 +241,15 @@ func TestDefaultSender(t *testing.T) {
 
 func TestAppAxfer(t *testing.T) {
 	axfer := `
-  tx_begin
+  itxn_begin
   int 77
-  tx_field XferAsset
-  tx_field AssetAmount
-  tx_field AssetReceiver
-  tx_field Sender
+  itxn_field XferAsset
+  itxn_field AssetAmount
+  itxn_field AssetReceiver
+  itxn_field Sender
   int axfer
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
@@ -272,13 +282,13 @@ func TestAppAxfer(t *testing.T) {
 	ep.Txn.Txn.ForeignAssets = save
 
 	noid := `
-  tx_begin
-  tx_field AssetAmount
-  tx_field AssetReceiver
-  tx_field Sender
+  itxn_begin
+  itxn_field AssetAmount
+  itxn_field AssetReceiver
+  itxn_field Sender
   int axfer
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 	testApp(t, "global CurrentApplicationAddress; txn Accounts 1; int 100"+noid+"int 1", ep,
 		fmt.Sprintf("Sender (%s) not opted in to 0", ledger.ApplicationID().Address()))
@@ -292,14 +302,14 @@ func TestAppAxfer(t *testing.T) {
 
 func TestExtraFields(t *testing.T) {
 	pay := `
-  tx_begin
-  int 7; tx_field AssetAmount;
-  tx_field Amount
-  tx_field Receiver
-  tx_field Sender
+  itxn_begin
+  int 7; itxn_field AssetAmount;
+  itxn_field Amount
+  itxn_field Receiver
+  itxn_field Sender
   int pay
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
@@ -312,34 +322,34 @@ func TestExtraFields(t *testing.T) {
 
 func TestBadField(t *testing.T) {
 	pay := `
-  tx_begin
-  int 7; tx_field AssetAmount;
-  tx_field Amount
-  tx_field Receiver
-  tx_field Sender
+  itxn_begin
+  int 7; itxn_field AssetAmount;
+  itxn_field Amount
+  itxn_field Receiver
+  itxn_field Sender
   int pay
-  tx_field TypeEnum
+  itxn_field TypeEnum
   txn Receiver
-  tx_field RekeyTo				// NOT ALLOWED
-  tx_submit
+  itxn_field RekeyTo				// NOT ALLOWED
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
 	ledger.NewApp(ep.Txn.Txn.Receiver, 888, basics.AppParams{})
 	testApp(t, "global CurrentApplicationAddress; txn Accounts 1; int 100"+pay, ep,
-		"invalid tx_field RekeyTo")
+		"invalid itxn_field RekeyTo")
 }
 
 func TestNumInner(t *testing.T) {
 	pay := `
-  tx_begin
+  itxn_begin
   int 1
-  tx_field Amount
+  itxn_field Amount
   txn Accounts 1
-  tx_field Receiver
+  itxn_field Receiver
   int pay
-  tx_field TypeEnum
-  tx_submit
+  itxn_field TypeEnum
+  itxn_submit
 `
 
 	ep, ledger := makeSampleEnv()
@@ -350,5 +360,77 @@ func TestNumInner(t *testing.T) {
 	testApp(t, pay+pay+pay+";int 1", ep)
 	testApp(t, pay+pay+pay+pay+";int 1", ep)
 	// In the sample proto, MaxInnerTransactions = 4
-	testApp(t, pay+pay+pay+pay+pay+";int 1", ep, "tx_submit with MaxInnerTransactions")
+	testApp(t, pay+pay+pay+pay+pay+";int 1", ep, "itxn_submit with MaxInnerTransactions")
+}
+
+func TestAssetCreate(t *testing.T) {
+	create := `
+  itxn_begin
+  int acfg
+  itxn_field TypeEnum
+  int 1000000
+  itxn_field ConfigAssetTotal
+  int 3
+  itxn_field ConfigAssetDecimals
+  byte "oz"
+  itxn_field ConfigAssetUnitName
+  byte "Gold"
+  itxn_field ConfigAssetName
+  byte "https://gold.rush/"
+  itxn_field ConfigAssetURL
+  itxn_submit
+  int 1
+`
+	ep, ledger := makeSampleEnv()
+	ledger.NewApp(ep.Txn.Txn.Receiver, 888, basics.AppParams{})
+	testApp(t, create, ep, "insufficient balance")
+	// Give it enough for fee.  Recall that we don't check min balance at this level.
+	ledger.NewAccount(ledger.ApplicationID().Address(), defaultEvalProto().MinTxnFee)
+	testApp(t, create, ep)
+}
+
+func TestAssetFreeze(t *testing.T) {
+	create := `
+  itxn_begin
+  int acfg                         ; itxn_field TypeEnum
+  int 1000000                      ; itxn_field ConfigAssetTotal
+  int 3                            ; itxn_field ConfigAssetDecimals
+  byte "oz"                        ; itxn_field ConfigAssetUnitName
+  byte "Gold"                      ; itxn_field ConfigAssetName
+  byte "https://gold.rush/"        ; itxn_field ConfigAssetURL
+  global CurrentApplicationAddress ; itxn_field ConfigAssetFreeze;
+  itxn_submit
+  itxn CreatedAssetID
+  int 889
+  ==
+`
+	ep, ledger := makeSampleEnv()
+	ledger.NewApp(ep.Txn.Txn.Receiver, 888, basics.AppParams{})
+	// Give it enough for fees.  Recall that we don't check min balance at this level.
+	ledger.NewAccount(ledger.ApplicationID().Address(), 12*defaultEvalProto().MinTxnFee)
+	testApp(t, create, ep)
+
+	freeze := `
+  itxn_begin
+  int afrz                    ; itxn_field TypeEnum
+  int 889                     ; itxn_field FreezeAsset
+  txn ApplicationArgs 0; btoi ; itxn_field FreezeAssetFrozen
+  txn Accounts 1              ; itxn_field FreezeAssetAccount
+  itxn_submit
+  int 1
+`
+	testApp(t, freeze, ep, "invalid Asset reference")
+	ep.Txn.Txn.ForeignAssets = []basics.AssetIndex{basics.AssetIndex(889)}
+	ep.Txn.Txn.ApplicationArgs = [][]byte{{0x01}}
+	testApp(t, freeze, ep, "does not hold Asset")
+	ledger.NewHolding(ep.Txn.Txn.Receiver, 889, 55, false)
+	testApp(t, freeze, ep)
+	holding, err := ledger.AssetHolding(ep.Txn.Txn.Receiver, 889)
+	require.NoError(t, err)
+	require.Equal(t, true, holding.Frozen)
+	ep.Txn.Txn.ApplicationArgs = [][]byte{{0x00}}
+	testApp(t, freeze, ep)
+	holding, err = ledger.AssetHolding(ep.Txn.Txn.Receiver, 889)
+	require.NoError(t, err)
+	require.Equal(t, false, holding.Frozen)
 }
