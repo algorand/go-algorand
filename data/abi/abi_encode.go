825e98d7148a74a78cfb948166b19658b7c157fa
Hang Su <hang.su@algorand.com>
2021-09-21 17:28:18 -0400

    minor

--- data/abi/abi_encode.go
+++ data/abi/abi_encode.go
@@ -145,7 +145,7 @@ func (v Value) Encode() ([]byte, error) {
 	case Tuple:
 		return tupleEncoding(v)
 	default:
-		return []byte{}, fmt.Errorf("bruh you should not be here in encoding: unknown type error")
+		return []byte{}, fmt.Errorf("Encoding: unknown type error")
 	}
 }
 
