e46c34619a963e65ca1dc3065ea7bc6e44231583
Tsachi Herman <tsachi.herman@algorand.com>
2021-09-21 12:28:27 -0400

    suggested changes

--- rpcs/blockService.go
+++ rpcs/blockService.go
@@ -23,6 +23,7 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gorilla/mux"
 
@@ -72,6 +73,7 @@ type BlockService struct {
 	fallbackEndpoints       fallbackEndpoints
 	enableArchiverFallback  bool
 	log                     logging.Logger
+	closeWaitGroup          sync.WaitGroup
 }
 
 // EncodedBlockCert defines how GetBlockBytes encodes a block and its certificate
@@ -125,13 +127,14 @@ func (bs *BlockService) Start() {
 		bs.net.RegisterHandlers(handlers)
 	}
 	bs.stop = make(chan struct{})
-	bs.ledger.WaitGroupAdd()
+	bs.closeWaitGroup.Add(1)
 	go bs.listenForCatchupReq(bs.catchupReqs, bs.stop)
 }
 
 // Stop servicing catchup requests over ws
 func (bs *BlockService) Stop() {
 	close(bs.stop)
+	bs.closeWaitGroup.Wait()
 }
 
 // ServerHTTP returns blocks
@@ -238,9 +241,9 @@ func (bs *BlockService) processIncomingMessage(msg network.IncomingMessage) (n n
 
 // listenForCatchupReq handles catchup getblock request
 func (bs *BlockService) listenForCatchupReq(reqs <-chan network.IncomingMessage, stop chan struct{}) {
+	defer bs.closeWaitGroup.Done()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	defer bs.ledger.WaitGroupDone()
 	for {
 		select {
 		case reqMsg := <-reqs:
