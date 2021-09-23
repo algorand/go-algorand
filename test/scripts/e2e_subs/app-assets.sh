595caa5e5186cb35d89167efa0c208d0f682ba6c
shiqi.zheng@algorand.com <shiqi.zheng@algorand.com>
2021-09-23 19:44:23 -0400

    tests for acfg, afrz

--- test/scripts/e2e_subs/app-assets.sh
+++ test/scripts/e2e_subs/app-assets.sh
@@ -36,9 +36,14 @@ function balance {
 [ "$(balance "$ACCOUNT")" = 999998999000 ]
 [ "$(balance "$SMALL")" =        1000000 ]
 
-function held_assets {
+function created_assets {
     acct=$1;
-    goal account info -a "$acct" | awk '/Held Assets:/,/Created Apps:/' | grep "ID*"
+    goal account info -a "$acct" | awk '/Created Assets:/,/Held Assets:/' | grep "ID*" | awk -F'[, ]' '{print $4}'
+}
+
+function created_supply {
+    acct=$1;
+    goal account info -a "$acct" | awk '/Created Assets:/,/Held Assets:/' | grep "ID*" | awk -F'[, ]' '{print $7}'
 }
 
 function asset_bal {
@@ -97,6 +102,11 @@ function clawback_addr {
     grep -o -E 'Clawback address: [A-Z0-9]{58}' | awk '{print $3}'
 }
 
+function payin {
+    amount=$1; shift
+    ${gcmd} clerk send -f "$SMALL" -t "$APPACCT" -a "$amount" "$@"
+}
+
 T=$TEMPDIR
 
 function sign {
@@ -112,7 +122,7 @@ ASSETID=$(asset-create 1000000  --name "e2e" --unitname "e" | asset-id)
 [ "$(balance "$SMALL")" = 997000 ] # 1000 fee
 
 ${gcmd} clerk send -a 1000000 -f "$ACCOUNT" -t "$APPACCT"
-appl "optin():void" --app-arg="int:$ASSETID" --foreign-asset="$ASSETID" --from="$SMALL"
+appl "optin():void" --foreign-asset="$ASSETID" --from="$SMALL"
 [ "$(balance "$APPACCT")" = 999000 ] # 1000 fee
 [ "$(balance "$SMALL")" = 996000 ]
 
@@ -129,9 +139,8 @@ ${gcmd} clerk rawsend -f "$T/group.stx"
 [ "$(balance "$SMALL")" =        994000 ] # 2 fees
 [ "$(balance "$APPACCT")" =      999000 ]
 
-
 # Withdraw 100 in app. Confirm that inner txn is visible to transaction API.
-TXID=$(appl "withdraw(uint64):void" --foreign-asset="$ASSETID" --app-arg="int:$ASSETID" --app-arg="int:100" --from="$SMALL" | app-txid)
+TXID=$(appl "withdraw(uint64):void" --app-arg="int:100"  --foreign-asset="$ASSETID" --from="$SMALL" | app-txid)
 [ "$(rest "/v2/transactions/pending/$TXID" \
         | jq '.["inner-txns"][0].txn.txn.aamt')" = 100 ]
 [ "$(rest "/v2/transactions/pending/$TXID?format=msgpack" | msgpacktool -d \
@@ -145,78 +154,120 @@ rest "/v2/blocks/$ROUND" | jq .block.txns[0].dt.itx
 [ "$(balance "$SMALL")" =        993000 ] # 1 fee
 [ "$(balance "$APPACCT")" =        998000 ] # fee paid by app
 
-appl "withdraw(uint64):void" --foreign-asset="$ASSETID" --app-arg="int:$ASSETID" --app-arg="int:100" --fee 2000 --from="$SMALL"
+appl "withdraw(uint64):void" --app-arg="int:100" --foreign-asset="$ASSETID"  --fee 2000 --from="$SMALL"
 [ "$(asset_bal "$SMALL")" = 999200 ]   #  100 asset withdrawn
 [ "$(balance "$SMALL")" = 991000 ]   # 2000 fee
 [ "$(asset_bal "$APPACCT")" = 800 ] # 100 asset  withdrawn
 [ "$(balance "$APPACCT")" = 998000 ] # fee credit used
 
 # Try to withdraw too much
-appl "withdraw(uint64):void" --foreign-asset="$ASSETID" --app-arg="int:$ASSETID" --app-arg="int:1000" --from="$SMALL"  && exit 1
+appl "withdraw(uint64):void" --app-arg="int:1000"  --foreign-asset="$ASSETID" --from="$SMALL"  && exit 1
 [ "$(asset_bal "$SMALL")" = 999200 ]   # no change
 [ "$(asset_bal "$APPACCT")" = 800 ]   # no change
 [ "$(balance "$SMALL")" = 991000 ]
 [ "$(balance "$APPACCT")" = 998000 ]
 
 # Show that it works AT exact asset balance
-appl "withdraw(uint64):void" --foreign-asset="$ASSETID" --app-arg="int:$ASSETID" --app-arg="int:800" --from="$SMALL"
+appl "withdraw(uint64):void" --app-arg="int:800" --foreign-asset="$ASSETID" --from="$SMALL"
 [ "$(asset_bal "$SMALL")" = 1000000 ]
 [ "$(asset_bal "$APPACCT")" = 0 ]
 [ "$(balance "$SMALL")" = 990000 ]
 [ "$(balance "$APPACCT")" = 997000 ]
 
-
-# set appacct as clawback address on asset
-${gcmd} asset config --manager $SMALL --assetid $ASSETID --new-clawback $APPACCT
-cb_addr=$(${gcmd} asset info --assetid $ASSETID | clawback_addr)
-[ "$cb_addr" = "$APPACCT" ] #app is set as clawback address
-
-# app can transfer asset between accounts
 USER=$(${gcmd} account new | awk '{ print $6 }') #new account
 ${gcmd} clerk send -a 1000000 -f "$ACCOUNT" -t "$USER" #fund account
 asset-optin -f "$USER" -t "$USER"  --assetid "$ASSETID" #opt in to asset
-# transfer asset from $SMALL to $USER
-appl "transfer(uint64):void" --app-arg="int:$ASSETID" --app-arg="int:1000" --app-arg="addr:$USER" --foreign-asset="$ASSETID" --from="$SMALL" --app-account="$USER"
-[ $(assets "$USER") = "e2e" ]
+# SET $USER as clawback address
+${gcmd} asset config --manager $SMALL --assetid $ASSETID --new-clawback $USER
+cb_addr=$(${gcmd} asset info --assetid $ASSETID | clawback_addr)
+[ "$cb_addr" = "$USER" ]
+${gcmd} asset send -f "$SMALL" -t "$USER" -a "1000" --assetid "$ASSETID" --clawback "$USER"
 [ $(asset_bal "$USER") = 1000 ]
 [ $(asset_bal "$SMALL") = 999000 ]
-# transfer asset from $USER to $SMALL
-appl "transfer(uint64):void" --app-arg="int:$ASSETID" --app-arg="int:100" --app-arg="addr:$SMALL" --foreign-asset="$ASSETID" --from="$USER" --app-account="$SMALL"
-[ $(asset_bal "$USER") = 900 ]
-[ $(asset_bal "$SMALL") = 999100 ]
-# $USER does not have clawback auth
-${gcmd} asset send -f "$SMALL" -t "$USER" -a "$amount" --assetid "$ASSETID" --clawback "$USER" && exit 1
-#rekey $USER to "$APPACCT"
+# rekey $USER to "$APPACCT"
 ${gcmd} clerk send --from "$USER" --to "$USER" -a 0 --rekey-to "$APPACCT"
+# $USER should still have clawback auth. should have been authorized by "$APPACCT"
+${gcmd} asset send -f "$SMALL" -t "$USER" -a "1000" --assetid "$ASSETID" --clawback "$USER" && exit 1
+
+USER2=$(${gcmd} account new | awk '{ print $6 }') #new account
+${gcmd} clerk send -a 1000000 -f "$ACCOUNT" -t "$USER2" #fund account
+asset-optin -f "$USER2" -t "$USER2"  --assetid "$ASSETID" #opt in to asset
+# set $APPACCT as clawback address on asset
+${gcmd} asset config --manager $SMALL --assetid $ASSETID --new-clawback $APPACCT
+cb_addr=$(${gcmd} asset info --assetid $ASSETID | clawback_addr)
+[ "$cb_addr" = "$APPACCT" ] #app is set as clawback address
+# transfer asset from $SMALL to $USER
+appl "transfer(uint64):void"  --app-arg="int:1000"  --foreign-asset="$ASSETID" --from="$SMALL" --app-account="$USER2"
+[ $(asset_bal "$USER2") = 1000 ]
+[ $(asset_bal "$SMALL") = 998000 ]
+# transfer asset from $USER to $SMALL
+appl "transfer(uint64):void"  --app-arg="int:100" --foreign-asset="$ASSETID" --from="$USER2" --app-account="$SMALL"
+[ $(asset_bal "$USER2") = 900 ]
+[ $(asset_bal "$SMALL") = 998100 ]
 
 # opt in more assets
 ASSETID2=$(asset-create 1000000  --name "alpha" --unitname "a"  | asset-id)
-appl "optin():void" --app-arg="int:$ASSETID2" --foreign-asset="$ASSETID2" --from="$SMALL"
+appl "optin():void" --foreign-asset="$ASSETID2" --from="$SMALL"
 ASSETID3=$(asset-create 1000000  --name "beta" --unitname "b"  | asset-id)
-appl "optin():void" --app-arg="int:$ASSETID3" --foreign-asset="$ASSETID3" --from="$SMALL"
+appl "optin():void" --foreign-asset="$ASSETID3" --from="$SMALL"
 
 IDs="
 $ASSETID \n
 $ASSETID2 \n
 $ASSETID3 \n
 "
-[[ "$(held_assets "$APPACCT")" = $IDs ]]  # account has 3 assets
+[[ "$(assets "$APPACCT")" = $IDs ]]  # account has 3 assets
 
 # opt out of assets
-appl "close(uint64):void" --app-arg="int:$ASSETID2" --foreign-asset="$ASSETID2" --from="$SMALL"
+appl "close():void"  --foreign-asset="$ASSETID2" --from="$SMALL"
 IDs="
 $ASSETID \n
 $ASSETID3 \n
 "
-[[ "$(held_assets "$APPACCT")" = "$IDs" ]] # account has 2 assets
-appl "close(uint64):void" --app-arg="int:$ASSETID" --foreign-asset="$ASSETID" --from="$SMALL"
-appl "close(uint64):void" --app-arg="int:$ASSETID3" --foreign-asset="$ASSETID3" --from="$SMALL"
-[[ "$(held_assets "$APPACCT")" = "" ]] # account has no assets
+[[ "$(assets "$APPACCT")" = "$IDs" ]] # account has 2 assets
+appl "close():void" --foreign-asset="$ASSETID" --from="$SMALL"
+appl "close():void" --foreign-asset="$ASSETID3" --from="$SMALL"
+[[ "$(assets "$APPACCT")" = "" ]] # account has no assets
 
+# app creates asset
+appl "create(uint64):void" --app-arg="int:1000000" --from="$SMALL"
+[ "$(created_assets "$APPACCT")" = "X" ]
+[ "$(created_supply "$APPACCT")" = 1000000 ]
 
-#app creates asset
-#mint asset
-#app afrz
+# mint asset
+APPASSETID=$(asset_ids "$APPACCT")
+asset-optin -f "$SMALL" -t "$SMALL"  --assetid "$APPASSETID" #opt in to asset
+appl "mint():void" --from="$SMALL" --foreign-asset="$APPASSETID" -o "$T/mint.tx"
+payin 1000 -o "$T/pay1.tx"
+cat "$T/mint.tx" "$T/pay1.tx" | ${gcmd} clerk group -i - -o "$T/group.tx"
+sign group
+${gcmd} clerk rawsend -f "$T/group.stx"
 
+IDs="
+$ASSETID \n
+$ASSETID2 \n
+$ASSETID3 \n
+$APPASSETID
+"
+[[ "$(assets "$SMALL")" = "$IDs" ]] # has new asset
+[[ "$(asset_bal "$SMALL")" = "999100\n1000000\n1000000\n1000" ]] # correct balances
+[ "$(asset_bal "$APPACCT")" = 999000 ] # 1k sent
+
+# freeze asset
+appl "freeze(uint64):void"  --app-arg="int:1" --foreign-asset="$APPASSETID" --from="$SMALL"
+# fail since asset is frozen on $SMALL
+appl "mint():void" --from="$SMALL" -o "$T/mint.tx" --foreign-asset="$APPASSETID"
+payin 1000 -o "$T/pay1.tx"
+cat "$T/mint.tx" "$T/pay1.tx" | ${gcmd} clerk group -i - -o "$T/group.tx"
+sign group
+${gcmd} clerk rawsend -f "$T/group.stx"  && exit 1
+# unfreeze asset
+appl "freeze(uint64):void" --app-arg="int:0" --foreign-asset="$APPASSETID" --from="$SMALL"
+appl "mint():void" --from="$SMALL" -o "$T/mint.tx" --foreign-asset="$APPASSETID"
+payin 1000 -o "$T/pay1.tx"
+cat "$T/mint.tx" "$T/pay1.tx" | ${gcmd} clerk group -i - -o "$T/group.tx"
+sign group
+${gcmd} clerk rawsend -f "$T/group.stx"
+[[ "$(asset_bal "$SMALL")" = "999100\n1000000\n1000000\n2000" ]] # minted 1000
 
 date "+${scriptname} OK %Y%m%d_%H%M%S"
