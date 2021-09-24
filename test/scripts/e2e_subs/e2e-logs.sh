21ad780d62d168c72793a55fb62ab126b255e151
shiqi.zheng@algorand.com <shiqi.zheng@algorand.com>
2021-09-24 15:05:58 -0400

    moving logs e2e to scripts

--- test/scripts/e2e_subs/e2e-logs.sh
+++ test/scripts/e2e_subs/e2e-logs.sh
@@ -30,13 +30,32 @@ TEAL=test/scripts/e2e_subs/tealprogs
 gcmd="goal -w ${WALLET}"
 
 ACCOUNT=$(${gcmd} account list|awk '{ print $3 }')
-
+# app create
 TXID=$(${gcmd} app create --creator "${ACCOUNT}" --approval-prog=${TEAL}/logs.teal --global-byteslices 4 --global-ints 0 --local-byteslices 0 --local-ints 1  --clear-prog=${TEAL}/approve-all.teal | app-txid)
-#[ "$(rest "/v2/transactions/pending/$TXID" \
-#        | jq '.["inner-txns"][0].txn.txn.amt')" = 20000 ]
-logs= $(rest "/v2/transactions/pending/$TXID" | jq -r '.logs[]')
-#print ${logs}
-#[ "$(rest "/v2/transactions/pending/$TXID" | jq '.logs | length')" = 32 ]
+# log len
+[ "$(rest "/v2/transactions/pending/$TXID" | jq '.logs | length')" = 32 ]
+# log content
+EXP=(B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ \` a b )
+i=0
+for log in $(rest "/v2/transactions/pending/$TXID" | jq -r '.logs | .[]')
+  do
+    c=`echo -n "${log}" | base64 --decode`
+    [ "$c" = "${EXP[i]}" ]
+    ((i++))
+  done
 
+APPID=$(rest "/v2/transactions/pending/$TXID" | jq '.["application-index"]')
+# app call
+TXID=$(${gcmd} app call --app-id "${APPID}" --from "$ACCOUNT" | app-txid)
+# log len
+[ "$(rest "/v2/transactions/pending/$TXID" | jq '.logs | length')" = 32 ]
+# log content
+i=0
+for log in $(rest "/v2/transactions/pending/$TXID" | jq -r '.logs | .[]')
+  do
+    c=`echo -n "${log}" | base64 --decode`
+    [ "$c" = "${EXP[i]}" ]
+    ((i++))
+  done
 
 date "+${scriptname} OK %Y%m%d_%H%M%S"
