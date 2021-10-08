87b9d759cb5b36b80ee5cdaf34aeb783073f24ce
Hang Su <hang.su@algorand.com>
2021-10-08 14:46:30 -0400

    update encode/decode from types

--- data/abi/abi_encode.go
+++ data/abi/abi_encode.go
@@ -22,6 +22,409 @@ import (
 	"math/big"
 )
 
+// typeCastToTuple cast an array-like ABI type into an ABI tuple type.
+func (t Type) typeCastToTuple(tupLen ...int) (Type, error) {
+	var childT []Type
+
+	switch t.abiTypeID {
+	case String:
+		if len(tupLen) != 1 {
+			return Type{}, fmt.Errorf("String type conversion to tuple need 1 length argument")
+		}
+		childT = make([]Type, tupLen[0])
+		for i := 0; i < tupLen[0]; i++ {
+			childT[i] = MakeByteType()
+		}
+	case Address:
+		childT = make([]Type, addressByteSize)
+		for i := 0; i < addressByteSize; i++ {
+			childT[i] = MakeByteType()
+		}
+	case ArrayStatic:
+		childT = make([]Type, t.staticLength)
+		for i := 0; i < int(t.staticLength); i++ {
+			childT[i] = t.childTypes[0]
+		}
+	case ArrayDynamic:
+		if len(tupLen) != 1 {
+			return Type{}, fmt.Errorf("Dynamic Array type conversion to tuple need 1 length argument")
+		}
+		childT = make([]Type, tupLen[0])
+		for i := 0; i < tupLen[0]; i++ {
+			childT[i] = t.childTypes[0]
+		}
+	default:
+		return Type{}, fmt.Errorf("type cannot support conversion to tuple")
+	}
+
+	tuple, err := MakeTupleType(childT)
+	if err != nil {
+		return Type{}, err
+	}
+	return tuple, nil
+}
+
+// Encode is an ABI type method to encode go values into bytes following ABI encoding rules
+func (t Type) Encode(value interface{}) ([]byte, error) {
+	switch t.abiTypeID {
+	case Uint, Ufixed:
+		return encodeInt(value, t.bitSize)
+	case Bool:
+		boolValue, ok := value.(bool)
+		if !ok {
+			return []byte{}, fmt.Errorf("cannot cast value to bool in bool encoding")
+		}
+		if boolValue {
+			return []byte{0x80}, nil
+		}
+		return []byte{0x00}, nil
+	case Byte:
+		byteValue, ok := value.(byte)
+		if !ok {
+			return []byte{}, fmt.Errorf("cannot cast value to byte in byte encoding")
+		}
+		return []byte{byteValue}, nil
+	case ArrayStatic, Address:
+		castedType, err := t.typeCastToTuple()
+		if err != nil {
+			return []byte{}, err
+		}
+		return castedType.Encode(value)
+	case ArrayDynamic, String:
+		var length int
+		stringValue, okString := value.(string)
+		dynamicArrayValue, okArray := value.([]interface{})
+		if okString {
+			length = len([]byte(stringValue))
+		} else if okArray {
+			length = len(dynamicArrayValue)
+		} else {
+			return []byte{}, fmt.Errorf("cannot cast value to string or array dynamic in encoding")
+		}
+		castedType, err := t.typeCastToTuple(length)
+		if err != nil {
+			return []byte{}, err
+		}
+		return castedType.Encode(value)
+	case Tuple:
+		return encodeTuple(value, t.childTypes)
+	default:
+		return []byte{}, fmt.Errorf("cannot infer type for encoding")
+	}
+}
+
+// encodeInt encodes int-alike golang values to bytes, following ABI encoding rules
+func encodeInt(intValue interface{}, bitSize uint16) ([]byte, error) {
+	var bigInt big.Int
+
+	switch intValue.(type) {
+	case int8:
+		if intValue.(int8) < 0 {
+			return []byte{}, fmt.Errorf("passed in int value should be non negative")
+		}
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(int8)))
+	case uint8:
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(uint8)))
+	case int16:
+		if intValue.(int16) < 0 {
+			return []byte{}, fmt.Errorf("passed in int value should be non negative")
+		}
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(int16)))
+	case uint16:
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(uint16)))
+	case int32:
+		if intValue.(int16) < 0 {
+			return []byte{}, fmt.Errorf("passed in int value should be non negative")
+		}
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(int32)))
+	case uint32:
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(uint32)))
+	case int64:
+		if intValue.(int16) < 0 {
+			return []byte{}, fmt.Errorf("passed in int value should be non negative")
+		}
+		bigInt = *new(big.Int).SetUint64(uint64(intValue.(int64)))
+	case uint64:
+		bigInt = *new(big.Int).SetUint64(intValue.(uint64))
+	case *big.Int:
+		bigInt = *new(big.Int).Set(intValue.(*big.Int))
+	default:
+		return []byte{}, fmt.Errorf("cannot infer go type for uint encode")
+	}
+
+	bytes := bigInt.Bytes()
+	if len(bytes) > int(bitSize/8) {
+		return []byte{}, fmt.Errorf("input value bit size %d > abi type bit size %d", len(bytes)*8, bitSize)
+	}
+
+	zeroPadding := make([]byte, bitSize/8-uint16(len(bytes)))
+	buffer := append(zeroPadding, bytes...)
+	return buffer, nil
+}
+
+// encodeTuple encodes slice-of-interface of golang values to bytes, following ABI encoding rules
+func encodeTuple(value interface{}, childT []Type) ([]byte, error) {
+	if len(childT) >= (1 << 16) {
+		return []byte{}, fmt.Errorf("abi child type number exceeds uint16 maximum")
+	}
+	values, ok := value.([]interface{})
+	if !ok {
+		return []byte{}, fmt.Errorf("cannot cast value to slice for tuple encoding")
+	}
+	if len(values) != len(childT) {
+		return []byte{}, fmt.Errorf("cannot encode abi tuple: value slice length != child type number")
+	}
+
+	// for each tuple element value, it has a head/tail component
+	// we create slots for head/tail bytes now, store them and concat them later
+	heads := make([][]byte, len(childT))
+	tails := make([][]byte, len(childT))
+	isDynamicIndex := make(map[int]bool)
+
+	for i := 0; i < len(childT); i++ {
+		if childT[i].IsDynamic() {
+			// if it is a dynamic value, the head component is not pre-determined
+			// we store an empty placeholder first, since we will need it in byte length calculation
+			headsPlaceholder := []byte{0x00, 0x00}
+			heads[i] = headsPlaceholder
+			// we keep track that the index points to a dynamic value
+			isDynamicIndex[i] = true
+			tailEncoding, err := childT[i].Encode(values[i])
+			if err != nil {
+				return []byte{}, err
+			}
+			tails[i] = tailEncoding
+			isDynamicIndex[i] = true
+		} else if childT[i].abiTypeID == Bool {
+			// search previous bool
+			before := findBoolLR(childT, i, -1)
+			// search after bool
+			after := findBoolLR(childT, i, 1)
+			// append to heads and tails
+			if before%8 != 0 {
+				return []byte{}, fmt.Errorf("cannot encode abi tuple: expected before has number of bool mod 8 == 0")
+			}
+			if after > 7 {
+				after = 7
+			}
+			compressed, err := compressBools(values[i : i+after+1])
+			if err != nil {
+				return []byte{}, err
+			}
+			heads[i] = []byte{compressed}
+			i += after
+			isDynamicIndex[i] = false
+		} else {
+			encodeTi, err := childT[i].Encode(values[i])
+			if err != nil {
+				return []byte{}, err
+			}
+			heads[i] = encodeTi
+			isDynamicIndex[i] = false
+		}
+	}
+
+	// adjust heads for dynamic type
+	// since head size can be pre-determined (for we are storing static value and dynamic value index in head)
+	// we accumulate the head size first
+	// (also note that though head size is pre-determined, head value is not necessarily pre-determined)
+	headLength := 0
+	for _, headTi := range heads {
+		headLength += len(headTi)
+	}
+
+	// when we iterate through the heads (byte slice), we need to find heads for dynamic values
+	// the head should correspond to the start index: len( head(x[1]) ... head(x[N]) tail(x[1]) ... tail(x[i-1]) ).
+	tailCurrLength := 0
+	for i := 0; i < len(heads); i++ {
+		if isDynamicIndex[i] {
+			// calculate where the index of dynamic value encoding byte start
+			headValue := headLength + tailCurrLength
+			if headValue >= (1 << 16) {
+				return []byte{}, fmt.Errorf("cannot encode abi tuple: encode length exceeds uint16 maximum")
+			}
+			binary.BigEndian.PutUint16(heads[i], uint16(headValue))
+		}
+		// accumulate the current tailing dynamic encoding bytes length.
+		tailCurrLength += len(tails[i])
+	}
+
+	// concat everything as the abi encoded bytes
+	encoded := make([]byte, 0, headLength+tailCurrLength)
+	for _, head := range heads {
+		encoded = append(encoded, head...)
+	}
+	for _, tail := range tails {
+		encoded = append(encoded, tail...)
+	}
+	return encoded, nil
+}
+
+// compressBools takes a slice of interface{} (which can be casted to bools) length <= 8
+// and compress the bool values into a uint8 integer
+func compressBools(boolSlice []interface{}) (uint8, error) {
+	var res uint8 = 0
+	if len(boolSlice) > 8 {
+		return 0, fmt.Errorf("compressBools: cannot have slice length > 8")
+	}
+	for i := 0; i < len(boolSlice); i++ {
+		temp, ok := boolSlice[i].(bool)
+		if !ok {
+			return 0, fmt.Errorf("compressBools: cannot cast slice element to bool")
+		}
+		if temp {
+			res |= 1 << uint(7-i)
+		}
+	}
+	return res, nil
+}
+
+// Decode is an ABI type method to decode bytes to go values from ABI encoding rules
+func (t Type) Decode(encoded []byte) (interface{}, error) {
+	switch t.abiTypeID {
+	case Uint, Ufixed:
+		if len(encoded) != int(t.bitSize)/8 {
+			return nil,
+				fmt.Errorf("uint/ufixed decode: expected byte length %d, but got byte length %d", t.bitSize/8, len(encoded))
+		}
+		return new(big.Int).SetBytes(encoded), nil
+	case Bool:
+		if len(encoded) != 1 {
+			return Value{}, fmt.Errorf("boolean byte should be length 1 byte")
+		}
+		if encoded[0] == 0x00 {
+			return false, nil
+		} else if encoded[0] == 0x80 {
+			return true, nil
+		}
+		return Value{}, fmt.Errorf("sinble boolean encoded byte should be of form 0x80 or 0x00")
+	case Byte:
+		if len(encoded) != 1 {
+			return Value{}, fmt.Errorf("byte should be length 1")
+		}
+		return encoded[0], nil
+	case ArrayStatic:
+		castedType, err := t.typeCastToTuple()
+		if err != nil {
+			return nil, err
+		}
+		return castedType.Decode(encoded)
+	case Address:
+		if len(encoded) != addressByteSize {
+			return nil, fmt.Errorf("address should be length 32")
+		}
+		var byteAssign [addressByteSize]byte
+		copy(byteAssign[:], encoded)
+		return byteAssign, nil
+	case ArrayDynamic:
+		if len(encoded) < lengthEncodeByteSize {
+			return Value{}, fmt.Errorf("dynamic array format corrupted")
+		}
+		dynamicLen := binary.BigEndian.Uint16(encoded[:lengthEncodeByteSize])
+		castedType, err := t.typeCastToTuple(int(dynamicLen))
+		if err != nil {
+			return nil, err
+		}
+		return castedType.Decode(encoded[lengthEncodeByteSize:])
+	case String:
+		if len(encoded) < lengthEncodeByteSize {
+			return Value{}, fmt.Errorf("string format corrupted")
+		}
+		stringLenBytes := encoded[:lengthEncodeByteSize]
+		byteLen := binary.BigEndian.Uint16(stringLenBytes)
+		if len(encoded[lengthEncodeByteSize:]) != int(byteLen) {
+			return nil, fmt.Errorf("string representation in byte: length not matching")
+		}
+		return string(encoded[lengthEncodeByteSize:]), nil
+	case Tuple:
+		return decodeTuple(encoded, t.childTypes)
+	default:
+		return []byte{}, fmt.Errorf("cannot infer type for decoding")
+	}
+}
+
+// decodeTuple decodes byte slice with ABI type slice, outputting a slice of golang interface values
+// following ABI encoding rules
+func decodeTuple(encoded []byte, childT []Type) ([]interface{}, error) {
+	dynamicSegments := make([]int, 0, len(childT)+1)
+	valuePartition := make([][]byte, 0, len(childT))
+	iterIndex := 0
+
+	for i := 0; i < len(childT); i++ {
+		if childT[i].IsDynamic() {
+			if len(encoded[iterIndex:]) < lengthEncodeByteSize {
+				return nil, fmt.Errorf("ill formed tuple dynamic typed value encoding")
+			}
+			dynamicIndex := binary.BigEndian.Uint16(encoded[iterIndex : iterIndex+lengthEncodeByteSize])
+			dynamicSegments = append(dynamicSegments, int(dynamicIndex))
+			valuePartition = append(valuePartition, nil)
+			iterIndex += lengthEncodeByteSize
+		} else if childT[i].abiTypeID == Bool {
+			// search previous bool
+			before := findBoolLR(childT, i, -1)
+			// search after bool
+			after := findBoolLR(childT, i, 1)
+			if before%8 == 0 {
+				if after > 7 {
+					after = 7
+				}
+				// parse bool in a byte to multiple byte strings
+				for boolIndex := uint(0); boolIndex <= uint(after); boolIndex++ {
+					boolMask := 0x80 >> boolIndex
+					if encoded[iterIndex]&byte(boolMask) > 0 {
+						valuePartition = append(valuePartition, []byte{0x80})
+					} else {
+						valuePartition = append(valuePartition, []byte{0x00})
+					}
+				}
+				i += after
+				iterIndex++
+			} else {
+				return nil, fmt.Errorf("expected before bool number mod 8 == 0")
+			}
+		} else {
+			// not bool ...
+			currLen, err := childT[i].ByteLen()
+			if err != nil {
+				return nil, err
+			}
+			valuePartition = append(valuePartition, encoded[iterIndex:iterIndex+currLen])
+			iterIndex += currLen
+		}
+	}
+
+	if len(dynamicSegments) > 0 {
+		dynamicSegments = append(dynamicSegments, len(encoded))
+		iterIndex = len(encoded)
+	}
+	if iterIndex < len(encoded) {
+		return nil, fmt.Errorf("input byte not fully consumed")
+	}
+	for i := 0; i < len(childT)-1; i++ {
+		if dynamicSegments[i] > dynamicSegments[i+1] {
+			return nil, fmt.Errorf("dynamic segment should display a [l, r] space with l <= r")
+		}
+	}
+
+	segIndex := 0
+	for i := 0; i < len(childT); i++ {
+		if childT[i].IsDynamic() {
+			valuePartition[i] = encoded[dynamicSegments[segIndex]:dynamicSegments[segIndex+1]]
+			segIndex++
+		}
+	}
+
+	values := make([]interface{}, len(childT))
+	for i := 0; i < len(childT); i++ {
+		var err error
+		values[i], err = childT[i].Decode(valuePartition[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return values, nil
+}
+
 // arrayToTuple casts an array-like ABI Value into an ABI Value of Tuple type.
 // This is used in both ABI Encoding and Decoding.
 func (v Value) arrayToTuple() (Value, error) {
