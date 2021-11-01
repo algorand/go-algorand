8b77908521a907cffc853dfa457ee9f44d4f88d6
Will Winder <wwinder.unh@gmail.com>
2021-11-01 09:10:54 -0400

    Avoid printing genesis path in error message.

--- node/node.go
+++ node/node.go
@@ -786,7 +786,7 @@ func (node *AlgorandFullNode) loadParticipationKeys() error {
 	genesisDir := filepath.Join(node.rootDir, node.genesisID)
 	files, err := ioutil.ReadDir(genesisDir)
 	if err != nil {
-		return fmt.Errorf("AlgorandFullNode.loadPartitipationKeys: could not read directory %v: %v", genesisDir, err)
+		return fmt.Errorf("AlgorandFullNode.loadPartitipationKeys: could not read genesis directory: %v", err)
 	}
 
 	// For each of these files
