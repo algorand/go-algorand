bd074ea7d3113f50068e6d9228d4f8efc56527b6
Or Aharonee <or.aharonee@algorand.com>
2021-11-22 13:27:34 +0200

    Replace falcon with Ed25519 since it takes much less time

--- crypto/merklekeystore/keystore_test.go
+++ crypto/merklekeystore/keystore_test.go
@@ -490,19 +490,19 @@ func TestValidityPeriod(t *testing.T) {
 	store := initTestDB(a)
 	firstValid := uint64(0)
 	lastValid := maxValidPeriod
-	_, err := New(firstValid, lastValid, crypto.FalconType, store)
+	_, err := New(firstValid, lastValid, crypto.Ed25519Type, store)
 	a.NoError(err)
 
 	store = initTestDB(a)
 	firstValid = uint64(0)
 	lastValid = maxValidPeriod + 1
-	_, err = New(firstValid, lastValid, crypto.FalconType, store)
+	_, err = New(firstValid, lastValid, crypto.Ed25519Type, store)
 	a.Error(err)
 
 	store = initTestDB(a)
 	firstValid = uint64(0)
 	lastValid = maxValidPeriod - 1
-	_, err = New(firstValid, lastValid, crypto.FalconType, store)
+	_, err = New(firstValid, lastValid, crypto.Ed25519Type, store)
 	a.NoError(err)
 }
 
