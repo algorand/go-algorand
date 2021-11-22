e1a2212e72cbc1d33c5dd58c8ae8351017164632
algonautshant <shant@algorand.com>
2021-11-22 10:26:31 -0500

    Fixes to rest-participation-key - Test to make sure RES has the right input before counting line numbers for result size. - Rest RES to empty so that the same output is not recycled in case of an error. - exit 1 in case of an error - Reduce LAST_ROUND from 1200000 to 120 - "Get List of Keys" before getting NUM_IDS_3 otherwise it will recycle old RES value.

--- test/scripts/e2e_subs/rest-participation-key.sh
+++ test/scripts/e2e_subs/rest-participation-key.sh
@@ -9,33 +9,48 @@ date "+$0 start %Y%m%d_%H%M%S"
 # Use admin token for both get and post
 export USE_ADMIN=true
 
-pushd "${TEMPDIR}" || exit
+pushd "${TEMPDIR}" || exit 1
 
 FIRST_ROUND=0
 # A really large (but arbitrary) last valid round
-LAST_ROUND=1200000
+LAST_ROUND=120
 
 NAME_OF_TEMP_PARTKEY="tmp.${FIRST_ROUND}.${LAST_ROUND}.partkey"
 
 algokey part generate --first ${FIRST_ROUND} --last ${LAST_ROUND} --keyfile ${NAME_OF_TEMP_PARTKEY} --parent ${ACCOUNT}
 
-popd || exit
+popd || exit 1
 
 call_and_verify "Get List of Keys" "/v2/participation" 200 'address'
 
 # Find out how many keys there are installed so far
+if [[ $RES != *"address"*"effective-first-valid"* ]]; then
+    echo "Unexpected output: " $RES
+    exit 1
+fi
 NUM_IDS_1=$(echo "$RES" | python3 -c 'import json,sys;o=json.load(sys.stdin);print(len(o))')
+RES=""
 
 call_post_and_verify "Install a basic participation key" "/v2/participation" 200 ${NAME_OF_TEMP_PARTKEY} 'partId'
 
 # Get the returned participation id from the RESULT (aka $RES) variable
+if [[ $RES != *"partId"* ]]; then
+    echo "Unexpected output: " $RES
+    exit 1
+fi
 INSTALLED_ID=$(echo "$RES" | python3 -c 'import json,sys;o=json.load(sys.stdin);print(o["partId"])')
+RES=""
 
 # Should contain the installed id
 call_and_verify "Get List of Keys" "/v2/participation" 200 'address' "${INSTALLED_ID}"
 
 # Get list of keys
+if [[ $RES != *"address"*"effective-first-valid"* ]]; then
+    echo "Unexpected output: " $RES
+    exit 1
+fi
 NUM_IDS_2=$(echo "$RES" | python3 -c 'import json,sys;o=json.load(sys.stdin);print(len(o))')
+RES=""
 
 if [[ $((NUM_IDS_1 + 1)) -ne $NUM_IDS_2 ]]; then
   printf "\n\nFailed test.  New number of IDs (%s) is not one more than old ID count(%s)\n\n" "${NUM_IDS_2}" "${NUM_IDS_1}"
@@ -51,7 +66,13 @@ call_delete_and_verify "Delete the specific ID" "/v2/participation/${INSTALLED_I
 call_delete_and_verify "Delete the specific ID" "/v2/participation/${INSTALLED_ID}" 404 true 'participation id not found'
 
 # Get list of keys
+call_and_verify "Get List of Keys" "/v2/participation" 200 'address'
+if [[ $RES != *"address"*"effective-first-valid"* ]]; then
+    echo "Unexpected output: " $RES
+    exit 1
+fi
 NUM_IDS_3=$(echo "$RES" | python3 -c 'import json,sys;o=json.load(sys.stdin);print(len(o))')
+RES=""
 
 if [[ "$NUM_IDS_3" -ne "$NUM_IDS_1" ]]; then
   printf "\n\nFailed test.  New number of IDs (%s) is not equal to original ID count (%s)\n\n" "${NUM_IDS_3}" "${NUM_IDS_1}"
