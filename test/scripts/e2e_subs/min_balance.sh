035432d8456658f814f9101ab62d0bf34edbde66
Zeph Grunschlag <zeph@algorand.com>
2021-12-10 09:47:43 -0600

    simple goal e2e test is passing

--- test/scripts/e2e_subs/min_balance.sh
+++ test/scripts/e2e_subs/min_balance.sh
@@ -12,6 +12,9 @@ echo "NETDIR=$NETDIR"
 # Directory of this bash program
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "DIR=$DIR"
+
+
 gcmd="goal -w ${WALLET}"
 
 ACCOUNT=$(${gcmd} account list|awk '{ print $3 }')
@@ -23,6 +26,14 @@ echo `$gcmd account balance -a $ACCOUNT`
 
 echo `$gcmd account info -a $ACCOUNT`
 
-echo `$gcmd account export -a $ACCOUNT`
+MINBAL=$(${gcmd} account info -a ${ACCOUNT}|grep 'Minimum Balance:'| awk '{ print $3 }')
 
-exit 1 
+echo "Minimum Balance --> $MINBAL"
+
+EXPECTED="100000"
+if [[ ${MINBAL} != ${EXPECTED} ]]; then
+    date '+min_balance FAIL goal account info should return expected Minimum Ballance %Y%m%d_%H%M%S'
+    false
+fi
+
+# see ./min_balance.py for more complicated scenarios
