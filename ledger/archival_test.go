babc32ff44d5188e6914abb884ca78d2329880ba
algonautshant <shant@algorand.com>
2021-12-15 23:42:33 -0500

    slow down the adding of blocks

--- ledger/archival_test.go
+++ ledger/archival_test.go
@@ -28,6 +28,7 @@ import (
 	"reflect"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -745,6 +746,7 @@ func TestArchivalFromNonArchival(t *testing.T) {
 			stxnib := makeSignedTxnInBlock(tx)
 			blk.Payset = append(blk.Payset, stxnib)
 			blk.BlockHeader.TxnCounter++
+			time.Sleep(10*time.Millisecond)
 		}
 
 		err := l.AddBlock(blk, agreement.Certificate{})
