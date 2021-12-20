f7c3cbb489d13ffba29de5f56343299761e32fe7
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-12-20 08:42:23 -0500

    adding absolute value and comments with reasoning

--- cmd/tealdbg/server.go
+++ cmd/tealdbg/server.go
@@ -30,9 +30,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	// WebSocketBufferSize is the size of the buffer for the
+	// tealdbg/cdt websocket session.
+	// A buffer that is too small will cause the session to choke
+	// during the `getScriptSource` call and the session cannot recover.
+	WebSocketReadBufferSize  = 81920
+	WebSocketWriteBufferSize = 81920
+)
+
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  20480 * 4,
-	WriteBufferSize: 20480 * 4,
+	ReadBufferSize:  WebSocketReadBufferSize,
+	WriteBufferSize: WebSocketWriteBufferSize,
 	CheckOrigin: func(r *http.Request) bool {
 		if len(r.Header.Get("Origin")) == 0 {
 			return true
