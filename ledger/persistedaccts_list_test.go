a8bc7e753af545a0cbf62e0516d4f9c5fd697ddc
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2021-12-21 21:15:47 -0500

    ledger refactoring: more tests and fixes (#3344)
    Added lruResources and persistedResourcesDataList tests similar to their accounts counterparties.

--- ledger/persistedaccts_list_test.go
+++ ledger/persistedaccts_list_test.go
@@ -23,53 +23,50 @@ import (
 	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
-func checkLen(list *persistedAccountDataList) int {
-	if isEmpty(list) {
-		return 0
-	}
-	return countListSize(&list.root)
+type dataListNode interface {
+	getNext() dataListNode
+	getPrev() dataListNode
 }
 
-func countListSize(head *persistedAccountDataListNode) (counter int) {
-	for i := head.next; i != head && i != nil; i = i.next {
+type dataList interface {
+	getRoot() dataListNode
+}
+
+func (l *persistedAccountDataList) getRoot() dataListNode {
+	return &l.root
+}
+
+func (l *persistedAccountDataListNode) getNext() dataListNode {
+	// get rid of returning nil wrapped into an interface to let i = x.getNext(); i != nil work.
+	if l.next == nil {
+		return nil
+	}
+	return l.next
+}
+
+func (l *persistedAccountDataListNode) getPrev() dataListNode {
+	if l.prev == nil {
+		return nil
+	}
+	return l.prev
+}
+
+func checkLen(list dataList) int {
+	if list.getRoot().getNext() == list.getRoot() {
+		return 0
+	}
+
+	return countListSize(list.getRoot())
+}
+
+func countListSize(head dataListNode) (counter int) {
+	for i := head.getNext(); i != head && i != nil; i = i.getNext() {
 		counter++
 	}
 	return counter
 }
 
-func TestRemoveFromList(t *testing.T) {
-	partitiontest.PartitionTest(t)
-	l := newPersistedAccountList()
-	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
-	e2 := l.pushFront(&persistedAccountData{addr: basics.Address{2}})
-	e3 := l.pushFront(&persistedAccountData{addr: basics.Address{3}})
-	checkListPointers(t, l, []*persistedAccountDataListNode{e3, e2, e1})
-
-	l.remove(e2)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e3, e1})
-	l.remove(e3)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e1})
-}
-
-func TestAddingNewNodeWithAllocatedFreeList(t *testing.T) {
-	partitiontest.PartitionTest(t)
-	l := newPersistedAccountList().allocateFreeNodes(10)
-	checkListPointers(t, l, []*persistedAccountDataListNode{})
-	if countListSize(l.freeList) != 10 {
-		t.Errorf("free list did not allocate nodes")
-		return
-	}
-	// test elements
-	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
-	checkListPointers(t, l, []*persistedAccountDataListNode{e1})
-
-	if countListSize(l.freeList) != 9 {
-		t.Errorf("free list did not provide a node on new list entry")
-		return
-	}
-}
-
-func checkListLen(t *testing.T, l *persistedAccountDataList, len int) bool {
+func checkListLen(t *testing.T, l dataList, len int) bool {
 	if n := checkLen(l); n != len {
 		t.Errorf("l.Len() = %d, want %d", n, len)
 		return true
@@ -77,41 +74,81 @@ func checkListLen(t *testing.T, l *persistedAccountDataList, len int) bool {
 	return false
 }
 
-// inspect that the list seems like the array
-func checkListPointers(t *testing.T, l *persistedAccountDataList, es []*persistedAccountDataListNode) {
-	root := &l.root
+func TestRemoveFromListAD(t *testing.T) {
+	partitiontest.PartitionTest(t)
+	l := newPersistedAccountList()
+	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
+	e2 := l.pushFront(&persistedAccountData{addr: basics.Address{2}})
+	e3 := l.pushFront(&persistedAccountData{addr: basics.Address{3}})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e3, e2, e1})
+
+	l.remove(e2)
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e3, e1})
+	l.remove(e3)
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e1})
+}
+
+func TestAddingNewNodeWithAllocatedFreeListAD(t *testing.T) {
+	partitiontest.PartitionTest(t)
+	l := newPersistedAccountList().allocateFreeNodes(10)
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{})
+	if countListSize(l.freeList) != 10 {
+		t.Errorf("free list did not allocate nodes")
+		return
+	}
+	// test elements
+	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e1})
+
+	if countListSize(l.freeList) != 9 {
+		t.Errorf("free list did not provide a node on new list entry")
+		return
+	}
+}
+
+func checkListPointers(t *testing.T, l dataList, es []dataListNode) {
+	root := l.getRoot()
 
 	if failed := checkListLen(t, l, len(es)); failed {
 		return
 	}
 
-	if failed := zeroListInspection(t, l, es, root); failed {
+	if failed := zeroListInspection(t, l, len(es), root); failed {
 		return
 	}
 
-	// len(es) > 0
 	pointerInspection(t, es, root)
 }
 
-func zeroListInspection(t *testing.T, l *persistedAccountDataList, es []*persistedAccountDataListNode, root *persistedAccountDataListNode) bool {
+// inspect that the list seems like the array
+func checkListPointersAD(t *testing.T, l *persistedAccountDataList, es []*persistedAccountDataListNode) {
+	es2 := make([]dataListNode, len(es))
+	for i, el := range es {
+		es2[i] = el
+	}
+
+	checkListPointers(t, l, es2)
+}
+
+func zeroListInspection(t *testing.T, l dataList, len int, root dataListNode) bool {
 	// zero length lists must be the zero value or properly initialized (sentinel circle)
-	if len(es) == 0 {
-		if l.root.next != nil && l.root.next != root || l.root.prev != nil && l.root.prev != root {
-			t.Errorf("l.root.next = %p, l.root.prev = %p; both should both be nil or %p", l.root.next, l.root.prev, root)
+	if len == 0 {
+		if l.getRoot().getNext() != nil && l.getRoot().getNext() != root || l.getRoot().getPrev() != nil && l.getRoot().getPrev() != root {
+			t.Errorf("l.root.next = %p, l.root.prev = %p; both should both be nil or %p", l.getRoot().getNext(), l.getRoot().getPrev(), root)
 		}
 		return true
 	}
 	return false
 }
 
-func pointerInspection(t *testing.T, es []*persistedAccountDataListNode, root *persistedAccountDataListNode) {
+func pointerInspection(t *testing.T, es []dataListNode, root dataListNode) {
 	// check internal and external prev/next connections
 	for i, e := range es {
 		prev := root
 		if i > 0 {
 			prev = es[i-1]
 		}
-		if p := e.prev; p != prev {
+		if p := e.getPrev(); p != prev {
 			t.Errorf("elt[%d](%p).prev = %p, want %p", i, e, p, prev)
 		}
 
@@ -119,16 +156,16 @@ func pointerInspection(t *testing.T, es []*persistedAccountDataListNode, root *p
 		if i < len(es)-1 {
 			next = es[i+1]
 		}
-		if n := e.next; n != next {
+		if n := e.getNext(); n != next {
 			t.Errorf("elt[%d](%p).next = %p, want %p", i, e, n, next)
 		}
 	}
 }
 
-func TestMultielementListPositioning(t *testing.T) {
+func TestMultielementListPositioningAD(t *testing.T) {
 	partitiontest.PartitionTest(t)
 	l := newPersistedAccountList()
-	checkListPointers(t, l, []*persistedAccountDataListNode{})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{})
 	// test elements
 	e2 := l.pushFront(&persistedAccountData{addr: basics.Address{2}})
 	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
@@ -136,73 +173,73 @@ func TestMultielementListPositioning(t *testing.T) {
 	e4 := l.pushFront(&persistedAccountData{addr: basics.Address{4}})
 	e5 := l.pushFront(&persistedAccountData{addr: basics.Address{5}})
 
-	checkListPointers(t, l, []*persistedAccountDataListNode{e5, e4, e3, e1, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e5, e4, e3, e1, e2})
 
 	l.move(e4, e1)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e5, e3, e1, e4, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e5, e3, e1, e4, e2})
 
 	l.remove(e5)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e3, e1, e4, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e3, e1, e4, e2})
 
 	l.move(e1, e4) // swap in middle
-	checkListPointers(t, l, []*persistedAccountDataListNode{e3, e4, e1, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e3, e4, e1, e2})
 
 	l.moveToFront(e4)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e4, e3, e1, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e4, e3, e1, e2})
 
 	l.remove(e2)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e4, e3, e1})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e4, e3, e1})
 
 	l.moveToFront(e3) // move from middle
-	checkListPointers(t, l, []*persistedAccountDataListNode{e3, e4, e1})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e3, e4, e1})
 
 	l.moveToFront(e1) // move from end
-	checkListPointers(t, l, []*persistedAccountDataListNode{e1, e3, e4})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e1, e3, e4})
 
 	l.moveToFront(e1) // no movement
-	checkListPointers(t, l, []*persistedAccountDataListNode{e1, e3, e4})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e1, e3, e4})
 
 	e2 = l.pushFront(&persistedAccountData{addr: basics.Address{2}})
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2, e1, e3, e4})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2, e1, e3, e4})
 
 	l.remove(e3) // removing from middle
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2, e1, e4})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2, e1, e4})
 
 	l.remove(e4) // removing from end
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2, e1})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2, e1})
 
 	l.move(e2, e1) // swapping between two elements
-	checkListPointers(t, l, []*persistedAccountDataListNode{e1, e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e1, e2})
 
 	l.remove(e1) // removing front
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2})
 
 	l.move(e2, l.back()) // swapping element with itself.
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2})
 
 	l.remove(e2) // remove last one
-	checkListPointers(t, l, []*persistedAccountDataListNode{})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{})
 }
 
-func TestSingleElementListPositioning(t *testing.T) {
+func TestSingleElementListPositioningAD(t *testing.T) {
 	partitiontest.PartitionTest(t)
 	l := newPersistedAccountList()
-	checkListPointers(t, l, []*persistedAccountDataListNode{})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{})
 	e := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
-	checkListPointers(t, l, []*persistedAccountDataListNode{e})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e})
 	l.moveToFront(e)
-	checkListPointers(t, l, []*persistedAccountDataListNode{e})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e})
 	l.remove(e)
-	checkListPointers(t, l, []*persistedAccountDataListNode{})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{})
 }
 
-func TestRemovedNodeShouldBeMovedToFreeList(t *testing.T) {
+func TestRemovedNodeShouldBeMovedToFreeListAD(t *testing.T) {
 	partitiontest.PartitionTest(t)
 	l := newPersistedAccountList()
 	e1 := l.pushFront(&persistedAccountData{addr: basics.Address{1}})
 	e2 := l.pushFront(&persistedAccountData{addr: basics.Address{2}})
 
-	checkListPointers(t, l, []*persistedAccountDataListNode{e2, e1})
+	checkListPointersAD(t, l, []*persistedAccountDataListNode{e2, e1})
 
 	e := l.back()
 	l.remove(e)
@@ -214,5 +251,4 @@ func TestRemovedNodeShouldBeMovedToFreeList(t *testing.T) {
 		}
 	}
 	t.Error("expected the removed node to appear at the freelist")
-
 }
