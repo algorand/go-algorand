3904ed8f42bd8403931585a6cc0f5fe4f3748412
Shant Karakashian <55754073+algonautshant@users.noreply.github.com>
2022-01-12 16:51:05 -0500

    signer.KeyDilution need not depend on config package (#3265)
    crypto package need not depend on config.
    There is an unnecessary dependency on config.
    
    signer.KeyDilution takes the `config.ConsensusParams` as argument to pick the DefaultKeyDilution from it. 
    This introduces dependency from the crypto package to config package. 
    Instead, only the DefaultKeyDilution value can be passed to signer.KeyDilution.

--- crypto/onetimesig.go
+++ crypto/onetimesig.go
@@ -20,11 +20,9 @@ import (
 	"encoding/binary"
 	"fmt"
 
-	"github.com/algorand/go-deadlock"
-
-	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/logging"
 	"github.com/algorand/go-algorand/protocol"
+	"github.com/algorand/go-deadlock"
 )
 
 // A OneTimeSignature is a cryptographic signature that is produced a limited
@@ -432,10 +430,10 @@ type OneTimeSigner struct {
 }
 
 // KeyDilution returns the appropriate key dilution value for a OneTimeSigner.
-func (ots OneTimeSigner) KeyDilution(params config.ConsensusParams) uint64 {
+func (ots OneTimeSigner) KeyDilution(defaultKeyDilution uint64) uint64 {
 	if ots.OptionalKeyDilution != 0 {
 		return ots.OptionalKeyDilution
 	}
 
-	return params.DefaultKeyDilution
+	return defaultKeyDilution
 }
