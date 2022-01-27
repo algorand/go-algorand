9169a002f62b996c85b0e5224c1c8caf66c9b7ab
algoidan <idan@algorand.com>
2022-01-27 15:35:46 +0200

    CR fix

--- crypto/merklearray/proof_test.go
+++ crypto/merklearray/proof_test.go
@@ -25,102 +25,108 @@ import (
 
 func TestProofSerialization(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	a := require.New(t)
 
-	a := make(TestArray, 3)
+	array := make(TestArray, 3)
 	for i := uint64(0); i < 3; i++ {
-		crypto.RandBytes(a[i][:])
+		crypto.RandBytes(array[i][:])
 	}
 
-	tree, err := Build(a, crypto.HashFactory{HashType: crypto.Sha512_256})
-	require.NoError(t, err)
+	tree, err := Build(array, crypto.HashFactory{HashType: crypto.Sha512_256})
+	a.NoError(err)
 
 	// creates a proof with missing child
 	p, err := tree.ProveSingleLeaf(2)
-	require.NoError(t, err)
+	a.NoError(err)
 
 	data := p.GetFixedLengthHashableRepresentation()
-	require.Equal(t, len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
+	a.Equal(len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
 
 	// check the padded results
 	zeroDigest := make([]byte, crypto.Sha512_256Size)
 	i := 0
+	proofData := data[1:]
 	for ; i < (MaxEncodedTreeDepth - 2); i++ {
-		require.Equal(t, zeroDigest, data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+		a.Equal(zeroDigest, proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	}
 
 	// first proof digest is nil -> so the HashableRepresentation is zeros
-	require.Equal(t, crypto.GenericDigest(nil), p.Path[0])
-	require.Equal(t, zeroDigest, data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+	a.Equal(crypto.GenericDigest(nil), p.Path[0])
+	a.Equal(zeroDigest, proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	i++
 
-	require.Equal(t, []byte(p.Path[1]), data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+	a.Equal([]byte(p.Path[1]), proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 
 	//VC
-	tree, err = BuildVectorCommitmentTree(a, crypto.HashFactory{HashType: crypto.Sha512_256})
-	require.NoError(t, err)
+	tree, err = BuildVectorCommitmentTree(array, crypto.HashFactory{HashType: crypto.Sha512_256})
+	a.NoError(err)
 
 	// creates a proof with missing child
 	p, err = tree.ProveSingleLeaf(2)
-	require.NoError(t, err)
+	a.NoError(err)
 
 	data = p.GetFixedLengthHashableRepresentation()
-	require.Equal(t, len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
+	a.Equal(len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
 
 	// check the padded results
 	zeroDigest = make([]byte, crypto.Sha512_256Size)
 	i = 0
+	proofData = data[1:]
 	for ; i < (MaxEncodedTreeDepth - 2); i++ {
-		require.Equal(t, zeroDigest, data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+		a.Equal(zeroDigest, proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	}
 
-	require.Equal(t, []byte(p.Path[0]), data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+	a.Equal([]byte(p.Path[0]), proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	i++
-	require.Equal(t, []byte(p.Path[1]), data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+	a.Equal([]byte(p.Path[1]), proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 
 }
 
 func TestProofSerializationMaxTree(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	a := require.New(t)
 
-	a := make(TestArray, MaxNumLeavesOnEncodedTree)
+	array := make(TestArray, MaxNumLeavesOnEncodedTree)
 	for i := uint64(0); i < MaxNumLeavesOnEncodedTree; i++ {
-		crypto.RandBytes(a[i][:])
+		crypto.RandBytes(array[i][:])
 	}
 
-	tree, err := BuildVectorCommitmentTree(a, crypto.HashFactory{HashType: crypto.Sha512_256})
-	require.NoError(t, err)
+	tree, err := BuildVectorCommitmentTree(array, crypto.HashFactory{HashType: crypto.Sha512_256})
+	a.NoError(err)
 
 	p, err := tree.ProveSingleLeaf(2)
-	require.NoError(t, err)
+	a.NoError(err)
 
 	data := p.GetFixedLengthHashableRepresentation()
-	require.Equal(t, len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
+	a.Equal(len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
 
-	i := 0
-	for ; i < MaxEncodedTreeDepth; i++ {
-		require.Equal(t, []byte(p.Path[i]), data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+	proofData := data[1:]
+	for i := 0; i < MaxEncodedTreeDepth; i++ {
+		a.Equal([]byte(p.Path[i]), proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	}
 }
 
 func TestProofSerializationOneLeafTree(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	a := require.New(t)
 
-	a := make(TestArray, 1)
-	crypto.RandBytes(a[0][:])
+	array := make(TestArray, 1)
+	crypto.RandBytes(array[0][:])
 
-	tree, err := BuildVectorCommitmentTree(a, crypto.HashFactory{HashType: crypto.Sha512_256})
-	require.NoError(t, err)
+	tree, err := BuildVectorCommitmentTree(array, crypto.HashFactory{HashType: crypto.Sha512_256})
+	a.NoError(err)
 
 	p, err := tree.ProveSingleLeaf(0)
-	require.NoError(t, err)
+	a.NoError(err)
 
 	data := p.GetFixedLengthHashableRepresentation()
-	require.Equal(t, len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
+	a.Equal(len(data), 1+(MaxEncodedTreeDepth*crypto.Sha512_256Size))
 
 	zeroDigest := make([]byte, crypto.Sha512_256Size)
-	i := 0
-	for ; i < MaxEncodedTreeDepth; i++ {
-		require.Equal(t, zeroDigest, data[1+crypto.Sha512_256Size*i:1+crypto.Sha512_256Size*(i+1)])
+
+	proofData := data[1:]
+	for i := 0; i < MaxEncodedTreeDepth; i++ {
+		a.Equal(zeroDigest, proofData[crypto.Sha512_256Size*i:crypto.Sha512_256Size*(i+1)])
 	}
 
 }
