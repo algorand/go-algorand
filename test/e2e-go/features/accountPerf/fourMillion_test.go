41d1d4396c7d7d2bee0cf6c4161d0b115f12b60a
algonautshant <shant@algorand.com>
2022-02-02 00:57:57 -0500

    cleanup, add assets

--- test/e2e-go/features/accountPerf/fourMillion_test.go
+++ test/e2e-go/features/accountPerf/fourMillion_test.go
@@ -14,8 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
-// Check that devmode is functioning as designed.
-package devmode
+package fataccount
 
 import (
 	"fmt"
@@ -36,10 +35,10 @@ import (
 	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
-func queuePayments(wg *sync.WaitGroup, c libgoal.Client, q <-chan *transactions.SignedTxn) {
+func queuePayments(queueWg *sync.WaitGroup, c libgoal.Client, q <-chan *transactions.SignedTxn) {
 	counter := 0
-	for stxn := range q{
-		
+	for stxn := range q {
+
 		if stxn == nil {
 			break
 		}
@@ -47,29 +46,23 @@ func queuePayments(wg *sync.WaitGroup, c libgoal.Client, q <-chan *transactions.
 			_, err := c.BroadcastTransaction(*stxn)
 			if err == nil {
 				counter++
-				if counter % 100 == 0 {
-					fmt.Println("broadcasted: ", counter)
-				}
 				break
 			}
-
 			fmt.Printf("Error broadcasting transaction: %v\n", err)
 			time.Sleep(config.Consensus[protocol.ConsensusCurrentVersion].AgreementFilterTimeout)
 		}
 	}
-	wg.Done()
+	queueWg.Done()
 }
 
-func signer(t *testing.T, wg *sync.WaitGroup, client libgoal.Client, txnChan <-chan *transactions.Transaction, sigTxnChan chan<- *transactions.SignedTxn) {
+func signer(t *testing.T, sigWg *sync.WaitGroup, client libgoal.Client, txnChan <-chan *transactions.Transaction, sigTxnChan chan<- *transactions.SignedTxn) {
 	for txn := range txnChan {
-		
 		if txn == nil {
 			break
 		}
-
 		walletHandle, err := client.GetUnencryptedWalletHandle()
 		require.NoError(t, err)
-		
+
 		stxn, err := client.SignTransactionWithWallet(walletHandle, nil, *txn)
 		if err != nil {
 			fmt.Printf("Error signing: %v\n", err)
@@ -77,43 +70,26 @@ func signer(t *testing.T, wg *sync.WaitGroup, client libgoal.Client, txnChan <-c
 		require.NoError(t, err)
 		sigTxnChan <- &stxn
 	}
-	wg.Done()
+	sigWg.Done()
 }
 
-func zeroSub(a, b int) int {
+func zeroSub(a, b uint64) uint64 {
 	if a > b {
-		return a-b
+		return a - b
 	}
-	return 0	
+	return 0
 }
 
 func Test5MAssets(t *testing.T) {
 	partitiontest.PartitionTest(t)
 
-	// Make the network progress faster
-	consensus := make(config.ConsensusProtocols)
-	fastProtocol := config.Consensus[protocol.ConsensusCurrentVersion]
-	fastProtocol.ApprovedUpgrades = map[protocol.ConsensusVersion]uint64{}
-	fastProtocol.AgreementFilterTimeoutPeriod0 = 400 * time.Millisecond
-	fastProtocol.AgreementFilterTimeout = 400 * time.Millisecond
-	//	consensus[protocol.ConsensusFuture] = fastProtocol
-	consensus[protocol.ConsensusCurrentVersion] = fastProtocol
-
-	// Setup the fixture with the modified fast consensus
 	var fixture fixtures.RestClientFixture
-	//	fixture.SetConsensus(consensus)
 
-	fixture.Setup(t, filepath.Join("nettemplates", "DevModeOneWallet.json")) //OneNodeFuture.json"))//DevModeOneWallet.json"))
-	// fixture.Setup(t, filepath.Join("nettemplates", "OneNodeFuture.json"))//OneNodeFuture.json"))//DevModeOneWallet.json"))
+	fixture.Setup(t, filepath.Join("nettemplates", "DevModeOneWallet.json"))
 	defer fixture.Shutdown()
 	client := fixture.LibGoalClient
 
-	numberOfAccounts := 6000 // 6K
-	//	numberOfAssets := 60     // 6M
-
-	// We will create three new accounts, transfer some amount of money into
-	// the first account, and then transfer a smaller amount to the second
-	// account while closing out the rest into the third.
+	numberOfThreads := 8
 
 	accountList, err := fixture.GetWalletsSortedByBalance()
 	require.NoError(t, err)
@@ -121,191 +97,157 @@ func Test5MAssets(t *testing.T) {
 
 	status, err := client.Status()
 	require.NoError(t, err)
-	fmt.Println(status)
-	if true {
-		var sigWg sync.WaitGroup
-		txnChan := make(chan *transactions.Transaction, 100)
-		sigTxnChan := make(chan *transactions.SignedTxn, 100)
-		for nthread := 0; nthread < 10; nthread++ {
-			sigWg.Add(1)
-			go signer(t, &sigWg, client, txnChan, sigTxnChan)
-		}
+	var sigWg sync.WaitGroup
+	var queueWg sync.WaitGroup
+	txnChan := make(chan *transactions.Transaction, 100)
+	sigTxnChan := make(chan *transactions.SignedTxn, 100)
 
-
-		suggestedParams, err := client.SuggestedParams()
-		require.NoError(t, err)
-		var genesisHash crypto.Digest
-		copy(genesisHash[:], suggestedParams.GenesisHash)
-
-
-		go func() {
-			sender, err := basics.UnmarshalChecksumAddress(baseAcct)
-			require.NoError(t, err)
-
-			params, err := client.SuggestedParams()
-			require.NoError(t, err)
-			proto := config.Consensus[protocol.ConsensusVersion(params.ConsensusVersion)]
-
-			round := 0
-			for txi := 0; txi < numberOfAccounts; txi++ {
-				if txi%100 == 0 {
-					fmt.Println("making txn: ", txi)
-				}
-
-				walletHandle, err := client.GetUnencryptedWalletHandle()
-				require.NoError(t, err)
-
-				accti, err := client.GenerateAddress(walletHandle)
-				require.NoError(t, err)
-
-				receiver, err := basics.UnmarshalChecksumAddress(accti)
-				require.NoError(t, err)
-
-				sround := zeroSub(round, 200)
-				txn := transactions.Transaction{
-					Type: protocol.PaymentTx,
-					Header: transactions.Header{
-						Sender:      sender,
-						Fee:         basics.MicroAlgos{Raw: config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee},
-						FirstValid:  basics.Round(sround),
-						LastValid:   basics.Round(sround) + basics.Round(proto.MaxTxnLife),
-						GenesisHash: genesisHash,
-					},
-					PaymentTxnFields: transactions.PaymentTxnFields{
-						Receiver: receiver,
-						Amount:   basics.MicroAlgos{Raw: 100000},
-					},
-				}
-				round++
-				txnChan <- &txn
-			}
-			fmt.Println("******************** closing txnChan")
-			close(txnChan)
-
-			sigWg.Wait()
-			fmt.Println("******************** closing sigTxnChan")
-			close(sigTxnChan)
-		}()
-
-		var queueWg sync.WaitGroup
-		queueTxns := make(chan *transactions.SignedTxn, 100)
-
-		go func() {		
-			//		var sigTxns []*transactions.SignedTxn
-			for stxn := range sigTxnChan{
-				
-				if stxn == nil {
-					break
-				}
-				queueTxns <- stxn
-				//			sigTxns = append(sigTxns, stxn)
-			}
-			fmt.Println("******************** closing queueTxns")
-			close(queueTxns)
-
-		}()
-
-		for nthread := 0; nthread < 10; nthread++ {
-			queueWg.Add(1)
-			go queuePayments(&queueWg, client, queueTxns)
-		}
-
-		queueWg.Wait()
-		status, err = client.Status()
-		require.NoError(t, err)
-
-		fmt.Printf("Waiting for round %d to start benchmark..\n", status.LastRound+1)
-		//		status, err = client.WaitForRound(status.LastRound + 1)
-		//		require.NoError(t, err)
-
-
-		fmt.Printf("Starting to issue %d transactions\n", numberOfAccounts)
-
-
-		//		for _, stxn := range sigTxns {
-		//			queueTxns <- stxn
-		//		}
-		//		close(queueTxns)
-
-		fmt.Printf("Waiting for transactions to be queued to algod..\n")
-		queueWg.Wait()
-
-		fmt.Printf("wait done..\n")
-
-		// Now, send a payment with low fee, so that when this transaction
-		// clears, we can infer that all previous transactions also cleared.
-		var tx transactions.Transaction
-		for {
-			tx, err = client.SendPaymentFromUnencryptedWallet(baseAcct, baseAcct, config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee, 0, nil)
-			if err == nil {
-				break
-			}
-
-			fmt.Printf("Error broadcasting final transaction: %v\n", err)
-			time.Sleep(5 * time.Second)
-		}
-		_, err = fixture.WaitForConfirmedTxn(status.LastRound+100, baseAcct, tx.ID().String())
-		fmt.Printf("Waiting for confirmation transaction to commit..\n")
-		require.NoError(t, err)
+	for nthread := 0; nthread < numberOfThreads; nthread++ {
+		sigWg.Add(1)
+		go signer(t, &sigWg, client, txnChan, sigTxnChan)
 	}
 
+	suggestedParams, err := client.SuggestedParams()
+	require.NoError(t, err)
+	var genesisHash crypto.Digest
+	copy(genesisHash[:], suggestedParams.GenesisHash)
 
+	// Call to different scenarios
+	go scenarioA(t, client, baseAcct, genesisHash, txnChan)
 
-	/*
+	for nthread := 0; nthread < numberOfThreads; nthread++ {
+		queueWg.Add(1)
+		go queuePayments(&queueWg, client, sigTxnChan)
+	}
 
+	sigWg.Wait()
+	close(sigTxnChan)
+	queueWg.Wait()
+	status, err = client.Status()
+	require.NoError(t, err)
+
+	var tx transactions.Transaction
+	for {
+		tx, err = client.SendPaymentFromUnencryptedWallet(baseAcct, baseAcct, config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee, 0, nil)
+		if err == nil {
+			break
+		}
+
+		fmt.Printf("Error broadcasting final transaction: %v\n", err)
+		time.Sleep(config.Consensus[protocol.ConsensusCurrentVersion].AgreementFilterTimeout)
+	}
+	_, err = fixture.WaitForConfirmedTxn(status.LastRound+100, baseAcct, tx.ID().String())
+	fmt.Printf("Waiting for confirmation transaction to commit..\n")
+	require.NoError(t, err)
+}
+
+func activateAccountTransaction(
+	t *testing.T,
+	client libgoal.Client,
+	round uint64,
+	sender basics.Address,
+	tLife uint64,
+	genesisHash crypto.Digest) (txn transactions.Transaction, receiver basics.Address) {
+
+	walletHandle, err := client.GetUnencryptedWalletHandle()
+	require.NoError(t, err)
+
+	address, err := client.GenerateAddress(walletHandle)
+	require.NoError(t, err)
+
+	receiver, err = basics.UnmarshalChecksumAddress(address)
+	require.NoError(t, err)
+
+	sround := zeroSub(round, 200)
+	txn = transactions.Transaction{
+		Type: protocol.PaymentTx,
+		Header: transactions.Header{
+			Sender:      sender,
+			Fee:         basics.MicroAlgos{Raw: config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee},
+			FirstValid:  basics.Round(sround),
+			LastValid:   basics.Round(sround + tLife),
+			GenesisHash: genesisHash,
+		},
+		PaymentTxnFields: transactions.PaymentTxnFields{
+			Receiver: receiver,
+			Amount:   basics.MicroAlgos{Raw: 100000000},
+		},
+	}
+	return
+}
+
+func createAssetTransaction(
+	t *testing.T,
+	round uint64,
+	sender basics.Address,
+	tLife uint64,
+	genesisHash crypto.Digest) (assetTx transactions.Transaction) {
+
+	sround := zeroSub(round, 200)
+	assetTx = transactions.Transaction{
+		Type: protocol.AssetConfigTx,
+		Header: transactions.Header{
+			Sender:      sender,
+			Fee:         basics.MicroAlgos{Raw: config.Consensus[protocol.ConsensusCurrentVersion].MinTxnFee},
+			FirstValid:  basics.Round(sround),
+			LastValid:   basics.Round(sround + tLife),
+			GenesisHash: genesisHash,
+		},
+		AssetConfigTxnFields: transactions.AssetConfigTxnFields{
+			AssetParams: basics.AssetParams{
+				Total:         100+round,
+				DefaultFrozen: false,
+				Manager:       sender,
+			},
+		},
+	}
+	return
+}
+
+func scenarioA(
+	t *testing.T,
+	client libgoal.Client,
+	baseAcct string,
+	genesisHash crypto.Digest,
+	txnChan chan<- *transactions.Transaction) {
+
+	numberOfAccounts := 600 // 6K
+	numberOfAssets := 60000   // 6M
+
+	assetsPerAccount := numberOfAssets / numberOfAccounts
+
+	sender, err := basics.UnmarshalChecksumAddress(baseAcct)
+	require.NoError(t, err)
+
+	params, err := client.SuggestedParams()
+	require.NoError(t, err)
+	tLife := config.Consensus[protocol.ConsensusVersion(params.ConsensusVersion)].MaxTxnLife
+
+	createdAccounts := make([]basics.Address, 0, numberOfAccounts)
 	
-	// Transfer some money to acct0, as well as other random accounts to
-	// fill up the Merkle tree with more than one element.
-	for i := 0; i < numberOfAccounts; i++ {
-		
-		walletHandle, err := client.GetUnencryptedWalletHandle()
-		require.NoError(t, err)
-
-		accti, err := client.GenerateAddress(walletHandle)
-		require.NoError(t, err)
-
-		if i%10 == 0 {
-			fmt.Println("done: ", i)
+	round := uint64(0)
+	for txi := 0; txi < numberOfAccounts; txi++ {
+		if txi%100 == 0 {
+			fmt.Println("account create txn: ", txi)
 		}
+		txn, newAccount := activateAccountTransaction(t, client, round, sender, tLife, genesisHash)
+		txnChan <- &txn
 
-		for x := 0; x < 5; x++ {
-			_, err = client.SendPaymentFromUnencryptedWallet(baseAcct, accti, 1000, 10000000, nil)
-			if err == nil {
-				break
-			}
-			fmt.Println(err)
-			time.Sleep(1 * time.Second)
-			fmt.Println(x)
-			//			require.NoError(t, err)
-		}
-
-		for j := 0; j < numberOfAssets/numberOfAccounts; j++ {
-
-		}
-
+		createdAccounts = append(createdAccounts, newAccount)
+		round++
 	}
-*/
-	fmt.Println("done")
 
-	/*
+	for nai, na := range createdAccounts {
+		for asi := 0; asi < assetsPerAccount; asi++ {
+			if asi%100 == 0 {
+				fmt.Printf("asset create: acct: %d asset %d\n", nai, asi)
+			}
 
-		// Start devmode network, and make sure everything is primed by sending a transaction.
-		var fixture fixtures.RestClientFixture
-		fixture.SetupNoStart(t, filepath.Join("nettemplates", "DevModeOneWallet.json"))
-		fixture.Start()
-		defer fixture.Shutdown()
-		sender, err := fixture.GetRichestAccount()
-		require.NoError(t, err)
+			atx := createAssetTransaction(t, round, na, tLife, genesisHash)
+			txnChan <- &atx
+			round++
+		}
+	}
 
-
-		// Add accounts
-		key := crypto.GenerateSignatureSecrets(crypto.Seed{})
-		receiver := basics.Address(key.SignatureVerifier)
-		txn := fixture.SendMoneyAndWait(0, 100000, 1000, sender.Address, receiver.String(), "")
-
-
-		txn = fixture.SendMoneyAndWait(0, 333, 1000, receiver.Address, sender.String(), "")
-		fmt.Println(txn)
-
-	*/
+	close(txnChan)
 }
