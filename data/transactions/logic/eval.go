e943119f8fd33b16da16943b86614266a3eb03b0
John Jannotti <jj@cs.brown.edu>
2022-03-14 12:49:08 -0400

    Simplify implementation

--- data/transactions/logic/eval.go
+++ data/transactions/logic/eval.go
@@ -1104,11 +1104,8 @@ func opSHA256(cx *EvalContext) {
 // The NIST SHA3-256 is implemented for compatibility with ICON
 func opSHA3_256(cx *EvalContext) {
 	last := len(cx.stack) - 1
-	hasher := sha3.New256()
-	hasher.Write(cx.stack[last].Bytes)
-	hv := make([]byte, 0, hasher.Size())
-	hv = hasher.Sum(hv)
-	cx.stack[last].Bytes = hv
+	hash := sha3.Sum256(cx.stack[last].Bytes)
+	cx.stack[last].Bytes = hash[:]
 }
 
 // The Keccak256 variant of SHA-3 is implemented for compatibility with Ethereum
