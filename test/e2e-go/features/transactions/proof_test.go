6904fcb2f633d815f0942076f1ccf97c4af76309
Or Aharonee <or.aharonee@algorand.com>
2022-03-29 16:43:38 +0300

    adding some tests

--- test/e2e-go/features/transactions/proof_test.go
+++ test/e2e-go/features/transactions/proof_test.go
@@ -20,6 +20,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/crypto"
 	"github.com/algorand/go-algorand/crypto/merklearray"
 	"github.com/algorand/go-algorand/protocol"
@@ -46,7 +47,6 @@ func (tme *TxnMerkleElemRaw) ToBeHashed() (protocol.HashID, []byte) {
 	return protocol.TxnMerkleLeaf, txnMerkleToRaw(tme.Txn, tme.Stib)
 }
 
-// TODO Stateproof: validate the new SHA256 header in this test as well
 func TestTxnMerkleProof(t *testing.T) {
 	partitiontest.PartitionTest(t)
 	defer fixtures.ShutdownSynchronizedTest(t)
@@ -156,4 +156,62 @@ func TestTxnMerkleProof(t *testing.T) {
 		t.Logf("blk.TxnRoot : %v \nproof path %v \ndepth: %d \nStibhash %v\nIndex: %d", blk.TxnRoot.DigestSha256.ToSlice(), proofSHA256.Path, proofSHA256.TreeDepth, proofrespSHA256.Stibhash, proofrespSHA256.Idx)
 		a.NoError(err)
 	}
+}
+
+// make sure transaction proof SHA256 does not exist in current consensus version (until we upgrade)
+func TestTxnMerkleProofSHA256(t *testing.T) {
+	partitiontest.PartitionTest(t)
+	defer fixtures.ShutdownSynchronizedTest(t)
+
+	t.Parallel()
+	a := require.New(fixtures.SynchronizedTest(t))
+
+	var fixture fixtures.RestClientFixture
+	// TODO Stateproof: when adding SHA256 header to the current consensus version, use the template of last version not supporting it instead of this one.
+	fixture.Setup(t, filepath.Join("nettemplates", "TwoNodes50Each.json"))
+	defer fixture.Shutdown()
+	client := fixture.LibGoalClient
+
+	// We will create three new accounts, transfer some amount of money into
+	// the first account, and then transfer a smaller amount to the second
+	// account while closing out the rest into the third.
+
+	accountList, err := fixture.GetWalletsSortedByBalance()
+	a.NoError(err)
+	baseAcct := accountList[0].Address
+
+	walletHandle, err := client.GetUnencryptedWalletHandle()
+	a.NoError(err)
+
+	acct0, err := client.GenerateAddress(walletHandle)
+	a.NoError(err)
+
+	status, err := client.Status()
+	a.NoError(err)
+
+	// Transfer some money to acct0, as well as other random accounts to
+	// fill up the Merkle tree with more than one element.
+	// we do not want to have a full tree in order the catch an empty element edge case
+	for i := 0; i < 5; i++ {
+		accti, err := client.GenerateAddress(walletHandle)
+		a.NoError(err)
+
+		_, err = client.SendPaymentFromUnencryptedWallet(baseAcct, accti, 1000, 10000000, nil)
+		a.NoError(err)
+	}
+
+	tx, err := client.SendPaymentFromUnencryptedWallet(baseAcct, acct0, 1000, 10000000, nil)
+	a.NoError(err)
+
+	txid := tx.ID()
+	confirmedTx, err := fixture.WaitForConfirmedTxn(status.LastRound+10, baseAcct, txid.String())
+	a.NoError(err)
+
+	blk, err := client.BookkeepingBlock(confirmedTx.ConfirmedRound)
+	a.NoError(err)
+	proto := config.Consensus[blk.CurrentProtocol]
+	a.False(proto.EnableSHA256TxnRootHeader)
+
+	a.NotEqual(crypto.Digest{}, blk.TxnRoot.DigestSha512_256)
+	a.Equal(crypto.Digest{}, blk.TxnRoot.DigestSha256) // should be empty since not yet supported
 }
