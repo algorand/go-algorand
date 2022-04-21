3aabd7d507d11f558275f4fc36cdd838e0f18e31
Will Winder <wwinder.unh@gmail.com>
2022-04-21 11:28:59 -0400

    Revert "Fix test error."
    This reverts commit 303231f23a995b3b2ff5ec8890ce6ea6b236e5d1.

--- cmd/algod/main_test.go
+++ cmd/algod/main_test.go
@@ -29,22 +29,21 @@ import (
 )
 
 func BenchmarkAlgodStartup(b *testing.B) {
-	var args algodArgs
 	tmpDir, err := ioutil.TempDir(os.TempDir(), "BenchmarkAlgodStartup")
 	require.NoError(b, err)
 	defer os.RemoveAll(tmpDir)
 	genesisFile, err := ioutil.ReadFile("../../installer/genesis/devnet/genesis.json")
 	require.NoError(b, err)
 
-	args.dataDirectory = tmpDir
+	dataDirectory = &tmpDir
 	bInitAndExit := true
-	args.initAndExit = bInitAndExit
+	initAndExit = &bInitAndExit
 	b.StartTimer()
 	for n := 0; n < b.N; n++ {
 		err := ioutil.WriteFile(filepath.Join(tmpDir, config.GenesisJSONFile), genesisFile, 0766)
 		require.NoError(b, err)
 		fmt.Printf("file %s was written\n", filepath.Join(tmpDir, config.GenesisJSONFile))
-		run(args)
+		run()
 		os.RemoveAll(tmpDir)
 		os.Mkdir(tmpDir, 0766)
 	}
