aad8b5a2f77055a6dc6141e9c8ce996fef0d61a1
AlgoStephenAkiki <85183435+AlgoStephenAkiki@users.noreply.github.com>
2022-05-03 16:54:17 -0400

    Enhancement: Migrate goal account commands to REST API (#3916)

--- libgoal/participation.go
+++ libgoal/participation.go
@@ -18,7 +18,6 @@ package libgoal
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 	"path/filepath"
@@ -27,76 +26,34 @@ import (
 	"github.com/algorand/go-algorand/daemon/algod/api/server/v2/generated"
 	"github.com/algorand/go-algorand/data/account"
 	"github.com/algorand/go-algorand/data/basics"
-	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/util/db"
 )
 
 // chooseParticipation chooses which participation keys to use for going online
 // based on the address, round number, and available participation databases
-func (c *Client) chooseParticipation(address basics.Address, round basics.Round) (part account.Participation, err error) {
-	genID, err := c.GenesisID()
+func (c *Client) chooseParticipation(address basics.Address, round basics.Round) (part generated.ParticipationKey, err error) {
+	parts, err := c.ListParticipationKeys()
 	if err != nil {
 		return
 	}
 
-	// Get a list of files in the participation keys directory
-	keyDir := filepath.Join(c.DataDir(), genID)
-	files, err := ioutil.ReadDir(keyDir)
-	if err != nil {
-		return
-	}
-	// This lambda will be used for finding the desired file.
-	checkIfFileIsDesiredKey := func(file os.FileInfo, expiresAfter basics.Round) (part account.Participation, err error) {
-		var handle db.Accessor
-		var partCandidate account.PersistedParticipation
-
-		// If it can't be a participation key database, skip it
-		if !config.IsPartKeyFilename(file.Name()) {
-			return
-		}
-
-		filename := file.Name()
-
-		// Fetch a handle to this database
-		handle, err = db.MakeErasableAccessor(filepath.Join(keyDir, filename))
-		if err != nil {
-			// Couldn't open it, skip it
-			return
-		}
-
-		// Fetch an account.Participation from the database
-		partCandidate, err = account.RestoreParticipation(handle)
-		if err != nil {
-			// Couldn't read it, skip it
-			handle.Close()
-			return
-		}
-		defer partCandidate.Close()
-
-		// Return the Participation valid for this round that relates to the passed address
+	// Loop through each of the participation keys; pick the one that expires farthest in the future.
+	var expiry uint64 = 0
+	for _, info := range parts {
+		// Choose the Participation valid for this round that relates to the passed address
 		// that expires farthest in the future.
 		// Note that algod will sign votes with all possible Participations. so any should work
 		// in the short-term.
 		// In the future we should allow the user to specify exactly which partkeys to register.
-		if partCandidate.FirstValid <= round && round <= partCandidate.LastValid && partCandidate.Parent == address && partCandidate.LastValid > expiresAfter {
-			part = partCandidate.Participation
+		if info.Key.VoteFirstValid <= uint64(round) && uint64(round) <= info.Key.VoteLastValid && info.Address == address.String() && info.Key.VoteLastValid > expiry {
+			part = info
+			expiry = part.Key.VoteLastValid
 		}
-		return
-	}
 
-	// Loop through each of the files; pick the one that expires farthest in the future.
-	var expiry basics.Round
-	for _, info := range files {
-		// Use above lambda so the deferred handle closure happens each loop
-		partCandidate, err := checkIfFileIsDesiredKey(info, expiry)
-		if err == nil && (!partCandidate.Parent.IsZero()) {
-			part = partCandidate
-			expiry = part.LastValid
-		}
 	}
-	if part.Parent.IsZero() {
+	if part.Address == "" {
 		// Couldn't find one
-		err = fmt.Errorf("Couldn't find a participation key database for address %v valid at round %v in directory %v", address.GetUserAddress(), round, keyDir)
+		err = fmt.Errorf("couldn't find a participation key database for address %v valid at round %v in participation registry", address.GetUserAddress(), round)
 		return
 	}
 	return
@@ -117,8 +74,12 @@ func (c *Client) GenParticipationKeys(address string, firstValid, lastValid, key
 }
 
 // GenParticipationKeysTo creates a .partkey database for a given address, fills
-// it with keys, and saves it in the specified output directory.
+// it with keys, and saves it in the specified output directory. If the output
+// directory is empty, the key will be installed.
 func (c *Client) GenParticipationKeysTo(address string, firstValid, lastValid, keyDilution uint64, outDir string) (part account.Participation, filePath string, err error) {
+
+	install := outDir == ""
+
 	// Parse the address
 	parsedAddr, err := basics.UnmarshalChecksumAddress(address)
 	if err != nil {
@@ -127,16 +88,9 @@ func (c *Client) GenParticipationKeysTo(address string, firstValid, lastValid, k
 
 	firstRound, lastRound := basics.Round(firstValid), basics.Round(lastValid)
 
-	// If output directory wasn't specified, store it in the current ledger directory.
-	if outDir == "" {
-		// Get the GenesisID for use in the participation key path
-		var genID string
-		genID, err = c.GenesisID()
-		if err != nil {
-			return
-		}
-
-		outDir = filepath.Join(c.DataDir(), genID)
+	// If we are installing, generate in the temp dir
+	if install {
+		outDir = os.TempDir()
 	}
 	// Connect to the database
 	partKeyPath, err := participationKeysPath(outDir, parsedAddr, firstRound, lastRound)
@@ -152,6 +106,14 @@ func (c *Client) GenParticipationKeysTo(address string, firstValid, lastValid, k
 		return
 	}
 
+	// If the key is being installed, remove it afterwards.
+	if install {
+		// Explicitly ignore any errors
+		defer func(name string) {
+			_ = os.Remove(name)
+		}(partKeyPath)
+	}
+
 	partdb, err := db.MakeErasableAccessor(partKeyPath)
 	if err != nil {
 		return
@@ -165,79 +127,15 @@ func (c *Client) GenParticipationKeysTo(address string, firstValid, lastValid, k
 	newPart, err := account.FillDBWithParticipationKeys(partdb, parsedAddr, firstRound, lastRound, keyDilution)
 	part = newPart.Participation
 	partdb.Close()
+
+	if err != nil {
+		return
+	}
+
+	if install {
+		_, err = c.AddParticipationKey(partKeyPath)
+	}
 	return part, partKeyPath, err
-}
-
-// InstallParticipationKeys creates a .partkey database for a given address,
-// based on an existing database from inputfile.  On successful install, it
-// deletes the input file.
-func (c *Client) InstallParticipationKeys(inputfile string) (part account.Participation, filePath string, err error) {
-	proto, ok := c.consensus[protocol.ConsensusCurrentVersion]
-	if !ok {
-		err = fmt.Errorf("Unknown consensus protocol %s", protocol.ConsensusCurrentVersion)
-		return
-	}
-
-	// Get the GenesisID for use in the participation key path
-	var genID string
-	genID, err = c.GenesisID()
-	if err != nil {
-		return
-	}
-
-	outDir := filepath.Join(c.DataDir(), genID)
-
-	inputdb, err := db.MakeErasableAccessor(inputfile)
-	if err != nil {
-		return
-	}
-	defer inputdb.Close()
-
-	partkey, err := account.RestoreParticipationWithSecrets(inputdb)
-	if err != nil {
-		return
-	}
-
-	if partkey.Parent == (basics.Address{}) {
-		err = fmt.Errorf("Cannot install partkey with missing (zero) parent address")
-		return
-	}
-
-	newdbpath, err := participationKeysPath(outDir, partkey.Parent, partkey.FirstValid, partkey.LastValid)
-	if err != nil {
-		return
-	}
-
-	newdb, err := db.MakeErasableAccessor(newdbpath)
-	if err != nil {
-		return
-	}
-
-	newpartkey := partkey
-	newpartkey.Store = newdb
-	err = newpartkey.PersistWithSecrets()
-	if err != nil {
-		newpartkey.Close()
-		return
-	}
-
-	// After successful install, remove the input copy of the
-	// partkey so that old keys cannot be recovered after they
-	// are used by algod.  We try to delete the data inside
-	// sqlite first, so the key material is zeroed out from
-	// disk blocks, but regardless of whether that works, we
-	// delete the input file.  The consensus protocol version
-	// is irrelevant for the maxuint64 round number we pass in.
-	errCh := partkey.DeleteOldKeys(basics.Round(math.MaxUint64), proto)
-	err = <-errCh
-	if err != nil {
-		newpartkey.Close()
-		return
-	}
-	os.Remove(inputfile)
-	part = newpartkey.Participation
-	newpartkey.Close()
-	return part, newdbpath, nil
 }
 
 // ListParticipationKeys returns the available participation keys,
@@ -247,51 +145,5 @@ func (c *Client) ListParticipationKeys() (partKeyFiles generated.ParticipationKe
 	if err == nil {
 		partKeyFiles, err = algod.GetParticipationKeys()
 	}
-	return
-}
-
-// ListParticipationKeyFiles returns the available participation keys,
-// as a map from database filename to Participation key object.
-func (c *Client) ListParticipationKeyFiles() (partKeyFiles map[string]account.Participation, err error) {
-	genID, err := c.GenesisID()
-	if err != nil {
-		return
-	}
-
-	// Get a list of files in the participation keys directory
-	keyDir := filepath.Join(c.DataDir(), genID)
-	files, err := ioutil.ReadDir(keyDir)
-	if err != nil {
-		return
-	}
-
-	partKeyFiles = make(map[string]account.Participation)
-	for _, file := range files {
-		// If it can't be a participation key database, skip it
-		if !config.IsPartKeyFilename(file.Name()) {
-			continue
-		}
-
-		filename := file.Name()
-
-		// Fetch a handle to this database
-		handle, err := db.MakeErasableAccessor(filepath.Join(keyDir, filename))
-		if err != nil {
-			// Couldn't open it, skip it
-			continue
-		}
-
-		// Fetch an account.Participation from the database
-		part, err := account.RestoreParticipation(handle)
-		if err != nil {
-			// Couldn't read it, skip it
-			handle.Close()
-			continue
-		}
-
-		partKeyFiles[filename] = part.Participation
-		part.Close()
-	}
-
 	return
 }
--- test/scripts/e2e_subs/goal-partkey-commands.sh
+++ test/scripts/e2e_subs/goal-partkey-commands.sh
@@ -45,3 +45,54 @@ if [[ "$OUTPUT" != "2" ]]; then echo "Two Participation IDs should have been fou
 OUTPUT=$(goal account listpartkeys -d "$ALGORAND_DATA" -d "$ALGORAND_DATA2" 2>&1)
 EXPECTED_ERR="Only one data directory can be specified for this command."
 if [[ "$OUTPUT" != "$EXPECTED_ERR" ]]; then echo -e "Unexpected output from multiple data directories with 'listpartkeys': \n$OUTPUT"; exit 1; fi
+
+create_and_fund_account () {
+  local TEMP_ACCT=$(${gcmd} account new|awk '{ print $6 }')
+  ${gcmd} clerk send -f "$INITIAL_ACCOUNT" -t "$TEMP_ACCT" -a 1000000 > /dev/null
+  echo "$TEMP_ACCT"
+}
+
+# given key should be installed and have the expected yes/no state
+# $1 - yes or no
+# $2 - a participation id
+# $3 - error message
+verify_registered_state () {
+  # look for participation ID anywhere in the partkeyinfo output
+  if ! goal account partkeyinfo | grep -q "$2"; then
+    fail_test "Key was not installed properly: $3"
+  fi
+
+  # looking for yes/no, and the 8 character head of participation id in this line:
+  # yes         LFMT...RHJQ  4UPT6AQC...               4            0     3000
+  if ! goal account listpartkeys | grep -q "$1.*$(echo "$2" | cut -c1-8)\.\.\."; then
+    fail_test "Unexpected key state: $3"
+  fi
+}
+
+# goal account installpartkey
+# install manually generated participation keys (do not register)
+NEW_ACCOUNT_1=$(create_and_fund_account)
+algokey part generate --keyfile test_partkey --first 0 --last 3000 --parent "$NEW_ACCOUNT_1"
+PARTICIPATION_ID_1=$(goal account installpartkey --delete-input --partkey test_partkey|awk '{ print $7 }')
+verify_registered_state "no" "$PARTICIPATION_ID_1" "goal account installpartkey"
+
+# goal account addpartkey
+# generate and install participation keys (do not register)
+NEW_ACCOUNT_2=$(create_and_fund_account)
+PARTICIPATION_ID_2=$(goal account addpartkey -a "$NEW_ACCOUNT_2" --roundFirstValid 0 --roundLastValid 3000|awk '{ print $7 }')
+verify_registered_state "no" "$PARTICIPATION_ID_2" "goal account addpartkey"
+
+# goal account renewpartkeys
+# generate, install, and register
+NEW_ACCOUNT_3=$(create_and_fund_account)
+PARTICIPATION_ID_3=$(${gcmd} account renewpartkey --roundLastValid 3000 -a "$NEW_ACCOUNT_3"|tail -n 1|awk '{ print $7 }')
+verify_registered_state "yes" "$PARTICIPATION_ID_3" "goal account renewpartkey"
+
+# goal account changeonlinstatus (--account)
+verify_registered_state "no" "$PARTICIPATION_ID_1" "goal account installpartkey (before)"
+${gcmd} account changeonlinestatus -a "$NEW_ACCOUNT_1"
+verify_registered_state "yes" "$PARTICIPATION_ID_1" "goal account installpartkey (after)"
+
+# goal account renewallpartkeys
+# goal account changeonlinstatus (--partkey)
+# These do not work as I expected them to. Do they work? I don't know, we should try to remove it.
