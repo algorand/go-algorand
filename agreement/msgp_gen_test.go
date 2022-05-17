8bb8a4a1ea867318566aa1132e7961edacbc142c
cce <51567+cce@users.noreply.github.com>
2022-05-17 15:22:37 -0400

    CI: ensure msgp generator has been run and is clean (#3978)
    This should ensure make msgp has been run for changes
    that impact msgp serialization on CI builds.
    
    It looks like #3829 was merged but missed changes to agreement/msgp_gen.go
    and gci updates from algorand/msgp#14 were not incorporated into #3919.

--- agreement/msgp_gen_test.go
+++ agreement/msgp_gen_test.go
@@ -8,9 +8,10 @@ package agreement
 import (
 	"testing"
 
+	"github.com/algorand/msgp/msgp"
+
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/test/partitiontest"
-	"github.com/algorand/msgp/msgp"
 )
 
 func TestMarshalUnmarshalCertificate(t *testing.T) {
