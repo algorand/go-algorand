a74255a6b8c907f87c910f086d26a7b9b40f5cad
cce <51567+cce@users.noreply.github.com>
2022-05-20 15:57:00 -0400

    don't set nodeNopeInfo by default

--- network/wsNetwork.go
+++ network/wsNetwork.go
@@ -687,9 +687,6 @@ func (wn *WebsocketNetwork) setup() {
 	if wn.config.DNSSecurityRelayAddrEnforced() {
 		preferredResolver = dnssec.MakeDefaultDnssecResolver(wn.config.FallbackDNSResolverAddress, wn.log)
 	}
-	if wn.nodeInfo == nil {
-		wn.nodeInfo = &nopeNodeInfo{}
-	}
 	maxIdleConnsPerHost := int(wn.config.ConnectionsRateLimitingCount)
 	wn.dialer = makeRateLimitingDialer(wn.phonebook, preferredResolver)
 	wn.transport = makeRateLimitingTransport(wn.phonebook, 10*time.Second, &wn.dialer, maxIdleConnsPerHost)
