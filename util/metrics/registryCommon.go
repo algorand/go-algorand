0c8a2c5223a2f14f169de66aae98912f3cd4153a
chris erway <chris.erway@algorand.com>
2022-05-24 23:04:03 -0400

    add TestSanitizePrometheusName

--- util/metrics/registryCommon.go
+++ util/metrics/registryCommon.go
@@ -43,10 +43,8 @@ func sanitizeTelemetryName(name string) string {
 	return sanitizeTelemetryCharactersRegexp.ReplaceAllString(name, "_")
 }
 
-var sanitizePrometheusCharactersRegexp = regexp.MustCompile("(^[^a-zA-Z_]|[^a-zA-Z0-9_])")
-
 // sanitizePrometheusName ensures a metric name reported to telemetry doesn't contain any
 // non-alphanumeric characters (apart from _) and doesn't start with a number.
 func sanitizePrometheusName(name string) string {
-	return sanitizePrometheusCharactersRegexp.ReplaceAllString(name, "_")
+	return strings.ReplaceAll(sanitizeTelemetryName(name), "-", "_")
 }
