a3003ec5668d20e81c9b1d24b09dfaf8eff524fc
Or Aharonee <or.aharonee@algorand.com>
2022-05-26 16:21:32 +0300

    Add missing HashType to GetProof endpoint (#3985)

--- daemon/algod/api/server/v2/handlers.go
+++ daemon/algod/api/server/v2/handlers.go
@@ -638,6 +638,7 @@ func (v2 *Handlers) GetProof(ctx echo.Context, round uint64, txid string, params
 			Stibhash:  stibhash[:],
 			Idx:       uint64(idx),
 			Treedepth: uint64(proof.TreeDepth),
+			Hashtype:  hashtype,
 		}
 
 		return ctx.JSON(http.StatusOK, response)
