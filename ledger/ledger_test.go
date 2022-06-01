4dd0ae74322ce5b464bf678b1a8d97baeb858ce7
chris erway <chris.erway@algorand.com>
2022-06-01 15:54:39 -0400

    run TestLedgerMemoryLeak

--- ledger/ledger_test.go
+++ ledger/ledger_test.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/algorand/go-algorand/data/account"
 	"github.com/algorand/go-algorand/util/db"
+	"github.com/algorand/go-deadlock"
 
 	"github.com/stretchr/testify/require"
 
@@ -1570,19 +1571,20 @@ func TestListAssetsAndApplications(t *testing.T) {
 func TestLedgerMemoryLeak(t *testing.T) {
 	partitiontest.PartitionTest(t)
 
-	t.Skip() // for manual runs only
+	// t.Skip() // for manual runs only
 	dbName := fmt.Sprintf("%s.%d", t.Name(), crypto.RandUint64())
 	genesisInitState, initKeys := ledgertesting.GenerateInitState(t, protocol.ConsensusCurrentVersion, 10000000000)
 	const inMem = false
 	cfg := config.GetDefaultLocal()
 	cfg.Archival = true
 	log := logging.TestingLog(t)
-	log.SetLevel(logging.Info) // prevent spamming with ledger.AddValidatedBlock debug message
+	log.SetLevel(logging.Info)   // prevent spamming with ledger.AddValidatedBlock debug message
+	deadlock.Opts.Disable = true // catchpoint writing might take long
 	l, err := OpenLedger(log, dbName, inMem, genesisInitState, cfg)
 	require.NoError(t, err)
 	defer l.Close()
 
-	const maxBlocks = 10000
+	const maxBlocks = 1_000_000
 	nftPerAcct := make(map[basics.Address]int)
 	lastBlock, err := l.Block(l.Latest())
 	require.NoError(t, err)
@@ -1601,8 +1603,8 @@ func TestLedgerMemoryLeak(t *testing.T) {
 		keys[addr] = initKeys[addr]
 	}
 
-	fmt.Printf("%s\t%s\t%s\t%s\n", "Round", "TotalAlloc, MB", "HeapAlloc, MB", "LiveObj")
-	fmt.Printf("%s\t%s\t%s\t%s\n", "-----", "--------------", "-------------", "-------")
+	fmt.Printf("%s\t%s\t%s\t%s\t%s\n", "Round", "TotalAlloc, MB", "HeapAlloc, MB", "LiveObj", "Mallocs")
+	fmt.Printf("%s\t%s\t%s\t%s\t%s\n", "-----", "--------------", "-------------", "-------", "-------")
 
 	curAddressIdx := 0
 	// run for maxBlocks rounds
@@ -1690,7 +1692,7 @@ func TestLedgerMemoryLeak(t *testing.T) {
 			var rtm runtime.MemStats
 			runtime.ReadMemStats(&rtm)
 			const meg = 1024 * 1024
-			fmt.Printf("%5d\t%14d\t%13d\t%7d\n", latest, rtm.TotalAlloc/meg, rtm.HeapAlloc/meg, rtm.Mallocs-rtm.Frees)
+			fmt.Printf("%5d\t%14d\t%13d\t%7d\t%7d\n", latest, rtm.TotalAlloc/meg, rtm.HeapAlloc/meg, rtm.Mallocs-rtm.Frees, rtm.Mallocs)
 
 			// Use the code below to generate memory profile if needed for debugging
 			// memprofile := fmt.Sprintf("%s-memprof-%d", t.Name(), i)
