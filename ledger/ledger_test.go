6c2643a1802f1b6be6fb427d1eaa0826858fbaf0
Pavel Zbitskiy <pavel@algorand.com>
2022-06-03 10:43:39 -0400

    Fixes to TestLedgerMemoryLeak

--- ledger/ledger_test.go
+++ ledger/ledger_test.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/algorand/go-algorand/data/account"
 	"github.com/algorand/go-algorand/util/db"
+	"github.com/algorand/go-deadlock"
 
 	"github.com/stretchr/testify/require"
 
@@ -1577,12 +1578,13 @@ func TestLedgerMemoryLeak(t *testing.T) {
 	cfg := config.GetDefaultLocal()
 	cfg.Archival = true
 	log := logging.TestingLog(t)
-	log.SetLevel(logging.Info) // prevent spamming with ledger.AddValidatedBlock debug message
+	log.SetLevel(logging.Info)   // prevent spamming with ledger.AddValidatedBlock debug message
+	deadlock.Opts.Disable = true // catchpoint writing might take long
 	l, err := OpenLedger(log, dbName, inMem, genesisInitState, cfg)
 	require.NoError(t, err)
 	defer l.Close()
 
-	const maxBlocks = 10000
+	const maxBlocks = 1_000_000
 	nftPerAcct := make(map[basics.Address]int)
 	lastBlock, err := l.Block(l.Latest())
 	require.NoError(t, err)
@@ -1687,20 +1689,22 @@ func TestLedgerMemoryLeak(t *testing.T) {
 			l.WaitForCommit(latest)
 		}
 		if latest%1000 == 0 || i%1000 == 0 && i > 0 {
+			// pct := debug.SetGCPercent(-1) // prevent CG in between memory stats reading and heap profiling
+
 			var rtm runtime.MemStats
 			runtime.ReadMemStats(&rtm)
 			const meg = 1024 * 1024
 			fmt.Printf("%5d\t%14d\t%13d\t%7d\n", latest, rtm.TotalAlloc/meg, rtm.HeapAlloc/meg, rtm.Mallocs-rtm.Frees)
 
 			// Use the code below to generate memory profile if needed for debugging
-			// memprofile := fmt.Sprintf("%s-memprof-%d", t.Name(), i)
+			// memprofile := fmt.Sprintf("%s-memprof-%d", t.Name(), latest)
 			// f, err := os.Create(memprofile)
 			// require.NoError(t, err)
 			// err = pprof.WriteHeapProfile(f)
 			// require.NoError(t, err)
 			// f.Close()
-			// fmt.Printf("Profile %s created\n", memprofile)
 
+			// debug.SetGCPercent(pct)
 		}
 	}
 }
