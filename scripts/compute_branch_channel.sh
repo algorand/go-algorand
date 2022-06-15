35b0f3bdee7f5c151fb634115fc128b23e78cd59
egieseke <eric_gieseke@yahoo.com>
2022-06-15 13:57:18 -0400

    Revert use of branch parent to determine branch channel.

--- scripts/compute_branch_channel.sh
+++ scripts/compute_branch_channel.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/env bash
 
 # If workspace isn't clean, it's 'dev'.
-BRANCH="$1"
 
-if [ "${BRANCH}" = "master" ]; then
+if [ "$1" = "master" ]; then
     echo "master"
-    exit 0
-elif [ "${BRANCH}" = "rel/nightly" ]; then
+elif [ "$1" = "rel/nightly" ]; then
     echo "nightly"
-    exit 0
-elif [ "${BRANCH}" = "rel/stable" ]; then
+elif [ "$1" = "rel/stable" ]; then
     echo "stable"
-    exit 0
-elif [ "${BRANCH}" = "rel/beta" ]; then
+elif [ "$1" = "rel/beta" ]; then
     echo "beta"
-    exit 0
-elif [ "${BRANCH}" = "feature/alphanet" ]; then
+elif [ "$1" = "feature/alphanet" ]; then
     echo "alpha"
-    exit 0
-fi
-
-#get parent of current branch
-#credit to https://stackoverflow.com/questions/3161204/find-the-parent-branch-of-a-git-branch
-BRANCHPARENT="$(git show-branch | grep '\*' | grep -v "${BRANCH}" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//' || ${BRANCH})"
-BRANCHPARENT=${BRANCHPARENT:-$BRANCH}
-
-if [ "${BRANCHPARENT}" = "rel/stable" ]; then
-    echo "stable"
-elif [ "${BRANCHPARENT}" = "rel/beta" ]; then
-    echo "beta"
-elif [ "${BRANCHPARENT}" = "feature/alphanet" ]; then
+elif [ "$1" = "build-alphanet" ]; then
     echo "alpha"
 else
     echo "dev"
 fi
+
