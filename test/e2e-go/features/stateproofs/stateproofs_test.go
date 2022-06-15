04e3c5d0ca94c5b51bb40b53a49a1bf0a0538ff0
algoidan <idan@algorand.com>
2022-06-15 10:59:10 +0300

    refactor stateproof e2e

--- test/e2e-go/features/stateproofs/stateproofs_test.go
+++ test/e2e-go/features/stateproofs/stateproofs_test.go
@@ -25,24 +25,24 @@ import (
 
 	"github.com/algorand/go-algorand/config"
 	sp "github.com/algorand/go-algorand/crypto/stateproof"
+	"github.com/algorand/go-algorand/daemon/algod/api/client"
 	"github.com/algorand/go-algorand/data/basics"
 	"github.com/algorand/go-algorand/data/bookkeeping"
 	"github.com/algorand/go-algorand/data/stateproofmsg"
 	"github.com/algorand/go-algorand/data/transactions"
+	"github.com/algorand/go-algorand/libgoal"
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/test/framework/fixtures"
 	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
+const expectedNumberOfStateProofs = uint64(4)
+
 func TestStateProofs(t *testing.T) {
 	partitiontest.PartitionTest(t)
 	defer fixtures.ShutdownSynchronizedTest(t)
 
-	// TODO Stateproof: the Parallel should stay disable for now. Parallel might
-	// cause problem since we change the global future param to use 16 cc round.
-	//t.Parallel()
 	r := require.New(fixtures.SynchronizedTest(t))
-	expectedNumberOfStateProofs := uint64(4)
 
 	configurableConsensus := make(config.ConsensusProtocols)
 	consensusVersion := protocol.ConsensusVersion("test-fast-stateproofs")
@@ -65,29 +65,12 @@ func TestStateProofs(t *testing.T) {
 	restClient, err := fixture.NC.AlgodClient()
 	r.NoError(err)
 
-	node0Client := fixture.GetLibGoalClientForNamedNode("Node0")
-	node0Wallet, err := node0Client.GetUnencryptedWalletHandle()
-	r.NoError(err)
-	node0AccountList, err := node0Client.ListAddresses(node0Wallet)
-	r.NoError(err)
-	node0Account := node0AccountList[0]
-
-	node1Client := fixture.GetLibGoalClientForNamedNode("Node1")
-	node1Wallet, err := node1Client.GetUnencryptedWalletHandle()
-	r.NoError(err)
-	node1AccountList, err := node1Client.ListAddresses(node1Wallet)
-	r.NoError(err)
-	node1Account := node1AccountList[0]
-
 	var lastStateProofBlock bookkeeping.Block
+	var lastStateProofMessage stateproofmsg.Message
 	libgoal := fixture.LibGoalClient
 	for rnd := uint64(1); rnd <= consensusParams.StateProofInterval*(expectedNumberOfStateProofs+1); rnd++ {
 		// send a dummy payment transaction.
-		minTxnFee, _, err := fixture.CurrentMinFeeAndBalance()
-		r.NoError(err)
-
-		_, err = node0Client.SendPaymentFromUnencryptedWallet(node0Account, node1Account, minTxnFee, rnd, nil)
-		r.NoError(err)
+		sendPayment(r, &fixture, rnd)
 
 		err = fixture.WaitForRound(rnd, 30*time.Second)
 		r.NoError(err)
@@ -109,55 +92,84 @@ func TestStateProofs(t *testing.T) {
 			}
 		}
 
-		var prevStateProofMessage stateproofmsg.Message
 		for lastStateProofBlock.Round() != 0 && lastStateProofBlock.Round()+basics.Round(consensusParams.StateProofInterval) < blk.StateProofTracking[protocol.StateProofBasic].StateProofNextRound {
 			nextStateProofRound := uint64(lastStateProofBlock.Round()) + consensusParams.StateProofInterval
 
-			t.Logf("found a cert for round %d at round %d", nextStateProofRound, blk.Round())
-			// Find the cert transaction
-			res, err := restClient.TransactionsByAddr(transactions.StateProofSender.String(), 0, rnd, expectedNumberOfStateProofs+1)
-			r.NoError(err)
-
-			var stateProof sp.StateProof
-			var stateProofMessage stateproofmsg.Message
-			stateProofFound := false
-			for _, txn := range res.Transactions {
-				r.Equal(txn.Type, string(protocol.StateProofTx))
-				r.True(txn.StateProof != nil)
-				if txn.StateProof.StateProofIntervalLatestRound == nextStateProofRound {
-					err = protocol.Decode(txn.StateProof.StateProof, &stateProof)
-					r.NoError(err)
-					err = protocol.Decode(txn.StateProof.StateProofMessage, &stateProofMessage)
-					r.NoError(err)
-					stateProofFound = true
-				}
-			}
-			r.True(stateProofFound)
-
-			nextStateProofBlock, err := libgoal.BookkeepingBlock(nextStateProofRound)
-			r.NoError(err)
-
-			if !prevStateProofMessage.MsgIsZero() {
-				//if we have a previous stateproof message we can verify the current stateproof using data from it
-				verifier := sp.MkVerifierWithLnProvenWeight(prevStateProofMessage.VotersCommitment, prevStateProofMessage.LnProvenWeight, consensusParams.StateProofStrengthTarget)
-				err = verifier.Verify(uint64(nextStateProofBlock.Round()), stateProofMessage.IntoStateProofMessageHash(), &stateProof)
-				r.NoError(err)
-			}
-			var votersRoot = make([]byte, sp.HashSize)
-			copy(votersRoot[:], lastStateProofBlock.StateProofTracking[protocol.StateProofBasic].StateProofVotersCommitment)
-
-			provenWeight, overflowed := basics.Muldiv(lastStateProofBlock.StateProofTracking[protocol.StateProofBasic].StateProofVotersTotalWeight.Raw, uint64(consensusParams.StateProofWeightThreshold), 1<<32)
-			r.False(overflowed)
-
-			verifier, err := sp.MkVerifier(votersRoot, provenWeight, consensusParams.StateProofStrengthTarget)
-			r.NoError(err)
-
-			err = verifier.Verify(uint64(nextStateProofBlock.Round()), stateProofMessage.IntoStateProofMessageHash(), &stateProof)
-			r.NoError(err)
-			prevStateProofMessage = stateProofMessage
+			t.Logf("found a state proof for round %d at round %d", nextStateProofRound, blk.Round())
+			// Find the state proof transaction
+			stateProofMessage, nextStateProofBlock := verifyStateProofForRound(r, libgoal, restClient, nextStateProofRound, lastStateProofMessage, lastStateProofBlock, consensusParams)
+			lastStateProofMessage = stateProofMessage
 			lastStateProofBlock = nextStateProofBlock
 		}
 	}
 
 	r.Equalf(consensusParams.StateProofInterval*expectedNumberOfStateProofs, uint64(lastStateProofBlock.Round()), "the expected last state proof block wasn't the one that was observed")
+}
+
+func sendPayment(r *require.Assertions, fixture *fixtures.RestClientFixture, rnd uint64) {
+	node0Client := fixture.GetLibGoalClientForNamedNode("Node0")
+	node0Wallet, err := node0Client.GetUnencryptedWalletHandle()
+	r.NoError(err)
+	node0AccountList, err := node0Client.ListAddresses(node0Wallet)
+	r.NoError(err)
+	node0Account := node0AccountList[0]
+
+	node1Client := fixture.GetLibGoalClientForNamedNode("Node1")
+	node1Wallet, err := node1Client.GetUnencryptedWalletHandle()
+	r.NoError(err)
+	node1AccountList, err := node1Client.ListAddresses(node1Wallet)
+	r.NoError(err)
+	node1Account := node1AccountList[0]
+
+	minTxnFee, _, err := fixture.CurrentMinFeeAndBalance()
+	r.NoError(err)
+
+	_, err = node0Client.SendPaymentFromUnencryptedWallet(node0Account, node1Account, minTxnFee, 1, []byte{byte(rnd)})
+	r.NoError(err)
+}
+
+func verifyStateProofForRound(r *require.Assertions, libgoal libgoal.Client, restClient client.RestClient, nextStateProofRound uint64, prevStateProofMessage stateproofmsg.Message, lastStateProofBlock bookkeeping.Block, consensusParams config.ConsensusParams) (stateproofmsg.Message, bookkeeping.Block) {
+	curRound, err := libgoal.CurrentRound()
+	r.NoError(err)
+
+	res, err := restClient.TransactionsByAddr(transactions.StateProofSender.String(), 0, curRound, expectedNumberOfStateProofs+1)
+	r.NoError(err)
+
+	var stateProof sp.StateProof
+	var stateProofMessage stateproofmsg.Message
+	stateProofFound := false
+	for _, txn := range res.Transactions {
+		r.Equal(txn.Type, string(protocol.StateProofTx))
+		r.True(txn.StateProof != nil)
+		if txn.StateProof.StateProofIntervalLatestRound == nextStateProofRound {
+			err = protocol.Decode(txn.StateProof.StateProof, &stateProof)
+			r.NoError(err)
+			err = protocol.Decode(txn.StateProof.StateProofMessage, &stateProofMessage)
+			r.NoError(err)
+			stateProofFound = true
+		}
+	}
+	r.True(stateProofFound)
+
+	nextStateProofBlock, err := libgoal.BookkeepingBlock(nextStateProofRound)
+	r.NoError(err)
+
+	if !prevStateProofMessage.MsgIsZero() {
+		//if we have a previous stateproof message we can verify the current stateproof using data from it
+		verifier := sp.MkVerifierWithLnProvenWeight(prevStateProofMessage.VotersCommitment, prevStateProofMessage.LnProvenWeight, consensusParams.StateProofStrengthTarget)
+		err = verifier.Verify(uint64(nextStateProofBlock.Round()), stateProofMessage.IntoStateProofMessageHash(), &stateProof)
+		r.NoError(err)
+	}
+	var votersRoot = make([]byte, sp.HashSize)
+	copy(votersRoot[:], lastStateProofBlock.StateProofTracking[protocol.StateProofBasic].StateProofVotersCommitment)
+
+	provenWeight, overflowed := basics.Muldiv(lastStateProofBlock.StateProofTracking[protocol.StateProofBasic].StateProofVotersTotalWeight.Raw, uint64(consensusParams.StateProofWeightThreshold), 1<<32)
+	r.False(overflowed)
+
+	verifier, err := sp.MkVerifier(votersRoot, provenWeight, consensusParams.StateProofStrengthTarget)
+	r.NoError(err)
+
+	err = verifier.Verify(uint64(nextStateProofBlock.Round()), stateProofMessage.IntoStateProofMessageHash(), &stateProof)
+	r.NoError(err)
+	return stateProofMessage, nextStateProofBlock
 }
