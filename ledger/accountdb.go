bc763a79794c76a0836ecfe083e3fcbda5c43531
Nicholas Guo <nicholas.guo@algorand.com>
2022-06-27 13:06:54 -0700

    Revert "Temporary disable empty voting data check (#4097)"
    This reverts commit 6a9cb0b90af0a9487eb5190fe0ad30f21da612df.

--- ledger/accountdb.go
+++ ledger/accountdb.go
@@ -3614,9 +3614,8 @@ func onlineAccountsNewRoundImpl(
 								prevAcct = updated
 							}
 						}
-						// TODO: restore after migrating offline accounts and clearing state proof PK
-						// } else if !newAcct.IsVotingEmpty() {
-						// 	err = fmt.Errorf("non-empty voting data for non-online account %s: %v", data.address.String(), newAcct)
+					} else if !newAcct.IsVotingEmpty() {
+						err = fmt.Errorf("non-empty voting data for non-online account %s: %v", data.address.String(), newAcct)
 					}
 				}
 			} else {
