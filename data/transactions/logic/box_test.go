3c46e5965e8a28ca815c78a108fd793cd1164ad4
Zeph Grunschlag <zeph@algorand.com>
2022-06-30 02:12:44 -0500

    go fmt

--- data/transactions/logic/box_test.go
+++ data/transactions/logic/box_test.go
@@ -308,26 +308,26 @@ func TestMakeBoxKey(t *testing.T) {
 
 	type testCase struct {
 		description string
-		name string
-		app  basics.AppIndex
-		key string
-		err string
+		name        string
+		app         basics.AppIndex
+		key         string
+		err         string
 	}
 
 	pp := func(tc testCase) string {
 		return fmt.Sprintf("<<<%s>>> (name, app) = (%#v, %d) --should--> key = %#v (err = [%s])", tc.description, tc.name, tc.app, tc.key, tc.err)
 	}
 
-  	var testCases = []testCase{
+	var testCases = []testCase{
 		// COPACETIC:
-		{"zero appid",  	"stranger", 0,  		"bx:\x00\x00\x00\x00\x00\x00\x00\x00stranger", 	""},
-		{"typical",  		"348-8uj",  131231, 	"bx:\x00\x00\x00\x00\x00\x02\x00\x9f348-8uj", 	""},
-		{"empty box name", 	"", 		42,         "bx:\x00\x00\x00\x00\x00\x00\x00*", 			""},
-		{"random byteslice", "{\xbb\x04\a\xd1\xe2\xc6I\x81{", 13475904583033571713,  "bx:\xbb\x04\a\xd1\xe2\xc6I\x81{\xbb\x04\a\xd1\xe2\xc6I\x81{", ""},
+		{"zero appid", "stranger", 0, "bx:\x00\x00\x00\x00\x00\x00\x00\x00stranger", ""},
+		{"typical", "348-8uj", 131231, "bx:\x00\x00\x00\x00\x00\x02\x00\x9f348-8uj", ""},
+		{"empty box name", "", 42, "bx:\x00\x00\x00\x00\x00\x00\x00*", ""},
+		{"random byteslice", "{\xbb\x04\a\xd1\xe2\xc6I\x81{", 13475904583033571713, "bx:\xbb\x04\a\xd1\xe2\xc6I\x81{\xbb\x04\a\xd1\xe2\xc6I\x81{", ""},
 
 		// ERRORS:
-		{ "too short"	, 	"", 		0, 			"stranger",					"GetAppNameFromKey() cannot extract AppIndex as key (stranger) too short (length=8)" }, 
-		{ "wrong prefix", 	"", 		0, 			"strangersINTHEdark",		"GetAppNameFromKey() illegal app box prefix in key (strangersINTHEdark). Expected prefix 'bx:'" }, 
+		{"too short", "", 0, "stranger", "GetAppNameFromKey() cannot extract AppIndex as key (stranger) too short (length=8)"},
+		{"wrong prefix", "", 0, "strangersINTHEdark", "GetAppNameFromKey() illegal app box prefix in key (strangersINTHEdark). Expected prefix 'bx:'"},
 	}
 
 	for _, tc := range testCases {
@@ -342,6 +342,5 @@ func TestMakeBoxKey(t *testing.T) {
 			require.EqualError(t, err, tc.err, pp(tc))
 		}
 	}
-
 
 }
