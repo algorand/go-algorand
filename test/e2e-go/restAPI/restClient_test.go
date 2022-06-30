a957b507914a23b89ff951efacfe96a57755dd50
Hang Su <hang.su@algorand.com>
2022-06-30 11:45:45 -0400

    minor, explaining test

--- test/e2e-go/restAPI/restClient_test.go
+++ test/e2e-go/restAPI/restClient_test.go
@@ -1366,6 +1366,9 @@ end:
 		return
 	}
 
+	// helper function, take operation and a slice of box names
+	// then submit transaction group containing all operations on box names
+	// Then we check these boxes are appropriately created/deleted
 	operateAndMatchRes := func(operation string, boxNames []string) {
 		if operation == "create" {
 			for _, box := range boxNames {
@@ -1448,6 +1451,7 @@ end:
 
 	for i := 0; i < len(testingBoxNames); i += 16 {
 		var strSliceTest []string
+		// grouping box names to operate, and create such boxes
 		if i+16 >= len(testingBoxNames) {
 			strSliceTest = testingBoxNames[i:]
 		} else {
@@ -1458,6 +1462,7 @@ end:
 
 	for i := 0; i < len(testingBoxNames); i += 16 {
 		var strSliceTest []string
+		// grouping box names to operate, and delete such boxes
 		if i+16 >= len(testingBoxNames) {
 			strSliceTest = testingBoxNames[i:]
 		} else {
