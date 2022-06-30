951cd2ac2e2e9a2ec718ca6b6750a5116945d45e
Zeph Grunschlag <zeph@algorand.com>
2022-06-30 01:38:37 -0500

    remove comment about taking lock

--- ledger/acctupdates.go
+++ ledger/acctupdates.go
@@ -843,10 +843,9 @@ func (aul *accountUpdatesLedgerEvaluator) LookupAsset(rnd basics.Round, addr bas
 }
 
 func (aul *accountUpdatesLedgerEvaluator) LookupKv(rnd basics.Round, key string) (*string, error) {
-	// TODO: should I explicitly be avoiding the lock as I'm doing here?
 	// TODO: don't merge with this print statement
 	fmt.Printf("*accountUpdatesLedgerEvaluator.LookupKv(): rnd-->%d, key-->%s", rnd, key)
-	return aul.au.lookupKv(rnd, key, false)
+	return aul.au.lookupKv(rnd, key, false /* don't sync */)
 }
 
 // GetCreatorForRound returns the asset/app creator for a given asset/app index at a given round
