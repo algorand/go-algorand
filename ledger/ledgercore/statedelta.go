b6adad7d586e06791566ae5a2e58b0da9267ea78
Pavel Zbitskiy <pavel@algorand.com>
2022-06-30 16:47:58 -0400

    Fix exceed mem allocation in state deltas

--- ledger/ledgercore/statedelta.go
+++ ledger/ledgercore/statedelta.go
@@ -182,7 +182,7 @@ func MakeStateDelta(hdr *bookkeeping.BlockHeader, prevTimestamp int64, hint int,
 	return StateDelta{
 		Accts:    MakeAccountDeltas(hint),
 		Txids:    make(map[transactions.Txid]IncludedTransactions, hint),
-		Txleases: make(map[Txlease]basics.Round, hint),
+		Txleases: make(map[Txlease]basics.Round),
 		// asset or application creation are considered as rare events so do not pre-allocate space for them
 		Creatables:               make(map[basics.CreatableIndex]ModifiedCreatable),
 		Hdr:                      hdr,
