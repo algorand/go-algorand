dda285f5cfbbb933ef5d8fb4e9d05a50d9d99348
Jonathan Weiss <jonathan.weiss@algorand.com>
2022-07-05 10:07:26 +0300

    adding unit test to top n accounts

--- ledger/accountdb_test.go
+++ ledger/accountdb_test.go
@@ -2735,3 +2735,35 @@ func TestAccountWithNoStateProofKeysReturnsNonZeroStateProofID(t *testing.T) {
 	onlineAccount := accountDataToOnline(basics.Address{}, &ledgercore.AccountData{}, config.ConsensusParams{}) // *ledgercore.OnlineAccount
 	require.Equal(t, merklesignature.MssNoKeysCommitment, onlineAccount.StateProofID.Commitment)
 }
+
+func TestTopNAccountsThatHaveNoMssKeys(t *testing.T) {
+	partitiontest.PartitionTest(t)
+
+	proto := config.Consensus[protocol.ConsensusCurrentVersion]
+
+	dbs, _ := dbOpenTest(t, true)
+	setDbLogging(t, dbs)
+	defer dbs.Close()
+
+	tx, err := dbs.Wdb.Handle.Begin()
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	accts := ledgertesting.RandomAccounts(20, true)
+	for address, data := range accts {
+		data.StateProofID = merklesignature.Commitment{}
+		accts[address] = data
+	}
+
+	newDB := accountsInitTest(t, tx, accts, proto)
+	require.True(t, newDB)
+
+	checkAccounts(t, tx, 0, accts)
+
+	topn, err := accountsOnlineTop(tx, 0, uint64(len(accts)), proto)
+	require.NoError(t, err)
+
+	for _, account := range topn {
+		require.Equal(t, merklesignature.MssNoKeysCommitment, account.StateProofID.Commitment)
+	}
+}
