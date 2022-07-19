3481dd033459ad2c5f75f7e211600fdc0235e8ea
Jacob Daitzman <jdtzmn@gmail.com>
2022-07-19 10:49:36 -0400

    Use `sprintf` in cmd/goal/box.go
    Co-authored-by: John Jannotti <jannotti@gmail.com>

--- cmd/goal/box.go
+++ cmd/goal/box.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -67,7 +66,7 @@ var appBoxInfoCmd = &cobra.Command{
 		// Get box info
 		box, err := client.GetApplicationBoxByName(appIdx, boxName)
 		if err != nil {
-			if strings.Contains(err.Error(), fmt.Sprintf("{\"message\":\"box not found\"}")) {
+			if strings.Contains(err.Error(), `{"message":"box not found"}`) {
 				reportErrorf("No box found for appid %d with name %s", appIdx, boxName)
 			}
 			reportErrorf(errorRequestFail, err)
