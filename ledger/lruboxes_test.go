4fb59f5c9f31714bead739de3e02209a7dff36b9
algoidurovic <91566643+algoidurovic@users.noreply.github.com>
2022-08-02 13:38:13 -0400

    unsaved changes

--- ledger/lruboxes_test.go
+++ ledger/lruboxes_test.go
@@ -205,7 +205,7 @@ func benchLruWriteBoxes(b *testing.B, fillerBoxes []cachedBoxData, boxes []cache
 	b.StopTimer()
 	var baseBoxes lruBoxes
 	// setting up the baseBoxes with a predefined cache size
-	baseBoxes.init(logging.TestingLog(b), baseBoxesPendingAccountsBufferSize, baseBoxesPendingAccountsWarnThreshold)
+	baseBoxes.init(logging.TestingLog(b), baseBoxesPendingBufferSize, baseBoxesPendingWarnThreshold)
 	for i := 0; i < b.N; i++ {
 		baseBoxes = fillLRUBoxes(baseBoxes, fillerBoxes)
 
