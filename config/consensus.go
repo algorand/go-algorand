f27fd90d3bc74ca33a5215941d1f3cc2e92449f8
Brian Olson <brian.olson@gmail.com>
2022-08-08 10:57:46 -0400

    remove wat

--- config/consensus.go
+++ config/consensus.go
@@ -1183,13 +1183,6 @@ func initConsensusProtocols() {
 	vFuture.MaxTxnBytesPerBlock = 5 * 1024 * 1024
 
 	Consensus[protocol.ConsensusFuture] = vFuture
-
-	// bolson's hackery -- 20220622_144944
-	vFuWat := vFuture
-	vFuWat.ApprovedUpgrades = map[protocol.ConsensusVersion]uint64{}
-	vFuWat.MaxTxnBytesPerBlock = 5 * 1024 * 1024
-	vFuWat.AgreementFilterTimeoutPeriod0 = 3400 * time.Millisecond
-	Consensus[protocol.ConsensusVersion("wat")] = vFuWat
 }
 
 // Global defines global Algorand protocol parameters which should not be overridden.
