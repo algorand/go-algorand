e90f48c03e246ec0c9733064773b46b7c379f7fb
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-08-16 20:42:58 -0400

    Ensure all expect tests run by avoiding double partitioning

--- test/e2e-go/cli/algod/expect/algod_expect_test.go
+++ test/e2e-go/cli/algod/expect/algod_expect_test.go
@@ -19,12 +19,11 @@ import (
 	"testing"
 
 	"github.com/algorand/go-algorand/test/framework/fixtures"
-	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
 // TestAlgodWithExpect Process all expect script files with suffix Test.exp within the test/e2e-go/cli/algod/expect directory
 func TestAlgodWithExpect(t *testing.T) {
-	partitiontest.PartitionTest(t)
+	// partitiontest.PartitionTest(t) Since each expect test is assigned a partition in `et.Run`, avoid partitioning here.  Creates double partitioning.
 	defer fixtures.ShutdownSynchronizedTest(t)
 
 	et := fixtures.MakeExpectTest(t)
