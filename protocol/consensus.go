c5d0e6509e95a0b8b6101b581a247fe7d1d64ad2
chris erway <chris.erway@algorand.com>
2022-08-22 16:44:30 -0400

    bump ConsensusCurrentVersion

--- protocol/consensus.go
+++ protocol/consensus.go
@@ -204,7 +204,7 @@ const ConsensusFuture = ConsensusVersion(
 
 // ConsensusCurrentVersion is the latest version and should be used
 // when a specific version is not provided.
-const ConsensusCurrentVersion = ConsensusV34
+const ConsensusCurrentVersion = ConsensusV35
 
 // Error is used to indicate that an unsupported protocol has been detected.
 type Error ConsensusVersion
