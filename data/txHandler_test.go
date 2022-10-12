948c316740c6399c572e3ad3a1e7a1a20eea718e
algonautshant <shant@algorand.com>
2022-10-12 19:18:56 -0400

    draft of perf test

--- data/txHandler_test.go
+++ data/txHandler_test.go
@@ -17,6 +17,7 @@
 package data
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"math/rand"
@@ -125,6 +126,160 @@ func BenchmarkTxHandlerProcessing(b *testing.B) {
 		b.ResetTimer()
 		for i := range signedTransactionGroups {
 			verify.TxnGroup(signedTransactionGroups[i], hdr, vtc, l)
+		}
+	})
+}
+
+func BenchmarkIncomingTxHandlerProcessing(b *testing.B) {
+	const numUsers = 100
+	log := logging.TestingLog(b)
+	log.SetLevel(logging.Warn)
+	secrets := make([]*crypto.SignatureSecrets, numUsers)
+	addresses := make([]basics.Address, numUsers)
+
+	genesis := make(map[basics.Address]basics.AccountData)
+	for i := 0; i < numUsers; i++ {
+		secret := keypair()
+		addr := basics.Address(secret.SignatureVerifier)
+		secrets[i] = secret
+		addresses[i] = addr
+		genesis[addr] = basics.AccountData{
+			Status:     basics.Online,
+			MicroAlgos: basics.MicroAlgos{Raw: 10000000000000},
+		}
+	}
+
+	genesis[poolAddr] = basics.AccountData{
+		Status:     basics.NotParticipating,
+		MicroAlgos: basics.MicroAlgos{Raw: config.Consensus[protocol.ConsensusCurrentVersion].MinBalance},
+	}
+
+	require.Equal(b, len(genesis), numUsers+1)
+	genBal := bookkeeping.MakeGenesisBalances(genesis, sinkAddr, poolAddr)
+	ledgerName := fmt.Sprintf("%s-mem-%d", b.Name(), b.N)
+	const inMem = true
+	cfg := config.GetDefaultLocal()
+	cfg.Archival = true
+	ledger, err := LoadLedger(log, ledgerName, inMem, protocol.ConsensusCurrentVersion, genBal, genesisID, genesisHash, nil, cfg)
+	require.NoError(b, err)
+
+	l := ledger
+
+	cfg.TxPoolSize = 75000
+	cfg.EnableProcessBlockStats = false
+	tp := pools.MakeTransactionPool(l.Ledger, cfg, logging.Base())
+	backlogPool := execpool.MakeBacklog(nil, 0, execpool.LowPriority, nil)
+	txHandler := MakeTxHandler(tp, l, &mocks.MockNetwork{}, "", crypto.Digest{}, backlogPool)
+
+	var outChan chan *txBacklogMsg
+	
+	// Make a test backlog worker, which is simiar to backlogWorker, but sends the results
+	// through the outChan instead of passing it to postprocessCheckedTxn
+	go func() {
+		defer handler.backlogWg.Done()
+		for {
+			// prioritize the postVerificationQueue
+			select {
+			case wi, ok := <-handler.postVerificationQueue:
+				if !ok {
+					return
+				}
+				outChan <- wi
+				// restart the loop so that we could empty out the post verification queue.
+				continue
+			default:
+			}
+
+			// we have no more post verification items. wait for either backlog queue item or post verification item.
+			select {
+			case wi, ok := <-handler.backlogQueue:
+				if !ok {
+					return
+				}
+				if handler.checkAlreadyCommitted(wi) {
+					continue
+				}
+
+				handler.streamVerifierChan <- verify.VerificationElement{TxnGroup: wi.unverifiedTxGroup, Context: wi}
+
+			case wi, ok := <-handler.postVerificationQueue:
+				if !ok {
+					return
+				}
+				outChan <- wi
+
+			case <-handler.ctx.Done():
+				return
+			}
+		}
+	}()
+
+	goodTxnGroups := make(map[Uint64], interface{})
+	badTxnGroups := make(map[Uint64], interface{})
+	makeSignedTxnGroups := func(N int) [][]transactions.SignedTxn {
+		maxGrpSize := proto.MaxTxGroupSize
+		ret := make([][]transactions.SignedTxn, 0, N)
+		for u := 0; u < N; u++ {
+			grpSize := rand.Intn(maxGrpSize)
+			var txGroup transactions.TxGroup
+			txns := make([]transactions.Transaction, grpSize)
+			for g := 0; g < grpSize; g++ {
+				// generate transactions
+				noteField := make([]byte, binary.MaxVarintLen64)
+				binary.PutUvarint(noteField, u)
+				tx := transactions.Transaction{
+					Type: protocol.PaymentTx,
+					Header: transactions.Header{
+						Sender:     addresses[(u+g)%numUsers],
+						Fee:        basics.MicroAlgos{Raw: proto.MinTxnFee * 2},
+						FirstValid: 0,
+						LastValid:  basics.Round(proto.MaxTxnLife),
+						Note:       noteField,
+					},
+					PaymentTxnFields: transactions.PaymentTxnFields{
+						Receiver: addresses[(u+g+1)%numUsers],
+						Amount:   basics.MicroAlgos{Raw: mockBalancesMinBalance + (rand.Uint64() % 10000)},
+					},
+				}
+				txGroup.TxGroupHashes = append(txGroup.TxGroupHashes, crypto.HashObj(tx))
+				txns = append(txns, tx)
+			}
+			groupHash := crypto.HashObj(txGroup)
+			signedTxGroup = make([]transactions.SignedTxn, 0, grpSize)
+			for g, txn := range txns {
+				txn.Group = groupHash
+				signedTx := txn.Sign(secrets[(u+g)%numUsers])
+				signedTxGroup = append(signedTxGroup, signedTx)
+			}
+			// randomly make bad signatures
+			if rand.Float32() < 0.3 {
+				signedTxGroup[rand.Intn(grpSize)].Sig[0] = signedTxGroup[rand.Intn(grpSize)].Sig[0] + 1
+				badTxnGroups[u] = struct{}{}
+			} else {
+				goodTxnGroups[u] = struct{}{}
+			}
+			ret = append(ret, signedTxGroup)
+		}
+		return ret
+	}
+
+	b.Run("handleIncomingTxns", func(b *testing.B) {
+		signedTransactionGroups := makeSignedTxnGroups(b.N)
+		encodedSignedTransactionGroups := make([]IncomingMessage, b.N)
+		for _, stxngrp := range signedTransactionGroups {
+			encodedSignedTransactionGroups =
+				append(encodedSignedTransactionGroups, IncomingMessage{Data: protocol.Encode(&stxngrp)})
+		}
+		b.ResetTimer()
+		for i := range encodedSignedTransactionGroups {
+			txHandler.processIncomingTxn(encodedSignedTransactionGroups[i])
+		}
+	})
+	b.Run("verifyIncomingTxns", func(b *testing.B) {
+		signedTransactionGroups := makeSignedTxnGroups(b.N)
+		b.ResetTimer()
+		for wi := range outChan {
+			
 		}
 	})
 }
