e5becf82f5d27f32f80e0ee5a9c9ac398a087479
Ian Suvak <ian.suvak@gmail.com>
2022-10-21 08:58:30 -0400

    Apply suggestions from code review
    Co-authored-by: cce <51567+cce@users.noreply.github.com>

--- agreement/bundle.go
+++ agreement/bundle.go
@@ -202,7 +202,7 @@ func (b unauthenticatedBundle) verifyAsync(ctx context.Context, l LedgerReader, 
 
 		rv := rawVote{Sender: auth.Sender, Round: b.Round, Period: b.Period, Step: b.Step, Proposal: b.Proposal}
 		uv := unauthenticatedVote{R: rv, Cred: auth.Cred, Sig: auth.Sig}
-		_ = avv.verifyVote(ctx, l, uv, uint64(i), message{}, results)
+       avv.verifyVote(ctx, l, uv, uint64(i), message{}, results) //nolint:errcheck // verifyVote will call EnqueueBacklog, which blocks until the verify task is queued, or returns an error when ctx.Done(), which we are already checking.
 	}
 
 	// create verification requests for equivocation votes
