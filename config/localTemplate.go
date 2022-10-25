9edc24e9e0065700ef9946a970ee460bac271bc0
Brian Olson <brian.olson@gmail.com>
2022-10-25 15:41:10 -0400

    version moved to 24

--- config/localTemplate.go
+++ config/localTemplate.go
@@ -455,7 +455,7 @@ type Local struct {
 
 	// EnableUsageLog enables 10Hz log of CPU and RAM usage.
 	// Also adds 'algod_ram_usage` (number of bytes in use) to /metrics
-	EnableUsageLog bool `version[23]:"false"`
+	EnableUsageLog bool `version[24]:"false"`
 }
 
 // DNSBootstrapArray returns an array of one or more DNS Bootstrap identifiers
