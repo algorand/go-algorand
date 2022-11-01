1e97e3d63c970862d2766c937e619e4b6cfd80bd
Eric Gieseke <eric_gieseke@yahoo.com>
2022-11-01 12:59:52 -0400

    Update alphanet genesis file and topology (#4721)

--- test/testdata/deployednettemplates/recipes/alphanet/gen_topology.py
+++ test/testdata/deployednettemplates/recipes/alphanet/gen_topology.py
@@ -2,7 +2,7 @@ import json
 import os
 
 node_types = {"R":5, "N":20, "NPN":10}
-node_size = {"R":"-c5d.4xl", "N":"-c5d.4xl", "NPN":"-c5d.4xl"}
+node_size = {"R":"-c5d.4xl", "N":"-c5d.2xl", "NPN":"-c5d.2xl"}
 regions = [
     "AWS-US-EAST-1",
     "AWS-US-WEST-1",
