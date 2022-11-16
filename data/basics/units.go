c60ee713acaa05bcc5711dee7e5742c680a2c7e5
Or Aharonee <17099688+Aharonee@users.noreply.github.com>
2022-11-16 14:14:41 +0200

    .

--- data/basics/units.go
+++ data/basics/units.go
@@ -138,8 +138,8 @@ func (round Round) RoundUpToMultipleOf(n Round) Round {
 
 // RoundDownToMultipleOf rounds down round to a multiple of n.
 func (round Round) RoundDownToMultipleOf(n Round) Round {
-	if round == 0 {
-		return round
+	if n == 0 {
+		return 0
 	}
 	return (round / n) * n
 }
