26c2f7990b59d1a51f6a773caacdd34b84a41085
Eric Warehime <eric.warehime@gmail.com>
2022-11-16 14:53:29 -0800

    msgp gen protocol

--- protocol/msgp_gen.go
+++ protocol/msgp_gen.go
@@ -39,6 +39,14 @@ import (
 //     |-----> Msgsize
 //     |-----> MsgIsZero
 //
+// NodeType
+//     |-----> MarshalMsg
+//     |-----> CanMarshalMsg
+//     |-----> (*) UnmarshalMsg
+//     |-----> (*) CanUnmarshalMsg
+//     |-----> Msgsize
+//     |-----> MsgIsZero
+//
 // StateProofType
 //        |-----> MarshalMsg
 //        |-----> CanMarshalMsg
@@ -246,6 +254,52 @@ func (z NetworkID) Msgsize() (s int) {
 // MsgIsZero returns whether this is a zero value
 func (z NetworkID) MsgIsZero() bool {
 	return z == ""
+}
+
+// MarshalMsg implements msgp.Marshaler
+func (z NodeType) MarshalMsg(b []byte) (o []byte) {
+	o = msgp.Require(b, z.Msgsize())
+	o = msgp.AppendUint64(o, uint64(z))
+	return
+}
+
+func (_ NodeType) CanMarshalMsg(z interface{}) bool {
+	_, ok := (z).(NodeType)
+	if !ok {
+		_, ok = (z).(*NodeType)
+	}
+	return ok
+}
+
+// UnmarshalMsg implements msgp.Unmarshaler
+func (z *NodeType) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	{
+		var zb0001 uint64
+		zb0001, bts, err = msgp.ReadUint64Bytes(bts)
+		if err != nil {
+			err = msgp.WrapError(err)
+			return
+		}
+		(*z) = NodeType(zb0001)
+	}
+	o = bts
+	return
+}
+
+func (_ *NodeType) CanUnmarshalMsg(z interface{}) bool {
+	_, ok := (z).(*NodeType)
+	return ok
+}
+
+// Msgsize returns an upper bound estimate of the number of bytes occupied by the serialized message
+func (z NodeType) Msgsize() (s int) {
+	s = msgp.Uint64Size
+	return
+}
+
+// MsgIsZero returns whether this is a zero value
+func (z NodeType) MsgIsZero() bool {
+	return z == 0
 }
 
 // MarshalMsg implements msgp.Marshaler
