d114060adb4c056d1cd8c7e6404e760f5fb03c92
Eric Warehime <eric.warehime@gmail.com>
2022-11-29 17:09:12 -0800

    Move Delta creation, add test

---
+++ daemon/algod/api/server/v2/delta.go
@@ -0,0 +1,207 @@
+// Copyright (C) 2019-2022 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package v2
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"github.com/algorand/go-algorand/config"
+	"github.com/algorand/go-algorand/daemon/algod/api/server/v2/generated/model"
+	"github.com/algorand/go-algorand/data/basics"
+	"github.com/algorand/go-algorand/ledger/ledgercore"
+)
+
+// convertAppResourceRecordToGenerated takes ledgercore.AppResourceRecord and converts it to v2.model.AppResourceRecord
+func convertAppResourceRecordToGenerated(app ledgercore.AppResourceRecord) model.AppResourceRecord {
+	var appLocalState *model.ApplicationLocalState = nil
+	if app.State.LocalState != nil {
+		localState := convertTKVToGenerated(&app.State.LocalState.KeyValue)
+		appLocalState = &model.ApplicationLocalState{
+			Id:       uint64(app.Aidx),
+			KeyValue: localState,
+			Schema: model.ApplicationStateSchema{
+				NumByteSlice: app.State.LocalState.Schema.NumByteSlice,
+				NumUint:      app.State.LocalState.Schema.NumUint,
+			},
+		}
+	}
+	var appParams *model.ApplicationParams = nil
+	if app.Params.Params != nil {
+		globalState := convertTKVToGenerated(&app.Params.Params.GlobalState)
+		appParams = &model.ApplicationParams{
+			ApprovalProgram:   app.Params.Params.ApprovalProgram,
+			ClearStateProgram: app.Params.Params.ClearStateProgram,
+			Creator:           app.Addr.String(),
+			ExtraProgramPages: numOrNil(uint64(app.Params.Params.ExtraProgramPages)),
+			GlobalState:       globalState,
+			GlobalStateSchema: &model.ApplicationStateSchema{
+				NumByteSlice: app.Params.Params.GlobalStateSchema.NumByteSlice,
+				NumUint:      app.Params.Params.GlobalStateSchema.NumUint,
+			},
+			LocalStateSchema: &model.ApplicationStateSchema{
+				NumByteSlice: app.Params.Params.LocalStateSchema.NumByteSlice,
+				NumUint:      app.Params.Params.LocalStateSchema.NumUint,
+			},
+		}
+	}
+	return model.AppResourceRecord{
+		Address:              app.Addr.String(),
+		AppIndex:             uint64(app.Aidx),
+		AppDeleted:           app.Params.Deleted,
+		AppParams:            appParams,
+		AppLocalStateDeleted: app.State.Deleted,
+		AppLocalState:        appLocalState,
+	}
+}
+
+// convertAssetResourceRecordToGenerated takes ledgercore.AppResourceRecord and converts it to v2.model.AppResourceRecord
+func convertAssetResourceRecordToGenerated(asset ledgercore.AssetResourceRecord) model.AssetResourceRecord {
+	var assetHolding *model.AssetHolding = nil
+	if asset.Holding.Holding != nil {
+		assetHolding = &model.AssetHolding{
+			Amount:   asset.Holding.Holding.Amount,
+			AssetID:  uint64(asset.Aidx),
+			IsFrozen: asset.Holding.Holding.Frozen,
+		}
+	}
+	var assetParams *model.AssetParams = nil
+	if asset.Params.Params != nil {
+		assetParams = &model.AssetParams{
+			Clawback:      strOrNil(asset.Params.Params.Clawback.String()),
+			Creator:       asset.Addr.String(),
+			Decimals:      uint64(asset.Params.Params.Decimals),
+			DefaultFrozen: &asset.Params.Params.DefaultFrozen,
+			Freeze:        strOrNil(asset.Params.Params.Freeze.String()),
+			Manager:       strOrNil(asset.Params.Params.Manager.String()),
+			MetadataHash:  byteOrNil(asset.Params.Params.MetadataHash[:]),
+			Name:          strOrNil(asset.Params.Params.AssetName),
+			NameB64:       byteOrNil([]byte(base64.StdEncoding.EncodeToString([]byte(asset.Params.Params.AssetName)))),
+			Reserve:       strOrNil(asset.Params.Params.Reserve.String()),
+			Total:         asset.Params.Params.Total,
+			UnitName:      strOrNil(asset.Params.Params.UnitName),
+			UnitNameB64:   byteOrNil([]byte(base64.StdEncoding.EncodeToString([]byte(asset.Params.Params.UnitName)))),
+			Url:           strOrNil(asset.Params.Params.URL),
+			UrlB64:        byteOrNil([]byte(base64.StdEncoding.EncodeToString([]byte(asset.Params.Params.URL)))),
+		}
+	}
+	return model.AssetResourceRecord{
+		Address:             asset.Addr.String(),
+		AssetIndex:          uint64(asset.Aidx),
+		AssetHoldingDeleted: asset.Holding.Deleted,
+		AssetHolding:        assetHolding,
+		AssetParams:         assetParams,
+		AssetDeleted:        asset.Params.Deleted,
+	}
+}
+
+// StateDeltaToLedgerDelta converts ledgercore.StateDelta to v2.model.LedgerStateDelta
+func StateDeltaToLedgerDelta(sDelta ledgercore.StateDelta, consensus config.ConsensusParams, rewardsLevel uint64, round uint64) (response model.LedgerStateDelta, err error) {
+	var accts []model.AccountBalanceRecord
+	var apps []model.AppResourceRecord
+	var assets []model.AssetResourceRecord
+	var keyValues []model.KvDelta
+	var modifiedApps []model.ModifiedApp
+	var modifiedAssets []model.ModifiedAsset
+	var txLeases []model.TxLease
+
+	for key, kvDelta := range sDelta.KvMods {
+		var keyBytes = []byte(key)
+		keyValues = append(keyValues, model.KvDelta{
+			Key:   &keyBytes,
+			Value: &kvDelta.Data,
+		})
+	}
+
+	for _, record := range sDelta.Accts.Accts {
+		var ot basics.OverflowTracker
+		pendingRewards := basics.PendingRewards(&ot, consensus, record.MicroAlgos, record.RewardsBase, rewardsLevel)
+
+		amountWithoutPendingRewards, overflowed := basics.OSubA(record.MicroAlgos, pendingRewards)
+		if overflowed {
+			return response, errors.New("overflow on pending reward calculation")
+		}
+
+		ad := basics.AccountData{}
+		ledgercore.AssignAccountData(&ad, record.AccountData)
+		a, err := AccountDataToAccount(record.Addr.String(), &ad, basics.Round(round), &consensus, amountWithoutPendingRewards)
+		if err != nil {
+			return response, err
+		}
+
+		accts = append(accts, model.AccountBalanceRecord{
+			AccountData: a,
+			Address:     record.Addr.String(),
+		})
+	}
+
+	for _, app := range sDelta.Accts.GetAllAppResources() {
+		apps = append(apps, convertAppResourceRecordToGenerated(app))
+	}
+
+	for _, asset := range sDelta.Accts.GetAllAssetResources() {
+		assets = append(assets, convertAssetResourceRecordToGenerated(asset))
+	}
+
+	for createIdx, mod := range sDelta.Creatables {
+		switch mod.Ctype {
+		case basics.AppCreatable:
+			modifiedApps = append(modifiedApps, model.ModifiedApp{
+				Created: mod.Created,
+				Creator: mod.Creator.String(),
+				Id:      uint64(createIdx),
+			})
+		case basics.AssetCreatable:
+			modifiedAssets = append(modifiedAssets, model.ModifiedAsset{
+				Created: mod.Created,
+				Creator: mod.Creator.String(),
+				Id:      uint64(createIdx),
+			})
+		default:
+			return response, fmt.Errorf("unable to determine type of creatable for modified creatable with index %d", createIdx)
+		}
+	}
+
+	for lease, expRnd := range sDelta.Txleases {
+		txLeases = append(txLeases, model.TxLease{
+			Expiration: uint64(expRnd),
+			Lease:      lease.Lease[:],
+			Sender:     lease.Sender.String(),
+		})
+	}
+
+	response = model.LedgerStateDeltaResponse{
+		Accts: &model.AccountDeltas{
+			Accounts: &accts,
+			Apps:     &apps,
+			Assets:   &assets,
+		},
+		ModifiedApps:   &modifiedApps,
+		ModifiedAssets: &modifiedAssets,
+		KvMods:         &keyValues,
+		PrevTimestamp:  numOrNil(uint64(sDelta.PrevTimestamp)),
+		StateProofNext: numOrNil(uint64(sDelta.StateProofNext)),
+		Totals: &model.AccountTotals{
+			NotParticipating: sDelta.Totals.NotParticipating.Money.Raw,
+			Offline:          sDelta.Totals.Offline.Money.Raw,
+			Online:           sDelta.Totals.Online.Money.Raw,
+			RewardsLevel:     sDelta.Totals.RewardsLevel,
+		},
+		TxLeases: &txLeases,
+	}
+	return
+}
