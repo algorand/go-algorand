4a74a242d753e92c7cd0d7f38725f16a34c86705
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-12-07 16:35:07 -0800

    Try to appease the new parallel linter

--- ledger/simulation/simulator_test.go
+++ ledger/simulation/simulator_test.go
@@ -211,7 +211,9 @@ func TestPayTxn(t *testing.T) {
 	t.Parallel()
 
 	for _, signed := range []bool{true, false} {
+		signed := signed
 		t.Run(fmt.Sprintf("signed=%t", signed), func(t *testing.T) {
+			t.Parallel()
 			l, accounts, makeTxnHeader := prepareSimulatorTest(t)
 			defer l.Close()
 			s := simulation.MakeSimulator(l)
@@ -245,7 +247,9 @@ func TestOverspendPayTxn(t *testing.T) {
 	t.Parallel()
 
 	for _, signed := range []bool{true, false} {
+		signed := signed
 		t.Run(fmt.Sprintf("signed=%t", signed), func(t *testing.T) {
+			t.Parallel()
 			l, accounts, makeTxnHeader := prepareSimulatorTest(t)
 			defer l.Close()
 			s := simulation.MakeSimulator(l)
@@ -282,7 +286,9 @@ func TestAuthAddrTxn(t *testing.T) {
 	t.Parallel()
 
 	for _, signed := range []bool{true, false} {
+		signed := signed
 		t.Run(fmt.Sprintf("signed=%t", signed), func(t *testing.T) {
+			t.Parallel()
 			l, accounts, makeTxnHeader := prepareSimulatorTest(t)
 			defer l.Close()
 			s := simulation.MakeSimulator(l)
