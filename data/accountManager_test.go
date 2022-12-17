89ed6752880b4a8ec24a45e10cdf5dc1b481d289
Shant Karakashian <55754073+algonautshant@users.noreply.github.com>
2022-12-16 20:26:51 -0500

    txHandler: msig and lsig benchmarks (#4908)
    Co-authored-by: Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>

--- data/accountManager_test.go
+++ data/accountManager_test.go
@@ -43,6 +43,11 @@ import (
 
 func TestAccountManagerKeys(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	if testing.Short() {
+		t.Log("this is a long test and skipping for -short")
+		return
+	}
+
 	registry := &mocks.MockParticipationRegistry{}
 	testAccountManagerKeys(t, registry, false)
 }
@@ -85,6 +90,11 @@ func registryCloseTest(t testing.TB, registry account.ParticipationRegistry, dbf
 
 func TestAccountManagerKeysRegistry(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	if testing.Short() {
+		t.Log("this is a long test and skipping for -short")
+		return
+	}
+
 	registry, dbName := getRegistryImpl(t, false, true)
 	defer registryCloseTest(t, registry, dbName)
 	testAccountManagerKeys(t, registry, true)
