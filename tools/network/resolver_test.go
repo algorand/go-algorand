78a472c4b22fc60066931cc81c519da417f6821c
Michael Diamant <michaeldiamant@users.noreply.github.com>
2023-01-03 14:04:13 -0500

    tests: split TestResolver to enable more tests on CircleCI (#4944)

--- tools/network/resolver_test.go
+++ tools/network/resolver_test.go
@@ -24,18 +24,16 @@ import (
 	"testing"
 	"time"
 
-	"github.com/algorand/go-algorand/test/partitiontest"
 	"github.com/stretchr/testify/require"
+
+	"github.com/algorand/go-algorand/test/partitiontest"
 )
 
-func TestResolver(t *testing.T) {
+func TestResolverWithDefaultDNSResolution(t *testing.T) {
 	partitiontest.PartitionTest(t)
+	t.Parallel()
 
-	if strings.ToUpper(os.Getenv("CIRCLECI")) == "TRUE" {
-		t.Skip("Disabled on CircleCI while investigating Cloudflare DNS resolution issue")
-	}
-
-	// start with a resolver that has no specific DNS address defined.
+	// configure a resolver that has no specific DNS address defined.
 	// we want to make sure that it will go to the default DNS server ( 8.8.8.8 )
 	resolver := Resolver{}
 	cname, addrs, err := resolver.LookupSRV(context.Background(), "telemetry", "tls", "devnet.algodev.network")
@@ -43,18 +41,35 @@ func TestResolver(t *testing.T) {
 	require.Equal(t, "_telemetry._tls.devnet.algodev.network.", cname)
 	require.True(t, len(addrs) == 1)
 	require.Equal(t, defaultDNSAddress, resolver.EffectiveResolverDNS())
+}
+
+func TestResolverWithCloudflareDNSResolution(t *testing.T) {
+	partitiontest.PartitionTest(t)
+	t.Parallel()
+
+	if strings.ToUpper(os.Getenv("CIRCLECI")) == "TRUE" {
+		t.Skip("Disabled on CircleCI while investigating Cloudflare DNS resolution issue")
+	}
+
+	resolver := Resolver{}
 
 	// specify a specific resolver to work with ( cloudflare DNS server is 1.1.1.1 )
 	cloudFlareIPAddr, _ := net.ResolveIPAddr("ip", "1.1.1.1")
 	resolver = Resolver{
 		dnsAddress: *cloudFlareIPAddr,
 	}
-	cname, addrs, err = resolver.LookupSRV(context.Background(), "telemetry", "tls", "devnet.algodev.network")
+	cname, addrs, err := resolver.LookupSRV(context.Background(), "telemetry", "tls", "devnet.algodev.network")
 	require.NoError(t, err)
 	require.Equal(t, "_telemetry._tls.devnet.algodev.network.", cname)
 	require.True(t, len(addrs) == 1)
 	require.Equal(t, "1.1.1.1", resolver.EffectiveResolverDNS())
+}
 
+func TestResolverWithInvalidDNSResolution(t *testing.T) {
+	partitiontest.PartitionTest(t)
+	t.Parallel()
+
+	resolver := Resolver{}
 	// specify an invalid dns resolver ip address and examine the fail case.
 	dummyIPAddr, _ := net.ResolveIPAddr("ip", "255.255.128.1")
 	resolver = Resolver{
@@ -62,7 +77,7 @@ func TestResolver(t *testing.T) {
 	}
 	timingOutContext, timingOutContextFunc := context.WithTimeout(context.Background(), time.Duration(100)*time.Millisecond)
 	defer timingOutContextFunc()
-	cname, addrs, err = resolver.LookupSRV(timingOutContext, "telemetry", "tls", "devnet.algodev.network")
+	cname, addrs, err := resolver.LookupSRV(timingOutContext, "telemetry", "tls", "devnet.algodev.network")
 	require.Error(t, err)
 	require.Equal(t, "", cname)
 	require.True(t, len(addrs) == 0)
