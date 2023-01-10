2a4431099fefc51be958ffa6e54e8e808b989af3
Brian Olson <brian.olson@gmail.com>
2023-01-10 16:57:47 -0500

    gzip in, statistics out

--- test/scripts/latency_plot.py
+++ test/scripts/latency_plot.py
@@ -6,9 +6,18 @@
 # pip install matplotlib
 
 import argparse
+import gzip
+import math
 import statistics
 
 from matplotlib import pyplot as plt
+
+def mmstdm(data):
+    dmin = min(data)
+    dmax = max(data)
+    dmean = statistics.mean(data)
+    dstd = statistics.pstdev(data)
+    return f'[{dmin}/{dmean} ({dstd})/{dmax}]'
 
 def main():
     ap = argparse.ArgumentParser()
@@ -16,18 +25,30 @@ def main():
     args = ap.parse_args()
 
     data = []
-    with open(args.latency_log) as fin:
-        for line in fin:
-            rec = int(line.strip())/1000000000.0
-            data.append(rec)
+    if args.latency_log.endswith('.gz'):
+        fin = gzip.open(args.latency_log, 'rt')
+        outname = args.latency_log.replace('.gz', '')
+    else:
+        fin = open(args.latency_log, 'rt')
+        outname = args.latency_log
+    for line in fin:
+        rec = int(line.strip())/1000000000.0
+        data.append(rec)
+    fin.close()
 
     #subd = data[50:-50]
     subd = data
     print('min {:.2f}s'.format(min(subd)))
     print('max {:.2f}s'.format(max(subd)))
     plt.plot(subd)
-    plt.savefig(args.latency_log + '.png', format='png')
-    plt.savefig(args.latency_log + '.svg', format='svg')
+    plt.savefig(outname + '.png', format='png')
+    plt.savefig(outname + '.svg', format='svg')
+    subd.sort()
+    some = int(math.log(len(data))*4)
+    lowest = subd[:some]
+    highest = subd[-some:]
+    print(f'lowest-{some}: {mmstdm(lowest)}')
+    print(f'highest-{some}: {mmstdm(highest)}')
 
 if __name__ == '__main__':
     main()
