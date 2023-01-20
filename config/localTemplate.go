8445d6761b5173cda7ae4d91fe894054a795e55a
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2023-01-20 14:20:37 -0500

    network: increase max incoming connections limit to 2400 (#5018)
    * Decrease connections per ip addr limit to 15

--- config/localTemplate.go
+++ config/localTemplate.go
@@ -59,7 +59,7 @@ type Local struct {
 	// what we should tell people to connect to
 	PublicAddress string `version[0]:""`
 
-	MaxConnectionsPerIP int `version[3]:"30"`
+	MaxConnectionsPerIP int `version[3]:"30" version[27]:"15"`
 
 	// 0 == disable
 	PeerPingPeriodSeconds int `version[0]:"0"`
@@ -76,8 +76,8 @@ type Local struct {
 
 	// IncomingConnectionsLimit specifies the max number of long-lived incoming
 	// connections. 0 means no connections allowed. Must be non-negative.
-	// Estimating 5MB per incoming connection, 5MB*800 = 4GB
-	IncomingConnectionsLimit int `version[0]:"-1" version[1]:"10000" version[17]:"800"`
+	// Estimating 1.5MB per incoming connection, 1.5MB*2400 = 3.6GB
+	IncomingConnectionsLimit int `version[0]:"-1" version[1]:"10000" version[17]:"800" version[27]:"2400"`
 
 	// BroadcastConnectionsLimit specifies the number of connections that
 	// will receive broadcast (gossip) messages from this node.  If the
