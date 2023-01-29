28d6ba8429226a0db394bb942b7ab7af3360f0a7
abebeos <110243666+abebeos@users.noreply.github.com>
2023-01-29 09:28:28 +0200

    move datadir to util package, re #5068

---
+++ cmd/util/datadir/report.go
@@ -0,0 +1,36 @@
+// Copyright (C) 2019-2023 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package datadir
+
+// TODO: eliminate duplication with cmd/algocfg/report.go
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func reportInfof(format string, args ...interface{}) {
+	fmt.Printf(format+"\n", args...)
+}
+
+func reportErrorln(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+	os.Exit(1)
+}
+
+
