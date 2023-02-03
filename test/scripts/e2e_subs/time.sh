b5464e18f85aacfcc9e9644b11b0010998ea5d8a
Will Winder <wwinder.unh@gmail.com>
2023-02-03 16:38:19 -0500

    Update shell script.

--- test/scripts/e2e_subs/time.sh
+++ test/scripts/e2e_subs/time.sh
@@ -9,9 +9,13 @@ export SHELLOPTS
 
 # make sure the time is updating
 for i in {1..20}; do
-  TIME=$(goal node status | grep 'Time since last block: '|awk '{ print $5 }')
-  if [[ "$TIME" != "0.0s" ]]; then
+  output=$(goal node status)
+  if [[ $output == *"Time since last block: 0.0s"* ]]; then
     exit 0
   fi
   sleep 0.5
 done
+
+echo "Time since last block is still 0.0s after 10 seconds"
+goal node status
+exit 1
