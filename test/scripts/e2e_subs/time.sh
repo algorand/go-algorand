c2c4d737f73855caf83cc4cc4678003599b24e3f
Will Winder <wwinder.unh@gmail.com>
2023-02-03 15:47:36 -0500

    fix time.sh

--- test/scripts/e2e_subs/time.sh
+++ test/scripts/e2e_subs/time.sh
@@ -7,13 +7,11 @@ date "+${scriptname} start %Y%m%d_%H%M%S"
 set -exo pipefail
 export SHELLOPTS
 
-WALLET=$1
-
 # make sure the time is updating
 for i in {1..20}; do
   TIME=$(goal node status | grep 'Time since last block: '|awk '{ print $5 }')
   if [[ "$TIME" != "0.0s" ]]; then
-    return
+    exit 0
   fi
   sleep 0.5
 done
