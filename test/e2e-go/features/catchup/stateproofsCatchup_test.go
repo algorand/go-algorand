3d303b0bc22bc76b59eccae66e1477b90d190b79
algoidan <idan@algorand.com>
2023-03-08 10:25:35 +0200

    fix flaky test

--- test/e2e-go/features/catchup/stateproofsCatchup_test.go
+++ test/e2e-go/features/catchup/stateproofsCatchup_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/algorand/go-algorand/config"
 	"github.com/algorand/go-algorand/data/basics"
 	"github.com/algorand/go-algorand/libgoal"
-	"github.com/algorand/go-algorand/nodecontrol"
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/test/framework/fixtures"
 	"github.com/algorand/go-algorand/test/partitiontest"
@@ -54,12 +53,13 @@ func getStateProofNextRound(a *require.Assertions, goalClient *libgoal.Client, r
 func TestStateProofInReplayCatchpoint(t *testing.T) {
 	// Overview of this test:
 	// Configure consensus to generate a state proof in the target catchpoint's replay rounds.
+	// i.e the node will have to "replay" the state proof transaction after fast catchup.
 	// Start a two-node network (primary has 100%, using has 0%)
-	// create a web proxy, have the using node use it as a peer, blocking all requests for round #2. ( and allowing everything else )
+	// create a web proxy, have the using node use it as a peer, blocking all requests for round #2 and allowing everything else
+	// (This disables the node's ability to use regular catchup)
 	// Let it run until the first usable catchpoint, as computed in getFirstCatchpointRound, is generated.
-	// instruct the using node to catchpoint catchup from the proxy, and restart the using node.
-	// wait until the using node is caught up to catchpointRound+1, skipping the "impossible" hole of round #2 and
-	// participating in consensus.
+	// instruct the using node to fast catchup.
+	// wait until the using node is caught up to catchpointRound+1, skipping the "impossible" hole of round #2.
 	// Verify that the blocks replayed to the using node contained a state proof transaction.
 	partitiontest.PartitionTest(t)
 	defer fixtures.ShutdownSynchronizedTest(t)
@@ -97,16 +97,15 @@ func TestStateProofInReplayCatchpoint(t *testing.T) {
 	_, err = usingNodeRestClient.Catchup(catchpointLabel)
 	a.NoError(err)
 
-	// restart the usingNode to double check stateproof service survives restarts during fast catchup
-	// 1. ensure the fast catchup started
+	// waiting for fastcatchup to start
 	attempt := 0
 	const sleepTime = 1 * time.Millisecond // too large duration makes catchup to complete
 	const maxAttempts = 500
 	for {
 		status, err := usingNodeRestClient.Status()
 		a.NoError(err)
-		t.Logf("status.Catchpoint = %v", status.Catchpoint)
 		if status.Catchpoint != nil && len(*status.Catchpoint) > 0 {
+			t.Logf("Fast catchup from %d to %s is in progress", status.LastRound, *status.Catchpoint)
 			break
 		}
 		if attempt > maxAttempts {
@@ -116,29 +115,7 @@ func TestStateProofInReplayCatchpoint(t *testing.T) {
 		attempt++
 	}
 
-	// restart the node
-	err = usingNode.StopAlgod()
-	a.NoError(err)
-
-	alreadyRunning, err := usingNode.StartAlgod(nodecontrol.AlgodStartArgs{
-		PeerAddress:       wp.GetListenAddress(),
-		ListenIP:          "",
-		RedirectOutput:    true,
-		RunUnderHost:      false,
-		TelemetryOverride: "",
-		ExitErrorCallback: usingNodeErrorsCollector.nodeExitWithError,
-	})
-	a.NoError(err)
-	a.False(alreadyRunning)
-
-	usingNodeRestClient = fixture.GetAlgodClientForController(usingNode)
-	// ensure the node restores the previous fast catchup state
-	st, err := usingNodeRestClient.Status()
-	a.NoError(err)
-	a.NotNil(st.Catchpoint)
-	a.NotEmpty(*st.Catchpoint)
-	t.Logf("Fast catchup from %d to %s is in progress", st.LastRound, *st.Catchpoint)
-
+	// wait for fastcatchup to complete and the node is synced
 	err = fixture.ClientWaitForRoundWithTimeout(usingNodeRestClient, uint64(targetCatchpointRound+1))
 	a.NoError(err)
 
