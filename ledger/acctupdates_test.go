717995b2d1b0bfa3b42509fee62855e4b34862d6
Ignacio Corderi <icorderi@msn.com>
2023-03-17 13:13:23 -0300

    refactor: upgrade `MakeAccountsOptimizedReader` from a testing only method.

--- ledger/acctupdates_test.go
+++ ledger/acctupdates_test.go
@@ -1046,7 +1046,7 @@ func TestListCreatables(t *testing.T) {
 		require.NoError(t, err)
 
 		au := &accountUpdates{}
-		au.accountsq, err = tx.Testing().MakeAccountsOptimizedReader()
+		au.accountsq, err = tx.MakeAccountsOptimizedReader()
 		require.NoError(t, err)
 
 		// ******* All results are obtained from the cache. Empty database *******
