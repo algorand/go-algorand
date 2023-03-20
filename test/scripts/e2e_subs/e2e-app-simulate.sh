9a0e6c9e2bee21134de63f6cb348fc9c01e781b0
Hang Su <hang.su@algorand.com>
2023-03-20 11:35:57 -0400

    rewritten with jq, some more coverage on txn group

--- test/scripts/e2e_subs/e2e-app-simulate.sh
+++ test/scripts/e2e_subs/e2e-app-simulate.sh
@@ -17,10 +17,23 @@ gcmd="goal -w ${WALLET}"
 
 ACCOUNT=$(${gcmd} account list|awk '{ print $3 }')
 
+CONST_TRUE="true"
+CONST_FALSE="false"
+
 ${gcmd} clerk send -a 10000 -f ${ACCOUNT} -t ${ACCOUNT} -o pay1.tx
 ${gcmd} clerk send -a 10000 -f ${ACCOUNT} -t ${ACCOUNT} -o pay2.tx
 
 cat pay1.tx pay2.tx | ${gcmd} clerk group -i - -o grouped.tx
+
+# We first test transaction group simulation WITHOUT signatures
+RES=$(${gcmd} clerk simulate -t grouped.tx | jq '."would-succeed"')
+
+if [[ $RES != $CONST_FALSE ]]; then
+    date '+app-simulate-test FAIL the simulation transaction group without signatures should not succeed %Y%m%d_%H%M%S'
+    false
+fi
+
+# We then test transaction group simulation WITH signatures
 ${gcmd} clerk split -i grouped.tx -o grouped.tx
 
 ${gcmd} clerk sign -i grouped-0.tx -o grouped-0.stx
@@ -28,10 +41,9 @@ ${gcmd} clerk sign -i grouped-1.tx -o grouped-1.stx
 
 cat grouped-0.stx grouped-1.stx > grouped.stx
 
-RES=$(${gcmd} clerk simulate -t grouped.stx)
-EXPSUCCESS='"would-succeed": true'
+RES=$(${gcmd} clerk simulate -t grouped.stx | jq '."would-succeed"')
 
-if [[ $RES != *"${EXPSUCCESS}"* ]]; then
+if [[ $RES != $CONST_TRUE ]]; then
     date '+app-simulate-test FAIL should pass to simulate self pay transaction group %Y%m%d_%H%M%S'
     false
 fi
@@ -41,7 +53,6 @@ fi
 #######################################################
 
 printf '#pragma version 2\nint 1' > "${TEMPDIR}/simple-v2.teal"
-printf '#pragma version 3\nint 1' > "${TEMPDIR}/simple-v3.teal"
 
 # Real Create
 RES=$(${gcmd} app method --method "create(uint64)uint64" --arg "1234" --create --approval-prog ${DIR}/tealprogs/app-abi-method-example.teal --clear-prog ${TEMPDIR}/simple-v2.teal --global-byteslices 0 --global-ints 0 --local-byteslices 1 --local-ints 0 --extra-pages 0 --from $ACCOUNT 2>&1 || true)
@@ -59,28 +70,24 @@ ${gcmd} app method --method "empty()void" --app-id $APPID --from $ACCOUNT 2>&1 -
 # SIMULATE without a signature first
 RES=$(${gcmd} clerk simulate -t empty.tx)
 
-EXPFAIL='"would-succeed": false'
-
-FAIL_REASON_SIG_MISSING='"missing-signature": true'
-
 # confirm that without signature, the simulation should fail
-if [[ $RES != *"${EXPFAIL}"* ]]; then
+if [[ $(echo "$RES" | jq '."would-succeed"') != $CONST_FALSE ]]; then
     date '+app-simulate-test FAIL the simulation call to empty()void without signature should not succeed %Y%m%d_%H%M%S'
     false
 fi
 
 # check again the simulation failing reason
-if [[ $RES != *"${FAIL_REASON_SIG_MISSING}"* ]]; then
+if [[ $(echo "$RES" | jq '."txn-groups"[0]."txn-results"[0]."missing-signature"') != $CONST_TRUE ]]; then
     date '+app-simulate-test FAIL the simulation call to empty()void without signature should fail with missing-signature %Y%m%d_%H%M%S'
     false
 fi
 
 # SIMULATE with a signature
 ${gcmd} clerk sign -i empty.tx -o empty.stx
-RES=$(${gcmd} clerk simulate -t empty.stx)
+RES=$(${gcmd} clerk simulate -t empty.stx | jq '."would-succeed"')
 
 # with signature, simulation app-call should succeed
-if [[ $RES != *"${EXPSUCCESS}"* ]]; then
+if [[ $RES != $CONST_TRUE ]]; then
     date '+app-simulate-test FAIL the simulation call to empty()void should succeed %Y%m%d_%H%M%S'
     false
 fi
