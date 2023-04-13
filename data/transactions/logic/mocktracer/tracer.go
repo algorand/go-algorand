acd3f5556635708f696d209a3233c748e2b6bda5
Eric Warehime <eric.warehime@gmail.com>
2023-04-13 13:36:58 -0700

    Add block hooks to eval tracer

--- data/transactions/logic/mocktracer/tracer.go
+++ data/transactions/logic/mocktracer/tracer.go
@@ -17,6 +17,7 @@
 package mocktracer
 
 import (
+	"github.com/algorand/go-algorand/data/bookkeeping"
 	"github.com/algorand/go-algorand/data/transactions"
 	"github.com/algorand/go-algorand/data/transactions/logic"
 	"github.com/algorand/go-algorand/protocol"
@@ -26,6 +27,8 @@ import (
 type EventType string
 
 const (
+	// BeforeBlockEvent represents the logic.EvalTracer.BeforeBlock event
+	BeforeBlockEvent EventType = "BeforeBlock"
 	// BeforeTxnGroupEvent represents the logic.EvalTracer.BeforeTxnGroup event
 	BeforeTxnGroupEvent EventType = "BeforeTxnGroup"
 	// AfterTxnGroupEvent represents the logic.EvalTracer.AfterTxnGroup event
@@ -42,6 +45,8 @@ const (
 	BeforeOpcodeEvent EventType = "BeforeOpcode"
 	// AfterOpcodeEvent represents the logic.EvalTracer.AfterOpcode event
 	AfterOpcodeEvent EventType = "AfterOpcode"
+	// AfterBlockEvent represents the logic.EvalTracer.AfterBlock event
+	AfterBlockEvent EventType = "AfterBlock"
 )
 
 // Event represents a logic.EvalTracer event
@@ -62,6 +67,14 @@ type Event struct {
 
 	// only for AfterOpcode, AfterProgram, AfterTxn, and AfterTxnGroup
 	HasError bool
+
+	// only for BeforeBlock, AfterBlock
+	BlockHash bookkeeping.BlockHash
+}
+
+// BeforeBlock creates a new Event with the type BeforeBlockEvent
+func BeforeBlock(blockHash bookkeeping.BlockHash) Event {
+	return Event{Type: BeforeBlockEvent, BlockHash: blockHash}
 }
 
 // BeforeTxnGroup creates a new Event with the type BeforeTxnGroupEvent
@@ -104,6 +117,11 @@ func AfterOpcode(hasError bool) Event {
 	return Event{Type: AfterOpcodeEvent, HasError: hasError}
 }
 
+// AfterBlock creates a new Event with the type AfterBlockEvent
+func AfterBlock(blockHash bookkeeping.BlockHash) Event {
+	return Event{Type: AfterBlockEvent, BlockHash: blockHash}
+}
+
 // OpcodeEvents returns a slice of events that represent calling `count` opcodes
 func OpcodeEvents(count int, endsWithError bool) []Event {
 	events := make([]Event, 0, count*2)
@@ -129,6 +147,11 @@ func FlattenEvents(rows [][]Event) []Event {
 // Tracer is a mock tracer that implements logic.EvalTracer
 type Tracer struct {
 	Events []Event
+}
+
+// BeforeBlock mocks the logic.EvalTracer.BeforeBlock method
+func (d *Tracer) BeforeBlock(hdr *bookkeeping.BlockHeader) {
+	d.Events = append(d.Events, BeforeBlock(hdr.Hash()))
 }
 
 // BeforeTxnGroup mocks the logic.EvalTracer.BeforeTxnGroup method
@@ -169,4 +192,9 @@ func (d *Tracer) BeforeOpcode(cx *logic.EvalContext) {
 // AfterOpcode mocks the logic.EvalTracer.AfterOpcode method
 func (d *Tracer) AfterOpcode(cx *logic.EvalContext, evalError error) {
 	d.Events = append(d.Events, AfterOpcode(evalError != nil))
+}
+
+// AfterBlock mocks the logic.EvalTracer.BeforeBlock method
+func (d *Tracer) AfterBlock(hdr *bookkeeping.BlockHeader) {
+	d.Events = append(d.Events, AfterBlock(hdr.Hash()))
 }
