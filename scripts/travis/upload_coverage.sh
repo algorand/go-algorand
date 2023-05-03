0aef416bf30ab6391ea2e56f1de372ab72732064
John Lee <john.lee@algorand.com>
2023-05-03 08:58:30 -0400

    Use env var if present

--- scripts/travis/upload_coverage.sh
+++ scripts/travis/upload_coverage.sh
@@ -2,5 +2,8 @@
 
 set -eo pipefail
 
-# The following is intentional - hardcoding a token for public repos is recommended here to allow fork access
-/usr/bin/env bash scripts/travis/codecov -t 8b4a1f91-f154-4c26-b84c-c9aaa90159c6
+if [[ -z "$CODECOV_TOKEN" ]]; then
+  /usr/bin/env bash scripts/travis/codecov
+else
+  /usr/bin/env bash scripts/travis/codecov -t $CODECOV_TOKEN
+fi
