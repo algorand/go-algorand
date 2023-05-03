dd51ec95ffe81110751a87e9b4ea30ffb2f64fda
John Lee <john.lee@algorand.com>
2023-05-03 09:16:56 -0400

    Try to use Circle SHA1 instead of token and see if it works

--- scripts/travis/upload_coverage.sh
+++ scripts/travis/upload_coverage.sh
@@ -2,8 +2,15 @@
 
 set -eo pipefail
 
-if [[ -z "$CODECOV_TOKEN" ]]; then
+# if [[ -z "$CODECOV_TOKEN" ]]; then
+#   /usr/bin/env bash scripts/travis/codecov
+# else
+#   /usr/bin/env bash scripts/travis/codecov -t $CODECOV_TOKEN
+# fi
+
+if [[ -z "$CIRCLE_SHA1" ]]; then
   /usr/bin/env bash scripts/travis/codecov
 else
-  /usr/bin/env bash scripts/travis/codecov -t $CODECOV_TOKEN
+  echo "INFO: Using sha1 $CIRCLE_SHA1"
+  /usr/bin/env bash scripts/travis/codecov -C $CIRCLE_SHA1
 fi
