01c444018e39e4e024cac6b56e2f45a49c50df85
John Lee <john.lee@algorand.com>
2023-05-03 11:46:14 -0400

    Revert "Try to use Circle SHA1 instead of token and see if it works"
    This reverts commit dd51ec95ffe81110751a87e9b4ea30ffb2f64fda.

--- scripts/travis/upload_coverage.sh
+++ scripts/travis/upload_coverage.sh
@@ -2,15 +2,8 @@
 
 set -eo pipefail
 
-# if [[ -z "$CODECOV_TOKEN" ]]; then
-#   /usr/bin/env bash scripts/travis/codecov
-# else
-#   /usr/bin/env bash scripts/travis/codecov -t $CODECOV_TOKEN
-# fi
-
-if [[ -z "$CIRCLE_SHA1" ]]; then
+if [[ -z "$CODECOV_TOKEN" ]]; then
   /usr/bin/env bash scripts/travis/codecov
 else
-  echo "INFO: Using sha1 $CIRCLE_SHA1"
-  /usr/bin/env bash scripts/travis/codecov -C $CIRCLE_SHA1
+  /usr/bin/env bash scripts/travis/codecov -t $CODECOV_TOKEN
 fi
