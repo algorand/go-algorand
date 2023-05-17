7e14cb1cbd407d98d20ce4d307ed1900d6d25af2
Pavel Zbitskiy <pavel@algorand.com>
2023-05-17 17:04:49 -0400

    fix integration tests

--- test/scripts/e2e_subs/e2e-app-simulate.sh
+++ test/scripts/e2e_subs/e2e-app-simulate.sh
@@ -121,14 +121,14 @@ cat "${TEMPDIR}/grouped-0.stx" "${TEMPDIR}/grouped-1.stx" > "${TEMPDIR}/grouped.
 RES=$(${gcmd} clerk simulate -t "${TEMPDIR}/grouped.stx")
 
 if [[ $(echo "$RES" | jq '."txn-groups" | any(has("failure-message"))') != $CONST_TRUE ]]; then
-    data '+app-simulate-test FAIL should FAIL for overspending in simulate self pay transaction group %Y%m%d_%H%M%S'
+    date '+app-simulate-test FAIL should FAIL for overspending in simulate self pay transaction group %Y%m%d_%H%M%S'
     false
 fi
 
 OVERSPEND_INFO="overspend"
 
 if [[ $(echo "$RES" | jq '."txn-groups"[0]."failure-message"') != *"$OVERSPEND_INFO"* ]]; then
-    data '+app-simulate-test FAIL first overspending transaction in transaction group should contain message OVERSPEND %Y%m%d_%H%M%S'
+    date '+app-simulate-test FAIL first overspending transaction in transaction group should contain message OVERSPEND %Y%m%d_%H%M%S'
     false
 fi
 
