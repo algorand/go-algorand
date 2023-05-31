7f5d9c95eeeea2245c459836ba4f3ce061a3c5c6
algonautshant <shant@algorand.com>
2023-05-30 21:55:46 -0400

    create config v28

--- config/local_defaults.go
+++ config/local_defaults.go
@@ -20,7 +20,7 @@
 package config
 
 var defaultLocal = Local{
-	Version:                                    27,
+	Version:                                    28,
 	AccountUpdatesStatsInterval:                5000000000,
 	AccountsRebuildSynchronousMode:             1,
 	AgreementIncomingBundlesQueueLength:        15,
