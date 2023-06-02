e2499057d4a7a0abd47efda255f84232c03b1b0c
Pavel Zbitskiy <65323360+algorandskiy@users.noreply.github.com>
2023-06-02 12:24:19 -0400

    build: fix shadow linter in debug tools (#5442)

--- tools/debug/transplanter/main.go
+++ tools/debug/transplanter/main.go
@@ -25,7 +25,6 @@ import (
 	"io"
 	"net"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -212,18 +211,12 @@ func transcribeSnappyLog(filePath string, output chan txGroupItem, wg *sync.Wait
 	}
 	defer file.Close()
 
-	var headerDecoder headerDecoder
-	if strings.Contains(path.Base(filePath), "_v2") {
-		headerDecoder = decoderV2{}
-	} else {
-		headerDecoder = decoderV1{}
-	}
-
+	decoder := decoderV2{}
 	snappyReader := snappy.NewReader(file)
 	var n int
 
 	for {
-		headers, lenMsg, err := headerDecoder.decodeHeader(snappyReader)
+		headers, lenMsg, err := decoder.decodeHeader(snappyReader)
 		if err == io.EOF {
 			break
 		} else if err != nil {
@@ -395,7 +388,7 @@ func main() {
 			os.Exit(1)
 		}
 		syncRound := uint64(*roundStart) - cfg.MaxAcctLookback + 1
-		err := followerNode.SetSyncRound(syncRound)
+		err = followerNode.SetSyncRound(syncRound)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot configure catchup: %v", err)
 			os.Exit(1)
