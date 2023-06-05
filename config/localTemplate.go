913478a0e05dcbeff208fc90ea478e0fcad6884f
Shant Karakashian <55754073+algonautshant@users.noreply.github.com>
2023-06-05 12:21:34 -0400

    block rpc: Memory counter for http block requests (#5428)

--- config/localTemplate.go
+++ config/localTemplate.go
@@ -520,6 +520,10 @@ type Local struct {
 	// TxIncomingFilterMaxSize sets the maximum size for the de-duplication cache used by the incoming tx filter
 	// only relevant if TxIncomingFilteringFlags is non-zero
 	TxIncomingFilterMaxSize uint64 `version[28]:"500000"`
+
+	// BlockServiceHTTPMemCap is the memory capacity in bytes which is allowed for the block service to use for HTTP block requests.
+	// When it exceeds this capacity, it redirects the block requests to a different node
+	BlockServiceHTTPMemCap uint64 `version[28]:"500000000"`
 }
 
 // DNSBootstrapArray returns an array of one or more DNS Bootstrap identifiers
