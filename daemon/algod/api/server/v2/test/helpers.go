ff87e54bc2c35a17b9e2c02673c8451b36f68e23
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2023-06-16 15:52:28 -0400

    Remove indexer v1 from go-algorand codebase.

--- daemon/algod/api/server/v2/test/helpers.go
+++ daemon/algod/api/server/v2/test/helpers.go
@@ -40,7 +40,6 @@ import (
 	"github.com/algorand/go-algorand/ledger/simulation"
 	"github.com/algorand/go-algorand/logging"
 	"github.com/algorand/go-algorand/node"
-	"github.com/algorand/go-algorand/node/indexer"
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/util/db"
 )
@@ -219,10 +218,6 @@ func (m *mockNode) OnNewBlock(block bookkeeping.Block, delta ledgercore.StateDel
 
 func (m *mockNode) Uint64() uint64 {
 	return 1
-}
-
-func (m *mockNode) Indexer() (*indexer.Indexer, error) {
-	return nil, fmt.Errorf("indexer not implemented")
 }
 
 func (m *mockNode) GetTransactionByID(txid transactions.Txid, rnd basics.Round) (node.TxnWithStatus, error) {
