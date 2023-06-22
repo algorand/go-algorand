289b06255f00bfa8096d089c35b7af326f8f9c8a
Ian Suvak <ian.suvak@gmail.com>
2023-06-22 16:46:18 -0400

    gofmt shenanigans

--- crypto/merklearray/proof.go
+++ crypto/merklearray/proof.go
@@ -23,7 +23,6 @@ import (
 	"github.com/algorand/msgp/msgp"
 )
 
-// ////////////////////////////////////////////////////////////////////////////////////////
 // Proof is used to convince a verifier about membership of leaves: h0,h1...hn
 // at indexes i0,i1...in on a tree. The verifier has a trusted value of the tree
 // root hash.
@@ -31,28 +30,28 @@ import (
 // given the distribution of the elt positions and the depth of the tree,
 // the path length can increase up to 2^MaxTreeDepth / 2
 //
-// Consider two different reveals for the same tree:
-//               z5
-//       z3              z4
-//   y       z       z1      z2
-// q   r   s   t   u   v   w   x
-//a b c d e f g h i j k l m n o p
-//  ^
-// hints: [a, r, z, z4]
-// len(hints) = 4
-// You need a to combine with b to get q, need r to combine with the computed q and get y, and so on.
-//
-// The worst case is this:
-//                z5
-//        z3              z4
-//    y       z       z1      z2
-//  q   r   s   t   u   v   w   x
-// a b c d e f g h i j k l m n o p
-// ^   ^     ^   ^ ^   ^     ^   ^
-//
-// hints: [b, d, e, g, j, l, m, o]
-// len(hints) = 2^4/2
-// /////////////////////////////////////////////////////////////////////////////////////////
+// . Consider two different reveals for the same tree:
+// .
+// .                z5
+// .         z3              z4
+// .     y       z       z1      z2
+// .   q   r   s   t   u   v   w   x
+// .  a b c d e f g h i j k l m n o p
+// .    ^
+// . hints: [a, r, z, z4]
+// . len(hints) = 4
+// . You need a to combine with b to get q, need r to combine with the computed q and get y, and so on.
+// . The worst case is this:
+// .
+// .               z5
+// .        z3              z4
+// .    y       z       z1      z2
+// .  q   r   s   t   u   v   w   x
+// . a b c d e f g h i j k l m n o p
+// . ^   ^     ^   ^ ^   ^     ^   ^
+// .
+// . hints: [b, d, e, g, j, l, m, o]
+// . len(hints) = 2^4/2
 type Proof struct {
 	_struct struct{} `codec:",omitempty,omitemptyarray"`
 
@@ -69,6 +68,7 @@ type Proof struct {
 // SingleLeafProof is used to convince a verifier about membership of a specific
 // leaf h at index i on a tree. The verifier has a trusted value of the tree
 // root hash. it corresponds to merkle verification path.
+// The msgp directive belows tells msgp to not generate SingleLeafProofMaxSize method since we have one manually defined in this file.
 //
 //msgp:maxsize ignore SingleLeafProof
 type SingleLeafProof struct {
