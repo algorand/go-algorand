068b375151479bcebb79a6baace5755a93256cca
cce <51567+cce@users.noreply.github.com>
2023-06-27 11:28:09 -0400

    config: update MaxCatchpointDownloadDuration to 12h (#5503)

--- config/local_defaults.go
+++ config/local_defaults.go
@@ -95,7 +95,7 @@ var defaultLocal = Local{
 	MaxAPIBoxPerApplication:                    100000,
 	MaxAPIResourcesPerAccount:                  100000,
 	MaxAcctLookback:                            4,
-	MaxCatchpointDownloadDuration:              7200000000000,
+	MaxCatchpointDownloadDuration:              43200000000000,
 	MaxConnectionsPerIP:                        15,
 	MinCatchpointFileDownloadBytesPerSecond:    20480,
 	NetAddress:                                 "",
