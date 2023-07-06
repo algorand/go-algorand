1ae317a55aaa3ceb7ac31c698bbba5c195228729
Hang Su <hang.su@algorand.com>
2023-07-06 12:32:07 -0400

    more files, cdt mimicking adapter state

---
+++ cmd/tealdap/adapterState.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2019-2023 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+// TODO where we should accumulate states to rebuild
+// stack / states (box, global, local)
+// can copy from cdtState?
+// implementation should be a finite automata
