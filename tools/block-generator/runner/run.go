214e386302823ca77ea6af118470e04a36db679a
Will Winder <wwinder.unh@gmail.com>
2023-08-02 15:15:27 -0400

    Set ledger level to warn.

--- tools/block-generator/runner/run.go
+++ tools/block-generator/runner/run.go
@@ -456,6 +456,7 @@ func startGenerator(ledgerLogFile, configFile string, dbround uint64, genesisFil
 	f, err := os.OpenFile(ledgerLogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	util.MaybeFail(err, "unable to open ledger log file '%s'", ledgerLogFile)
 	log := logging.NewLogger()
+	log.SetLevel(logging.Warn)
 	log.SetOutput(f)
 
 	// Start generator.
