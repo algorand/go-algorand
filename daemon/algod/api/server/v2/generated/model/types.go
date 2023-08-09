867aa176336bbe6850f8ae99f07792e40ee68c03
algochoi <86622919+algochoi@users.noreply.github.com>
2023-08-09 09:42:05 -0400

    algod: Add API Endpoint to fetch TxIDs from block (#5611)
    Co-authored-by: Zeph Grunschlag <tzaffi@users.noreply.github.com>

--- daemon/algod/api/server/v2/generated/model/types.go
+++ daemon/algod/api/server/v2/generated/model/types.go
@@ -954,6 +954,12 @@ type BlockResponse struct {
 	Cert *map[string]interface{} `json:"cert,omitempty"`
 }
 
+// BlockTxidsResponse defines model for BlockTxidsResponse.
+type BlockTxidsResponse struct {
+	// BlockTxids Block transaction IDs.
+	BlockTxids []string `json:"blockTxids"`
+}
+
 // BoxResponse Box name and its content.
 type BoxResponse = Box
 
