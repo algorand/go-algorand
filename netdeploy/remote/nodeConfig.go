7ffd43870c97d90d0547e770f0b0038606034b43
Ian Suvak <ian.suvak@gmail.com>
2023-08-14 14:50:03 -0400

    Add omitempty to P2PPeerID

--- netdeploy/remote/nodeConfig.go
+++ netdeploy/remote/nodeConfig.go
@@ -59,8 +59,8 @@ type NodeConfigGoal struct {
 	Name               string
 	IsRelay            bool `json:",omitempty"`
 	Wallets            []NodeWalletData
-	P2PPeerID          peer.ID
-	DeadlockDetection  int    `json:"-"`
-	ConfigJSONOverride string `json:",omitempty"` // Raw json to merge into config.json after other modifications are complete
-	PeerList           string `json:",omitempty"` // Semicolon separated list of peers to connect to. Only applicable for non-relays
+	P2PPeerID          peer.ID `json:",omitempty"`
+	DeadlockDetection  int     `json:"-"`
+	ConfigJSONOverride string  `json:",omitempty"` // Raw json to merge into config.json after other modifications are complete
+	PeerList           string  `json:",omitempty"` // Semicolon separated list of peers to connect to. Only applicable for non-relays
 }
