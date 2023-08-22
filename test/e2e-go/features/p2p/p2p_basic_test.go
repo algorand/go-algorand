29ced0208dea1cf7ee04f8f48cbf2805652db129
Ian Suvak <ian.suvak@gmail.com>
2023-08-22 10:31:36 -0400

    Make e2e tests quicker using custom consensusprotocol

--- test/e2e-go/features/p2p/p2p_basic_test.go
+++ test/e2e-go/features/p2p/p2p_basic_test.go
@@ -21,7 +21,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/algorand/go-algorand/config"
+	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/test/framework/fixtures"
+	"github.com/algorand/go-algorand/test/partitiontest"
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,24 +32,37 @@ func testP2PWithConfig(t *testing.T, cfgname string) {
 	r := require.New(fixtures.SynchronizedTest(t))
 
 	var fixture fixtures.RestClientFixture
+
+	// Make protocol faster for shorter tests
+	consensus := make(config.ConsensusProtocols)
+	fastProtocol := config.Consensus[protocol.ConsensusCurrentVersion]
+	fastProtocol.ApprovedUpgrades = map[protocol.ConsensusVersion]uint64{}
+	fastProtocol.AgreementFilterTimeoutPeriod0 = 400 * time.Millisecond
+	fastProtocol.AgreementFilterTimeout = 400 * time.Millisecond
+	consensus[protocol.ConsensusCurrentVersion] = fastProtocol
+	fixture.SetConsensus(consensus)
+
 	fixture.Setup(t, filepath.Join("nettemplates", cfgname))
 	defer fixture.ShutdownImpl(true) // preserve logs in testdir
 
 	_, err := fixture.NC.AlgodClient()
 	r.NoError(err)
 
-	err = fixture.WaitForRound(10, 60*time.Second)
+	err = fixture.WaitForRound(10, 30*time.Second)
 	r.NoError(err)
 }
 
 func TestP2PTwoNodes(t *testing.T) {
+	partitiontest.PartitionTest(t)
 	testP2PWithConfig(t, "TwoNodes50EachP2P.json")
 }
 
 func TestP2PThreeNodesEvenDist(t *testing.T) {
+	partitiontest.PartitionTest(t)
 	testP2PWithConfig(t, "ThreeNodesEvenDistP2P.json")
 }
 
 func TestP2PFiveNodes(t *testing.T) {
+	partitiontest.PartitionTest(t)
 	testP2PWithConfig(t, "FiveNodesP2P.json")
 }
