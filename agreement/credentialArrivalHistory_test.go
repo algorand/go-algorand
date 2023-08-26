57c2e39a95d3887ef15a7787c63ad759a18c8bfe
Yossi Gilad <yossig2@gmail.com>
2023-08-25 21:57:03 -0400

    credential history tests

---
+++ agreement/credentialArrivalHistory_test.go
@@ -0,0 +1,60 @@
+package agreement
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCredentialHistoryStore(t *testing.T) {
+	size := 5
+	buffer := newCredentialArrivalHistory(5)
+	// last store call overwrites the first one
+	for i := 0; i < size+1; i++ {
+		buffer.store(time.Duration(i))
+	}
+
+	require.True(t, buffer.isFull())
+	require.Equal(t, time.Duration(size), buffer.history[0])
+	for i := 1; i < size; i++ {
+		require.Equal(t, time.Duration(i), buffer.history[i])
+	}
+}
+
+func TestCredentialHistoryReset(t *testing.T) {
+	size := 5
+	buffer := newCredentialArrivalHistory(5)
+	// last store call overwrites the first one
+	for i := 0; i < size+1; i++ {
+		buffer.store(time.Duration(i))
+	}
+
+	require.Equal(t, time.Duration(size), buffer.history[0])
+	for i := 1; i < size; i++ {
+		require.Equal(t, time.Duration(i), buffer.history[i])
+	}
+	require.True(t, buffer.isFull())
+	buffer.reset()
+	require.False(t, buffer.isFull())
+	buffer.store(time.Duration(100))
+	require.Equal(t, time.Duration(100), buffer.history[0])
+}
+
+func TestCredentialHistoryIsFull(t *testing.T) {
+	var buffer *credentialArrivalHistory
+	require.False(t, buffer.isFull())
+
+	size := 5
+	buffer = newCredentialArrivalHistory(size)
+	require.False(t, buffer.isFull())
+
+	for i := 0; i < size+10; i++ {
+		buffer.store(time.Duration(i))
+		if i < size-1 {
+			require.False(t, buffer.isFull())
+		} else {
+			require.True(t, buffer.isFull())
+		}
+	}
+}
