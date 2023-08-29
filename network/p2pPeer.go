576c6a9944c69acac41036a183e7167f10954f97
Ian Suvak <ian.suvak@gmail.com>
2023-08-29 11:34:23 -0400

    don't double close

--- network/p2pPeer.go
+++ network/p2pPeer.go
@@ -65,8 +65,9 @@ func (c *wsPeerConnP2PImpl) WriteMessage(_ int, buf []byte) error {
 	return err
 }
 
+// Do nothing for now since this doesn't actually close the connection just sends the close message
 func (c *wsPeerConnP2PImpl) CloseWithMessage([]byte, time.Time) error {
-	return c.stream.Close()
+	return nil
 }
 
 func (c *wsPeerConnP2PImpl) SetReadLimit(int64) {}
