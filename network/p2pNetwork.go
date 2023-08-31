d50aa31c42677ad9b92ce0779ea551a09c78bc45
Nickolai Zeldovich <nickolai@csail.mit.edu>
2023-08-30 22:30:29 -0400

    make lint happy

--- network/p2pNetwork.go
+++ network/p2pNetwork.go
@@ -241,6 +241,8 @@ func (n *P2PNetwork) DisconnectPeers() {
 func (n *P2PNetwork) RegisterHTTPHandler(path string, handler http.Handler) {
 }
 
+// RegisterHTTPHandlerFunc is like RegisterHTTPHandler but accepts
+// a callback handler function instead of a method receiver.
 func (n *P2PNetwork) RegisterHTTPHandlerFunc(path string, handler func(http.ResponseWriter, *http.Request)) {
 }
 
