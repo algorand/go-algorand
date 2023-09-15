666a012ee11c548df45f95423c7bf15bdcacef99
Pavel Zbitskiy <pavel@algorand.com>
2023-09-15 13:03:45 -0400

    chore: generalize persisted lists in ledger

--- util/list.go
+++ util/list.go
@@ -14,39 +14,39 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
-package ledger
+package util
 
-import "github.com/algorand/go-algorand/ledger/store/trackerdb"
-
-// persistedOnlineAccountDataList represents a doubly linked list.
-// must initiate with newPersistedAccountList.
-type persistedOnlineAccountDataList struct {
-	root     persistedOnlineAccountDataListNode  // sentinel list element, only &root, root.prev, and root.next are used
-	freeList *persistedOnlineAccountDataListNode // preallocated nodes location
+// List represents a doubly linked.
+// must initiate with NewList.
+type List[T any] struct {
+	root     ListNode[T]  // sentinel list element, only &root, root.prev, and root.next are used
+	freeList *ListNode[T] // preallocated nodes location
 }
 
-type persistedOnlineAccountDataListNode struct {
+// ListNode represent a list node holding next/prev pointers and a pointer to value of type T.
+type ListNode[T any] struct {
 	// Next and previous pointers in the doubly-linked list of elements.
 	// To simplify the implementation, internally a list l is implemented
 	// as a ring, such that &l.root is both the next element of the last
 	// list element (l.Back()) and the previous element of the first list
 	// element (l.Front()).
-	next, prev *persistedOnlineAccountDataListNode
+	next, prev *ListNode[T]
 
-	Value *trackerdb.PersistedOnlineAccountData
+	Value *T
 }
 
-func newPersistedOnlineAccountList() *persistedOnlineAccountDataList {
-	l := new(persistedOnlineAccountDataList)
+// NewList creates a new list for storing values of type T.
+func NewList[T any]() *List[T] {
+	l := new(List[T])
 	l.root.next = &l.root
 	l.root.prev = &l.root
 	// used as a helper but does not store value
-	l.freeList = new(persistedOnlineAccountDataListNode)
+	l.freeList = new(ListNode[T])
 
 	return l
 }
 
-func (l *persistedOnlineAccountDataList) insertNodeToFreeList(otherNode *persistedOnlineAccountDataListNode) {
+func (l *List[T]) insertNodeToFreeList(otherNode *ListNode[T]) {
 	otherNode.next = l.freeList.next
 	otherNode.prev = nil
 	otherNode.Value = nil
@@ -54,9 +54,9 @@ func (l *persistedOnlineAccountDataList) insertNodeToFreeList(otherNode *persist
 	l.freeList.next = otherNode
 }
 
-func (l *persistedOnlineAccountDataList) getNewNode() *persistedOnlineAccountDataListNode {
+func (l *List[T]) getNewNode() *ListNode[T] {
 	if l.freeList.next == nil {
-		return new(persistedOnlineAccountDataListNode)
+		return new(ListNode[T])
 	}
 	newNode := l.freeList.next
 	l.freeList.next = newNode.next
@@ -64,34 +64,34 @@ func (l *persistedOnlineAccountDataList) getNewNode() *persistedOnlineAccountDat
 	return newNode
 }
 
-func (l *persistedOnlineAccountDataList) allocateFreeNodes(numAllocs int) *persistedOnlineAccountDataList {
+// AllocateFreeNodes adds N nodes to the free list
+func (l *List[T]) AllocateFreeNodes(numAllocs int) *List[T] {
 	if l.freeList == nil {
 		return l
 	}
 	for i := 0; i < numAllocs; i++ {
-		l.insertNodeToFreeList(new(persistedOnlineAccountDataListNode))
+		l.insertNodeToFreeList(new(ListNode[T]))
 	}
 
 	return l
 }
 
 // Back returns the last element of list l or nil if the list is empty.
-func (l *persistedOnlineAccountDataList) back() *persistedOnlineAccountDataListNode {
-	isEmpty := func(list *persistedOnlineAccountDataList) bool {
+func (l *List[T]) Back() *ListNode[T] {
+	isEmpty := func(list *List[T]) bool {
 		// assumes we are inserting correctly to the list - using pushFront.
 		return list.root.next == &list.root
 	}
-
 	if isEmpty(l) {
 		return nil
 	}
 	return l.root.prev
 }
 
-// remove removes e from l if e is an element of list l.
+// Remove removes e from l if e is an element of list l.
 // It returns the element value e.Value.
 // The element must not be nil.
-func (l *persistedOnlineAccountDataList) remove(e *persistedOnlineAccountDataListNode) {
+func (l *List[T]) Remove(e *ListNode[T]) {
 	e.prev.next = e.next
 	e.next.prev = e.prev
 	e.next = nil // avoid memory leaks
@@ -100,15 +100,15 @@ func (l *persistedOnlineAccountDataList) remove(e *persistedOnlineAccountDataLis
 	l.insertNodeToFreeList(e)
 }
 
-// pushFront inserts a new element e with value v at the front of list l and returns e.
-func (l *persistedOnlineAccountDataList) pushFront(v *trackerdb.PersistedOnlineAccountData) *persistedOnlineAccountDataListNode {
+// PushFront inserts a new element e with value v at the front of list l and returns e.
+func (l *List[T]) PushFront(v *T) *ListNode[T] {
 	newNode := l.getNewNode()
 	newNode.Value = v
 	return l.insertValue(newNode, &l.root)
 }
 
 // insertValue inserts e after at, increments l.len, and returns e.
-func (l *persistedOnlineAccountDataList) insertValue(newNode *persistedOnlineAccountDataListNode, at *persistedOnlineAccountDataListNode) *persistedOnlineAccountDataListNode {
+func (l *List[T]) insertValue(newNode *ListNode[T], at *ListNode[T]) *ListNode[T] {
 	n := at.next
 	at.next = newNode
 	newNode.prev = at
@@ -118,10 +118,10 @@ func (l *persistedOnlineAccountDataList) insertValue(newNode *persistedOnlineAcc
 	return newNode
 }
 
-// moveToFront moves element e to the front of list l.
+// MoveToFront moves element e to the front of list l.
 // If e is not an element of l, the list is not modified.
 // The element must not be nil.
-func (l *persistedOnlineAccountDataList) moveToFront(e *persistedOnlineAccountDataListNode) {
+func (l *List[T]) MoveToFront(e *ListNode[T]) {
 	if l.root.next == e {
 		return
 	}
@@ -129,7 +129,7 @@ func (l *persistedOnlineAccountDataList) moveToFront(e *persistedOnlineAccountDa
 }
 
 // move moves e to next to at and returns e.
-func (l *persistedOnlineAccountDataList) move(e, at *persistedOnlineAccountDataListNode) *persistedOnlineAccountDataListNode {
+func (l *List[T]) move(e, at *ListNode[T]) *ListNode[T] {
 	if e == at {
 		return e
 	}
