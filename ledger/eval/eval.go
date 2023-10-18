7ddda294c106351736b4b29ec7db8e03bd5801bc
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2023-10-18 13:14:59 -0400

    Revert "Temp print statement - identify rounds needing the override."
    This reverts commit cbde753d5de20669805c7a4bede611d722566c82.

--- ledger/eval/eval.go
+++ ledger/eval/eval.go
@@ -1325,8 +1325,6 @@ func (eval *BlockEvaluator) endOfBlock() error {
 		} else {
 			if eval.block.StateProofTracking[protocol.StateProofBasic].StateProofOnlineTotalWeight != expectedVotersWeight {
 				actualVotersWeight := eval.block.StateProofTracking[protocol.StateProofBasic].StateProofOnlineTotalWeight
-
-				logging.Base().With("WEIGHTMISMATCH", "true").Warnf("StateProofOnlineTotalWeight wrong: %v != %v for round %v", actualVotersWeight, expectedVotersWeight, eval.block.Round())
 				var highWeight, lowWeight basics.MicroAlgos
 				if expectedVotersWeight.LessThan(actualVotersWeight) {
 					highWeight = actualVotersWeight
