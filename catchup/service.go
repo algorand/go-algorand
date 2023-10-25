e85936b8898c5258ec003d1c135e38ceceb51f1c
Pavel Zbitskiy <pavel@algorand.com>
2023-10-25 14:42:16 -0400

    remove debug metrics

--- catchup/service.go
+++ catchup/service.go
@@ -36,7 +36,6 @@ import (
 	"github.com/algorand/go-algorand/network"
 	"github.com/algorand/go-algorand/protocol"
 	"github.com/algorand/go-algorand/util/execpool"
-	"github.com/algorand/go-algorand/util/metrics"
 )
 
 const catchupPeersForSync = 10
@@ -295,7 +294,6 @@ func (s *Service) fetchAndWrite(ctx context.Context, r basics.Round, prevFetchCo
 
 		// Try to fetch, timing out after retryInterval
 		block, cert, blockDownloadDuration, err := s.innerFetch(ctx, r, peer)
-		totalBlocksFetched.Inc(nil)
 
 		if err != nil {
 			if err == errLedgerAlreadyHasBlock {
@@ -857,5 +855,3 @@ func createPeerSelector(net network.GossipNode, cfg config.Local, pipelineFetch 
 	}
 	return makePeerSelector(net, peerClasses)
 }
-
-var totalBlocksFetched = metrics.MakeCounter(metrics.MetricName{Name: "algod_catchup_blocks_fetched", Description: "Total number of blocks fetched with catchup"})
