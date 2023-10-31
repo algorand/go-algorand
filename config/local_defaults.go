7d32f6eaa5abaaabeeee1f6cee22ddf1acdf28cc
Pavel Zbitskiy <pavel@algorand.com>
2023-10-31 11:17:09 -0400

    config: migrate to v32

--- config/local_defaults.go
+++ config/local_defaults.go
@@ -20,7 +20,7 @@
 package config
 
 var defaultLocal = Local{
-	Version:                                    31,
+	Version:                                    32,
 	AccountUpdatesStatsInterval:                5000000000,
 	AccountsRebuildSynchronousMode:             1,
 	AgreementIncomingBundlesQueueLength:        15,
