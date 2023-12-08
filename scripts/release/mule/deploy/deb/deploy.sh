82825c30b8817fd5bb9f0b7a603490010dec5ced
John Lee <john.lee@algorand.com>
2023-12-08 17:05:00 -0500

    Re-enable updating repository

--- scripts/release/mule/deploy/deb/deploy.sh
+++ scripts/release/mule/deploy/deb/deploy.sh
@@ -32,50 +32,19 @@ aptly mirror update beta
 aptly repo import stable stable algorand algorand-devtools
 aptly repo import beta beta algorand-beta algorand-devtools-beta
 
-# KEY_PREFIX="$CHANNEL/$VERSION"
-# FILENAME_SUFFIX="${CHANNEL}_linux-amd64_${VERSION}.deb"
-# ALGORAND_KEY="$KEY_PREFIX/algorand_${FILENAME_SUFFIX}"
-# DEVTOOLS_KEY="$KEY_PREFIX/algorand-devtools_${FILENAME_SUFFIX}"
-
-# # `STAGING` could contain a "path" (i.e. "my_bucket/foo/bar"), but the
-# # `s3api` api expects it to be only the bucket name (i.e., "my_bucket").
-# BUCKET=$(awk -F/ '{ print $1 }' <<< "$STAGING")
-
-# # If the strings match then the objects are in the top-level of the bucket.
-# if [ "$STAGING" = "$BUCKET" ]
-# then
-#     BUCKET_PREFIX_PATH="$STAGING"
-# else
-#     # Remove matching prefix.
-#     BUCKET_PREFIX_PATH=${STAGING#$BUCKET"/"}
-# fi
-
-# for key in {"$ALGORAND_KEY","$DEVTOOLS_KEY"}
-# do
-#     key="$BUCKET_PREFIX_PATH/$key"
-#     if aws s3api head-object --bucket "$BUCKET" --key "$key"
-#     then
-#         aws s3 cp "s3://$BUCKET/$key" "$PACKAGES_DIR"
-#     else
-#         echo "[$0] The package \`$key\` failed to download."
-#     fi
-# done
-
 cp -f tmp/{algorand,algorand-devtools}_${CHANNEL}_linux-{amd64,arm64}_3.20.1.deb $PACKAGES_DIR
 
 if ls -A $PACKAGES_DIR
 then
-    echo "Would have added the following:"
-    ls -l $PACKAGES_DIR
-#     aptly repo add "$CHANNEL" "$PACKAGES_DIR"/*.deb
-#     aptly repo show -with-packages "$CHANNEL"
-#     aptly snapshot create "$SNAPSHOT" from repo "$CHANNEL"
-#     if ! aptly publish show "$CHANNEL" s3:algorand-releases: &> /dev/null
-#     then
-#         aptly publish snapshot -gpg-key=dev@algorand.com -origin=Algorand -label=Algorand "$SNAPSHOT" s3:algorand-releases:
-#     else
-#         aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
-#     fi
+    aptly repo add "$CHANNEL" "$PACKAGES_DIR"/*.deb
+    aptly repo show -with-packages "$CHANNEL"
+    aptly snapshot create "$SNAPSHOT" from repo "$CHANNEL"
+    if ! aptly publish show "$CHANNEL" s3:algorand-releases: &> /dev/null
+    then
+        aptly publish snapshot -gpg-key=dev@algorand.com -origin=Algorand -label=Algorand "$SNAPSHOT" s3:algorand-releases:
+    else
+        aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
+    fi
 else
     echo "[$0] The packages directory is empty, so there is nothing to add the \`$CHANNEL\` repo."
     exit 1
