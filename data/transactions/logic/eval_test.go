1cfb80a852bf8bb9c76921b88fe8ef12a391b047
Gary <982483+gmalouf@users.noreply.github.com>
2023-12-14 14:46:21 -0500

    Consensus: Upgrade to consensus version v39. (#5868)

--- data/transactions/logic/eval_test.go
+++ data/transactions/logic/eval_test.go
@@ -1234,6 +1234,10 @@ global AssetCreateMinBalance; int 1001; ==; &&
 global AssetOptInMinBalance; int 1001; ==; &&
 `
 
+const globalV11TestProgram = globalV10TestProgram + `
+// No new globals in v11
+`
+
 func TestGlobal(t *testing.T) {
 	partitiontest.PartitionTest(t)
 
@@ -1254,7 +1258,8 @@ func TestGlobal(t *testing.T) {
 		7:  {CallerApplicationAddress, globalV7TestProgram},
 		8:  {CallerApplicationAddress, globalV8TestProgram},
 		9:  {CallerApplicationAddress, globalV9TestProgram},
-		10: {AssetOptInMinBalance, globalV10TestProgram},
+		10: {GenesisHash, globalV10TestProgram},
+		11: {GenesisHash, globalV11TestProgram},
 	}
 	// tests keys are versions so they must be in a range 1..AssemblerMaxVersion plus zero version
 	require.LessOrEqual(t, len(tests), AssemblerMaxVersion+1)
@@ -1762,6 +1767,11 @@ assert
 int 1
 `
 
+const testTxnProgramTextV11 = testTxnProgramTextV10 + `
+assert
+int 1
+`
+
 func makeSampleTxn() transactions.SignedTxn {
 	var txn transactions.SignedTxn
 	copy(txn.Txn.Sender[:], []byte("aoeuiaoeuiaoeuiaoeuiaoeuiaoeui00"))
@@ -1865,17 +1875,17 @@ func TestTxn(t *testing.T) {
 
 	t.Parallel()
 	tests := map[uint64]string{
-		1: testTxnProgramTextV1,
-		2: testTxnProgramTextV2,
-		3: testTxnProgramTextV3,
-		4: testTxnProgramTextV4,
-		5: testTxnProgramTextV5,
-		6: testTxnProgramTextV6,
-		7: testTxnProgramTextV7,
-		8: testTxnProgramTextV8,
-		9: testTxnProgramTextV9,
-
+		1:  testTxnProgramTextV1,
+		2:  testTxnProgramTextV2,
+		3:  testTxnProgramTextV3,
+		4:  testTxnProgramTextV4,
+		5:  testTxnProgramTextV5,
+		6:  testTxnProgramTextV6,
+		7:  testTxnProgramTextV7,
+		8:  testTxnProgramTextV8,
+		9:  testTxnProgramTextV9,
 		10: testTxnProgramTextV10,
+		11: testTxnProgramTextV11,
 	}
 
 	for i, txnField := range TxnFieldNames {
