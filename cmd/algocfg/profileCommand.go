877090be830c2003ff9e44b56ed5a8f355258ae5
Gary <982483+gmalouf@users.noreply.github.com>
2024-01-19 16:02:29 -0500

    Config: Archival mode is no longer automatically enabled when netAddress is s… (#5904)

--- cmd/algocfg/profileCommand.go
+++ cmd/algocfg/profileCommand.go
@@ -69,7 +69,9 @@ var (
 	relay = configUpdater{
 		description: "Relay consensus messages across the network and support catchup.",
 		updateFunc: func(cfg config.Local) config.Local {
-			cfg.Archival = true
+			cfg.MaxBlockHistoryLookback = 20000
+			cfg.CatchpointFileHistoryLength = 3
+			cfg.CatchpointTracking = 2
 			cfg.EnableLedgerService = true
 			cfg.EnableBlockService = true
 			cfg.NetAddress = ":4160"
