dee44e7f8ffc1270f43b329ef4c4a3f8148a4c7d
John Lee <john.lee@algorand.com>
2024-02-07 14:06:14 -0500

    Comment out repository edits to test file copies

--- scripts/release/mule/deploy/deb/deploy.sh
+++ scripts/release/mule/deploy/deb/deploy.sh
@@ -8,6 +8,7 @@ PACKAGES_DIR=${PACKAGES_DIR:-~/packages}
 SNAPSHOT=${SNAPSHOT:-"${CHANNEL}-${VERSION}"}
 
 mkdir -p $PACKAGES_DIR
+rm -f $PACKAGES_DIR/*.deb
 
 aptly mirror update stable
 aptly mirror update beta
@@ -18,18 +19,18 @@ aptly repo import beta beta algorand-beta algorand-devtools-beta
 
 cp -f tmp/{algorand,algorand-devtools}_${CHANNEL}_linux-{amd64,arm64}_${VERSION}.deb $PACKAGES_DIR
 
-if ls -A $PACKAGES_DIR
-then
-    aptly repo add "$CHANNEL" "$PACKAGES_DIR"/*.deb
-    aptly repo show -with-packages "$CHANNEL"
-    aptly snapshot create "$SNAPSHOT" from repo "$CHANNEL"
-    if ! aptly publish show "$CHANNEL" s3:algorand-releases: &> /dev/null
-    then
-        aptly publish -batch snapshot -gpg-key=dev@algorand.com -origin=Algorand -label=Algorand "$SNAPSHOT" s3:algorand-releases:
-    else
-        aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
-    fi
-else
-    echo "[$0] The packages directory is empty, so there is nothing to add the \`$CHANNEL\` repo."
-    exit 1
-fi
+# if ls -A $PACKAGES_DIR
+# then
+#     aptly repo add "$CHANNEL" "$PACKAGES_DIR"/*.deb
+#     aptly repo show -with-packages "$CHANNEL"
+#     aptly snapshot create "$SNAPSHOT" from repo "$CHANNEL"
+#     if ! aptly publish show "$CHANNEL" s3:algorand-releases: &> /dev/null
+#     then
+#         aptly publish -batch snapshot -gpg-key=dev@algorand.com -origin=Algorand -label=Algorand "$SNAPSHOT" s3:algorand-releases:
+#     else
+#         aptly publish switch "$CHANNEL" s3:algorand-releases: "$SNAPSHOT"
+#     fi
+# else
+#     echo "[$0] The packages directory is empty, so there is nothing to add the \`$CHANNEL\` repo."
+#     exit 1
+# fi
