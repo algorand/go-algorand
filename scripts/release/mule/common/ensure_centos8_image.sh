3905154043ab5e887832f16094f6abc96b735253
John Lee <john.lee@algorand.com>
2024-03-04 18:31:37 -0500

    CICD: fix package signing issues (#5934) (#81)

---
+++ scripts/release/mule/common/ensure_centos8_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+# Ensure the centos8 docker image is built and available
+
+DOCKER_IMAGE="algorand/go-algorand-ci-linux-centos8:amd64-$(sha1sum scripts/configure_dev-deps.sh | cut -f1 -d' ')"
+MATCH=${DOCKER_IMAGE/:*/}
+
+echo "Checking for RPM image"
+if docker images $DOCKER_IMAGE | grep -qs $MATCH > /dev/null 2>&1; then
+  echo "Image exists"
+else
+  echo "RPM image doesn't exist, building"
+  docker build --platform=linux/amd64 --build-arg ARCH=amd64 \
+    --build-arg GOLANG_VERSION=$(./scripts/get_golang_version.sh) -t $DOCKER_IMAGE -f docker/build/cicd.centos8.Dockerfile .
+fi
