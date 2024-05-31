6f2d5fcd2a3eaba471fea3fb490dfed5a0c05b0f
Pavel Zbitskiy <pavel@algorand.com>
2024-05-31 12:57:56 -0400

    Add connection timestamp to topology parser

--- test/heapwatch/topology-extract-p2p.py
+++ test/heapwatch/topology-extract-p2p.py
@@ -12,71 +12,93 @@ find . -name 'nodelog.tar.gz' -print | xargs -I{} tar -zxf {}
 5. Run the visualizer `topology-viz.py top.json`
 """
 import argparse
+from datetime import datetime
 import json
 import re
 import os
 import sys
 
-# Regex patterns to find node IDs and connections
-node_pattern = r"P2P host created: peer ID (\w{52})"
-edge_pattern = r"Made outgoing connection to peer (\w{52})"
 
-ap = argparse.ArgumentParser()
-ap.add_argument('log_dir_path', help='logs directory path')
-ap.add_argument('-o', '--output', type=argparse.FileType('wt', encoding='utf-8'), help=f'save topology to the file specified instead of showing it')
+def main():
+    # Regex patterns to find node IDs and connections
+    node_pattern = r"P2P host created: peer ID (\w{52})"
+    edge_pattern = r"Made outgoing connection to peer (\w{52})"
 
-args = ap.parse_args()
+    ap = argparse.ArgumentParser()
+    ap.add_argument('log_dir_path', help='logs directory path')
+    ap.add_argument('-o', '--output', type=argparse.FileType('wt', encoding='utf-8'), help=f'save topology to the file specified instead of showing it')
+    ap.add_argument('-t', '--timestamp', action='store_true', help=f'store connection timestamp for each edge')
 
-# Directory containing log files
-log_dir_path = args.log_dir_path
+    args = ap.parse_args()
 
-nodes = []
-edges = []
-mapping = {}
+    # Directory containing log files
+    log_dir_path = args.log_dir_path
 
-# Iterate through all files in the specified directory
-for filename in os.listdir(log_dir_path):
-    if filename.endswith("-node.log"):
-        with open(os.path.join(log_dir_path, filename), 'r') as file:
-            mapped = filename[:len(filename) - len('-node.log')]
-            mapped = mapped.replace('relay', 'R')
-            mapped = mapped.replace('nonParticipatingNode', 'NPN')
-            mapped = mapped.replace('node', 'N')
-            node_id = None
-            for line in file:
-                # Check if line contains relevant substrings before parsing as JSON
-                if "P2P host created" in line or "Made outgoing connection to peer" in line:
-                    data = json.loads(line.strip())
-                    
-                    # Check for node creation
-                    if "P2P host created" in data.get("msg", ""):
-                        match = re.search(node_pattern, data["msg"])
-                        if match:
-                            node_id = match.group(1)
-                            nodes.append(node_id)
-                            mapping[node_id] = mapped
-                    
-                    # Check for connections
-                    elif "Made outgoing connection to peer" in data.get("msg", ""):
-                        match = re.search(edge_pattern, data["msg"])
-                        if match:
-                            target_node_id = match.group(1)
-                            match = re.findall(r"/p2p/(\w{52})", data["local"])
+    nodes = []
+    edges = []
+    mapping = {}
+
+    # Iterate through all files in the specified directory
+    for filename in os.listdir(log_dir_path):
+        if filename.endswith("-node.log"):
+            with open(os.path.join(log_dir_path, filename), 'r') as file:
+                mapped = filename[:len(filename) - len('-node.log')]
+                mapped = mapped.replace('relay', 'R')
+                mapped = mapped.replace('nonParticipatingNode', 'NPN')
+                mapped = mapped.replace('node', 'N')
+                node_id = None
+                for line in file:
+                    # Check if line contains relevant substrings before parsing as JSON
+                    if "P2P host created" in line or "Made outgoing connection to peer" in line:
+                        data = json.loads(line.strip())
+
+                        # Check for node creation
+                        if "P2P host created" in data.get("msg", ""):
+                            match = re.search(node_pattern, data["msg"])
                             if match:
-                                source_node_id = match[0]
-                            else:
-                                print('WARN: no local addr set', data, file=sys.stderr)
-                                source_node_id = node_id
-                            edges.append((source_node_id, target_node_id))
+                                node_id = match.group(1)
+                                nodes.append(node_id)
+                                mapping[node_id] = mapped
 
-result = {
-    "mapping": mapping,
-    "nodes": nodes,
-    "edges": edges
-}
+                        # Check for connections
+                        elif "Made outgoing connection to peer" in data.get("msg", ""):
+                            match = re.search(edge_pattern, data["msg"])
+                            if match:
+                                target_node_id = match.group(1)
+                                match = re.findall(r"/p2p/(\w{52})", data["local"])
+                                if match:
+                                    source_node_id = match[0]
+                                else:
+                                    print('WARN: no local addr set', data, file=sys.stderr)
+                                    source_node_id = node_id
 
-if args.output:
-    json.dump(result, args.output, indent=2)
-else:
-    json.dump(result, sys.stdout, indent=2)
-    print(file=sys.stdout)
+                                if args.timestamp:
+                                    # datetime is not serializable, so we store it as string for now
+                                    edge = (source_node_id, target_node_id, {'dt': data["time"]})
+                                else:
+                                    edge = (source_node_id, target_node_id)
+
+                                edges.append(edge)
+
+    result = {
+        "mapping": mapping,
+        "nodes": nodes,
+        "edges": edges
+    }
+
+    if args.timestamp and not args.output:
+        edges = sorted(edges, key=lambda x: x[2]['dt'])
+        for edge in edges:
+            ts = datetime.strptime(edge[2]['dt'], "%Y-%m-%dT%H:%M:%S.%f%z")
+            print('%15s %5s -> %-5s' % (ts.strftime('%H:%M:%S.%f'), mapping[edge[0]], mapping[edge[1]]))
+        return
+
+    if args.output:
+        json.dump(result, args.output, indent=2)
+    else:
+        json.dump(result, sys.stdout, indent=2)
+        print(file=sys.stdout)
+
+
+if __name__ == '__main__':
+    main()
--- test/heapwatch/topology-extract-ws.py
+++ test/heapwatch/topology-extract-ws.py
@@ -12,85 +12,104 @@ find . -name 'nodelog.tar.gz' -print | xargs -I{} tar -zxf {}
 5. Run the visualizer `topology-viz.py top.json`
 """
 import argparse
+from datetime import datetime
 import json
 import os
 import sys
 
-ap = argparse.ArgumentParser()
-ap.add_argument('log_dir_path', help='logs directory path')
-ap.add_argument('-i', '--inventory-file', type=argparse.FileType('rt', encoding='utf-8'), required=True, help='terraform inventory file path')
-ap.add_argument('-o', '--output', type=argparse.FileType('wt', encoding='utf-8'), help=f'save topology to the file specified instead of showing it')
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('log_dir_path', help='logs directory path')
+    ap.add_argument('-i', '--inventory-file', type=argparse.FileType('rt', encoding='utf-8'), required=True, help='terraform inventory file path')
+    ap.add_argument('-o', '--output', type=argparse.FileType('wt', encoding='utf-8'), help=f'save topology to the file specified instead of showing it')
+    ap.add_argument('-t', '--timestamp', action='store_true', help=f'store connection timestamp for each edge')
 
-args = ap.parse_args()
+    args = ap.parse_args()
 
-# Directory containing log files
-log_dir_path = args.log_dir_path
-inventory_file = args.inventory_file
+    # Directory containing log files
+    log_dir_path = args.log_dir_path
+    inventory_file = args.inventory_file
 
-nodes = []
-edges = []
-mapping = {}
+    nodes = []
+    edges = []
+    mapping = {}
 
-inventory = json.load(inventory_file)
+    inventory = json.load(inventory_file)
 
-ip_to_name = {}
-for k, v in inventory.items():
-    if k.startswith('name_'):
-        name = k.split('_')[1].upper()
-        if not isinstance(v, list) or len(v) != 1:
-            raise RuntimeError(f"Invalid inventory entry, expected a single item list: {k}={v}")
-        ip = v[0]
-        ip_to_name[ip] = name
-        # no need for mapping but keep the data compatible with the topology-viz script
-        mapping[name] = name
+    ip_to_name = {}
+    for k, v in inventory.items():
+        if k.startswith('name_'):
+            name = k.split('_')[1].upper()
+            if not isinstance(v, list) or len(v) != 1:
+                raise RuntimeError(f"Invalid inventory entry, expected a single item list: {k}={v}")
+            ip = v[0]
+            ip_to_name[ip] = name
+            # no need for mapping but keep the data compatible with the topology-viz script
+            mapping[name] = name
 
-# Iterate through all files in the specified directory
-for filename in os.listdir(log_dir_path):
-    if filename.endswith('-node.log'):
-        with open(os.path.join(log_dir_path, filename), 'r') as file:
-            mapped = filename[:len(filename) - len('-node.log')]
-            mapped = mapped.replace('relay', 'R')
-            mapped = mapped.replace('nonParticipatingNode', 'NPN')
-            mapped = mapped.replace('node', 'N')
-            nodes.append(mapped)
-            for line in file:
-                # Check if line contains relevant substrings before parsing as JSON
-                if "Accepted incoming connection from peer" in line or "Made outgoing connection to peer" in line:
-                    data = json.loads(line.strip())
-                    
-                    # Check for incoming connections
-                    if "Accepted incoming connection from peer" in data.get("msg", ""):
-                        remote = data['remote']
-                        remote_ip = remote.split(':')[0]
-                        remote_name = ip_to_name[remote_ip]
-                        source = remote_name
-                        target = mapped
-                        edges.append((source, target))
+    # Iterate through all files in the specified directory
+    for filename in os.listdir(log_dir_path):
+        if filename.endswith('-node.log'):
+            with open(os.path.join(log_dir_path, filename), 'r') as file:
+                mapped = filename[:len(filename) - len('-node.log')]
+                mapped = mapped.replace('relay', 'R')
+                mapped = mapped.replace('nonParticipatingNode', 'NPN')
+                mapped = mapped.replace('node', 'N')
+                nodes.append(mapped)
+                for line in file:
+                    # Check if line contains relevant substrings before parsing as JSON
+                    if "Accepted incoming connection from peer" in line or "Made outgoing connection to peer" in line:
+                        data = json.loads(line.strip())
 
-                    # Check for outgoing connections
-                    elif "Made outgoing connection to peer" in data.get('msg', ""):
-                        remote = data['remote']
-                        name: str = remote.split('.')[0]
-                        # check ip or name
-                        if name.isdigit():
+                        # Check for incoming connections
+                        if "Accepted incoming connection from peer" in data.get("msg", ""):
+                            remote = data['remote']
                             remote_ip = remote.split(':')[0]
                             remote_name = ip_to_name[remote_ip]
-                            target = remote_name
-                            source = mapped
-                            edges.append((source, target))
-                        else:
-                            target = name.upper()
-                            source = mapped
+                            source = remote_name
+                            target = mapped
                             edges.append((source, target))
 
-result = {
-    "mapping": mapping,
-    "nodes": nodes,
-    "edges": edges
-}
+                        # Check for outgoing connections
+                        elif "Made outgoing connection to peer" in data.get('msg', ""):
+                            remote = data['remote']
+                            name: str = remote.split('.')[0]
+                            # check ip or name
+                            if name.isdigit():
+                                remote_ip = remote.split(':')[0]
+                                remote_name = ip_to_name[remote_ip]
+                                target = remote_name
+                                source = mapped
+                            else:
+                                target = name.upper()
+                                source = mapped
 
-if args.output:
-    json.dump(result, args.output, indent=2)
-else:
-    json.dump(result, sys.stdout, indent=2)
-    print(file=sys.stdout)
+                            if args.timestamp:
+                                # datetime is not serializable, so we store it as string for now
+                                edge = (source, target, {'dt': data["time"]})
+                            else:
+                                edge = (source, target)
+
+                            edges.append(edge)
+
+    result = {
+        "mapping": mapping,
+        "nodes": nodes,
+        "edges": edges
+    }
+
+    if args.timestamp and not args.output:
+        edges = sorted(edges, key=lambda x: x[2]['dt'])
+        for edge in edges:
+            ts = datetime.strptime(edge[2]['dt'], "%Y-%m-%dT%H:%M:%S.%f%z")
+            print('%15s %5s -> %-5s' % (ts.strftime('%H:%M:%S.%f'), edge[0], edge[1]))
+        return
+
+    if args.output:
+        json.dump(result, args.output, indent=2)
+    else:
+        json.dump(result, sys.stdout, indent=2)
+        print(file=sys.stdout)
+
+if __name__ == '__main__':
+    main()
