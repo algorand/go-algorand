deb85211a84d9bd5afe810df503af52950bb3b68
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-10-16 22:29:26 +0200

    Adding support for developing in OpenSUSE Tumbleweed

--- scripts/install_linux_deps.sh
+++ scripts/install_linux_deps.sh
@@ -7,13 +7,17 @@ DISTRIB=$ID
 ARCH_DEPS="expect jq autoconf shellcheck sqlite python-virtualenv"
 UBUNTU_DEPS="libtool expect jq autoconf automake shellcheck sqlite3 python3-venv build-essential"
 FEDORA_DEPS="expect jq autoconf ShellCheck sqlite python-virtualenv"
+OPENSUSE_TUMBLEWEED_DEPS="libtool expect jq autoconf ShellCheck sqlite3 python312-virtualenv"
 
-case $DISTRIB in 
+case $DISTRIB in
     "arch" | "manjaro")
         pacman -S --refresh --needed --noconfirm $ARCH_DEPS
         ;;
     "fedora")
         dnf -y install $FEDORA_DEPS
+        ;;
+    "opensuse-tumbleweed")
+        zypper --non-interactive install $OPENSUSE_TUMBLEWEED_DEPS
         ;;
     *)
         apt-get update
