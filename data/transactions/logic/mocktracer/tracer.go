cc7f887399087c06121069c142de17dde114ac3d
cce <51567+cce@users.noreply.github.com>
2024-11-18 10:59:46 -0500

    eval: add DetailedEvalErrors method to EvalTracer interface (#6172)

--- data/transactions/logic/mocktracer/tracer.go
+++ data/transactions/logic/mocktracer/tracer.go
@@ -232,6 +232,9 @@ func (d *Tracer) AfterBlock(hdr *bookkeeping.BlockHeader) {
 	d.Events = append(d.Events, AfterBlock(hdr.Round))
 }
 
+// DetailedEvalErrors returns true, enabling detailed errors in tests.
+func (d *Tracer) DetailedEvalErrors() bool { return false }
+
 // copyDeltas makes a deep copy of the given ledgercore.StateDelta pointer, if it's not nil.
 // This is inefficient, but it should only be used for testing.
 func copyDeltas(deltas *ledgercore.StateDelta) *ledgercore.StateDelta {
