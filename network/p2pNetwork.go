59f1735e906c35490d4dbf80fadd3022a96dd1c5
cce <51567+cce@users.noreply.github.com>
2025-05-20 15:00:55 -0400

    also wire up wsPeer creation in p2pNetwork

--- network/p2pNetwork.go
+++ network/p2pNetwork.go
@@ -917,14 +917,15 @@ func (n *P2PNetwork) baseWsStreamHandler(ctx context.Context, p2pPeer peer.ID, s
 	}
 	peerCore := makePeerCore(ctx, n, n.log, n.handler.readBuffer, addr, client, addr)
 	wsp := &wsPeer{
-		wsPeerCore:    peerCore,
-		conn:          &wsPeerConnP2P{stream: stream},
-		outgoing:      !incoming,
-		identity:      netIdentPeerID,
-		peerType:      peerTypeP2P,
-		TelemetryGUID: pmi.telemetryID,
-		InstanceName:  pmi.instanceName,
-		features:      decodePeerFeatures(pmi.version, pmi.features),
+		wsPeerCore:            peerCore,
+		conn:                  &wsPeerConnP2P{stream: stream},
+		outgoing:              !incoming,
+		identity:              netIdentPeerID,
+		peerType:              peerTypeP2P,
+		TelemetryGUID:         pmi.telemetryID,
+		InstanceName:          pmi.instanceName,
+		features:              decodePeerFeatures(pmi.version, pmi.features),
+		enableVoteCompression: n.config.EnableVoteCompression,
 	}
 
 	localAddr, has := n.Address()
