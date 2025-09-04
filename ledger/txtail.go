2dc418587fc23f0f54ef98438b52ae4ef5acbb1d
cce <51567+cce@users.noreply.github.com>
2025-09-04 19:53:53 -0400

    Update ledger/txtail.go
    Co-authored-by: John Jannotti <jannotti@gmail.com>

--- ledger/txtail.go
+++ ledger/txtail.go
@@ -400,9 +400,8 @@ func (t *txTail) recentTailHash(offset uint64, retainSize uint64) (crypto.Digest
 	buffer := make([]byte, (retainSize)*crypto.DigestSize)
 	bufIdx := 0
 	t.tailMu.RLock()
-	lastOffset := min(
-		// size of interval [offset, lastOffset) is retainSize
-		offset+retainSize, uint64(len(t.roundTailHashes)))
+	// size of interval [offset, lastOffset) is retainSize
+	lastOffset := min(offset+retainSize, uint64(len(t.roundTailHashes)))
 	for i := offset; i < lastOffset; i++ {
 		copy(buffer[bufIdx:], t.roundTailHashes[i][:])
 		bufIdx += crypto.DigestSize
