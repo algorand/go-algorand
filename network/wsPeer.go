51a319d741ccaa365ee86e2934307215d5f0da6c
cce <51567+cce@users.noreply.github.com>
2025-10-29 21:40:41 -0400

    remove VP from default MOI tags

--- network/wsPeer.go
+++ network/wsPeer.go
@@ -78,7 +78,6 @@ var defaultSendMessageTags = map[protocol.Tag]bool{
 	protocol.TxnTag:               true,
 	protocol.UniEnsBlockReqTag:    true,
 	protocol.VoteBundleTag:        true,
-	protocol.VotePackedTag:        true, // Statefully compressed votes
 }
 
 // interface allows substituting debug implementation for *websocket.Conn
