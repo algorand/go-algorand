e8900da05227672aff6a2a960eb662458f150c12
Pavel Zbitskiy <pavel@algorand.com>
2026-02-11 15:37:06 -0500

    fix linter

--- network/p2p/pubsub.go
+++ network/p2p/pubsub.go
@@ -66,10 +66,7 @@ func deriveGossipSubParams(numOutgoingConns int) pubsub.GossipSubParams {
 	if params.Dlo <= 0 {
 		params.Dlo = params.D
 	}
-	params.Dhi = params.D * 2
-	if params.Dhi < params.D+2 {
-		params.Dhi = params.D + 2
-	}
+	params.Dhi = max(params.D*2, params.D+2)
 	params.Dscore = params.D * 2 / 3
 	params.Dout = params.D * 1 / 3
 	return params
