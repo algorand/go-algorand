ee82b36d894482daccebb9539b2595e8a0b1b86b
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2026-02-23 09:28:49 -0500

    Fix formatting issue.

--- ledger/acctupdates.go
+++ ledger/acctupdates.go
@@ -1241,7 +1241,7 @@ func (au *accountUpdates) lookupAssetResources(addr basics.Address, assetIDGT ba
 		deltaResults := make(map[basics.AssetIndex]ledgercore.AssetResourceRecord)
 		numDeltaDeleted := 0
 
-		for i := currentDeltaLen-1; i > 0; i-- {
+		for i := currentDeltaLen - 1; i > 0; i-- {
 			for _, rec := range au.deltas[i].Accts.AssetResources {
 				if rec.Addr != addr || rec.Aidx <= assetIDGT {
 					continue
