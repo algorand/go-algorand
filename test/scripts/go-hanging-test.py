2bac864828e06b259dbf15663da88e59fa61799a
John Jannotti <jannotti@gmail.com>
2026-04-13 12:56:23 -0400

    Update copyright to the Foundation

--- test/scripts/go-hanging-test.py
+++ test/scripts/go-hanging-test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Copyright (C) 2019-2024 Algorand, Inc.
+# Copyright (C) 2019-2026 Algorand Foundation Ltd.
 # This file is part of go-algorand
 #
 # go-algorand is free software: you can redistribute it and/or modify
