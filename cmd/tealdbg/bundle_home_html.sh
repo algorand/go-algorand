a1def56eb2be5ebd61acfdcaf5a6bd63ccaaf615
John Jannotti <jannotti@gmail.com>
2026-04-13 15:41:20 -0400

    Legal: Update copyright to the Foundation (#6611)

--- cmd/tealdbg/bundle_home_html.sh
+++ cmd/tealdbg/bundle_home_html.sh
@@ -3,7 +3,7 @@
 THISDIR=$(dirname $0)
 
 cat <<EOM | gofmt > $THISDIR/homepage.go
-// Copyright (C) 2019-2026 Algorand, Inc.
+// Copyright (C) 2019-2026 Algorand Foundation Ltd.
 // This file is part of go-algorand
 //
 // go-algorand is free software: you can redistribute it and/or modify
