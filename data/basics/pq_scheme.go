c54b91c6564be0580951c747bb68c19d840f5a49
cusma <cosimo.bassi@algorand.foundation>
2026-06-11 18:00:39 +0200

    fix: fee contributions type

--- data/basics/pq_scheme.go
+++ data/basics/pq_scheme.go
@@ -39,8 +39,8 @@ var (
 // authorized with a post-quantum scheme. It is expressed as a fixed-point multiple
 // of the basic min fee, with 1e6 meaning one basic min fee.
 const (
-	PQSchemeFalcon512FeeContribution  = 1e6 // Should be less than PQSchemeFalcon1024FeeContribution
-	PQSchemeFalcon1024FeeContribution = 2e6
+	PQSchemeFalcon512FeeContribution  Micros = 1e6 // should stay below the Falcon-1024 contribution
+	PQSchemeFalcon1024FeeContribution Micros = 2e6
 )
 
 // PQSchemeConsensusParams is the consensus-parameter surface needed by PQ scheme gates.
