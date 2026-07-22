3ddd9a42dead3702a8df41aef4d9fed93f622213
cce <51567+cce@users.noreply.github.com>
2026-07-22 14:03:35 -0400

    fix 386 build

--- ledger/store/trackerdb/pebbledbdriver/pebbledriver.go
+++ ledger/store/trackerdb/pebbledbdriver/pebbledriver.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
-//go:build !arm
+//go:build !arm && !386
 
 package pebbledbdriver
 
