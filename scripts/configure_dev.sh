d7dcb81b060ac65c12ec705ba5867839449a3ee3
John Lee <477932+onetechnical@users.noreply.github.com>
2026-09-21 10:56:34 -0400

    Try to update pacman

--- scripts/configure_dev.sh
+++ scripts/configure_dev.sh
@@ -170,6 +170,8 @@ elif [ "${OS}" = "darwin" ]; then
         lnav -i "$SCRIPTPATH/algorand_node_log.json"
     fi
 elif [ "${OS}" = "windows" ]; then
+    # Update pacman
+    $msys2 pacman -Syu
     if ! $msys2 pacman -S --disable-download-timeout --noconfirm git automake autoconf m4 libtool make mingw-w64-x86_64-gcc mingw-w64-x86_64-python mingw-w64-x86_64-jq unzip procps; then
         echo "Error installing pacman dependencies"
         exit 1
